3e9fbf0e20402ec6d8b58b9615653b764b246b5f
OlegKorniychuk <kornoleg576@gmail.com>
2022-01-02 22:39:39 +0200

    improved naming, implemented camelCase

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,19 +1,19 @@
 // Make nested array plane
 'use strict';
 
-function const_plane(arr) {
-  const res=[];
-  const length = arr.length;
+function plane(array) {
+  const result=[];
+  const length = array.length;
   for(let i = 0; i < length; i++) {
-    const value = arr[i];
+    const value = array[i];
     if (Array.isArray(value)) {
-      res.push(...const_plane(value));
-    } else{
-        res.push(value);
+      result.push(...plane(value));
+    } else {
+        result.push(value);
       };
   };
-  return res
+  return result;
 }
 
-const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
+const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
