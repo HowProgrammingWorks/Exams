1df08a868c7252ad867d0553fdacc2737a33afac
volkovily <volkov.ilyya@gmail.com>
2022-01-02 13:59:52 +0200

    Refactored "shuffle" to function
    changed the name to lowercase

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,9 +1,10 @@
 // Shuffle an array
 
-let  Shuffle = ( ...List) => {
+function shuffle( ...List) {
   const [ arr ] = List;
-  arr.sort(() => Math.random() - 0.5); { return arr }
+  arr.sort(() => Math.random() - 0.5); 
+  return arr;
 }
 
-const result = Shuffle(['a', 'b', 'c', 'd', 'e']);
+const result = shuffle(['a', 'b', 'c', 'd', 'e']);
 console.log(result);
