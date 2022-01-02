b7f83dfc2ce609cda2928ffb1cdfc6cc34ff4841
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-02 23:53:45 +0200

    Variables a and b changed to constants and renamed to a more meaningful name Function arguments have also been renamed.

--- Kotlin/src/compare.kt
+++ Kotlin/src/compare.kt
@@ -1,11 +1,10 @@
 //spread operator (vararg in kotlin) is unsupported in functional style in kotlin
-fun <K,V> compare(first_values: Map<K,V>, second_values: Map<K,V>): Boolean {
-    var a = first_values.keys
-    var b = second_values.keys
-    if(a.joinToString("-") != b.joinToString("-") ) return false
+fun <K,V> compare(FIRST_MAP: Map<K,V>, SECOND_MAP: Map<K,V>): Boolean {
+    val KEYS_OF_FIRST_MAP = FIRST_MAP.keys
+    val KEYS_OF_SECOND_MAP = SECOND_MAP.keys
     var e = true
-    for (c in a){
-        if (first_values[c] == second_values[c]) e = e && true
+    for (c in KEYS_OF_FIRST_MAP){
+        if (FIRST_MAP[c] == SECOND_MAP[c]) e = e && true
         else { e = e && false }
     }
     return e
