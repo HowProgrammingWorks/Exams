9df6cb2aaeca15b1fb2529cc08d0dfa9007060b1
VladimirDan <50290969+VladimirDan@users.noreply.github.com>
2022-01-03 00:36:49 +0200

    Update day.js
    виправив криві виступи

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,9 +3,9 @@
 let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 let parseDay = searchedDay => {
-  let dayNumber = days.findIndex(val => 
-		searchedDay.startsWith(val.toLowerCase())) + 1;
-    return dayNumber === 0 ? -1 : dayNumber;
+	let dayNumber = days.findIndex(val => 
+  	searchedDay.startsWith(val.toLowerCase())) + 1;
+	return dayNumber === 0 ? -1 : dayNumber;
 };
   
 
