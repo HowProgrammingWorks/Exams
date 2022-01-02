e5c65f9f1fb73ced3ba5d4465edc49b367d56af8
Alex Kirovich <96977619+AlexKirovich@users.noreply.github.com>
2022-01-02 22:20:39 +0200

    modified function so it retirns new string

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,11 +3,12 @@
 const getvaluebetween = (str, start, end) => {
   const firstIndex = str.indexOf(start) + start.length
   const lastIndex = str.indexOf(end)
+  let result = '';
     if ( firstIndex === -1 || lastIndex === -1) return ''
     else {
-      str =  str.substring(firstIndex, lastIndex)
+      result = str.substring(firstIndex, lastIndex);
     }
-  return str
+  return result;
 }
 
 const result = getvaluebetween('Hello <username> and bye!', '<', '>')
