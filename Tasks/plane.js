fd0e42bf99ea6c88af08715423cb471668692ea0
Nester44 <nesterov.mail4@gmail.com>
2022-01-02 20:36:08 +0200

    adding spaces and indents where it's needed, fixing function call

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -2,19 +2,21 @@
 // Make nested array plane
 
 const plane = function(arr, res=[]) {
-  for(let i = 0, length = arr.length; i < length; i++){
+  for (let i = 0, length = arr.length; i < length; i++) {
     const value = arr[i];
     const j = i
+
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
+    res.push(...plane(value));
     arr[i] = res[i - 1]
-    }else{
+    } else{
     arr[i] = res[j - 1]
     res.push(value);
     };
   };
+
   return res
 }
 
-const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
+const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
