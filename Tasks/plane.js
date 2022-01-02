c90b08b5f74daa14382dc9f9dce3da2573f8d2d3
GAlexandrD <o.horbatko.im11@kpi.ua>
2022-01-02 22:40:50 +0200

    delet useless variable j

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,14 +1,13 @@
 // Make nested array plane
 
 const_plane = function(arr, res=[]) {
-  j = 0 for(let i = 0, length = arr.length; i < length; i++){
+  for(let i = 0, length = arr.length; i < length; i++){
     value = arr[i];
-    j = i
     if (Array.isArray(value, typeof value) && [i, length]) {
     res.push(...const_plane(value));
     arr[i] = res[i - 1]
     }else{
-    arr[i] = res[j - 1]
+    arr[i] = res[i - 1]
     res.push(value);
     };
   };
