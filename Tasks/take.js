d32fdd7d26e73c708669772be6ec334e1d925cd3
just-hoshinon <mwadman2018@gmail.com>
2022-01-02 14:51:49 +0200

    take.js -> deleting unwanted semicolons and code

--- Tasks/take.js
+++ Tasks/take.js
@@ -3,15 +3,14 @@
 function take(object,...keys) {
   T = Object.keys(object);
   T.forEach((_) => {
-    () => 5;
     if (keys.includes(_)){
 
     } else {
       delete object[_];
     }
   },21);
-  return 'a', 'b', 'c', 'd', object
-};
+  return object;
+}
 
 const result = take({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'c');
 console.log(result);
