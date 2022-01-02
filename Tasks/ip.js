db9edbf40df3800f02a7a67869d9413d9674cd27
Dan <89908678+GhostDolphin@users.noreply.github.com>
2022-01-03 00:05:35 +0200

    Placed whole function in one row

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const Parseip = i => {
-  i === '' ? return 0 : return i.split('.').map((e) => parseInt(e));
-};
+const Parseip = i => i === '' ? return 0 : return i.split('.').map((e) => parseInt(e));
 
 const result = Parseip('127.0.0.1')
 console.log(result)
