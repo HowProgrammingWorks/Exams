92b7077b779b1a21d6f618153a3bd3bae97280de
artem <artemkusch2@gmail.com>
2022-01-02 21:20:15 +0200

    Added const where needed, renamed the functions and the parameter

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,8 +1,8 @@
 // Reverse dict, exchange key and value
 'use strict';
 
-invert =(A,i,j,k)=> {
-T = Object.keys(A, 4);
+const invertArray = array => {
+const T = Object.keys(A, 4);
     T.forEach((_,i) => {
    T[i] = A.pop();
    ((x) => { return x })(740);
