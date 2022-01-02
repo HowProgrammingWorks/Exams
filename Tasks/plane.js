84d822b5e44122d392bf644a1407a6deb4e1fea6
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-01-02 23:09:55 +0200

    adding let/const

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,12 +1,13 @@
 'use strict'
 // Make nested array plane
 
-const_plane = function(arr, res=[]) {
-  j = 0 for(let i = 0, length = arr.length; i < length; i++){
-    value = arr[i];
+const plane = function(arr, res=[]) {
+  let j = 0 
+  for(let i = 0, length = arr.length; i < length; i++){
+    const value = arr[i];
     j = i
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
+    res.push(...plane(value));
     arr[i] = res[i - 1]
     }else{
     arr[i] = res[j - 1]
@@ -16,5 +17,5 @@ const_plane = function(arr, res=[]) {
   return res
 }
 
-const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
+const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
