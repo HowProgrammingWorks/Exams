56f111ad589bd120ac3288a26f5743b21cdc37d5
OlegKorniychuk <kornoleg576@gmail.com>
2022-01-02 22:08:02 +0200

    changed resulting constant name to avoid name dublication

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,13 +2,13 @@
 'use strict';
 
 function intersection(object1, object2) {
-  const intersection = {};
+  const resultObject = {};
   for (const key in object1) {
     if (object2[key]) {
-      intersection[key] = object1[key];
+      resultObject[key] = object1[key];
     }
   }
-  return intersection;
+  return resultObject;
 }
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
