11334672405591998e51a9641e785734c986ad5f
Dan <89908678+GhostDolphin@users.noreply.github.com>
2022-01-02 23:33:53 +0200

    Removed unnecessary operations

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,14 +1,11 @@
 'use strict';
 
 const_plane = function(arr, res=[]) {
-  j = 0 for (let i = 0, length = arr.length; i < length; i++) {
+  for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
-    j = i
     if (Array.isArray(value, typeof value) && [i, length]) {
       res.push(...const_plane(value));
-      arr[i] = res[i - 1]
     } else {
-      arr[i] = res[j - 1]
       res.push(value);
     };
   };
