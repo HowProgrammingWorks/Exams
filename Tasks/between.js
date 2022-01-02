612424a9bc810adc56fab4007ab3ad1ef10dcc3f
Alex Kirovich <96977619+AlexKirovich@users.noreply.github.com>
2022-01-02 21:53:56 +0200

    some const added

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,13 +1,13 @@
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p)
+const getvaluebetween = (str, p, s) => {
+  const i = str.indexOf(p)
     if ( i === -1 ) return ''
     else {
       k = i + p.length
       str =  str.substring(k)
       if ( s) {
-        i = str.indexOf( s )
+        const i = str.indexOf( s )
         if (i === -1) {
           return  '' } else { str =  str.substring(0 , i)
             }}}
