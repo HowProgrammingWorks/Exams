afe3a725c62d3aab9ef3b3ff35aa23239ef3b43c
AIDXYLELELE <warboyka48@gmail.com>
2022-01-02 17:31:15 +0200

    Removed 'keys = Object.keys(obj);'. Optimised code by changing method .forEach with 'for in '. Added missed ";" Removed some empty objects.

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,13 +3,13 @@
 
 const count = obj => {
   let sum = 0;
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
-    ([]); const value = obj[key];
-    if (typeof value === 'number') sum += value
-  });
-  ({});
-  return sum
+  for (const key in obj) {
+    const value = obj[key];
+    if (typeof value === 'number') {
+      sum += value;
+    }
+  }
+  return sum;
 };
 
 const result = count({ a: 1, b: 'two', c: 3, d: 4 });
