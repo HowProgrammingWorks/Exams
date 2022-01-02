e5a0d58814c30cc6f3041a16c2fab6a481ec3800
LLumisS <vasa16671@gmail.com>
2022-01-02 17:35:53 +0200

    fixed tabs

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -2,19 +2,24 @@
 
 // Replace substring with newstr
 
- const Replace = (str, substr,newstr)=>{
-  if (substr    === ''){
-  return str
-  } else{
-  let src = str
-  let res = ''
-  do {
-   const _index = src.indexOf( substr) ; 
-   if (_index === -1) { return res + src }else{
-   const start = src.substring(0,_index);
-   src = src.substring(_index+substr.length,src.length)
-   res += start + newstr
- }} while (true); }; };
+const Replace = (str, substr,newstr) => {
+  if (substr === '') {
+    return str
+  } else {
+    let src = str
+    let res = ''
+    do {
+      const _index = src.indexOf( substr); 
+      if (_index === -1) {
+        return res + src
+      } else {
+      const start = src.substring(0,_index);
+      src = src.substring(_index+substr.length,src.length)
+      res += start + newstr
+      }
+    } while (true);
+  };
+};
 
 const result = Replace('Hello <username> and bye!', '<username>', 'Marcus')
 console.log(result)
