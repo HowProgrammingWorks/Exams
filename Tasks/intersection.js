9330d01d457d478a4f7dd865d998a02ec453000b
Dimdim28 <wwwdimanes1@gmail.com>
2022-01-02 19:19:25 +0200

    Agorithm does not change the initial objects

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,14 +1,15 @@
 // Finde an intersection of two dictionaries
 'use strict';
-const intersection = (obj1, obj2) => {
-  for (const key in obj1) {
-    if (key && obj2[key]) {
-      obj2[key] = obj1[key];
-    } else {
-      delete obj1[key];
-    }
-  }
-  return obj1;
-};
+const intersection = (obj1, obj2) =>  {                 
+ for (const key in obj1) {
+if (key && obj2[key]) {
+obj2[key] = obj1[key];   
+} else  {                                           
+delete obj1[key];                       
+}                                              
+}
+return obj1;
+}
+
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
