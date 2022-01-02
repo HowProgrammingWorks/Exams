bf88b15c1cdab9adca66333d2fb6e3f9649df2ab
AndrijShchegel <89912205+AndrijShchegel@users.noreply.github.com>
2022-01-02 22:01:49 +0200

    adding tabs, spaces and remove some

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,17 +1,17 @@
 // Remove values from array
 'use strict';
 
-const skip =(array,...remove)=> {
-let index = 0;
-for (const value of array) {
-for (const valueToDel of remove) {
-if (value === valueToDel) {
-  array.splice(index, 1);
-};
-}
-index++;
-}
-return array;
+const skip = (array,...remove) => {
+  let index = 0;
+  for (const value of array) {
+    for (const valueToDel of remove) {
+      if (value === valueToDel) {
+        array.splice(index, 1);
+      };
+    }
+    index++;
+  }
+  return array;
 }
 
 const result = skip([1, 2, 3, 4, 5], 2, 5);
