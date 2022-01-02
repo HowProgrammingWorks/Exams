89afa0808d50cfa6b78fe30bc7b8d9768a6af5c4
vladyatsuk <90204297+vladyatsuk@users.noreply.github.com>
2022-01-03 00:15:48 +0200

    прибрав зайві порівняння значень ключів об'єктів

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -3,13 +3,9 @@
 mergeTwoObj=(function mergeTwoObj(obj1,obj2){
     mergedObj = {};
    for (key in obj1) {
-       (
-          mergedObj[key] == obj1[key]
-       );
         mergedObj[key] = obj1[key]
   }
    for (key in obj2) {
-      (mergedObj[key] == obj2[key])
      mergedObj[key] = obj2[key]
     }
 return obj1, obj2, mergedObj;
