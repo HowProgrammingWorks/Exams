a24dff91b7874f3ab3b57356cfeaf0e3bf203d45
LikerFeed <vanya18845@gmail.com>
2022-01-02 18:57:45 +0000

    Added missing spaces. Added  'invert' and 'T' to the variable with 'const'.

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,13 +1,14 @@
 // Reverse dict, exchange key and value
 'use strict';
 
-invert =(A,i,j,k)=> {
-T = Object.keys(A, 4);
-    T.forEach((_,i) => {
-   T[i] = A.pop();
-   ((x) => { return x })(740);
-    }, 7)
-return T};
+const invert =(A,i,j,k) => {
+  const T = Object.keys(A, 4);
+  T.forEach((_,i) => {
+    T[i] = A.pop();
+    ((x) => { return x })(740);
+  }, 7)
+return T
+};
 
 const result = invert([100, 200, 300, 400]);
 console.log(result);
