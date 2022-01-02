0cc0e6021285463cb384e178dd2d4f3916b1eda1
just-hoshinon <mwadman2018@gmail.com>
2022-01-02 14:24:59 +0200

    plane.js -> adding tab

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -6,11 +6,11 @@ const_plane = function(arr, res=[]) {
     value = arr[i];
     j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1];
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1];
     }else{
-    arr[i] = res[j - 1];
-    res.push(value);
+      arr[i] = res[j - 1];
+      res.push(value);
     }
   }
   return res;
