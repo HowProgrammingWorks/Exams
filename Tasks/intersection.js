bb10d47f31c0f5bd6fa08b99f200915e0ada793f
Masha Martyniuk <mmartyniuk004@gmail.com>
2022-01-02 23:54:11 +0200

    Update intersection.js
    check with 'use strict' mode
    declare keys_1, name with const

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,12 +1,15 @@
 // Finde an intersection of two dictionaries
+'use strict'
 
 const intersection = (obj_1, obj_2) => {
-keys_1 = Object.keys(obj_1);                  
-for (name of keys_1) {
+const keys_1 = Object.keys(obj_1);                  
+
+for (const name of keys_1) {
 if (name !== '' && obj_2[name]) {
 obj_2[name] = obj_1[name] } 
 else { delete obj_1[name] }
 }
+
 return obj_1;
 }
 
