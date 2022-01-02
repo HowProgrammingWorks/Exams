5c0386c841a9d40d04fcfc631181a03d1425005c
Dan <89908678+GhostDolphin@users.noreply.github.com>
2022-01-02 23:37:51 +0200

    Improved semicolons placement

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -7,10 +7,10 @@ const_plane = function(arr) {
       res.push(...const_plane(value));
     } else {
       res.push(value);
-    };
-  };
-  return res
-}
+    }
+  }
+  return res;
+};
 
 const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
