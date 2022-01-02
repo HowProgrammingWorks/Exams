a67ca72ecbdff933c8473652672bd2028cbf2867
Dimdim28 <wwwdimanes1@gmail.com>
2022-01-02 19:52:11 +0200

    formatted

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -3,17 +3,17 @@
 
 const planeArr = function(arr) {
   const res = [];
- const length = arr.length;
- for(let i = 0; i < length; i++){
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
     const value = arr[i];
     if (Array.isArray(value)) {
-    res.push(...planeArr(value));
-    }else{
-    res.push(value);
+      res.push(...planeArr(value));
+    } else {
+      res.push(value);
     }
   }
   return res;
-}
+};
 
 const result = planeArr([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
