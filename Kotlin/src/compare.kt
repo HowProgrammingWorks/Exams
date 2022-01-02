3e2e15b9b7a9ddb3e7efad3f03d6f66ba6f8ab72
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-02 22:56:47 +0200

    Vararg is meaningless since only the first Map is used (parameters_List [0]) and only two dictionaries are compared

--- Kotlin/src/compare.kt
+++ Kotlin/src/compare.kt
@@ -1,6 +1,5 @@
 //spread operator (vararg in kotlin) is unsupported in functional style in kotlin
-fun <K,V> compare(first_values: Map<K,V>, vararg parameters_LIST: Map<K,V>): Boolean {
-    val second_values = parameters_LIST[0]
+fun <K,V> compare(first_values: Map<K,V>, second_values: Map<K,V>): Boolean {
     var a = first_values.keys
     var b = second_values.keys
     if(a.joinToString("-") != b.joinToString("-") ) return false
