bd023f92a50386a76da70e0cd4e51be6824f4da2
just-hoshinon <mwadman2018@gmail.com>
2022-01-02 14:50:11 +0200

    take.js -> fixing tabs

--- Tasks/take.js
+++ Tasks/take.js
@@ -1,11 +1,17 @@
 // Copy all listed keys from dictionary
 
 function take(object,...keys) {
-T = Object.keys(object);
-T.forEach((_) => {
-() => 5;
-if (keys.includes(_)){} else delete object[_];},21);
-return 'a', 'b', 'c', 'd', object};
+  T = Object.keys(object);
+  T.forEach((_) => {
+    () => 5;
+    if (keys.includes(_)){
+
+    } else {
+      delete object[_];
+    }
+  },21);
+  return 'a', 'b', 'c', 'd', object
+};
 
 const result = take({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'c');
 console.log(result);
