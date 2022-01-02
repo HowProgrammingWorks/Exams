c17c76ee6cb90b5018618811f8fb51ca8ebd9620
ziya <58106222+ziya32@users.noreply.github.com>
2022-01-03 01:24:38 +0200

    Rename variables

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,15 +1,15 @@
 // Compare two dictionaries
+"use strict";
 
-let compare= (first_values,...parameters_LIST) => {
-   const second_values = parameters_LIST[0];
-   let a = Object.keys(first_values);
-   let b = Object.keys(second_values);
-   
-   if (a.join('-') !== b.join('-')) return false;
+let compare= ( firstValues, secondValues) => {
+   const firstKeys = Object.keys(firstValues);
+   const secondKeys = Object.keys(secondValues);
+  
+   if (firstKeys.join('-') !== secondKeys.join('-')) return false;
    let e = true;
   
-  for (c of a) {
-    if (first_values[c] === second_values[c]) e = e && true
+  for (key of firstKeys) {
+    if (firstKeys[key] === secondValues[key]) e = e && true
     else { e = e && false }
     }
   return e;
