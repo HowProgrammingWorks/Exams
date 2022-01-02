0bbd35e092084726ffeb7c6cb8c1ff1667d77c33
cchebelok <lotokhinivan@gmail.com>
2022-01-03 00:27:54 +0200

    remove some spaces, rename variables

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,16 +1,16 @@
 // Merge two dictionaries
-'use strict'
+'use strict';
 
-const merge_two_objects = (object_1,object_2) =>{
-    let object_3 = {};
-   for (let i in object_1) {
-        object_3[i] = object_1[i];
+const mergeTwoObjects = (obj1, obj2) => {
+  const obj3 = {};
+  for (let i in obj1) {
+    obj3[i] = obj1[i];
   }
-   for (let j in object_2) {
-     object_3[j] = object_2[j];
-    }
-return object_3;
-}
+  for (let j in obj2) {
+    obj3[j] = obj2[j];
+  }
+  return obj3;
+};
 
-const result = merge_two_objects({ a: 'uno', b: 'due' }, { c: 'tre' });
+const result = mergeTwoObjects({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
