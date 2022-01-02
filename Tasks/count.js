4a98da9c84996835f586f37980f46dc44e439031
LikerFeed <vanya18845@gmail.com>
2022-01-02 18:27:24 +0000

    Removed useless pieces of code that clog it up.

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,14 +2,13 @@
 'use stict';
 
 const count = obj => {
-  let sum = 0;{}
+  let sum = 0;
   const keys = Object.keys(obj);
   keys.forEach((key) => {
-    ([]);const value = obj[key];
+    const value = obj[key];
     if (typeof value === 'number') sum += value 
   });
-  ({});
-  return sum
+  return sum;
 };
 
 const result = count({ a: 1, b: 'two', c: 3, d: 4 });
