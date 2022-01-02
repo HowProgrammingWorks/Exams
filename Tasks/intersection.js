fb4413fab730b6ef9ee9d5df674eba70200c05ea
AIDXYLELELE <warboyka48@gmail.com>
2022-01-02 19:19:11 +0200

    Add 'use strict'; Rewrote the function with 'const' and in the form of a lambda function;

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,17 +1,19 @@
 // Finde an intersection of two dictionaries
+'use strict';
 
-intersection=function intersection(object_1,object_2)  {
-first_keys = Object.keys(object_1);                    {
-} for (attribute_name of first_keys)                   {
-if (attribute_name !== '' && object_2[attribute_name]) {
-object_2[attribute_name] = object_1[attribute_name];   {
-}} else                                                {
-delete object_1[attribute_name];                       {
-                                                       }
-                  }{}
-                  }{}
-return object_1;
+const intersection = (obj1, obj2) {
+    first_keys = Object.keys(object_1); {
+    } for (attribute_name of first_keys) {
+        if (attribute_name !== '' && object_2[attribute_name]) {
+            object_2[attribute_name] = object_1[attribute_name]; {
+            }
+        } else {
+            delete object_1[attribute_name]; {
+            }
+        } { }
+    } { }
+    return object_1;
 }
 
-const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
+const result = intersection({ a: 'i', b: 'due' }, { a: ''x, c: 'tre' });
 console.log(result);
