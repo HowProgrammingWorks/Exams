fab4bceccdbb69c6ca3084c46b8536cde7157ca8
Alzex <alexzemly@gmail.com>
2022-01-02 20:07:39 +0200

    Fix name

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,11 +1,11 @@
 // Make nested array plane
 'use strict';
 
-const planeArray = arr => {
+const plane = arr => {
   const res = [];
   for (const value of arr) {
     if (Array.isArray(value)) {
-      res.push(...planeArray(value));
+      res.push(...plane(value));
     } else {
       res.push(value);
     }
@@ -13,5 +13,5 @@ const planeArray = arr => {
   return res;
 };
 
-const result = planeArray([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
+const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
