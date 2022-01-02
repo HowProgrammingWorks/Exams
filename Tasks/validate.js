3d471959601fef1d31ae64a82e73ca6633f3f775
Masha Martyniuk <mmartyniuk004@gmail.com>
2022-01-02 23:30:28 +0200

    Update validate.js
    change identifiers

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,14 +1,14 @@
 // Validate person name
 
-const isValidate = (T) => {
-if (T === '') return false;
-if (typeof T !== 'string') return false;
-for (C of T) {
-    if (C === ' ') continue;
-    if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
+const Valid = (name) => {
+if (name === '') return false;
+if (typeof name !== 'string') return false;
+for (words of name) {
+    if (words === ' ') continue;
+    if (words.toLowerCase().charCodeAt(0) >= 97 && words.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
   };
   return true;
 };
 
-const result = isValidate('Marcus Aurelius');
+const result = Valid('Marcus Aurelius');
 console.log(result);
