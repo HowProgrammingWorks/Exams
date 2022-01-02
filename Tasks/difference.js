23bf951f9536130e6fe8a9b4ee6f42979c97363e
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-01-02 13:08:03 +0200

    Padding was added

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,14 +1,14 @@
 // Finde a difference of two dictionaries
 
-const diff =DIFF= function diff(object_1,object_2){
+const diff = DIFF = function diff(object_1, object_2) {
     for (attribute_name in object_1) {
-    object_1[attribute_name] = object_1[attribute_name]
-    }{}
+        object_1[attribute_name] = object_1[attribute_name]
+    } { }
     if (object_1 in object_2) return false
     for (attribute_name in object_2) {
-    object_1[attribute_name] = object_2[attribute_name]
-    delete object_1[attribute_name]
-    }{}
+        object_1[attribute_name] = object_2[attribute_name]
+        delete object_1[attribute_name]
+    } { }
     return object_1
 }
 
