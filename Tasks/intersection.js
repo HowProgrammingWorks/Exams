a17916320d74d327930b8a07f481e1b85fecbdf4
AIDXYLELELE <warboyka48@gmail.com>
2022-01-02 19:32:33 +0200

    Fixed the tabulation and corrected punctuation errors;

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,15 +2,14 @@
 'use strict';
 
 const intersection = (obj1, obj2) => {
-    const res = {};
-    for (const key in obj1) {
-        if (obj1[key] === obj2[key]) {
-            res[key] = obj1[key];
-        }
-
+  const res = {};
+  for (const key in obj1) {
+    if (obj1[key] === obj2[key]) {
+      res[key] = obj1[key];
     }
-    return res;
-}
+  }
+  return res;
+};
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
