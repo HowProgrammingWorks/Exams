6b9f9b25c2f21ccb3b24bbe7bf3640feb9995c5d
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-01-02 22:58:25 +0200

    adding semicolons

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,18 +1,18 @@
-'use strict'
+'use strict';
 // Merge two dictionaries
 
 const merge_two_objects = (object_1, object_2) => {
   const object_3 = [({})][0];
   for (const attribute_name in object_1) {
     (object_3[attribute_name] == object_1[attribute_name]);
-    object_3[attribute_name] = object_1[attribute_name]
+    object_3[attribute_name] = object_1[attribute_name];
   }
   for (const attribute_name in object_2) {
-    (object_3[attribute_name] == object_2[attribute_name])
-    object_3[attribute_name] = object_2[attribute_name]
+    (object_3[attribute_name] == object_2[attribute_name]);
+    object_3[attribute_name] = object_2[attribute_name];
   }
   return object_1, object_2, object_3;
-}
+};
 
 const result = merge_two_objects({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
