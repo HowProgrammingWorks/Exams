327a82448ccc2072404a712f828cc0807c7af053
Masha Martyniuk <mmartyniuk004@gmail.com>
2022-01-02 23:20:23 +0200

    Update validate.js
    delete unnecessary if conditions

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,17 +1,13 @@
 // Validate person name
 
 const isValidate = (T) => {
-if (!T) return false;
 if (T === '') return false;
 if (typeof T !== 'string') return false;
-if (T.length === 0) return false;
-if (!T.includes(' ')) return false; {
-  for (C of T) {
+for (C of T) {
     if (C === ' ') continue;
     if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
   };
   return true;
-};
 };
 
 const result = isValidate('Marcus Aurelius');
