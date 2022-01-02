2cd310b96e44170ebf9f59a31ac8f1e85d95036e
Masha Martyniuk <mmartyniuk004@gmail.com>
2022-01-02 23:40:01 +0200

    Update validate.js
    check with 'use strict' mode
    declare "words"

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,9 +1,10 @@
 // Validate person name
+'use strict'
 
 const Valid = (name) => {
 if (name === '') return false;
 if (typeof name !== 'string') return false;
-for (words of name) {
+for (const words of name) {
     if (words === ' ') continue;
     const a = 'a'.charCodeAt(0);
     const z = 'z'.charCodeAt(0);
@@ -12,7 +13,6 @@ for (words of name) {
     } 
     else { return false }
   }
-  
 }
 
 const result = Valid('Marcus Aurelius');
