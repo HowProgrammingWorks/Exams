6dc934cbe6faebffe1b53409a17bebdecfcf6209
artem <artemkusch2@gmail.com>
2022-01-02 21:25:53 +0200

    Deleted useless code

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -2,12 +2,10 @@
 'use strict';
 
 const invertArray = array => {
-const invertedArray = Object.keys(array);
-    for (const index of invertedArray) {
-   T[i] = A.pop();
-   ((x) => { return x })(740);
-    }, 7)
-return T};
+    const invertedArray = Object.keys(array);
+    for (const index of invertedArray) invertedArray[index] = array.pop();
+    return invertedArray;
+};
 
-const result = invert([100, 200, 300, 400]);
+const result = invertArray([100, 200, 300, 400]);
 console.log(result);
