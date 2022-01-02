33daa77c52815b33212878fe978437c93cc78dfd
OlegKorniychuk <kornoleg576@gmail.com>
2022-01-02 22:04:43 +0200

    removed unnecessary firstKeys constant, changed 'for of' for 'for in'.

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,9 +2,8 @@
 'use strict';
 
 function intersection(object1, object2) {
-  const firstKeys = Object.keys(object1);
   const intersection = {};
-  for (const key of firstKeys) {
+  for (const key in object1) {
     if (object2[key]) {
       intersection[key] = object1[key];
     }
