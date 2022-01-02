70335aa1107f463bed4da04cdb699b1346d02c2c
LLumisS <vasa16671@gmail.com>
2022-01-02 17:37:52 +0200

    fixed semicolons + tabs

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -2,24 +2,24 @@
 
 // Replace substring with newstr
 
-const Replace = (str, substr,newstr) => {
+const Replace = (str, substr, newstr) => {
   if (substr === '') {
-    return str
+    return str;
   } else {
-    let src = str
-    let res = ''
+    let src = str;
+    let res = '';
     do {
-      const _index = src.indexOf( substr); 
+      const _index = src.indexOf(substr);
       if (_index === -1) {
-        return res + src
+        return res + src;
       } else {
-      const start = src.substring(0,_index);
-      src = src.substring(_index+substr.length,src.length)
-      res += start + newstr
+        const start = src.substring(0, _index);
+        src = src.substring(_index + substr.length, src.length);
+        res += start + newstr;
       }
     } while (true);
-  };
+  }
 };
 
-const result = Replace('Hello <username> and bye!', '<username>', 'Marcus')
-console.log(result)
+const result = Replace('Hello <username> and bye!', '<username>', 'Marcus');
+console.log(result);
