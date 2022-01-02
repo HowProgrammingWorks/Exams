b9a4e0c56430a9ba523dc765c204a33ed9cbda4c
Dimdim28 <wwwdimanes1@gmail.com>
2022-01-02 19:40:56 +0200

    fixed the loop

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -6,17 +6,14 @@ const_plane = function(arr) {
  const  j = 0;
  const length = arr.length;
  for(let i = 0; i < length; i++){
-    value = arr[i];
-    j = i
-    if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
+    const value = arr[i];
+    if (Array.isArray(value)) {
+    res.push(...plane(value));
     }else{
-    arr[i] = res[j - 1]
     res.push(value);
-    };
-  };
-  return res
+    }
+  }
+  return res;
 }
 
 const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
