7e11a355e8df798a851957a96bc3146ebe4d1e13
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-01-02 14:22:10 +0200

    unnecessary code that did not affect workflow was removed

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -5,11 +5,9 @@ const plane = (arr, res = []) => {
   for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
     j = i;
-    if (Array.isArray(value, typeof value) && [i, length]) {
+    if (Array.isArray(value)) {
       res.push(...plane(value));
-      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1];
       res.push(value);
     };
   };
