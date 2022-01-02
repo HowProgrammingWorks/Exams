f4ee5be973e1927051a956806d0ae1ef0ff4de2f
cchebelok <lotokhinivan@gmail.com>
2022-01-03 01:08:37 +0200

    add use strict, add some spaces and ;

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,18 +1,21 @@
 // Replace substring with newstr
+'use strict';
 
- Replace = (str, substr,newstr)=>{
-  if (substr    === ''){
-  return str
-  } else{
-  src = str
-  res = ''
+const Replace = (str, substr, newstr) => {
+  if (substr === '') {
+    return str;
+  } else {
+    let src = str;
+    let res = '';
   do {
    const _index = src.indexOf( substr) ; 
    if (_index === -1) { return res + src }else{
    const start = src.substring(0,_index);
    src = src.substring(_index+substr.length,src.length)
    res += start + newstr
- }} while (true); }; };
+ }} while (true); 
+  }
+};
 
 const result = Replace('Hello <username> and bye!', '<username>', 'Marcus')
 console.log(result)
