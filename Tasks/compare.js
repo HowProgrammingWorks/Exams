22895abb61a0ad5e68bf1434294ec060482f2a59
ziya <58106222+ziya32@users.noreply.github.com>
2022-01-03 01:32:54 +0200

    bug Fix and make better test

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -7,13 +7,18 @@ let compare= ( firstValues, secondValues) => {
   
    if (firstKeys.join('-') !== secondKeys.join('-')) return false;
   
-  for (key of firstKeys) {
-    if (firstKeys[key] !== secondValues[key]) return false; 
+  for (const key of firstKeys) {
+    if (firstValues[key] !== secondValues[key]) return false; 
   }
    
   return true;
 }
 
-const result = compare({ a: 1, c: 'hello' }, { a: 1, c: 'hello' });
+const result1 = compare({ a: 1, c: 'hello' }, { a: 1, c: 'hello' });
+const result2 = compare({ a: 1, c: 'Bye' }, { a: 1, c: 'hello' });
+const result3 = compare({ a: 2, c: 'hello' }, { a: 1, c: 'hello' });
+const result4 = compare({ a: 1, c: 'hello' }, { b: 1, c: 'hello' });
 
-console.log(result);
+
+
+console.dir({ result1, result2, result3, result4})
