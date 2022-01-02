00de8438bcb60b98c1f91cfbea54a5ce04a48ae8
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-01-02 23:11:05 +0200

    adding/removing semicolons

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,21 +1,21 @@
-'use strict'
+'use strict';
 // Make nested array plane
 
 const plane = function(arr, res=[]) {
-  let j = 0 
+  let j = 0;
   for(let i = 0, length = arr.length; i < length; i++){
     const value = arr[i];
-    j = i
+    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
     res.push(...plane(value));
-    arr[i] = res[i - 1]
+    arr[i] = res[i - 1];
     }else{
-    arr[i] = res[j - 1]
+    arr[i] = res[j - 1];
     res.push(value);
-    };
-  };
-  return res
-}
+    }
+  }
+  return res;
+};
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
