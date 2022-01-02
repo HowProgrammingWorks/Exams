1a5355982febdd6fefb599a88da7a300f6f58f0f
vladmoroca <vladmoroca@gmail.com>
2022-01-02 16:40:48 +0200

    remove {},it did nothing

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -4,13 +4,12 @@
 const isValidate = name => {
   if (!name) return false;
   if (typeof name !== 'string') return false;
-  if (!name.includes(' ')) return false; {
-    for (const letter of name) {
-      if (letter === ' ') continue;
-      if (!(letter.toLowerCase().charCodeAt(0) >= 97 && letter.toLowerCase().charCodeAt(0) <= 122)) {return false; }
-    }
-    return true;
+  if (!name.includes(' ')) return false;
+  for (const letter of name) {
+    if (letter === ' ') continue;
+    if (!(letter.toLowerCase().charCodeAt(0) >= 97 && letter.toLowerCase().charCodeAt(0) <= 122)) { return false; }
   }
+  return true;
 };
 
 const result = isValidate('Marcus Aurelius');
