31db2b56fad12ff52a675ccb07981a92a803fe60
Alzex <alexzemly@gmail.com>
2022-01-02 22:46:41 +0200

    Fix codestyle

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,16 +1,13 @@
 // Replace substring with newstr
 'use strict';
 
-const NOT_FOUND = -1;
-const BEGIN = 0;
-
 const replace = (str, subStr, newStr) => {
   if (subStr === '') {
     return str;
   }
   let res = '';
-  while (str.indexOf(subStr) !== NOT_FOUND) {
-    const start = str.substring(BEGIN, str.indexOf(subStr));
+  while (str.indexOf(subStr) !== -1) {
+    const start = str.substring(0, str.indexOf(subStr));
     str = str.substring(str.indexOf(subStr) + subStr.length, str.length);
     res += start + newStr;
   }
