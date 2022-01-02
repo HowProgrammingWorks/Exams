b3e727f42ca839ff4e74b8ecb6ba5489231b21e0
Maksym <chyrkovmaksym1@gmail.com>
2022-01-02 19:27:08 +0200

    changed tabs and removed spaces

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,17 +1,15 @@
 // Merge two dictionaries
-function mergeTwoDictionaries(first, second){
-    mergedDic = {};
-   for (key in first) {
-       (
-          mergedDic[key] == first[key]
-       );
-        mergedDic[key] = first[key]
+function mergeTwoDictionaries(first, second) {
+  mergedDic = {};
+  for (key in first) {
+    mergedDic[key] == first[key];
+    mergedDic[key] = first[key];
   }
-   for (key in second) {
-      (mergedDic[key] == second[key])
-     mergedDic[key] = second[key]
-    }
-return first, second, mergedDic;
+  for (key in second) {
+    mergedDic[key] == second[key];
+    mergedDic[key] = second[key];
+  }
+  return first, second, mergedDic;
 }
 
 const result = mergeTwoDictionaries({ a: 'uno', b: 'due' }, { c: 'tre' });
