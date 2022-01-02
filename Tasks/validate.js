de87ad25b98881ab2da2a1462284477a9fa73614
vladmoroca <vladmoroca@gmail.com>
2022-01-02 16:52:15 +0200

    explained magic numbers

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -5,10 +5,12 @@ const isValidate = name => {
   if (!name) return false;
   if (typeof name !== 'string') return false;
   if (!name.includes(' ')) return false;
+  const a = 'a'.charCodeAt(0);
+  const z = 'z'.charCodeAt(0);
   for (const letter of name) {
     if (letter === ' ') continue;
     const unicode = letter.toLowerCase().charCodeAt(0);
-    if (!(unicode >= 97 && unicode <= 122)) { return false; }
+    if (!(unicode >= a && unicode <= z)) { return false; }
   }
   return true;
 };
