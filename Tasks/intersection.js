422d48a92041972310c57aabbb6d2679f5ab42ed
Alex Kirovich <96977619+AlexKirovich@users.noreply.github.com>
2022-01-02 21:37:19 +0200

    modified function so it returns new object

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,14 +1,15 @@
 // Finde an intersection of two dictionaries
 
 const intersection = (obj1,obj2) => {
+  const result = {};
   for (key in obj1){
     if (obj2[key]) {
-      obj2[key] = obj1[key];
+      result[key] = obj1[key];
     } else{
       delete obj1[key];
     }
   }
-  return obj1;
+  return result;
 }
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
