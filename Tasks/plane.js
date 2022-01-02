9f867370a3e034b7380dbef815e598bef8f55b17
Hrystyna <yaskulskahrystyna@gmail.com>
2022-01-02 21:52:07 +0200

    Add/remowe semicolons and spaces

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -2,19 +2,20 @@
 'use strict'
 
 const_plane = function(arr, res=[]) {
-  j = 0 for(let i = 0, length = arr.length; i < length; i++){
+  j = 0; 
+  for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
-    j = i
-    if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
+    j = i;
+      if (Array.isArray(value, typeof value) && [i, length]) {
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1];
+    } else {
+    arr[i] = res[j - 1];
     res.push(value);
-    };
-  };
-  return res
-}
+    }
+  }
+  return res;
+};
 
 const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
