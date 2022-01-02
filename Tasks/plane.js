a7c6afcdde0c93593432af6322d2d3040514a346
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-01-02 23:21:47 +0200

    renaming array

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,20 +1,20 @@
 'use strict';
 // Make nested array plane
 
-const plane = (arr, res = []) => {
+const plane = (arr, flatArray = []) => {
   let j = 0;
   for (let i = 0, length = arr.length; i < length; i++) {
     const value = arr[i];
     j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-      res.push(...plane(value));
-      arr[i] = res[i - 1];
+      flatArray.push(...plane(value));
+      arr[i] = flatArray[i - 1];
     } else {
-      arr[i] = res[j - 1];
-      res.push(value);
+      arr[i] = flatArray[j - 1];
+      flatArray.push(value);
     }
   }
-  return res;
+  return flatArray;
 };
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
