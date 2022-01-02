12da61f96612b3259d6b4ed0b5c031a2ee5f064a
Hrystyna <yaskulskahrystyna@gmail.com>
2022-01-03 00:38:11 +0200

    Fix non descriptive names antipattern

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -2,11 +2,11 @@
 'use strict'
 
 const merge = (obj1,obj2) => {
-   const obj3 = {...obj1};
+   const merged = {...obj1};
    for (const name in obj2) {
-        obj3[name] = obj2[name]
+        merged[name] = obj2[name]
   }
-return obj3;
+return merged;
 };
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
