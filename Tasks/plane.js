01e54567db82804d612a080f2782f1449ac41f61
Dan <89908678+GhostDolphin@users.noreply.github.com>
2022-01-02 23:40:07 +0200

    Restyled conditions

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -3,11 +3,8 @@
 const plane = function(arr) {
   const res = [];
   for (const value of arr) {
-    if (Array.isArray(value)) {
-      res.push(...plane(value));
-    } else {
-      res.push(value);
-    }
+    if (Array.isArray(value)) res.push(...plane(value));
+    else res.push(value);
   }
   return res;
 };
