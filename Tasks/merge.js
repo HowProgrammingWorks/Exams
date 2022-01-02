3bfd3eb8ab2e28c5c5340ca4036e7b6cfb7ef47e
vladyatsuk <90204297+vladyatsuk@users.noreply.github.com>
2022-01-03 00:56:53 +0200

    значно спростив функцію, використавши spread-оператори, зробив стрілкою

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,15 +1,6 @@
 // Merge two dictionaries
 'use strict';
-const mergeTwoObj = function (obj1,obj2) {
-    mergedObj = {};
-   for (key in obj1) {
-        mergedObj[key] = obj1[key];
-  }
-   for (key in obj2) {
-     mergedObj[key] = obj2[key];
-    }
-return obj1, obj2, mergedObj;
-}
+const mergeTwoObj = (obj1, obj2) => ({...obj1, ...obj2});
 
 const result = mergeTwoObj({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
