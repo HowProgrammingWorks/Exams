61d6355e5ca142121ace154c51b6b51e1b7a33dd
Hrystyna <yaskulskahrystyna@gmail.com>
2022-01-02 23:28:35 +0200

    Remove meaningless comparison

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,13 +4,9 @@
 const merge = (obj1,obj2) => {
    const obj3 = [({})][0];
    for (const name in obj1) {
-       (
-          obj3[name] == obj1[name]
-       );
         obj3[name] = obj1[name]
   }
    for (const name in obj2) {
-      (obj3[name] == obj2[name])
      obj3[name] = obj2[name]
     }
 return obj1, obj2, obj3;
