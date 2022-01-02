3b67fbb1e35cf47508654e251f52df17bbf3279b
danil2205 <04danil@gmail.com>
2022-01-02 22:27:13 +0200

    month.js => add semicolons and deleted unnecessary, remove extra spaces and set normal tabulation

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,14 +1,14 @@
 // Get month number
 'use strict';
-const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
 
-const monthNum = (yourMonth) =>{
-  const len = months.length
-    for(let i=0;i<len;i++){;
-      if(yourMonth.toLowerCase().startsWith(months[i]))return(i+1)
-        };
-          return -1
-            }
+const monthNum = (yourMonth) => {
+  const len = months.length;
+  for (let i = 0; i < len; i++) {
+    if (yourMonth.toLowerCase().startsWith(months[i])) return i + 1;
+  }
+  return -1;
+};
 
-const result = monthNum('february')
-console.log(result)
+const result = monthNum('february');
+console.log(result);
