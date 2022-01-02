b8137ae0fd093713de47525e0a7c887f24ad883a
Alex Kirovich <96977619+AlexKirovich@users.noreply.github.com>
2022-01-02 21:34:10 +0200

    removed unnecessary condition

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,14 +1,14 @@
 // Finde an intersection of two dictionaries
 
 const intersection = (obj1,obj2) => {
-for (key in obj1){
-if (key !== '' && obj2[key]) {
-obj2[key] = obj1[key];
-} else{
-delete obj1[key];
-}
-}
-return obj1;
+  for (key in obj1){
+    if (obj2[key]) {
+      obj2[key] = obj1[key];
+    } else{
+      delete obj1[key];
+    }
+  }
+  return obj1;
 }
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
