b00278b55d4f3a8db8a8e3463f288acf18957821
Alex Kirovich <96977619+AlexKirovich@users.noreply.github.com>
2022-01-02 22:23:39 +0200

    camel case and some semicolons

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,15 +1,15 @@
 // Extract substring between prefix and suffix
 
-const getvaluebetween = (str, start, end) => {
-  const firstIndex = str.indexOf(start) + start.length
-  const lastIndex = str.indexOf(end)
+const getValueBetween = (str, start, end) => {
+  const firstIndex = str.indexOf(start) + start.length;
+  const lastIndex = str.indexOf(end);
   let result = '';
-    if ( firstIndex === -1 || lastIndex === -1) return ''
-    else {
-      result = str.substring(firstIndex, lastIndex);
-    }
+  if ( firstIndex === -1 || lastIndex === -1) return '';
+  else {
+    result = str.substring(firstIndex, lastIndex);
+  }
   return result;
-}
+};
 
-const result = getvaluebetween('Hello <username> and bye!', '<', '>')
+const result = getValueBetween('Hello <username> and bye!', '<', '>')
 console.log(result)
