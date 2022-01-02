0921fedf59e995799208092e6eff35af6eabb13c
Alex Kirovich <96977619+AlexKirovich@users.noreply.github.com>
2022-01-03 00:54:47 +0200

    strict mode enabled, code shortened a little bit more

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,13 +1,12 @@
 // Extract substring between prefix and suffix
+'use strict'
 
 const getValueBetween = (str, start, end) => {
   const firstIndex = str.indexOf(start) + start.length;
   const lastIndex = str.indexOf(end);
   let result = '';
   if ( firstIndex === -1 || lastIndex === -1) return '';
-  else {
-    result = str.substring(firstIndex, lastIndex);
-  }
+  else result = str.substring(firstIndex, lastIndex);
   return result;
 };
 
