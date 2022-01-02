dc448abb5cab517c3863ccb5f749f8113af1ef3d
Dan <89908678+GhostDolphin@users.noreply.github.com>
2022-01-02 23:54:21 +0200

    Removed testing

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -6,6 +6,3 @@ const plane = arr => {
     Array.isArray(val) ? res.push(...plane(val)) : res.push(val);
   return res;
 };
-
-const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
-console.log(result);
