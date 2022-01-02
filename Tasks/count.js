ed352d9547de54ef4c32d1e706c2a8390b6c4456
LikerFeed <vanya18845@gmail.com>
2022-01-02 18:13:22 +0000

    Added missing spaces. Added  'count' and 'keys' to the variable with 'const'. Added 'sum' to the variable with 'let'.

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,13 +1,16 @@
 // Sum all numver values in dict
 'use stict';
 
-count =obj=> {sum=0;{}
- keys = Object.keys(obj);
-   keys.forEach((key) => {
-      ([]);const value = obj[key];
-      if (typeof value === 'number')sum += value });
-     ({});
-   return sum};
+const count = obj => {
+  let sum = 0;{}
+  const keys = Object.keys(obj);
+  keys.forEach((key) => {
+    ([]);const value = obj[key];
+    if (typeof value === 'number') sum += value 
+  });
+  ({});
+  return sum
+};
 
 const result = count({ a: 1, b: 'two', c: 3, d: 4 });
 console.log(result);
