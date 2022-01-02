9be2890eb585187185db55b6cab62532b163fadb
danil2205 <04danil@gmail.com>
2022-01-02 22:53:41 +0200

    inc.js => remove extra spaces, set normal tabulation, put parentheses and semicolons, delete extra parentheses

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,14 +1,12 @@
 // Increment all numbers in dictionary
 'use strict';
-const incNumbers=(
-  (dict) => {
-   for (const key in dict) {
-      if ((typeof dict[ key ]).charAt(0).toUpperCase() ==='N'){
-      dict[ key ] = dict[key] + 1}
-   }
-  return dict;
+const incNumbers = (dict) => {
+  for (const key in dict) {
+    if ((typeof dict[key]).charAt(0).toUpperCase() ==='N'){
+    dict[key] = dict[key] + 1}
   }
-)
+  return dict;
+};
 
 const result = incNumbers({ a: 1, b: 2, c: 'hello', d: false });
 console.log(result);
