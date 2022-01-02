a79623c320d2ccc3f42d2b3da66832e6426e4e2d
Maksym <chyrkovmaksym1@gmail.com>
2022-01-02 19:29:25 +0200

    removed unnecessary logic

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -2,11 +2,9 @@
 function mergeTwoDictionaries(first, second) {
   mergedDic = {};
   for (key in first) {
-    mergedDic[key] == first[key];
     mergedDic[key] = first[key];
   }
   for (key in second) {
-    mergedDic[key] == second[key];
     mergedDic[key] = second[key];
   }
   return first, second, mergedDic;
