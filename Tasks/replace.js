4c120b4ae3a65f71be3a9bc69d008b8671243833
LLumisS <vasa16671@gmail.com>
2022-01-02 18:09:31 +0200

    deleted useless while + added extra condition

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -3,21 +3,15 @@
 // Replace substring with newstr
 
 const replace = (string, subString, newString) => {
-  if (subString === '') {
+  if ((subString === '') || (!string.includes(subString))) {
     return string;
   } else {
-    let part = string;
     let result = '';
-    do {
-      const index = part.indexOf(subString);
-      if (index === -1) {
-        return result + part;
-      } else {
-        const start = part.substring(0, index);
-        part = part.substring(index + subString.length, part.length);
-        result += start + newString;
-      }
-    } while (true);
+    const index = string.indexOf(subString);
+    const part = string.substring(index + subString.length, string.length);
+    const start = string.substring(0, index);
+    result += start + newString + part;
+    return result;
   }
 };
 
