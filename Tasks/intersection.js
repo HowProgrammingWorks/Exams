50d23942165879f1eeab3916c9c1cd3d9a5d97a1
OlegKorniychuk <kornoleg576@gmail.com>
2022-01-02 21:53:40 +0200

    removed unneccesary condition and made the function pure

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,14 +3,13 @@
 
 function intersection(object1, object2) {
   const firstKeys = Object.keys(object1);
-    for (const key of firstKeys) {
-      if (key !== '' && object2[key]) {
-        object2[key] = object1[key];
-      } else {
-          delete object1[key];
-        }
+  const intersection = {};
+  for (const key of firstKeys) {
+    if (object2[key]) {
+      intersection[key] = object1[key];
     }
-  return object1;
+  }
+  return intersection;
 }
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
