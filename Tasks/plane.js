2e1bf357803f0887befc9a1ecc6579082f924c4d
GAlexandrD <o.horbatko.im11@kpi.ua>
2022-01-02 23:28:12 +0200

    correct declaration of function and change its name

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,12 +1,12 @@
 // Make nested array plane
 
-const_plane = function(arr, res=[]) {
+const plane = function(arr, res=[]) {
   for(let value of arr){
-    if (Array.isArray(value)) res.push(...const_plane(value));
+    if (Array.isArray(value)) res.push(...plane(value));
     else res.push(value);
   }
   return res
 }
 
-const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
+const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
