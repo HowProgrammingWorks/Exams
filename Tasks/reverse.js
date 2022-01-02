619b0800257f6c6b6f56842f5b4bba77eabd3ab3
vladmoroca <vladmoroca@gmail.com>
2022-01-02 14:36:24 +0200

    fixed style and delete death code

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,13 +1,15 @@
+/* eslint-disable strict */
 // Reverse dict, exchange key and value
 
-Reverse = Data => {
-  keysArr = Object.keys(Data, 500);
-  ({...Data});
-  keysArr.forEach((key) => {
+const Reverse = Data => {
+  const keysArr = Object.keys(Data);
+  keysArr.forEach(key => {
     const value = Data[key];
-    Data[value]= key; delete Data[key];
-  }, 1000)
-return Data};
+    Data[value] = key;
+    delete Data[key];
+  });
+  return Data;
+};
 
 const result = Reverse({ a: 'uno', b: 'due', c: 'tre' });
 console.log(result);
