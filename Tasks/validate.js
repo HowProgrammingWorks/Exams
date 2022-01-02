631a62e5848e2007f6683c8d8e6b7d275975bc98
Nester44 <nesterov.mail4@gmail.com>
2022-01-02 16:13:51 +0200

    deleting unnecessary if

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -4,15 +4,13 @@
 
 const isValidate = (personName) => {
   if (!personName) return false;
-  if (personName === '') return false;
   if (typeof personName !== 'string') return false;
-  if (personName.length === 0) return false;
   if (!personName.includes(' ')) return false;
 
   for (C of personName) {
     if (C === ' ') continue;
     if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
-  };
+  }
 
   return true;
 };
