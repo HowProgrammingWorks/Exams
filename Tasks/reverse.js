679ccfaf4cb075c9a6367f4ba5f678991e5dc833
vladmoroca <vladmoroca@gmail.com>
2022-01-02 14:17:37 +0200

    fixed code style

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,11 +1,12 @@
 // Reverse dict, exchange key and value
 
-Reverse =Data=> {
- keysArr = Object.keys(Data, 500);({...Data});
-   keysArr.forEach((key) => {
-      const value = Data[key];
-      Data[value]= key; delete Data[key];
-   }, 1000)
+Reverse = Data => {
+  keysArr = Object.keys(Data, 500);
+  ({...Data});
+  keysArr.forEach((key) => {
+    const value = Data[key];
+    Data[value]= key; delete Data[key];
+  }, 1000)
 return Data};
 
 const result = Reverse({ a: 'uno', b: 'due', c: 'tre' });
