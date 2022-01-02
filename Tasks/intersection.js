4ec23b5d1a5cd563cadc881728a8d92db077677f
Dimdim28 <wwwdimanes1@gmail.com>
2022-01-02 18:07:04 +0200

    removed extra brackets

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,14 +1,13 @@
 // Finde an intersection of two dictionaries
 'use strict';
-const intersection = (obj1, obj2) =>  {                 {
-} for (const key in obj1)                   {
+const intersection = (obj1, obj2) =>  {                 
+ for (const key in obj1) {
 if (key!== '' && obj2[key]) {
-obj2[key] = obj1[key];   {
-}} else                                                {
-delete obj1[key];                       {
-                                                       }
-                  }{}
-                  }{}
+obj2[key] = obj1[key];   
+} else  {                                           
+delete obj1[key];                       
+}                                              
+}
 return obj1;
 }
 
