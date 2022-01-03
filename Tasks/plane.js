59b604c1734aa8b38c8f28189ababfa9e56d29fe
junkjuk <junkjuk5645@gmail.com>
2022-01-03 21:27:56 +0200

    (plane)Optimized return

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,15 +1,13 @@
 'use strict';
 
-const plane = arr => {
-  const res = arr.flatMap( element => {
+const plane = arr => {  
+  return arr.flatMap( element => {
     if (Array.isArray(element)) {
       return plane(element);
     } else {
       return element;
     }
-  });
-  
-  return res;
+  });;
 }
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
