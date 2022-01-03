4cadddd7acb5618718d978c4e9975906f598c37e
junkjuk <junkjuk5645@gmail.com>
2022-01-03 21:29:14 +0200

    (plane)Made the code shorter, but I don't know if it improved its readability

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const plane = arr => {  
-  return arr.flatMap( element => {
+const plane = arr => arr.flatMap( element => {
     if (Array.isArray(element)) {
       return plane(element);
     } else {
       return element;
     }
   });;
-}
+
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
