a49452e7f8b6e4ee4a9e4eedd746558a16e3f779
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-01-04 01:42:38 +0200

    use semicolons correctly

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,20 +1,20 @@
 // Make nested array plane
 
 const_plane = function(arr, res = []) {
-  j = 0
+  j = 0;
   for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
-    j = i
+    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
       res.push(...const_plane(value));
-      arr[i] = res[i - 1]
+      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1]
+      arr[i] = res[j - 1];
       res.push(value);
-    };
-  };
-  return res
-}
+    }
+  }
+  return res;
+};
 
 const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
