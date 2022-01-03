1f78df904386e0540f6884c8172af6033dc20c46
volkovily <volkov.ilyya@gmail.com>
2022-01-03 15:32:06 +0200

    Fixed spaces

--- Tasks/quotes.js
+++ Tasks/quotes.js
@@ -1,22 +1,22 @@
 // Change douple quotation to open or close quotation
 
 const quotes = (string) => {
- res = [];
- open = false;
- for (character of string) {
-   if (character === '"') {
-       if (!open) {
-           res.push('«');
-           open = true;
-       } else {
-           res.push('»');
-           open = false;
-       }
-   } else {
-       res.push(character);
-   }
- }
- return res.join('');
+  res = [];
+  open = false;
+  for (character of string) {
+    if (character === '"') {
+        if (!open) {
+            res.push('«');
+            open = true;
+        } else {
+            res.push('»');
+            open = false;
+        }
+    } else {
+        res.push(character);
+    }
+  }
+  return res.join('');
 }
 
 const result = quotes('Hello "Marcus"! Ave "Marcus"!');
