e34bb4caa2746a45d4c1e7e1fed466bc2de1bacd
vania <i.p.yeremenko@gmail.com>
2022-01-03 18:54:16 +0200

    adding semicolons

--- Tasks/words.js
+++ Tasks/words.js
@@ -3,25 +3,25 @@
 // Count words
 
 const words = (string) => {
-  let numberOfWords = 0
-  let flag = false
+  let numberOfWords = 0;
+  let flag = false;
   for (const element of string) {
     if (!flag) {
       if (element === ' ') {
-        if (flag === true) { flag = false } else { flag = false }
+        if (flag === true) { flag = false; } else { flag = false; }
       } else {
-        if (flag === true) { flag = true } else { flag = true }
-        numberOfWords++
+        if (flag === true) { flag = true; } else { flag = true; }
+        numberOfWords++;
       }
     } else {
       if (element === ' ') {
-        if (flag === true) { flag = false } else { flag = false }
+        if (flag === true) { flag = false; } else { flag = false; }
       } else {
-        if (flag === true) { flag = true } else { flag = true }
+        if (flag === true) { flag = true; } else { flag = true; }
       }
     }
   }
-  return numberOfWords
+  return numberOfWords;
 }
 
 const result = words('Hello Marcus Aureluis');
