860e0975e4de8d0407261efc9c0d01a00528d665
yarman1 <gordienko045@gmail.com>
2022-01-03 17:04:46 +0200

    zip.js => improve spaces in function

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,21 +1,18 @@
 // Find first word with two identical letters
 'use strict';
 
-const zip = (a, b) =>
-{
-let i = 0; j = 0
+const zip = (a, b) => {
+  let i = 0;
+  j = 0
   for (x of b) {
-    CELL = [a[i++],x];
-    if (i<j)
-    {
+    CELL = [a[i++], x];
+    if (i < j) {
       delete a[i++]
-    }
-  else
-    {
+    } else {
       (() => (b[j++]=CELL))();
     }
   }
-return b
+  return b
 };
 
 const result = zip(['Roma', 'Kiev', 'Beijing'], [100, 200, 300]);
