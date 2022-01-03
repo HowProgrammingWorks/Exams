ba203831e04aed1ab96b5eb0b3e2e25050c0c20c
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-03 21:36:47 +0200

    Revert "Code of replace.js file translated into Kotlin language in replace.kt"

--- Kotlin/src/replace.kt
+++ Kotlin/src/replace.kt
@@ -1,15 +1,16 @@
- fun replace(string: String, subString: String, newString: String){
-     if(subString == "") string
-      else{
-         var src = string
+ var Replace = lambda@{ str: String, substr: String, newstr: String ->
+     if(substr == ""){
+         str
+     } else{
+         var src = str
          var res = ""
          do{
-             val _index = src.indexOf(subString)
-             if(_index == -1) {return (res+src)} else{
+             val _index = src.indexOf(substr)
+             if(_index == -1) {return@lambda(res+src)} else{
                  val start = src.substring(0,_index)
-                 src = src.substring(_index+subString.length,src.length)
-                 res += start + newString
-             } }while (true); } ;};
+                 src = src.substring(_index+substr.length,src.length)
+                 res += start + newstr
+             }}while (true); } ;}
  fun main(){
      val result = Replace("Hello <username> and bye!", "<username>", "Marcus")
      println(result)
