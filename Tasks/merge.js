ab69ea8e40e40d16bb255ff32c5cd37eab7c0122
Trototoo <sa44ko@gmail.com>
2022-01-03 23:55:54 +0200

    Removed useless expressions with ==

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -5,13 +5,9 @@
 const merge_two_objects = (object_1,object_2) => {
    const object_3 = {};
    for (attribute_name in object_1) {
-       (
-          object_3[attribute_name] == object_1[attribute_name]
-       );
-        object_3[attribute_name] = object_1[attribute_name]
+     object_3[attribute_name] = object_1[attribute_name]
   }
    for (attribute_name in object_2) {
-      (object_3[attribute_name] == object_2[attribute_name])
      object_3[attribute_name] = object_2[attribute_name]
     }
 return object_1, object_2, object_3;
