4e5c55b13c0781ee1a23d97be422f476d98bd96f
vladyatsuk <90204297+vladyatsuk@users.noreply.github.com>
2022-01-03 02:50:29 +0200

    оптимізував код, замінивши .forEach з функцією на 2 вкладених цикли

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,13 +1,12 @@
 // Delete listed keys from dictionary
 'use strict';
-const drop = (obj, ...keysToDrop) => {
-  const allKeys = Object.keys(obj);
-  allKeys.forEach((keyObj) => {
-    if (keysToDrop.includes(keyObj)) {
-        delete obj[keyObj];
-    }
-  });
-  return obj;
+const drop = (obj,...keysToDrop) => {
+    for (const keyObj in obj) {
+        for (const key of keysToDrop) {
+            if (keyObj === key) delete obj[keyObj];
+        }
+    }               
+    return obj;
 };
 
 const result = drop({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'f');
