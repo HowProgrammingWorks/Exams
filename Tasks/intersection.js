d1d97b68907ac16d326bf67c9a0e77569c1f8cb7
Nikita Sutulov <notsid212@gmail.com>
2022-01-03 16:26:34 +0200

    Corrected the algorithm so that the function is now clean.

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,14 +2,13 @@
 
 const intersection = (object1, object2) => {
   const OBJECT1_KEYS = Object.keys(object1);
+  const intersected = {};
   for (const key of OBJECT1_KEYS) {
-    if (key !== '' && object2[key]) {
-      object2[key] = object1[key];
-    } else {
-      delete object1[key];
+    if (key && object2[key] === object1[key]) {
+      intersected[key] = object1[key];
     }
   }
-  return object1;
+  return intersected;
 };
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
