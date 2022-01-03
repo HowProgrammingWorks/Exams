834cf975da861d0f2beea3755e46aee670b7a16f
vladyatsuk <90204297+vladyatsuk@users.noreply.github.com>
2022-01-03 02:11:25 +0200

    додав відступи

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,13 +1,14 @@
 // Delete listed keys from dictionary
 'use strict';
-const drop=(obj,...keysToDrop)=>{
+const drop = (obj, ...keysToDrop) => {
   const allKeys = Object.keys(obj);
   allKeys.forEach((keyObj) => {
-  if (keysToDrop.includes(keyObj)) {
-delete obj[keyObj];
+    if (keysToDrop.includes(keyObj)) {
+        delete obj[keyObj];
     }
-    })
-return obj};
+  });
+  return obj;
+};
 
 const result = drop({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'f');
 console.log(result);
