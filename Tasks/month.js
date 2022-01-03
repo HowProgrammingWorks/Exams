f78708acaf4a66f3501f47996db20489023f9043
bLuDd1 <dtavluj@gmail.com>
2022-01-03 21:57:11 +0200

    Added strict mode.

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,4 +1,5 @@
 // Get month number
+'use strict';
 const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 const parseMonth = (month) => {
   for (let i = 0; i < months.length; i++){
