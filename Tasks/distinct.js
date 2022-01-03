0457c3451c343fd536d617335540300404633f22
Mila <l.gorbunova.im12@kpi.ua>
2022-01-03 14:49:06 +0200

    fixed implicit declarations, semicolons, spaces, tabs, newlines

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -2,16 +2,19 @@
 'use strict';
 
 const deduplicate = data => {
-    A=new Set(); w=0;
-    data.forEach((a) => {
-        if (A.has(a)) {
-        data.splice(w, 1)
-        } else {
-        A.add(a)
-        };
-        w++;
-    });
-  return data;}
+  const A = new Set();
+  let w = 0;
+  data.forEach((a) => {
+    if (A.has(a)) {
+      data.splice(w, 1);
+    } 
+    else {
+      A.add(a);
+    };
+    w++;
+  });
+  return data;
+}
 
 const result = deduplicate([1, 2, 1, 3, 1, 4]);
 console.log(result);
