a8ed192d7991ecfc9f4e3ac3f7a4c3e71754fc98
vita133 <vitandrukhiv@gmail.com>
2022-01-03 14:45:36 +0200

    removed meaningless assigments and unnecessary identifiers

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -2,15 +2,11 @@
 
 "use strict";
 const plane = (arr, res = []) => {
-  j = 0;
   for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
-    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
       res.push(...plane(value));
-      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1];
       res.push(value);
     }
   }
