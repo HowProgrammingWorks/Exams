9fa2407c103193c766c7c3a8bbe3722848ebfab6
vania <i.p.yeremenko@gmail.com>
2022-01-03 21:37:24 +0200

    semicolons

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -2,18 +2,18 @@
 
 // Remove values from array
 
-const Skip =(T,...Remove)=> {
+const Skip = (T, ...Remove) => {
   let x = 0;
   for (const C of T) {
     for (const X of Remove) {
       if (C === X) {
         T.splice(x, 1);
-      };
+      }
     }
     x++;
   }
   return T;
-}
+};
 
 const result = Skip([1, 2, 3, 4, 5], 2, 5);
 console.log(result);
