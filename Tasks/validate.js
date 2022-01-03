2647968d8af8557458d283b136f02f1fa8583a21
neroheroes6@gmail.com <neroheroes6@gmail.com>
2022-01-03 19:57:12 +0200

    Deleted unnecessary braces

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -5,11 +5,10 @@ const isValidate = name => {
   if (typeof name === 'string') {
     const regEx = /[A-Z]/;
     const words = name.split(' ');
-    for (let word of words) {
+    for (let word of words)
         if (regEx.test(word)) continue; else return false;
 
     return true;
-}
 }
   else return false;
 }
