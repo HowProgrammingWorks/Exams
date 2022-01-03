7f35532c99c11f98a36f81531c663fdd43bcaa46
bLuDd1 <dtavluj@gmail.com>
2022-01-03 21:56:46 +0200

    Added paddings where it's necessary. Deleted unnecessary 'return', empty space and lines

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,15 +1,10 @@
 // Get month number
-
-const months = [
-'jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'
-]
-
-const parseMonth= (month)=>{
-    for(let i=0;i<months.length;i++){
-      if(month.toLowerCase().startsWith(months[i]))return(i+1)
-        }
-          return -1
-            }
-
+const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+const parseMonth = (month) => {
+  for (let i = 0; i < months.length; i++){
+  if (month.toLowerCase().startsWith(months[i]))
+   return i + 1;
+   }
+};
 const result = parseMonth('february')
 console.log(result)
