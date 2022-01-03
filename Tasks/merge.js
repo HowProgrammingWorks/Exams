0466bbc22114ca2b0411863da8a9b59bb92be256
Trototoo <sa44ko@gmail.com>
2022-01-04 00:13:49 +0200

    Fixed tabulation, added ; and changed function name

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,17 +1,17 @@
 // Merge two dictionaries
 
-'use strict'
+"use strict";
 
-const merge_two_objects = (object_1,object_2) => {
-   const object_3 = {};
-   for (const key in object_1) {
-     object_3[key] = object_1[key]
+const mergeTwoObjects = (object_1, object_2) => {
+  const object_3 = {};
+  for (const key in object_1) {
+    object_3[key] = object_1[key];
   }
-   for (const key in object_2) {
-     object_3[key] = object_2[key]
-    }
-return object_3;
-}
+  for (const key in object_2) {
+    object_3[key] = object_2[key];
+  }
+  return object_3;
+};
 
-const result = merge_two_objects({ a: 'uno', b: 'due' }, { c: 'tre' });
+const result = mergeTwoObjects({ a: "uno", b: "due" }, { c: "tre" });
 console.log(result);
