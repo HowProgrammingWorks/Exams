d92791c41ab677bd7c8a98ee721fa1660730e467
Vadim Ulanovskiy <90156655+VeSpy4u@users.noreply.github.com>
2022-01-03 22:11:13 +0200

    Fix tabs and spaces

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,11 +1,14 @@
 // Copy all values from dict except listed
 
-except =(obj,...excision)=> {
- keys = Object.keys(obj);
-   keys.forEach((key) => {
-if (excision.includes(key)) { delete obj[key];}
+except = (obj, ...excision) => {
+  keys = Object.keys(obj);
+  keys.forEach((key) => {
+    if (excision.includes(key)) {
+      delete obj[key];
+    }
   });
-  return obj}
+  return obj
+}
 
 const result = except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
