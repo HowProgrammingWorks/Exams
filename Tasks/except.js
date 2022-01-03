7bf2f36ca2bfb961030a9528a8f36ded0e9e0e2d
Vadim Ulanovskiy <90156655+VeSpy4u@users.noreply.github.com>
2022-01-03 22:04:20 +0200

    Removing dead code

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,12 +1,10 @@
 // Copy all values from dict except listed
 
 EXCEPT =(incomingvaluesarray,...no)=> {
- intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+ intermediate_variable = Object.keys(incomingvaluesarray);
    intermediate_variable.forEach((Z) => {
-   [].sort(() => 2000);
-if (no.includes(Z)) { delete incomingvaluesarray[Z]; return;} else { return; delete incomingvaluesarray[Z]; }
-  }, 2000);
-  ({ key: 'value' });
+if (no.includes(Z)) { delete incomingvaluesarray[Z];}
+  });
   return incomingvaluesarray}
 
 const result = EXCEPT({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
