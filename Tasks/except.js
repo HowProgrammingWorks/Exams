e88b4e50ac073d8804526c4b1d0a17acd12b8731
Vadim Ulanovskiy <90156655+VeSpy4u@users.noreply.github.com>
2022-01-03 22:07:03 +0200

    Changing variables name

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,11 +1,11 @@
 // Copy all values from dict except listed
 
-EXCEPT =(incomingvaluesarray,...no)=> {
- intermediate_variable = Object.keys(incomingvaluesarray);
-   intermediate_variable.forEach((Z) => {
-if (no.includes(Z)) { delete incomingvaluesarray[Z];}
+except =(obj,...excision)=> {
+ keys = Object.keys(obj);
+   keys.forEach((key) => {
+if (excision.includes(key)) { delete obj[key];}
   });
-  return incomingvaluesarray}
+  return obj}
 
-const result = EXCEPT({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
+const result = except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
