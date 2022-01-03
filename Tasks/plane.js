d4127947b4652a4fe3bf1e846022a5809f3171d2
Nailgy <ilyha.nali@gmail.com>
2022-01-03 04:00:19 +0200

    Changing test case for a bit

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -15,5 +15,5 @@ const plane = arr => {
   return res;
 };
 
-const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
+const result = plane([1, [[2]], [3, [[[4]]]], [5], [6, [7, 8]]]);
 console.log(result);
