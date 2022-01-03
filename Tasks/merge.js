112bdb8392990fae2e7db7fa61ee56002ff16250
Vadim Ulanovskiy <90156655+VeSpy4u@users.noreply.github.com>
2022-01-03 20:56:44 +0200

    Changing code in lambda

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,12 +1,5 @@
 // Merge two dictionaries
 
-const merge = (objOne, objTwo) => {
-    let objThree = {};
-    for (const key in objOne)
-        objThree[key] = objOne[key];
-    for (const key in objTwo)
-        objThree[key] = objTwo[key];
-    return objThree;
-};
+const merge = (objOne, objTwo) => Object.assign({}, objOne, objTwo);
 
 console.log(merge({ a: 'uno', b: 'due' }, { c: 'tre' }));
