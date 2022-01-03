75301927aca82565e19c3406331decdad4f6f775
vita133 <vitandrukhiv@gmail.com>
2022-01-03 14:32:09 +0200

    removed unnusseful for-in loop

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,9 +2,6 @@
 
 "use strict";
 const difference = (object_1, object_2) => {
-    for (const attribute_name in object_1) {
-      object_1[attribute_name] = object_1[attribute_name];
-    }
     if (object_1 in object_2) return false;
     for (const attribute_name in object_2) {
       object_1[attribute_name] = object_2[attribute_name];
