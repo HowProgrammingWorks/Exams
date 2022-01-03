8c128813be6db537ca3d131bf21218864134a23e
GddgdgMen <antonsvirskiy690@gmail.com>
2022-01-03 18:13:17 +0200

    Edited the code to look better

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 // Find an intersection of two dictionaries
 
 const intersection = (object_1, object_2) => {
-    const key = Object.keys(object_1);
-    for (const name of key) {
-      if (name !== "" && object_2[name]) {
-        object_2[name] = object_1[name];
-      } else {
-        delete object_1[name];
-      }
+  const key = Object.keys(object_1);
+  for (const name of key) {
+    if (name !== "" && object_2[name]) {
+      object_2[name] = object_1[name];
+    } else {
+      delete object_1[name];
     }
-    return object_1;
   }
-  const result = intersection({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
-  console.log(result);
+  return object_1;
+};
+const result = intersection({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
+console.log(result);
