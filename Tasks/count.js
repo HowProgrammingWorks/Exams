636d3ec59154a858ba400abac2578bddd3fe22da
111Timur111 <93223108+111Timur111@users.noreply.github.com>
2022-01-03 22:11:05 +0200

    Update count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,12 +1,16 @@
-// Sum all numver values in dict
+// Sum all number values in dict
+'use strict';
 
-count =obj=> {sum=0;{}
- keys = Object.keys(obj);
-   keys.forEach((key) => {
-      ([]);const value = obj[key];
-      if (typeof value === 'number')sum += value });
-     ({});
-   return sum};
+const count = (dict) => {
+  let sum= 0;
+  const keys = Object.keys(dict);
+  for (const key of keys) {
+    const value = obj[key];
+    if (typeof value === 'number')sum += value;
+  }   
+  return sum;
+};
 
 const result = count({ a: 1, b: 'two', c: 3, d: 4 });
 console.log(result);
+
