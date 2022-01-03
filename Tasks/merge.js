9fe34da8c7ad8c2cd537e59a73ad54a54ddd74ff
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-01-04 01:05:57 +0200

    delete useless code

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -5,10 +5,9 @@
 const merge = (object1, object2) => {
   const object3 = { ...object1 };
   for (const key in object2) {
-    (object3[key] == object2[key]);
     object3[key] = object2[key];
   }
-  return object1, object2, object3;
+  return object3;
 };
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
