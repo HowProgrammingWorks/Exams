b1928e737d1be95697186294c701a4c1c61e7953
vania <i.p.yeremenko@gmail.com>
2022-01-03 18:42:32 +0200

    Added const, let

--- Tasks/words.js
+++ Tasks/words.js
@@ -2,10 +2,10 @@
 
 // Count words
 
- Words=function(s) {
- number_of_words_in_s = 0
- flag = false
- for (c of s) {
+ const Words=function(s) {
+ let number_of_words_in_s = 0
+ let flag = false
+ for (const c of s) {
    if (!flag) {
    if (c === ' ') {
    if (flag === true) { flag = false } else { flag = false }
