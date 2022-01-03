9a22013eefafd545c4b82f8380070266c9e2095c
Alina Dubchak <alina.dubchak.ev@gmail.com>
2022-01-03 04:08:29 +0200

    seveth step: a little bit estetick optimization

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,15 +1,14 @@
 'use strict'
 
-const inc = elements =>{
-     for ( let key in elements)
-      {
+const inc = elements => {
+     for ( let key in elements){
       if ((typeof elements[key]) == "number")
       {
         elements[key] = elements[key] + 1;
-      };
-   };
+      }
+   }
   return elements;
-  }
+  };
 
 
 const obj = ({ a: 1, b: 2, c: 'hello', d: false });
