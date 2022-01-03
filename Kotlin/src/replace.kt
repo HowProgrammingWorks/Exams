43f574ad3a54b867f9fa2c5295c4fb2fe06d2487
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-03 22:03:03 +0200

    The function signature has been changed: the parameters are renamed to more understandable

--- Kotlin/src/replace.kt
+++ Kotlin/src/replace.kt
@@ -1,17 +1,17 @@
-var Replace = lambda@{ str: String, substr: String, newstr: String ->
-    if(substr == ""){
-        str
+var replace = lambda@{ string: String, subString: String, newString: String ->
+    if(subString == ""){
+        string
     } else{
-        var src = str
+        var src = string
         var res = ""
         do{
-            val _index = src.indexOf(substr)
+            val _index = src.indexOf(subString)
             if(_index == -1) {return@lambda(res+src)} else{
                 val start = src.substring(0,_index)
-                src = src.substring(_index+substr.length,src.length)
-                res += start + newstr
+                src = src.substring(_index+subString.length,src.length)
+                res += start + newString
             }}while (true); } ;}
 fun main(){
-    val result = Replace("Hello <username> and bye!", "<username>", "Marcus")
+    val result = replace("Hello <username> and bye!", "<username>", "Marcus")
     println(result)
 }
