f33440a1f87591581126f8280ba0bb37a2828dd3
DaniilAlyeksyeyenko <da295615@gmail.com>
2022-01-03 18:09:14 +0200

    rewrite ternary operators

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,25 +1,26 @@
 'use strict';
+
 // Count words
 
 function words(str) {
- let countWords = 0;
- let flag = false;
- for (const char of str) {
-   if (!flag) {
-     if (char === ' ') {
-        flag = flag === true ? flag = false : false;
-    } else {
-        flag = flag === true ? flag = true : true;
+  let countWords = 0;
+  let flag = false;
+  for (const char of str) {
+    if (!flag) {
+      if (char === ' ') {
+        flag = false;
+      } else {
+        flag = true;
         countWords++;
-    }
-   } else if (char === ' ') {
-    flag = flag === true ? flag = false : false;
+      }
+    } else if (char === ' ') {
+      flag = false;
     } else {
-      flag = flag === true ? flag = true : true;
+      flag = true;
     }
-   }
-   return countWords;
- }
+  }
+  return countWords;
+}
 
 const result = words('Hello Marcus Aureluis');
 console.log(result);
