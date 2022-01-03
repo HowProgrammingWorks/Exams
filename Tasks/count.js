2b0a9e74e57a07315609ef9c6770937d9db51148
LikerFeed <vanya18845@gmail.com>
2022-01-03 15:48:45 +0000

    Used eslint for code cleanliness

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,12 +1,12 @@
 // Sum all number values in dict
-'use stict';
+'use strict';
 
 const count = obj => {
   let sum = 0;
   const keys = Object.keys(obj);
   keys.forEach((key) => {
     const value = obj[key];
-    if (typeof value === 'number') sum += value 
+    if (typeof value === 'number') sum += value;
   });
   return sum;
 };
