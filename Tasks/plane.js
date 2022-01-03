4def4ef8d02fb628c7734bf7542ed163a5ffc791
MytsV <mytsvictoria@gmail.com>
2022-01-03 08:05:50 +0200

    Rewrote the function with lambda syntax

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,9 +1,7 @@
 'use strict';
 // Make nested array plane
 
-const plane = function(arr) {
-  return arr.flatMap(value => (Array.isArray(value) ? plane(value) : value));
-};
+const plane = arr => arr.flatMap(value => (Array.isArray(value) ? plane(value) : value));
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
