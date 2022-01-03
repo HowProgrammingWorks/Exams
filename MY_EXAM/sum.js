3235a123ca2fb98d6ad62e4f1dc256ebee0432d0
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-03 18:54:15 +0200

    Update sum.js
    Remove usless code.
    Make the code workable by introducing new variable

--- MY_EXAM/sum.js
+++ MY_EXAM/sum.js
@@ -1,19 +1,14 @@
-// Sum all numbers from array
+// // Sum all numbers from array
 'use strict'
 function sum (s) {
-  for (let i of s) {
-  let t = typeof i;
+ let summ = 0
+ for (let i of s) {
+ let t = typeof i;
   if (t === 'number'){
-    if (sum.length > 0){
-       const new_Sum = sum[sum.length - 1] + i
-        sum.push(new_Sum)
-    }
-  }
-  };
-  sum[0];
-  return sum[sum.length - 1];
-  sum.push(sum.length - 1);
+    summ  += i 
+ }  
+ };
+ return summ
 }
-
 const result = sum([5, true, 'string', 7, 'hello']);
 console.log(result);
