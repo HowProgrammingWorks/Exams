f4433ff8f8bfb3b984663fd432b0864e261c34f3
neroheroes6@gmail.com <neroheroes6@gmail.com>
2022-01-03 19:49:48 +0200

    Added regular expression

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -3,6 +3,7 @@
 
 const isValidate = name => {
   if (typeof name === 'string') {
+    const regEx = /[A-Z]/;
     for (C of T) {
       if (C === ' ') continue;
       if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
