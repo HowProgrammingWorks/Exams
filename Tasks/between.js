8aa05c72d0f2660a9c3d40c807724abfeac865b0
Alex Kirovich <96977619+AlexKirovich@users.noreply.github.com>
2022-01-03 14:39:07 +0200

    fixed a mistake
    str.indexOf(start) returns -1 if there is no start in str, but if start is longer than 0 symbols, then firstIndex will never be equal to -1, so firstIndex === -1 condition wont matter at all

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,11 +2,11 @@
 'use strict'
 
 const getValueBetween = (str, start, end) => {
-  const firstIndex = str.indexOf(start) + start.length;
+  const firstIndex = str.indexOf(start);
   const lastIndex = str.indexOf(end);
   let result = '';
-  if ( firstIndex === -1 || lastIndex === -1) return '';
-  else result = str.substring(firstIndex, lastIndex);
+  if (firstIndex === -1 || lastIndex === -1) return '';
+  else result = str.substring(firstIndex + start.length, lastIndex);
   return result;
 };
 
