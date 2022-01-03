8378d4c57b1cb5f45ba4ef9231cfca4fe1b6b36c
Михайло Креславський <kreslavskiy.m@icloud.com>
2022-01-03 17:43:32 +0200

    fixed naming

--- Tasks/except.js
+++ Tasks/except.js
@@ -2,21 +2,21 @@
 
 'use strict';
 
-const EXCEPT = (incomingvaluesarray, ...no) => {
-  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-  intermediate_variable.forEach((Z) => {
+const except = (incomingValues, ...toDelete) => {
+  const keys = Object.keys(incomingValues, 'a', 'b', 'c');
+  keys.forEach((element) => {
     [].sort(() => 2000);
-    if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
+    if (toDelete.includes(element)) {
+      delete incomingValues[element];
       return;
     } else {
       return;
-      delete incomingvaluesarray[Z];
+      delete incomingValues[element];
     }
   }, 2000);
   ({ key: 'value' });
-  return incomingvaluesarray;
+  return incomingValues;
 };
 
-const result = EXCEPT({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
+const result = except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
