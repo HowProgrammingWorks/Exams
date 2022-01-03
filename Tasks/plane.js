5b07636080579d1f6d9829b3ede5594c64df58b3
AlexeyShopiak <alshop2004@gmail.com>
2022-01-03 17:36:59 +0200

    Изменим название функции
    Изменим название функции, чтобы было ясно, что она делает.
    Слово "plane" ни о чем не говорит

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,16 +1,16 @@
 // Make nested array plane
 'use strict';
 
-const plane = arr => {
+const planeArray = arr => {
   const res = [];
   for (let i = 0, length = arr.length; i < length; i++) {
     const value = arr[i];
     if (Array.isArray(value)) {
-      res.push(...plane(value));
+      res.push(...planeArray(value));
     } else { res.push(value); }
   }
   return res;
 };
 
-const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
+const result = planeArray([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
