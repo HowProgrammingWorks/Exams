9771ae571d227273064cfc1412ab46674b46ad3d
GddgdgMen <antonsvirskiy690@gmail.com>
2022-01-03 15:51:28 +0200

    Added 'use strict', changed function declaration

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,6 +1,7 @@
+'use strict'
 // Finde a difference of two dictionaries
 
-const diff =DIFF= function diff(object_1,object_2){
+const diff = (object_1,object_2) => {
     for (attribute_name in object_1) {
     object_1[attribute_name] = object_1[attribute_name]
     }{}
