8a3af6e608cf45ad9e0be5e150d3c1391931ac9a
AlinaG <88611367+alinagts@users.noreply.github.com>
2022-01-03 12:52:43 +0200

    created new array not to modify arr2

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -2,11 +2,12 @@
 // Find first word with two identical letters
 
 const zip = (arr1 = [], arr2 = []) => {
+  const result = [];
   for (let i = 0; i < arr1.length; i++) {
     const pair = [arr1[i], arr2[i]];
-    arr2[i]=pair;
+    result.push(pair);
   }
-return arr2;
+  return result;
 }
 
 const result = zip(['Roma', 'Kiev', 'Beijing'], [100, 200, 300]);
