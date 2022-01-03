46eaa567f3d6ea9e08a5b50a14ae7e8328669cd7
yarikkot04 <yarikkotenkoim13@gmail.com>
2022-01-03 22:09:40 +0200

    Added  'parseDay' to the variable with 'const'.

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,7 +4,7 @@
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-parseDay = s => {
+const parseDay = s => {
   let i; for (i = 0; i < days.length; i++) {
     if (s.startsWith(days[i].toLowerCase())) { return i + 1; }
   } return -1;
