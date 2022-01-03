d9df74fe8c08b98d94364b5057c0f5740e23b5b2
Vadim Ulanovskiy <90156655+VeSpy4u@users.noreply.github.com>
2022-01-03 23:14:28 +0200

    Don't change incoming parameters

--- Tasks/except.js
+++ Tasks/except.js
@@ -2,10 +2,11 @@
 // Copy all values from dict except listed
 
 const except = (obj, ...excision) => {
+  let improvedObj = {};
   for (const key of Object.keys(obj)) {
-    if (excision.includes(key)) delete obj[key];
+    if (!excision.includes(key)) improvedObj[key] = obj[key];
   }
-  return obj;
+  return improvedObj;
 };
 
 console.log(except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd'));
