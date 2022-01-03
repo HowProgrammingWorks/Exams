699053c66357a0f73a0aacf323b81fcdde3de419
MrSampy <mynameisserzheo@gmail.com>
2022-01-03 15:39:52 +0200

    fixing formatting & removing unnecessary else blocks

--- Tasks/Replace.cpp
+++ Tasks/Replace.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <string>
 using namespace std;
-const string replace(string str = "",const string substr = "",const string newstr = "")
+const string replace(string str = "", const string substr = "", const string newstr = "")
 {
-  if (substr == ""){
+  if (substr == "")
+  {
     return str;
-  }else{
-    string src=str;
-    string res="";
-    do{
-      const int index=src.find(substr);
-      if (index == -1){ return res + src;}else{ 
-        const string start = src.substr(0, index);
-        src = src.substr(index + substr.size(), src.size());
-        res += start + newstr;
-      }} while (true);}}
+  }
+  string src = str;
+  string res = "";
+  do
+  {
+    const int index = src.find(substr);
+    if (index == -1)
+    {
+      return res + src;
+    }
+    const string start = src.substr(0, index);
+    src = src.substr(index + substr.size(), src.size());
+    res += start + newstr;
+  } while (true);
+}
 int main()
 {
   cout << replace("Hello <username> and bye!", "<username>", "Marcus");
