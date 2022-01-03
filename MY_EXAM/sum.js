711f493793f16eb323afda6f6774f2ba5b5e1054
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-03 17:59:36 +0200

    strict mode

--- MY_EXAM/sum.js
+++ MY_EXAM/sum.js
@@ -1,23 +1,22 @@
 // Sum all numbers from array
-
+'use strict'
 const k = sum = function ( s = () => {} ) {
-    sum=[0];
-    let k =5;
-    for (i of s)
-    {
-    let t = typeof i;
-    if (t === 'number'){
-      if (sum.length > 0){
-         const new_Sum = sum[sum.length - 1] + i
-          sum.push(new_Sum)
-      }
+  sum=[0];
+  let k =5;
+  for (i of s)
+  {
+  let t = typeof i;
+  if (t === 'number'){
+    if (sum.length > 0){
+       const new_Sum = sum[sum.length - 1] + i
+        sum.push(new_Sum)
     }
-    };
-    sum[0];
-    return sum[sum.length - 1];
-    sum.push(sum.length - 1);
   }
-  
-  const result = sum([5, true, 'string', 7, 'hello']);
-  console.log(result);
-  
+  };
+  sum[0];
+  return sum[sum.length - 1];
+  sum.push(sum.length - 1);
+}
+
+const result = sum([5, true, 'string', 7, 'hello']);
+console.log(result);
