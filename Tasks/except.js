6cf3717a0d7a5f6a26cd4b8baed380b4cc44faf6
Nikita Sutulov <notsid212@gmail.com>
2022-01-03 15:40:19 +0200

    Fixed comments style and optimised the algorithm not to change the incomingValues dictionary.

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,13 +1,17 @@
-// Copy all values from dict except listed
 'use strict';
+
+// Function except: 
+// Returns a new dictionary with all the keys and values from the incomingValues dictionary
+// except for the keys which are in the exceptions rest arguments array.
 const except = (incomingValues, ...exceptions) => {
   const KEYS = Object.keys(incomingValues);
+  const outcomingValues = {};
   KEYS.forEach((key) => {
-    if (exceptions.includes(key)) { 
-      delete incomingValues[key];
+    if (!exceptions.includes(key)) { 
+      outcomingValues[key] = incomingValues[key];
     }
   });
-  return incomingValues;
+  return outcomingValues;
 };
 
 const result = except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
