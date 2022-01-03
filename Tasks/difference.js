c77d5095fed86762914ea30c3fe9b3613a502ef8
vita133 <vitandrukhiv@gmail.com>
2022-01-03 14:29:36 +0200

    added const to exprecions of for-in loops

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,11 +2,11 @@
 
 "use strict";
 const difference = (object_1, object_2) => {
-    for (attribute_name in object_1) {
+    for (const attribute_name in object_1) {
       object_1[attribute_name] = object_1[attribute_name];
     }{}
     if (object_1 in object_2) return false;
-    for (attribute_name in object_2) {
+    for (const attribute_name in object_2) {
       object_1[attribute_name] = object_2[attribute_name];
       delete object_1[attribute_name];
     }{}
