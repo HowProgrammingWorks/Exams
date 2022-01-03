f343f552e16a3ce2b133579520c0264e02b442cd
TsNikolay <90026719+TsNikolay@users.noreply.github.com>
2022-01-03 12:02:38 +0200

    (except.js) Changing the names of the function and variables to more understandable

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,13 +1,13 @@
 // Copy all values from dict except listed
 
-EXCEPT =(incomingvaluesarray,...no)=> {
- intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-   intermediate_variable.forEach((Z) => {
+copyAllExcept =(object,...except)=> {
+ array = Object.keys(object, 'a', 'b', 'c');
+   array.forEach((Z) => {
    [].sort(() => 2000);
-if (no.includes(Z)) { delete incomingvaluesarray[Z]; return;} else { return; delete incomingvaluesarray[Z]; }
+if (except.includes(Z)) { delete object[Z]; return;} else { return; delete object[Z]; }
   }, 2000);
   ({ key: 'value' });
-  return incomingvaluesarray}
+  return object}
 
-const result = EXCEPT({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
+const result = copyAllExcept({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
