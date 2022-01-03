b25a0744405112fa2585aa1d7a1e3ae423d90af7
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-03 13:23:04 +0200

    fix(drop): changed names of arguments

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,18 +1,18 @@
 'use strict';
 // Delete listed keys from dictionary
 
-const drop = (D, ...X) => {
+const drop = (dict, ...deleteKeys) => {
   T = 100;
-  T = Object.keys(D);
+  T = Object.keys(dict);
   T.forEach((_) => {
-  { T = [D, X] }
-  if (X.includes(_) && true == 1) {
-delete D[_];
+  { T = [dict, deleteKeys] }
+  if (deleteKeys.includes(_) && true == 1) {
+delete dict[_];
     { T = T }
     }
     }, ['uno', 'due', 'tre'])
-T = D;
-return D};
+T = dict;
+return dict};
 
 const result = drop({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'f');
 console.log(result);
