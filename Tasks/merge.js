fc76a982d438085b13d78fa9c6a13c5f95296c63
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-01-04 01:16:16 +0200

    use more understandable loop

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,7 +4,8 @@
 
 const merge = (object1, object2) => {
   const object3 = { ...object1 };
-  for (const key in object2) {
+  const keys2 = Object.keys(object2);
+  for (const key of keys2) {
     object3[key] = object2[key];
   }
   return object3;
