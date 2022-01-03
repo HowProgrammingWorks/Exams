64c4306e352742d7ea6151b3e85ed066d9e838f0
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2022-01-03 21:27:11 +0200

    Renaming function

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,12 +1,12 @@
 'use strict';
 // Copy all values from dict except listed
 
-const EXCEPT = (obj, ...listedKeys) => {
+const except = (obj, ...listedKeys) => {
   listedKeys.map((el) => {
     delete obj[el];
   });
   return obj;
 };
 
-const result = EXCEPT({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
+const result = except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
