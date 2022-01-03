7afcec8622d2f70643b949d13b705a6e5f767ba1
LikerFeed <vanya18845@gmail.com>
2022-01-03 17:04:54 +0000

    Changed the name of the variables.

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,15 +1,15 @@
 // Sum all number values in dict
 'use strict';
 
-const count = (obj) => {
+const sumNumbersInDict = (dict) => {
   let sum = 0;
-  const keys = Object.keys(obj);
+  const keys = Object.keys(dict);
   for (const key of keys) {
-    const value = obj[key];
+    const value = dict[key];
     if (typeof value === 'number') sum += value;
   }
   return sum;
 };
 
-const result = count({ a: 1, b: 'two', c: 3, d: 4 });
+const result = sumNumbersInDict({ a: 1, b: 'two', c: 3, d: 4 });
 console.log(result);
