455fc60bdfc487a5612941b057a2ad67aa546914
cchebelok <lotokhinivan@gmail.com>
2022-01-03 16:47:10 +0200

    chenge let to const in for in loop

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -3,10 +3,10 @@
 
 const mergeTwoObjects = (obj1, obj2) => {
   const obj3 = {};
-  for (let i in obj1) {
+  for (const i in obj1) {
     obj3[i] = obj1[i];
   }
-  for (let j in obj2) {
+  for (const j in obj2) {
     obj3[j] = obj2[j];
   }
   return obj3;
