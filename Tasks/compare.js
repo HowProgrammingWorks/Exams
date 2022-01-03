832c36341748008fd9afbeaa7d4dfad3a51c69e9
Nazar Vovk <nvovk.2004@gmail.com>
2022-01-03 11:01:19 +0200

    fix(compare): removed unnecessary evaluations

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -5,14 +5,12 @@ const compare = (firstObj, secondObj) => {
   const firstKeys = Object.keys(firstObj);
   const secondKeys = Object.keys(secondObj);
   if (firstKeys.join("-") !== secondKeys.join("-")) return false;
-  let evaluate = true;
+  
   for (const key of firstKeys) {
-    if (firstObj[key] === secondObj[key]) evaluate = evaluate && true;
-    else {
-      evaluate = evaluate && false;
-    }
+    if (firstObj[key] !== secondObj[key]) return false;
   }
-  return evaluate;
+
+  return true;
 };
 
 const result = compare({ a: 1, c: "hello" }, { a: 1, c: "hello" });
