433cd586b565c5ae85ef70349eacbdded435728c
Михайло Креславський <kreslavskiy.m@icloud.com>
2022-01-03 17:31:29 +0200

    initialized function and constant with const

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -2,9 +2,9 @@
 
 'use strict';
 
-DISTINCT = (data) => {
-  A = new Set();
-  w = 0;
+const DISTINCT = (data) => {
+  const A = new Set();
+  let w = 0;
   data.forEach((a) => {
     if (A.has(a)) {
       data.splice(w, 1);
