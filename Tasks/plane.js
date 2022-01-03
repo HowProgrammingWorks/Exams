320671eac9bfc464be194a0be0f6cee2a3bd383e
junkjuk <junkjuk5645@gmail.com>
2022-01-03 19:40:54 +0200

    Fixed syntax error, spacing and semicolons

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,18 +1,19 @@
-// Make nested array plane
+'use strict';
 
-const_plane = function(arr, res=[]) {
-  j = 0 for(let i = 0, length = arr.length; i < length; i++){
+const const_plane = function(arr, res=[]) {
+  let j = 0;
+  for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
-    j = i
+    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
-    };
-  };
-  return res
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1];
+    } else {
+      arr[i] = res[j - 1];
+      res.push(value);
+    }
+  }
+  return res;
 }
 
 const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
