35b85cf21ee38beabb2aa31a15ef2a07e31a0f13
leagerxd <misha.zabotin@gmail.com>
2022-01-03 15:54:55 +0200

    Added 'use strict'.

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,5 +1,7 @@
 // Increment all numbers in dictionary
 
+'use strict';
+
 let inc_numbers = (format_complete, ...rest_variables) => {
   for (delete_file in format_complete) {
     if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
