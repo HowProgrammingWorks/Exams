04171a4653c8bf1e2c250540ff7e7450d0c73e66
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-01-04 01:19:58 +0200

    add check for existing keys

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -6,7 +6,8 @@ const merge = (object1, object2) => {
   const object3 = { ...object1 };
   const keys2 = Object.keys(object2);
   for (const key of keys2) {
-    object3[key] = object2[key];
+    if (!(key in object3))
+      object3[key] = object2[key];
   }
   return object3;
 };
