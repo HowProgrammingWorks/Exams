983d98fac9c646a735d324e3a05e95c9f4c1f462
Valentyn13 <valentyn13git@gmail.com>
2022-01-03 20:27:57 +0200

    resultObj out of function

--- ExamTasks/types.js
+++ ExamTasks/types.js
@@ -1,18 +1,16 @@
 'use strict'
-
-const countTypes = (arr)=> {
- const resultObj = {
+const resultObj = {
   "number": 0,
   "string": 0,
   "boolean": 0,
-  }
-  
+  };
+const countTypes = (arr)=> {
   for (const key of arr) {
   const types = typeof key
   resultObj[types]++
   };
-  
-  return resultObj
+
+  return resultObj;
 }
 
 const result = countTypes([5, true, 'string', 7, 'hello']);
