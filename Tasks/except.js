2680046e16a3f52ca02bdf33e06acb40393e511e
Nikita Sutulov <notsid212@gmail.com>
2022-01-03 14:56:14 +0200

    Removed useless code.

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,9 +1,8 @@
 // Copy all values from dict except listed
 
 EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+  intermediate_variable = Object.keys(incomingvaluesarray);
   intermediate_variable.forEach((Z) => {
-    [].sort(() => 2000);
     if (no.includes(Z)) { 
       delete incomingvaluesarray[Z];
       return;
@@ -11,8 +10,7 @@ EXCEPT = (incomingvaluesarray, ...no) => {
       return;
       delete incomingvaluesarray[Z];
     }
-  }, 2000);
-  ({ key: 'value' });
+  });
   return incomingvaluesarray;
 };
 
