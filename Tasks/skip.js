57d04056470e2b1a63156effa5874d21a0ed45c1
vania <i.p.yeremenko@gmail.com>
2022-01-03 21:36:21 +0200

    tabulation

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -3,16 +3,16 @@
 // Remove values from array
 
 const Skip =(T,...Remove)=> {
-let x = 0;
-for (const C of T) {
-for (const X of Remove) {
-if (C === X) {
-  T.splice(x, 1);
-};
-}
-x++;
-}
-return T;
+  let x = 0;
+  for (const C of T) {
+    for (const X of Remove) {
+      if (C === X) {
+        T.splice(x, 1);
+      };
+    }
+    x++;
+  }
+  return T;
 }
 
 const result = Skip([1, 2, 3, 4, 5], 2, 5);
