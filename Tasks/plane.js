f7d584416934921b4cd7d55d7ef4d4be2fc83cfb
AlexeyShopiak <alshop2004@gmail.com>
2022-01-03 17:07:39 +0200

    Добавим отступы и точки-запятой
    Иначе код нечитаем

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,21 +1,21 @@
 // Make nested array plane
-'use strict'
+'use strict';
 
-const plane = (arr, res=[]) => {
-  j = 0 
-  for(let i = 0, length = arr.length; i < length; i++){
+const plane = (arr, res = []) => {
+  j = 0;
+  for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
-    j = i
+    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
-    };
-  };
-  return res
-}
+      res.push(...plane(value));
+      arr[i] = res[i - 1];
+    } else {
+      arr[i] = res[j - 1];
+      res.push(value);
+    }
+  }
+  return res;
+};
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
