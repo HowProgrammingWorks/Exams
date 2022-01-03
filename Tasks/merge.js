3b4f4f4abccdb097cef13baf69ccacaf9e87061f
konik56 <konik5629@gmail.com>
2022-01-03 13:52:27 +0200

    remove useless lines of code

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -3,14 +3,10 @@
 merge_two_objects=function merge_two_objects(object_1,object_2){
     object_3 = {};
    for (attribute_name in object_1) {
-       (
-          object_3[attribute_name] == object_1[attribute_name]
-       );
-        object_3[attribute_name] = object_1[attribute_name]
+        object_3[attribute_name] = object_1[attribute_name];
   };
    for (attribute_name in object_2) {
-      (object_3[attribute_name] == object_2[attribute_name])
-     object_3[attribute_name] = object_2[attribute_name]
+     object_3[attribute_name] = object_2[attribute_name];
     };
 return object_1, object_2, object_3;
 };
