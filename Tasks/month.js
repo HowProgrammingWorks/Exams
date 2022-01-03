3e2beec7380f4025ca6be9502d9be17ac27f03c0
bLuDd1 <dtavluj@gmail.com>
2022-01-03 22:08:09 +0200

    Added new variable. 'months' moved to 10th line.

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,11 +1,12 @@
 // Get month number
 'use strict';
-const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 const parseMonth = (month) => {
   for (let i = 0; i < months.length; i++){
-  if (month.toLowerCase().startsWith(months[i]))
+      const monthsPrefixes = months[i].toLowerCase()
+  if (month.startsWith(monthsPrefixes))
    return i + 1;
    }
 };
+const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 const result = parseMonth('february')
 console.log(result)
