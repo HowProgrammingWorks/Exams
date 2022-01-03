9419893bf089c167d4ecec5d29f42a5783ab8062
yarikkot04 <yarikkotenkoim13@gmail.com>
2022-01-03 22:52:21 +0200

    enter the arrays 'days' and 'toLowerCaseDay' to the function.

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,10 @@
 // Get day number
-
 'use strict';
 
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const toLowerCaseDay = new Array();
 // eslint-disable-next-line arrow-parens
 const parseDay = (searchDay) => {
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const toLowerCaseDay = new Array();
   for (let i = 0; i < days.length; i++) {
     toLowerCaseDay[i] = days[i].toLowerCase();
     if (searchDay.startsWith(toLowerCaseDay[i])) return i + 1;
