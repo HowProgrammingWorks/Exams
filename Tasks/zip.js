82fd33c6aa953321ef6d466b2662052090728e4c
MytsV <mytsvictoria@gmail.com>
2022-01-03 08:32:14 +0200

    Generalised function for multiple arguments

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,10 +1,12 @@
 'use strict';
 // Find first word with two identical letters
 
-const zip = (arrFirst, arrSecond) => {
-  const sliceLength = Math.min(arrFirst.length, arrSecond.length);
-  const slicedBase = arrFirst.slice(0, sliceLength);
-  return slicedBase.map((value, idx) => [value, arrSecond[idx]]);
+const zip = (...arrays) => {
+  const reduceToMinLength = (prev, curr) => Math.min(prev, curr.length);
+
+  const sliceLength = arrays.reduce(reduceToMinLength, arrays[0].length);
+  const indices = [...Array(sliceLength).keys()];
+  return indices.map(idx => arrays.map(array => array[idx]));
 };
 
 const result = zip(['Roma', 'Kiev', 'Beijing'], [100, 200, 300]);
