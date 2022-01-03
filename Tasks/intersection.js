c2d6461a23eaf0abf104ba2a866701bbcf78953c
Nikita Sutulov <notsid212@gmail.com>
2022-01-03 16:32:08 +0200

    Fixed comments styles.

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// Finde an intersection of two dictionaries
+// Function intersection
+// Returns a new object with all the keys and values which are in both object1 and object2
 const intersection = (object1, object2) => {
   const OBJECT1_KEYS = Object.keys(object1);
   const intersected = {};
