92a9f7f069a7dd22eae291f69f697088598e7c8c
MytsV <mytsvictoria@gmail.com>
2022-01-03 08:29:47 +0200

    Removed redundant default values. Rewrote the function with lambda syntax

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,7 +1,7 @@
 'use strict';
 // Find first word with two identical letters
 
-const zip = function(arrFirst = [], arrSecond = []) {
+const zip = (arrFirst, arrSecond) => {
   const sliceLength = Math.min(arrFirst.length, arrSecond.length);
   const slicedBase = arrFirst.slice(0, sliceLength);
   return slicedBase.map((value, idx) => [value, arrSecond[idx]]);
