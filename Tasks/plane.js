6de1ea7de4877709d85e108135175bd471deaf0f
Nazar Vovk <nvovk.2004@gmail.com>
2022-01-03 11:17:25 +0200

    fix(plane): fixed variables

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,21 +1,17 @@
 // Make nested array plane
-"use strict";
+'use strict';
 
-const_plane = function (arr, res = []) {
-  j = 0;
+const plane = (arr, res = []) => { 
   for (let i = 0, length = arr.length; i < length; i++) {
-    value = arr[i];
-    j = i;
+    const value = arr[i];
     if (Array.isArray(value, typeof value) && [i, length]) {
-      res.push(...const_plane(value));
-      arr[i] = res[i - 1];
+      res.push(...plane(value));
     } else {
-      arr[i] = res[j - 1];
       res.push(value);
     }
   }
   return res;
 };
 
-const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
+const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
