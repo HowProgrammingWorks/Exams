005284406878c4bbbd81e90002e40b10c64f5876
Trototoo <sa44ko@gmail.com>
2022-01-04 00:24:34 +0200

    Changed tabs and added ;

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,14 +1,14 @@
 // Finde a difference of two dictionaries
 
-'use strict';
+"use strict";
 
 const diff = (object_1, object_2) => {
-    for (const key in object_2) {
+  for (const key in object_2) {
     object_1[key] = object_2[key];
-    delete object_1[key]
-    }
-    return object_1
-}
+    delete object_1[key];
+  }
+  return object_1;
+};
 
-const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
+const result = diff({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
 console.log(result);
