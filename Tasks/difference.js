ee9ca782f3fb6b33a7b27f1835dd436e29b6cd29
vita133 <vitandrukhiv@gmail.com>
2022-01-03 14:36:33 +0200

    renamed variables

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,12 +1,12 @@
 // Finde a difference of two dictionaries
 
 "use strict";
-const difference = (object_1, object_2) => {
-    for (const attribute_name in object_2) {
-      object_1[attribute_name] = object_2[attribute_name];
-      delete object_1[attribute_name];
+const difference = (obj1, obj2) => {
+    for (const key in obj2) {
+      obj1[key] = obj2[key];
+      delete obj1[key];
     }
-    return object_1;
+    return obj1;
   };
 const result = difference({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
 console.log(result);
