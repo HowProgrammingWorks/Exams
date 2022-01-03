a128410d44ca69996b63494c54b54a6fa685bb1e
AlexeyShopiak <alshop2004@gmail.com>
2022-01-03 17:21:48 +0200

    Уберем ненужный код
    Эта часть кода вносит изменения во входящий масив, что не является нужным для нас,
    поскольку наша функция создает новый массив, без вмешательств в старый.

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -3,15 +3,11 @@
 
 const plane = arr => {
   const res = [];
-  let j = 0;
   for (let i = 0, length = arr.length; i < length; i++) {
     const value = arr[i];
-    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
       res.push(...plane(value));
-      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1];
       res.push(value);
     }
   }
