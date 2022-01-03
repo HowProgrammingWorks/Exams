e56d4d779a6e109100c2747cfd7417b6abdb8586
neroheroes6@gmail.com <neroheroes6@gmail.com>
2022-01-03 18:35:29 +0200

    Removed unnecessary parenthesis, added spaces on the 5th line

--- Tasks/types.js
+++ Tasks/types.js
@@ -2,15 +2,12 @@
 'use strict'
 
 const typeCounter = s => {
-  let typesList=(
-  {
+  let typesList = {
   "number": 0,
   "string": 0,
   "boolean": 0,
   }
-  )
-  for (i of s)
-  {
+  for (i of s) {
   const t = typeof i
   typesList[t]++
   };
