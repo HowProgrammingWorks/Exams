ec8cbe6eaaae896508f7663a645e73ee46f1bbb9
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-03 19:25:25 +0200

    The function is bound to the standard, since lambda is not an optimal option in this case. Also, the function name is rewritten with a lowercase letter

--- Kotlin/src/replace.kt
+++ Kotlin/src/replace.kt
@@ -1,11 +1,11 @@
- var Replace = lambda@{ str: String, substr: String, newstr: String ->
+ fun replace( str: String, substr: String, newstr: String){
      if(substr == "") str
       else{
          var src = str
          var res = ""
          do{
              val _index = src.indexOf(substr)
-             if(_index == -1) {return@lambda(res+src)} else{
+             if(_index == -1) {return (res+src)} else{
                  val start = src.substring(0,_index)
                  src = src.substring(_index+substr.length,src.length)
                  res += start + newstr
