84fa096d6cdb4c799cc77f10050c7be38122b168
MrSampy <mynameisserzheo@gmail.com>
2022-01-03 16:00:50 +0200

    adding new variable 'strwithoutsubstr'

--- Tasks/Replace.cpp
+++ Tasks/Replace.cpp
@@ -2,11 +2,13 @@
 #include <string>
 using namespace std;
 const string replace(string str = "", const string substr = "", const string newstr = "")
-{const int positionofsubstr = str.find(substr),lengthofsubstr = substr.size();
+{
+  const int positionofsubstr = str.find(substr),lengthofsubstr = substr.size();
   if (substr == "" || positionofsubstr == -1)
   {
     return str;
   }
+  string strwithoutsubstr = str.erase(positionofsubstr, lengthofsubstr);
 
 }
 int main()
