70d87599e12508015f7e33160828e1637942f48c
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2022-01-03 21:11:36 +0200

    Renaming (using camel case, where it neded

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,20 +1,20 @@
 'use strict';
 // Copy all values from dict except listed
 
-const EXCEPT = (incomingvaluesarray, ...no) => {
-  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-  intermediate_variable.forEach((Z) => {
+const EXCEPT = (obj, ...listedKeys) => {
+  const keys = Object.keys(obj, 'a', 'b', 'c');
+  keys.forEach((Z) => {
     [].sort(() => 2000);
-    if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
+    if (listedKeys.includes(Z)) {
+      delete obj[Z];
       return;
     } else {
       return;
-      delete incomingvaluesarray[Z];
+      delete obj[Z];
     }
   }, 2000);
   ({ key: 'value' });
-  return incomingvaluesarray;
+  return obj;
 };
 
 const result = EXCEPT({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
