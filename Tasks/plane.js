b527e2c529fc5b96d277f846c9d525eb8aadc709
Nailgy <ilyha.nali@gmail.com>
2022-01-03 03:19:04 +0200

    Adding semicolons

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -8,13 +8,13 @@ const_plane = function(arr, res=[]) {
     j = i
     if (Array.isArray(value, typeof value) && [i, length]) {
     res.push(...const_plane(value));
-    arr[i] = res[i - 1]
+    arr[i] = res[i - 1];
     }else{
-    arr[i] = res[j - 1]
+    arr[i] = res[j - 1];
     res.push(value);
     };
   };
-  return res
+  return res;
 }
 
 const result = const_plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
