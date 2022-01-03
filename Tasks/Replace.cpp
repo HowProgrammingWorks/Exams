3d91e529d687b0ba5f9b4bf046b4c82be13acd41
MrSampy <mynameisserzheo@gmail.com>
2022-01-03 15:55:27 +0200

    removing loop and variable 'res'

--- Tasks/Replace.cpp
+++ Tasks/Replace.cpp
@@ -3,21 +3,11 @@
 using namespace std;
 const string replace(string str = "", const string substr = "", const string newstr = "")
 {const int positionofsubstr = str.find(substr);
-  if (substr == "")
+  if (substr == "" )
   {
     return str;
   }
-  string res = "";
-  do
-  {
-    if (positionofsubstr == -1)
-    {
-      return res + str;
-    }
-    const string start = str.substr(0, positionofsubstr);
-    str = str.substr(positionofsubstr + substr.size(), str.size());
-    res += start + newstr;
-  } while (true);
+
 }
 int main()
 {
