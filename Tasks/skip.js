bc3a4843ec2b65d7c58cf06d2e989dfc2d4cb41e
yarikkot04 <yarikkotenkoim13@gmail.com>
2022-01-04 00:07:10 +0200

    Fixed anti-pattern of short variables T => array, X => element

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,15 +1,15 @@
 // Remove values from array
 //'use strict';
 
-const skipElements = (T, ...Remove) => {
-  for (let i = 0; i < T.length; i++) {
-    for (X of Remove) {
-      if (T[i] === X) {
-        T.splice(i, 1);
+const skipElements = (array, ...Remove) => {
+  for (let i = 0; i < array.length; i++) {
+    for (element of Remove) {
+      if (array[i] === element) {
+        array.splice(i, 1);
       }
     }
   }
-  return T;
+  return array;
 };
 
 const result = skipElements([1, 2, 3, 4, 5], 2, 5);
