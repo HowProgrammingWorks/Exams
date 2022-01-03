6ea39056ddc91ac9db331989a2f24de6fc2a50e5
Vadim Ulanovskiy <90156655+VeSpy4u@users.noreply.github.com>
2022-01-03 23:36:44 +0200

    Implement with for loops

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,6 +1,13 @@
 'use strict'
 // Merge two dictionaries
 
-const merge = (objOne, objTwo) => Object.assign({}, objOne, objTwo);
+const merge = (objOne, objTwo) => {
+    let objThree = {};
+    for (const key in objOne)
+        objThree[key] = objOne[key];
+    for (const key in objTwo)
+        objThree[key] = objTwo[key];
+    return objThree;
+};
 
 console.log(merge({ a: 'uno', b: 'due' }, { c: 'tre' }));
