dd948a192cc61d08aee2b2272e388059fcc0729f
olegoofy <martynenkooleg77@gmail.com>
2022-01-03 13:15:29 +0200

    Deleted unnecessary empty code blocks

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,15 +1,15 @@
 // Finde an intersection of two dictionaries
 
 intersection=function intersection(object_1,object_2)  {
-first_keys = Object.keys(object_1);                    {
-} for (attribute_name of first_keys)                   {
+first_keys = Object.keys(object_1);
+for (attribute_name of first_keys)                   {
 if (attribute_name !== '' && object_2[attribute_name]) {
-object_2[attribute_name] = object_1[attribute_name];   {
-}} else                                                {
-delete object_1[attribute_name];                       {
-                                                       }
-                  }{}
-                  }{}
+object_2[attribute_name] = object_1[attribute_name];
+} else                                                {
+delete object_1[attribute_name];
+
+                  }
+                  }
 return object_1;
 }
 
