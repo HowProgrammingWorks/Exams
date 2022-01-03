7faa0dacd29910c5d6e7fceb849e0eaf9f7f26bf
Nailgy <ilyha.nali@gmail.com>
2022-01-03 03:15:55 +0200

    Fixing error - adding semicolon

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -2,7 +2,8 @@
 'use strict';
 
 const_plane = function(arr, res=[]) {
-  j = 0 for(let i = 0, length = arr.length; i < length; i++){
+  j = 0; 
+  for(let i = 0, length = arr.length; i < length; i++){
     value = arr[i];
     j = i
     if (Array.isArray(value, typeof value) && [i, length]) {
