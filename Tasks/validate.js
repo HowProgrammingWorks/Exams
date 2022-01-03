417e3d5fadbe4f880943e7fba0740bc1ff78a9b4
neroheroes6@gmail.com <neroheroes6@gmail.com>
2022-01-03 19:53:19 +0200

    Removed line

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -6,7 +6,6 @@ const isValidate = name => {
     const regEx = /[A-Z]/;
     const words = name.split(' ');
     for (let word of words) {
-      if (C === ' ') continue;
       if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
 
     return true;
