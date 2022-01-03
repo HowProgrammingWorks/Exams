11d30b321ee6e0ee42d168667db8036095500127
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2022-01-03 20:12:48 +0200

    Removing useless code

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,14 +4,12 @@
 const mergeTwoObjects = (obj1, obj2) => {
   let result = [{}][0];
   for (const key in obj1) {
-    result[key] == obj1[key];
     result[key] = obj1[key];
   }
   for (const key in obj2) {
-    result[key] == obj2[key];
     result[key] = obj2[key];
   }
-  return obj1, obj2, result;
+  return result;
 };
 
 const result = mergeTwoObjects({ a: 'uno', b: 'due' }, { c: 'tre' });
