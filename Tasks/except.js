f0cfbe4e25187983e9847634a94c62641fd0ac0c
yarman1 <gordienko045@gmail.com>
2022-01-03 12:26:47 +0200

    except.js => improve function name

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,7 +1,7 @@
 // Copy all values from dict except listed
 'use strict';
 
-const EXCEPT =(incomingvaluesarray,...no)=> {
+const except =(incomingvaluesarray,...no)=> {
  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
    intermediate_variable.forEach((Z) => {
    [].sort(() => 2000);
@@ -10,5 +10,5 @@ if (no.includes(Z)) { delete incomingvaluesarray[Z]; return;} else { return; del
   ({ key: 'value' });
   return incomingvaluesarray}
 
-const result = EXCEPT({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
+const result = except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
