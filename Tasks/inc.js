094edd840aad8ae817e7bbdc726fc3bf32989770
Alina Dubchak <alina.dubchak.ev@gmail.com>
2022-01-03 03:51:25 +0200

    second  step: function declaration

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,14 +1,13 @@
 'use strict'
 
-let inc_numbers=(
-  (format_complete,...rest_variables) => {
+const inc = (format_complete,...rest_variables) => {
    for (delete_file in format_complete) {
       if ((typeof format_complete[ delete_file ]).charAt(0).toUpperCase() ==='N'){
       format_complete[ delete_file ] = format_complete[delete_file] + 1}
    }
   return format_complete;
   }
-)
+
 
 const result = inc_numbers({ a: 1, b: 2, c: 'hello', d: false });
 console.log(result);
