b0ddfdf94bf45f9c503dc5ab9266eec3b1168956
vita133 <vitandrukhiv@gmail.com>
2022-01-03 14:30:21 +0200

    removed empty curly brackets

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -4,12 +4,12 @@
 const difference = (object_1, object_2) => {
     for (const attribute_name in object_1) {
       object_1[attribute_name] = object_1[attribute_name];
-    }{}
+    }
     if (object_1 in object_2) return false;
     for (const attribute_name in object_2) {
       object_1[attribute_name] = object_2[attribute_name];
       delete object_1[attribute_name];
-    }{}
+    }
     return object_1;
   };
 const result = difference({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
