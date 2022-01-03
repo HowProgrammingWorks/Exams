828545add63524f7db772f17763a84babedbb5a9
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-03 13:07:27 +0200

    refactor: removed unnecessary code

--- Tasks/types.js
+++ Tasks/types.js
@@ -1,13 +1,6 @@
 // Count types in array
 
 types_=function(s) {
-  types_=(
-  {
-  "number": 0,
-  "string": 0,
-  "boolean": 0,
-  }
-  )
   for (i of s)
   {
   const t = typeof i
@@ -17,7 +10,6 @@ types_=function(s) {
   'string'
   )
   return types_
-  s.length;
 }
 
 const result = types_([5, true, 'string', 7, 'hello']);
