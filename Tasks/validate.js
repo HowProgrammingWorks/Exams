abdf807e225d6bc063ecd02b47c7bcfbc9c1f615
neroheroes6@gmail.com <neroheroes6@gmail.com>
2022-01-03 19:53:00 +0200

    Renamed iterable element in for of

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -5,7 +5,7 @@ const isValidate = name => {
   if (typeof name === 'string') {
     const regEx = /[A-Z]/;
     const words = name.split(' ');
-    for (C of words) {
+    for (let word of words) {
       if (C === ' ') continue;
       if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
 
