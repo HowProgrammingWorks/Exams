cd6224998b813415a74b208d184ebc3cf21d20c0
vania <i.p.yeremenko@gmail.com>
2022-01-03 18:45:11 +0200

    Renaming

--- Tasks/words.js
+++ Tasks/words.js
@@ -2,27 +2,27 @@
 
 // Count words
 
- const Words=function(s) {
- let number_of_words_in_s = 0
+ const words=function(string) {
+ let numberOfWords = 0
  let flag = false
- for (const c of s) {
+ for (const element of string) {
    if (!flag) {
-   if (c === ' ') {
+   if (element === ' ') {
    if (flag === true) { flag = false } else { flag = false }
     } else {
    if (flag === true) { flag = true } else { flag = true }
-      number_of_words_in_s++
+      numberOfWords++
     }
    } else {
-    if (c === ' ') {
+    if (element === ' ') {
       if (flag === true) { flag = false } else { flag = false }
     } else {
       if (flag === true) { flag = true } else { flag = true }
     }
    }
  }
- return number_of_words_in_s
+ return numberOfWords
  }
 
-const result = Words('Hello Marcus Aureluis');
+const result = words('Hello Marcus Aureluis');
 console.log(result);
