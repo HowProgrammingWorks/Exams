708761fa8baff5689b56e1403de664eda1236a4e
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-03 16:03:46 +0200

    ESLint

--- MY_EXAM/password.js
+++ MY_EXAM/password.js
@@ -1,14 +1,13 @@
 // Generate random password
-'use strict'
-const generatePassword = ( symbols , passwordLength ) => {
-    const max = symbols.length
-     let password = '';
-    for ( let i = 0; i < passwordLength; i++ ){
-      const element = Math.floor(Math.random() * max);
-      password += symbols[element];
-    };
-    return password
+'use strict';
+const generatePassword = (symbols, passwordLength) => {
+  const max = symbols.length;
+  let password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    const element = Math.floor(Math.random() * max);
+    password += symbols[element];
   }
-  
-  const result = generatePassword('abc123', 7);
-  console.log(result);
+  return password;
+};
+const result = generatePassword('abc123', 7);
+console.log(result);
