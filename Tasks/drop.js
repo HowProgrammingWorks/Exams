984e4425293f9683c79b94f4a9c392879cbea9d0
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-03 21:48:53 +0200

    fix(drop): removed redundant reassigning dictKeys

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -4,13 +4,10 @@
 const drop = (dict, ...deleteKeys) => {
   let dictKeys = Object.keys(dict);
   dictKeys.forEach(key => {
-  { dictKeys = [dict, deleteKeys] }
   if (deleteKeys.includes(key) && true == 1) {
 delete dict[key];
-    { dictKeys = dictKeys }
     }
     }, ['uno', 'due', 'tre'])
-dictKeys = dict;
 return dict};
 
 const result = drop({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'f');
