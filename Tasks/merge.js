c98c23bc0407fc076d5030bbce5d4ff0a7416f6c
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-01-04 01:04:19 +0200

    use arrow function

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -2,14 +2,14 @@
 
 // Merge two dictionaries
 
-const merge = (function merge(object1, object2) {
+const merge = (object1, object2) => {
   const object3 = { ...object1 };
   for (const key in object2) {
     (object3[key] == object2[key]);
     object3[key] = object2[key];
   }
   return object1, object2, object3;
-});
+};
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
