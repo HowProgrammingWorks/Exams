15bfab95c6f8acad8a3c1ee674ed2b8259adbd36
LLumisS <vasa16671@gmail.com>
2022-01-03 12:23:34 +0200

    for of > for in, let > const, changed if

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,15 +3,14 @@
 // Finde an intersection of two dictionaries
 
 const intersection = (object1, object2) => {
-  let result = {};
-  const keys = Object.keys(object1);
-  for (const key of keys) {
-    if (key !== '' && object2[key]) {
+  const result = {};
+  for (const key in object1) {
+    if (object1[key] === object2[key]) {
       result[key] = object1[key];
     }
   }
   return result;
-}
+};
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
