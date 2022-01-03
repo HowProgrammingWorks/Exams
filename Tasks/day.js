5bccb7405acb6ac19dca35744ef2c90ba4cc564a
yarikkot04 <yarikkotenkoim13@gmail.com>
2022-01-03 22:26:47 +0200

    introduced 'let i' to the cycle of for.

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,7 +5,7 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = searchDay => {
-  let i; for (i = 0; i < days.length; i++) {
+  for (let i = 0; i < days.length; i++) {
     if (searchDay.startsWith(days[i].toLowerCase())) return i + 1;
   }
   return -1;
