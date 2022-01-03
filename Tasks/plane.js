ccdc9c3ec8c934e247b9959f429b74b6ba176611
MytsV <mytsvictoria@gmail.com>
2022-01-03 07:58:29 +0200

    Fixed implicit declarations and naming conventions

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,12 +1,12 @@
 // Make nested array plane
 
-const_plane = function(arr, res = []) {
-  j = 0
+const plane = function(arr, res = []) {
+  let j = 0
   for (let i = 0, length = arr.length; i < length; i++) {
-    value = arr[i];
+    const value = arr[i];
     j = i
     if (Array.isArray(value, typeof value) && [i, length]) {
-      res.push(...const_plane(value));
+      res.push(...plane(value));
       arr[i] = res[i - 1]
     } else {
       arr[i] = res[j - 1]
