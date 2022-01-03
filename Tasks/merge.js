fc92e2c4a53019234fc80c21d0a5678887a0620d
Maksym <chyrkovmaksym1@gmail.com>
2022-01-03 14:09:00 +0200

    [merge.js] used arrow function

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,8 +1,6 @@
 // Merge two dictionaries
 'use strict';
-function mergeTwoDictionaries(first, second) {
-  return { ...first, ...second };
-}
+const mergeTwoDictionaries = (first, second) => ({ ...first, ...second });
 
 const result = mergeTwoDictionaries({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
