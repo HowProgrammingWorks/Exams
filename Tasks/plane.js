5f08ac95223b361a91ad6f2608d880e2f467c661
GAlexandrD <o.horbatko.im11@kpi.ua>
2022-01-03 10:46:14 +0200

    encapsulate result in a more secure way, using a closure

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const plane = function(arr, result=[]) {
-  for(let item of arr){
-    if (Array.isArray(item)) result.push(...plane(item));
-    else result.push(item);
+const plane = function(arr) {
+  const result = [];
+
+  const closedPlane = arr =>{
+    for(let item of arr){
+      if (Array.isArray(item)) closedPlane(item);
+      else result.push(item);
+    }
+    return result
   }
-  return result
+
+  return closedPlane(arr)
 }
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
