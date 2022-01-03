fdc0a0daa79b595d5f13f2ac6a770bb88f8dd82b
MytsV <mytsvictoria@gmail.com>
2022-01-03 08:04:43 +0200

    Replaced for loop with flatMap call including ternary operator

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -2,15 +2,7 @@
 // Make nested array plane
 
 const plane = function(arr) {
-  const res = [];
-  for (const value of arr) {
-    if (Array.isArray(value)) {
-      res.push(...plane(value));
-    } else {
-      res.push(value);
-    }
-  }
-  return res;
+  return arr.flatMap(value => (Array.isArray(value) ? plane(value) : value));
 };
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
