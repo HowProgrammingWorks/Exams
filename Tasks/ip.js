306188dfdd45dc37eb9693575dd670c23290453b
Alina Dubchak <alina.dubchak.ev@gmail.com>
2022-01-03 04:15:30 +0200

    sixth step: estetick optimization

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -3,9 +3,10 @@
 const parseIp = ip => {
   const value = [];
   const splitArr = ip.split('.');
-  for ( let i=0; i<splitArr.length; i++) {
+  for ( let i=0; i<splitArr.length; i++)
+   {
     value[i] = parseInt(splitArr[i]);
-    };
+   };
   return value;
 };
 
