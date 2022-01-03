ae1ace591f97a866c4e3d8c2f6decb75742e30c5
Alina Dubchak <alina.dubchak.ev@gmail.com>
2022-01-03 03:53:43 +0200

    third  step: parameter function

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const inc = (format_complete,...rest_variables) => {
+const inc = elements => {
    for (delete_file in format_complete) {
       if ((typeof format_complete[ delete_file ]).charAt(0).toUpperCase() ==='N'){
       format_complete[ delete_file ] = format_complete[delete_file] + 1}
