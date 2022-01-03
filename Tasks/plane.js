677a779f7ae812a0599170e2d0429d4bdc236451
TsNikolay <90026719+TsNikolay@users.noreply.github.com>
2022-01-03 17:01:57 +0200

    (plane.js) Arrow function, for instead of for..in, eslint, make the variable res not a parameter

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,20 +1,21 @@
-'use strict'
+"use strict"
 
 // Make nested array plane
 
-function planeArray (array , res = []) {
-  
-  for (let index in array) {
+const planeArray = (array) => {
+const res = [];
+
+  for (let index = 0; index < array.length; index++) {
     const value = array[index];
-    
+
     if (Array.isArray(value)) {
-    res.push(...planeArray(value));
-     } else {
-    res.push(value);
+      res.push(...planeArray(value));
+    } else {
+      res.push(value);
     }
   }
   return res;
-}
+};
 
 const result = planeArray([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
