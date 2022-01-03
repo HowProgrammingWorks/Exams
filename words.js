2c00503b5ae98f424a794b8a08b50ebab4fe7390
gonnagetbetter <denissss906@gmail.com>
2022-01-03 18:18:37 +0200

    Impoved code readability

--- words.js
+++ words.js
@@ -3,19 +3,15 @@
 // Count words
 
 const Words = function(str) {
-  let number_of_words_in_s = 0;
-  let flag = false;
-  for ( let char of str) {
-    if (!flag) {
-      if (char !== ' ') { 
-        flag = true;
-        number_of_words_in_s++
-      }
-    } else {
-      if (char === ' ') flag = false;
-    }
+  let wordsInString = 0;
+  let charAfterSpace = true;
+  for (const char of str) {
+    if (charAfterSpace === true && char !== ' ') { 
+        charAfterSpace = false;
+        wordsInString++
+      } else if (char === ' ') charAfterSpace = true;
   }
-return number_of_words_in_s
+return wordsInString
 }
 
 const result = Words('Hello Marcus Aureluis');
