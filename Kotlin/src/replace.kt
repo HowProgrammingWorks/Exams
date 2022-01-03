2858efaf6e4e353963c6dc3cc038d6f5cf964c17
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-03 22:59:13 +0200

    1) Added padding 2) Changed the return expression to an optimized result variable 3) Added indexSubString variable for better code work and understanding 4) Removed unnecessary semicolons

--- Kotlin/src/replace.kt
+++ Kotlin/src/replace.kt
@@ -1,15 +1,23 @@
 var replace = lambda@{ string: String, subString: String, newString: String ->
-    if(subString == "") string
+    if(subString == "")  string
     else{
         var stringElse = string
         var resultElse = ""
         do{
             val index = stringElse.indexOf(subString)
-            if(index == -1) {return@lambda(resultElse+stringElse)} else{
+            if(index == -1){
+                resultElse += stringElse
+                return@lambda resultElse
+            }
+            else{
                 val subStringElse = stringElse.substring(0,index)
-                stringElse = stringElse.substring(index+subString.length,stringElse.length)
+                val indexSubString = index + subString.length
+                stringElse = stringElse.substring(indexSubString,stringElse.length)
                 resultElse += subStringElse + newString
-            }}while (true); } ;}
+            }
+        }while (true)
+    }
+}
 fun main(){
     val result = replace("Hello <username> and bye!", "<username>", "Marcus")
     println(result)
