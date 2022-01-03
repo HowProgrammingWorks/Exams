2e1cc606bab6741a7b98b55b5668e35c1766a281
olegoofy <martynenkooleg77@gmail.com>
2022-01-03 14:17:50 +0200

    Added keywords (const)

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,7 +1,7 @@
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray,...no) => {
-    intermediate_variable = Object.keys(incomingvaluesarray);
+const EXCEPT = (incomingvaluesarray,...no) => {
+    const intermediate_variable = Object.keys(incomingvaluesarray);
     intermediate_variable.forEach((Z) => {
         if (no.includes(Z)) {
             delete incomingvaluesarray[Z];
