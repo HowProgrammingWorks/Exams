63250bf314d26eb5092ed872bea6894a9dfa1b57
GddgdgMen <antonsvirskiy690@gmail.com>
2022-01-03 17:55:30 +0200

    Fixed errors, deleted excess code, renamed certain parts

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,14 +1,11 @@
-'use strict'
-// Finde a difference of two dictionaries
+"use strict";
+// Find a difference of two dictionaries
 
-const diff = (object1,object2) => {
-    if (object1 in object2) return false
-    for (const name in object2) {
-    object_1[name] = object2[name]
-    delete object1[name]
-    }
-    return object1
+const diff = (object1, object2) => {
+  for (const key in object2) {
+    delete object1[key];
+  }
+  return object1;
 }
-
 const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
