75efa28f949b874e001133ae608436679d78724d
ziya <58106222+ziya32@users.noreply.github.com>
2022-01-03 02:33:12 +0200

    Make better code,Delete useless sections

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,18 +1,17 @@
 // Replace substring with newstr
 
- const replace = (string, subString,newString)=>{
-  if (subString    === ''){
-  return string;
-  } else{
+ const replace = (string, subString, newString) =>{
+  if (subString  === '') return string; 
   let copyString = string;
   let result = '';
-  do {
+  while(true){
    const index = copyString.indexOf(subString) ; 
-   if (index === -1) { return res + copyString }else{
+   if (index === -1)  return result + copyString; 
    const start = copyString.substring(0,index);
    copyString = copyString.substring(index + subString.length,copyString.length)
    result += start + newString;
- }} while (true); }; };
+  }  
+ };
 
 const result = replace('Hello <username> and bye!', '<username>', 'Marcus')
 console.log(result)
