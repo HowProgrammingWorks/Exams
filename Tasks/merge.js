cded2c0724bf5218110b8fed598fe861d6ec0533
Гайдей Мария, ИМ-12 <89260967+jinworldwildhandsome@users.noreply.github.com>
2022-01-04 00:41:24 +0200

    removed comparisons of elements of objects that don't affect the execution of the function, its result and logic

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,15 +1,11 @@
 // Merge two dictionaries
 'use strict';
 const merge =(object_1, object_2) => {
-    const result = {};
+    const result = {}; 
     for (attribute_name in object_1) {
-        (
-            object_3[attribute_name] == object_1[attribute_name]
-        );
         object_3[attribute_name] = object_1[attribute_name]
     }
     for (attribute_name in object_2) {
-        (object_3[attribute_name] == object_2[attribute_name])
         object_3[attribute_name] = object_2[attribute_name]
     }
     return object_1, object_2, object_3;
