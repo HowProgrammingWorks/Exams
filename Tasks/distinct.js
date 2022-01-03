3936a97d5892d339502c5491d728482388be4e1f
konik56 <konik5629@gmail.com>
2022-01-03 13:19:12 +0200

    chande way to get unique array using feature of Set collection

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,16 +1,9 @@
 // Deduplicate values in array
 
 eaveUniqueValues = function(array) {
-  arrayForIfElse=[]; startSpl=0;
-    array.forEach((elem) => {
-        if (arrayForIfElse.includes(elem)) {
-        array.splice(startSpl, 1);
-        } else {
-         arrayForIfElse.push(elem);
-        };
-        startSpl++;
-    });
-  return array;};
+  const uniqueSet = new Set(array);
+  const uniqueArray = Array.from(uniqueSet);
+  return uniqueArray;};
 
 const result = eaveUniqueValues([1, 2, 1, 3, 1, 4]);
 console.log(result);
