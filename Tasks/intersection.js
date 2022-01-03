fb5f7097fcad20960f179b6330b4446ffd36e122
Nailgy <ilyha.nali@gmail.com>
2022-01-03 02:55:45 +0200

    Using res = {} instead of changing arguments

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const intersection = (obj1, obj2) => {
+  const res = {};
   for (const key in obj1) {
     if (obj2[key]) {
       obj2[key] = obj1[key];   
@@ -9,7 +10,7 @@ const intersection = (obj1, obj2) => {
     delete obj1[key];                       
     }
   }
-  return obj1;
+  return res;
 };
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
