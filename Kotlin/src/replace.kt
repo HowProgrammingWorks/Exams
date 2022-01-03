9840abc69aaa8fb3fd9d79a17d27d2a44dac5f31
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-03 19:53:47 +0200

    modified signature: renamed function parameters to a more understandable language

--- Kotlin/src/replace.kt
+++ Kotlin/src/replace.kt
@@ -1,14 +1,14 @@
- fun replace( string: String, subString: String, newString: String){
-     if(substr == "") str
+ fun replace(string: String, subString: String, newString: String){
+     if(subString == "") string
       else{
-         var src = str
+         var src = string
          var res = ""
          do{
-             val _index = src.indexOf(substr)
+             val _index = src.indexOf(subString)
              if(_index == -1) {return (res+src)} else{
                  val start = src.substring(0,_index)
-                 src = src.substring(_index+substr.length,src.length)
-                 res += start + newstr
+                 src = src.substring(_index+subString.length,src.length)
+                 res += start + newString
              } }while (true); } ;};
  fun main(){
      val result = Replace("Hello <username> and bye!", "<username>", "Marcus")
