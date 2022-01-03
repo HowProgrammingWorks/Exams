0115ca2878c2984c10a7ff3dbfd84ef0a643e3b8
bLuDd1 <dtavluj@gmail.com>
2022-01-03 21:34:17 +0200

    Added paddings where it's necessary. 'i' assigned in for loop.

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,9 +2,11 @@
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-const parseDay= (day)=>{
-  let i; for(i=0; i<days.length; i++){
-  if(day.startsWith(days[i].toLowerCase())){return i +1};};return -1;
+const parseDay = (day) => {
+  for (let i = 0; i < days.length; i++) {
+  if (day.startsWith(days[i].toLowerCase()))
+    return i + 1;
+  }
 };
 
 const result = parseDay('friday')
