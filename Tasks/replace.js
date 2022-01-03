ae9dbef9977e8719e09e6c947b5dd44e400db793
ziya <58106222+ziya32@users.noreply.github.com>
2022-01-03 14:45:09 +0200

    Eslint

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,20 +1,20 @@
 // Replace substring with newstr
-"use strict";
- const replace = (string, subString, newString) =>{
-  if (subString  === '') return string; 
+'use strict';
+const replace = (string, subString, newString) => {
+  if (subString  ===  '') return string;
   let copyString = string;
   let result = '';
-  while(true){
-   const index = copyString.indexOf(subString) ; 
-   if (index === -1)  return result + copyString; 
-   const start = copyString.substring(0,index);
-   copyString = copyString.substring(index + subString.length,copyString.length)
-   result += start + newString;
-  }  
- };
+  while (true) {
+    const index = copyString.indexOf(subString);
+    if (index  ===  -1)  return result + copyString;
+    const start  =  copyString.substring(0,  index);
+    copyString  =  copyString.substring(index + subString.length,  copyString.length);
+    result += start + newString;
+  }
+};
 
-const result1 = replace('Hello <username> and bye!', '<username>', 'Marcus')
-const result2 = replace('<name> welcome!We are happy to see you <name>!','<name>','Marcus');
-const result3 = replace('<<name>>','<name>','Marcus');
+const result1 = replace('Hello <username> and bye!',  '<username>',  'Marcus');
+const result2 = replace('<name> welcome!We are happy to see you <name>!',  '<name>',  'Marcus');
+const result3 = replace('<<name>>',  '<name>',  'Marcus');
 
-console.dir({result1, result2,result3});
+console.dir({ result1,  result2,  result3 });
