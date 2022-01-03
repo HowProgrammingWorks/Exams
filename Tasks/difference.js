20b9b356de1cfbcd3114c09f9be6ec6a566db587
Mila <l.gorbunova.im12@kpi.ua>
2022-01-03 13:09:11 +0200

    fixed semicolons and tabs

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,16 +2,16 @@
 'use strict';
 
 const dictDifference = (obj1, obj2) => {
-    for (const key in obj1) {
-    obj1[key] = obj1[key]
-    }{}
-    if (obj1 in obj2) return false
-    for (const key in obj2) {
-    obj1[key] = obj2[key]
-    delete obj1[key]
-    }{}
-    return obj1
-}
+  for (const key in obj1) {
+    obj1[key] = obj1[key];
+  }{}
+  if (obj1 in obj2) return false;
+  for (const key in obj2) {
+    obj1[key] = obj2[key];
+    delete obj1[key];
+  }{}
+  return obj1;
+};
 
 const result = dictDifference({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
