7049f225cf7ef1b468c99475cb613b91f8d7c032
Maksym Kryvenok <maxim.kryvenok@gmail.com>
2022-01-03 14:09:01 +0200

    Непотрібний else #2

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,15 +5,13 @@ const GetValueBetween = (string, prefix, suffix) => {
   const prefixIndex = string.indexOf(prefix);
   if (prefixIndex === -1)
     return "";
-  else {
-    const k = prefixIndex + prefix.length;
-    let result = string.substring(k);
-    if (suffix) {
-      const suffixIndex = result.indexOf(suffix);
-      if (suffixIndex === -1)
-        return "";
-      result = result.substring(0, suffixIndex);
-    }
+  const k = prefixIndex + prefix.length;
+  let result = string.substring(k);
+  if (suffix) {
+    const suffixIndex = result.indexOf(suffix);
+    if (suffixIndex === -1)
+      return "";
+    result = result.substring(0, suffixIndex);
   }
   return result;
 };
