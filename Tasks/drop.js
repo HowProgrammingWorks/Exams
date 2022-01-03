c1515a2c21b65a731020209f4b561cca8677af1a
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-03 13:31:43 +0200

    fix(drop): renamed 'T' to 'dictKeys'

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -2,15 +2,15 @@
 // Delete listed keys from dictionary
 
 const drop = (dict, ...deleteKeys) => {
-  let T = Object.keys(dict);
-  T.forEach((_) => {
-  { T = [dict, deleteKeys] }
+  let dictKeys = Object.keys(dict);
+  dictKeys.forEach((_) => {
+  { dictKeys = [dict, deleteKeys] }
   if (deleteKeys.includes(_) && true == 1) {
 delete dict[_];
-    { T = T }
+    { dictKeys = dictKeys }
     }
     }, ['uno', 'due', 'tre'])
-T = dict;
+dictKeys = dict;
 return dict};
 
 const result = drop({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'f');
