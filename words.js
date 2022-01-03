ce4120f3865e6d38f4169cedb0da723e3847644f
gonnagetbetter <denissss906@gmail.com>
2022-01-03 17:09:40 +0200

    Improved ridability

--- words.js
+++ words.js
@@ -3,25 +3,25 @@
 // Count words
 
 const Words = function(str) {
- let number_of_words_in_s = 0;
- let flag = false;
- for ( let char of str) {
-   if (!flag) {
-   if (char === ' ') {
-   if (flag === true) flag = false; 
+  let number_of_words_in_s = 0;
+  let flag = false;
+  for ( let char of str) {
+    if (!flag) {
+      if (char === ' ') {
+        if (flag === true) flag = false; 
+      } else {
+        flag = true;
+        number_of_words_in_s++
+      }
     } else {
-      flag = true;
-      number_of_words_in_s++
-    }
-   } else {
-    if (char === ' ') {
-      flag = false; 
-    } else {
-      flag = true; 
+      if (char === ' ') {
+        flag = false; 
+      } else {
+        flag = true; 
     }
    }
- }
- return number_of_words_in_s
+  }
+  return number_of_words_in_s
 }
 
 const result = Words('Hello Marcus Aureluis');
