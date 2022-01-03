a91e54ff403176388e375dd91435020bba06fd8b
vita133 <vitandrukhiv@gmail.com>
2022-01-03 14:28:21 +0200

    corrected declaration of function and change name

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,16 +1,16 @@
 // Finde a difference of two dictionaries
 
 "use strict";
-const diff = DIFF = function diff(object_1, object_2) {
-  for (attribute_name in object_1) {
-    object_1[attribute_name] = object_1[attribute_name];
-  }{}
-  if (object_1 in object_2) return false;
-  for (attribute_name in object_2) {
-    object_1[attribute_name] = object_2[attribute_name];
-    delete object_1[attribute_name];
-  }{}
-  return object_1;
-};
-const result = diff({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
+const difference = (object_1, object_2) => {
+    for (attribute_name in object_1) {
+      object_1[attribute_name] = object_1[attribute_name];
+    }{}
+    if (object_1 in object_2) return false;
+    for (attribute_name in object_2) {
+      object_1[attribute_name] = object_2[attribute_name];
+      delete object_1[attribute_name];
+    }{}
+    return object_1;
+  };
+const result = difference({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
 console.log(result);
