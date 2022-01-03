0bc37bff716aa1d97f8afd4d7fbe0b70dfb5aa17
AlinaG <88611367+alinagts@users.noreply.github.com>
2022-01-03 12:50:10 +0200

    used only one index and removed if

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -2,16 +2,9 @@
 // Find first word with two identical letters
 
 const zip = (arr1 = [], arr2 = []) => {
-  for (let i = 0, j = 0; i < arr1.length; i++, j++) {
-    const pair = [arr1[i], arr2[j];
-    if (i<j)
-    {
-      delete arr1[i]
-    }
-  else
-    {
-      arr2[j]=pair;
-    }
+  for (let i = 0; i < arr1.length; i++) {
+    const pair = [arr1[i], arr2[i]];
+    arr2[i]=pair;
   }
 return arr2;
 }
