618ef10229028bb236bbbb3563c8f1497c164002
Михайло Креславський <kreslavskiy.m@icloud.com>
2022-01-03 17:32:48 +0200

    fixed naming

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -2,19 +2,19 @@
 
 'use strict';
 
-const DISTINCT = (data) => {
-  const A = new Set();
-  let w = 0;
-  data.forEach((a) => {
-    if (A.has(a)) {
-      data.splice(w, 1);
+const distinct = (array) => {
+  const data = new Set();
+  let i = 0;
+  array.forEach((a) => {
+    if (data.has(a)) {
+      array.splice(i, 1);
     } else {
-      A.add(a);
+      data.add(a);
     }
-    w++;
+    i++;
   });
-  return data;
+  return array;
 };
 
-const result = DISTINCT([1, 2, 1, 3, 1, 4]);
+const result = distinct([1, 2, 1, 3, 1, 4]);
 console.log(result);
