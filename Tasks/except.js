35ed1dcd324137ef594d3cf8f755c3ea1b30b717
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2022-01-03 21:08:12 +0200

    Adding const

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,8 +1,8 @@
 'use strict';
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+const EXCEPT = (incomingvaluesarray, ...no) => {
+  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
   intermediate_variable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
