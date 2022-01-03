201010ff7f4a022996731817e1e98c5206684efd
volkovily <volkov.ilyya@gmail.com>
2022-01-03 18:02:37 +0200

    Added missing semicolon and space

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,10 +1,11 @@
 // Shuffle an array
 
+
 const shuffle = (...List) => {
   const [arr] = List;
   arr.sort(() => Math.random() - 0.5);
   return arr;
-}
+};
 
 const result = shuffle(['a', 'b', 'c', 'd', 'e']);
 console.log(result);
