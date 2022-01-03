92bbb04284b2ade8f4312fb14252370f54099502
Nikita Sutulov <notsid212@gmail.com>
2022-01-03 15:07:13 +0200

    Fixed naming style for identifiers.

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,14 +1,14 @@
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray);
-  intermediate_variable.forEach((Z) => {
-    if (no.includes(Z)) { 
-      delete incomingvaluesarray[Z];
+except = (incomingValues, ...exceptions) => {
+  KEYS = Object.keys(incomingValues);
+  KEYS.forEach((key) => {
+    if (exceptions.includes(key)) { 
+      delete incomingValues[key];
     }
   });
-  return incomingvaluesarray;
+  return incomingValues;
 };
 
-const result = EXCEPT({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
+const result = except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
