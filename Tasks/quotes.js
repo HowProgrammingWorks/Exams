8de295131567b150543e837886db4e5833920aaf
volkovily <volkov.ilyya@gmail.com>
2022-01-03 17:53:51 +0200

    Identifiers that never reassigned now use const

--- Tasks/quotes.js
+++ Tasks/quotes.js
@@ -1,23 +1,22 @@
 // Change douple quotation to open or close quotation
 
-const quotes = (string) => {
-  let res = [];
+const quotes = string => {
+  const res = [];
   let open = false;
-  for (let character of string) {
+  for (const character of string) {
     if (character === '"') {
-        if (!open) {
-            res.push('«');
-            open = true;
-        } else {
-            res.push('»');
-            open = false;
-        }
+      if (!open) {
+        res.push('«');
+        open = true;
+      } else {
+        res.push('»');
+        open = false;
+      }
     } else {
-        res.push(character);
+      res.push(character);
     }
   }
   return res.join('');
-}
-
+};
 const result = quotes('Hello "Marcus"! Ave "Marcus"!');
 console.log(result);
