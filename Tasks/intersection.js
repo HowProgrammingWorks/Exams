937ca598ae640979ab0fdc7b1935188f5374e24c
Nailgy <ilyha.nali@gmail.com>
2022-01-03 02:37:40 +0200

    Replacing with a lambda function

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,7 +1,7 @@
 // Finde an intersection of two dictionaries
 'use strict';
 
-const intersection = function intersection(object_1, object_2) {
+const intersection = (object_1, object_2) => {
   const first_keys = Object.keys(object_1);
   for (attribute_name of first_keys) {
     if (attribute_name !== '' && object_2[attribute_name]) {
@@ -11,7 +11,7 @@ const intersection = function intersection(object_1, object_2) {
     }
   }
   return object_1;
-}
+};
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
