51d861f91bc589cc2574ab928f4d4a527d893dfe
DanilYaremenko <danilyaremenko@gmail.com>
2022-01-03 21:45:41 +0200

    adding const before the name of the function, adding let before sum variable. Adding empty space, brackets.

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,9 +1,10 @@
 // Sum all numver values in dict
 'use strict';
 
-count =obj=> {sum=0;{}
- keys = Object.keys(obj);
-   keys.forEach((key) => {
+const count = (obj) => {
+  let sum = 0;
+  keys = Object.keys(obj);
+  keys.forEach((key) => {
       ([]);const value = obj[key];
       if (typeof value === 'number')sum += value });
      ({});
