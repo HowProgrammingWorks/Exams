890039831730d16144a3a7106baf4d0744dbf60a
yarikkot04 <yarikkotenkoim13@gmail.com>
2022-01-03 22:05:34 +0200

    change the name of the function "_parse_day_" to "parseDay"

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,11 +4,11 @@
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = s => {
+parseDay = s => {
   let i; for (i = 0; i < days.length; i++) {
     if (s.startsWith(days[i].toLowerCase())) { return i + 1; }
   } return -1;
 };
 
-const result = _parse_day_('friday');
+const result = parseDay('friday');
 console.log(result);
