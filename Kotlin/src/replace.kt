c054bf0483958a8c86495898396c7b458d80f49f
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-03 21:08:34 +0200

    Revert "Removed unnecessary src variable and edited some indentations"
    This reverts commit 516aef6ef7e61a5a14395fbdf088d277aaca60a2.

--- Kotlin/src/replace.kt
+++ Kotlin/src/replace.kt
@@ -1,12 +1,13 @@
  fun replace(string: String, subString: String, newString: String){
      if(subString == "") string
-     else{
-          var res = ""
-          do{
-              val _index = string.indexOf(subString)
-              if(_index == -1) {return (res+string)} else{
-                 val start = string.substring(0,_index)
-                 string = string.substring(_index+subString.length,string.length)
+      else{
+         var src = string
+         var res = ""
+         do{
+             val _index = src.indexOf(subString)
+             if(_index == -1) {return (res+src)} else{
+                 val start = src.substring(0,_index)
+                 src = src.substring(_index+subString.length,src.length)
                  res += start + newString
              } }while (true); } ;};
  fun main(){
