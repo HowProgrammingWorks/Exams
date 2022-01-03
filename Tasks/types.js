256c57d0d6a672c02a7e35244e77355f20d4c5da
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-03 13:14:50 +0200

    style: formatted missing semi colons

--- Tasks/types.js
+++ Tasks/types.js
@@ -2,11 +2,11 @@
 
 const types_ = function(s) {
   for (const i of s) {
-    const t = typeof i
-    types_[t]++
-  };
-  s.push('string')
-  return types_
+    const t = typeof i;
+    types_[t]++;
+  }
+  s.push('string');
+  return types_;
 }
 
 const result = types_([5, true, 'string', 7, 'hello']);
