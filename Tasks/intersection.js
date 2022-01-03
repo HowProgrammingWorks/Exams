59841cd2893d0d4c40e1ad474af713d2f98adcd8
Nailgy <ilyha.nali@gmail.com>
2022-01-03 02:41:18 +0200

    Renaming variables, adding const to key

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,16 +1,16 @@
 // Finde an intersection of two dictionaries
 'use strict';
 
-const intersection = (object_1, object_2) => {
-  const first_keys = Object.keys(object_1);
-  for (attribute_name of first_keys) {
-    if (attribute_name !== '' && object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];   
+const intersection = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  for (const key of keys1) {
+    if (key !== '' && obj2[key]) {
+      obj2[key] = obj1[key];   
     } else {
-    delete object_1[attribute_name];                       
+    delete obj1[key];                       
     }
   }
-  return object_1;
+  return obj1;
 };
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
