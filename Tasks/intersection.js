bf488b3441e6933613763753b763cdf5cf1287eb
olegoofy <martynenkooleg77@gmail.com>
2022-01-03 13:36:27 +0200

    Function declaration changed to lambda

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,6 +1,6 @@
 // Finde an intersection of two dictionaries
 
-const intersection = function intersection (object1,object2) {
+const intersection = (object1,object2) => {
     const resObject = {};
     for (const key in object1) {
         if (object1[key] === object2[key]) {
