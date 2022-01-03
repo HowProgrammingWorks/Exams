2e3c49137378089107e1afba2962d8c8c0cfe96c
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-03 23:35:54 +0200

    fix(drop): fixed spaces and indents

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -4,11 +4,13 @@
 const drop = (dict, ...deleteKeys) => {
   const dictKeys = Object.keys(dict);
   dictKeys.forEach(key => {
-  if (deleteKeys.includes(key)) {
-delete dict[key];
+    if (deleteKeys.includes(key)) {
+      delete dict[key];
     }
-    })
-return dict};
+  })
+
+  return dict
+};
 
 const result = drop({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'f');
 console.log(result);
