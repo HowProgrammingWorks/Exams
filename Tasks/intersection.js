320e7e6e1ca346b10d8c09755a48238bf41710a4
olegoofy <martynenkooleg77@gmail.com>
2022-01-03 13:35:43 +0200

    Changed branching and added result object

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,15 +1,13 @@
 // Finde an intersection of two dictionaries
 
 const intersection = function intersection (object1,object2) {
+    const resObject = {};
     for (const key in object1) {
-        if (key !== '' && object2[key]) {
-            object2[key] = object1[key];
-        }
-        else {
-            delete object1[key];
+        if (object1[key] === object2[key]) {
+            resObject[key] = object1[key];
         }
     }
-return object1;
+return resObject;
 }
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
