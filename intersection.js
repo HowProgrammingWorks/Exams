e84fa9f6de16b88f047af5b3f28073115ec26b09
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-04 14:05:31 +0200

    |intersection.js(3)|   fixed spacing and  " {} "

--- intersection.js
+++ intersection.js
@@ -1,15 +1,13 @@
 // Finde an intersection of two dictionaries
 'use strict'
 const intersection =(object_1,object_2)=> {
-const first_keys = Object.keys(object_1);                    
- for (let attribute_name of first_keys)                   {
+  const first_keys = Object.keys(object_1);
+ for (let attribute_name of first_keys){
 if (attribute_name !== '' && object_2[attribute_name]) {
-object_2[attribute_name] = object_1[attribute_name];   {
-}} else                                                {
-delete object_1[attribute_name];                       {
-                                                       }
-                  }
-                  }
+object_2[attribute_name] = object_1[attribute_name];
+} else 
+delete object_1[attribute_name]; 
+}
 return object_1;
 }
 
