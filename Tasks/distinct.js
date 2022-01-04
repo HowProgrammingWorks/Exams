4a157a6353ca1f674091cc6670fe0abe9c593eaa
Dolaran <gyv220427@gmail.com>
2022-01-04 04:08:06 +0200

    Update distinct.js
    code formatting

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -2,17 +2,19 @@
 
 // Deduplicate values in array
 
-DISTINCT =data=> {
-    A=new Set(); w=0;
-    data.forEach((a) => {
-        if (A.has(a)) {
-        data.splice(w, 1)
-        } else {
-        A.add(a)
-        };
-        w++;
-    });
-  return data;}
+DISTINCT = (data) => {
+  A = new Set();
+  w = 0;
+  data.forEach((a) => {
+    if (A.has(a)) {
+      data.splice(w, 1);
+    } else {
+      A.add(a);
+    }
+    w++;
+  });
+  return data;
+};
 
 const result = DISTINCT([1, 2, 1, 3, 1, 4]);
 console.log(result);
