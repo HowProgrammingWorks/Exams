f41a05b11bf894bd5b04af407ae5bcce02df3a5e
Anyta8989 <96947496+Anyta8989@users.noreply.github.com>
2022-01-04 22:58:35 +0200

    renamed variables

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,15 +1,15 @@
 // Validate person name
 'use strict'
 
-const isValid = (T) => {
-if (!T) return false;
-if (T === '') return false;
-if (typeof T !== 'string') return false;
-if (T.length === 0) return false;
-if (!T.includes(' ')) return false; {
-  for (C of T) {
-    if (C === ' ') continue;
-    if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
+const isValid = (name) => {
+if (!name) return false;
+if (name === '') return false;
+if (typeof(name) !== 'string') return false;
+if (name.length === 0) return false;
+if (!name.includes(' ')) return false; {
+  for (char of name) {
+    if (char === ' ') continue;
+    if (char.toLowerCase().charCodeAt(0) >= 97 && char.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
   };
   return true;
 };
