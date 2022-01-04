f7ff48779a9b2d90c239411b07cc14ee909524a2
Maksym Kryvenok <maxim.kryvenok@gmail.com>
2022-01-04 13:36:33 +0200

    Прийняв до уваги зауваження
    P.S. замість посилання на 2 пул, там повинен бути звичайний хештег

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,16 +1,15 @@
 // Extract substring between prefix and suffix
 "use strict";
 
-const GetValueBetween = (string, prefix, suffix) => {
+const getValueBetween = (string, prefix, suffix) => {
   let result = "";
   const prefixIndex = string.indexOf(prefix);
   const suffixIndex = suffix ? string.indexOf(suffix) : string.length;
-  if (prefixIndex === -1 || suffixIndex === -1)
-    return result;
+  if (prefixIndex === -1 || suffixIndex === -1) return result;
   const excludedPrefixIndex = prefixIndex + prefix.length;
   result = string.substring(excludedPrefixIndex, suffixIndex);
   return result;
 };
 
-const valueBetween = GetValueBetween("Hello <username> and bye!", "<", ">");
+const valueBetween = getValueBetween("Hello <username> and bye!", "<", ">");
 console.log(valueBetween);
--- Tasks/zip.js
+++ Tasks/zip.js
@@ -2,13 +2,11 @@
 
 "use strict";
 
-const zip = (array1 = [], array2 = []) => {
+const zip = (array1, array2) => {
   const result = [];
-  const biggerLength = (array1.length > array2.length) ? array1.length : array2.length;
-  for (let i = 0; i < biggerLength; i++) {
-    const tempCell = [array1[i], array2[i]];
-    result[i] = tempCell;
-  }
+  const biggerLength =  Math.max(array1.length, array2.length);
+  for (let i = 0; i < biggerLength; i++)
+    result.push([array1[i], array2[i]]);
   return result;
 };
 
