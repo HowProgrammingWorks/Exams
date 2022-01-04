8146e9e46ba3c3a47fd523ffdf78e0caae7d9930
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-04 12:20:42 +0200

    |sum.js(8)|   verefied with ESLint

--- sum.js
+++ sum.js
@@ -1,15 +1,15 @@
 // Sum all numbers from array
-'use strict'
+'use strict';
 const sumNumbers = (array) => {
-  let calcResult = 0
-  for ( const value of array) {
-  const type = typeof value;
-  if (type === 'number'){
-    calcResult += value
+  let calcResult = 0;
+  for (const value of array) {
+    const type = typeof value;
+    if (type === 'number') {
+      calcResult += value;
     }
   }
-  return calcResult
-  };
+  return calcResult;
+};
 
 const result = sumNumbers([5, true, 'string', 7, 'hello']);
 console.log(result);
