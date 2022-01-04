1e3d544744a5395b367f53e70170c9b5840bfc06
Valentyn13 <valentyn13git@gmail.com>
2022-01-04 18:13:11 +0200

    Add empty line and use prettier

--- ExamTasks/types.js
+++ ExamTasks/types.js
@@ -1,17 +1,19 @@
-'use strict'
+"use strict";
+
 const resultObj = {
-  "number": 0,
-  "string": 0,
-  "boolean": 0,
-  };
-const countTypes = (arr)=> {
+  number: 0,
+  string: 0,
+  boolean: 0,
+};
+const countTypes = (arr) => {
   for (const key of arr) {
-  const types = typeof key
-  resultObj[types]++
-  };
+    const types = typeof key;
+    resultObj[types]++;
+  }
 
   return resultObj;
-}
+};
 
-const result = countTypes([5, true, 'string', 7, 'hello']);
+const result = countTypes([5, true, "string", 7, "hello"]);
 console.log(result);
+
