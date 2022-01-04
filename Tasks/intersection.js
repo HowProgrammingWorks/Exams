ec7816a7024261905f89c53ce8b702c95a2305c5
Anyta8989 <96947496+Anyta8989@users.noreply.github.com>
2022-01-05 00:25:49 +0200

    removed extra braces

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,16 +2,13 @@
 'use strict'
 
 intersection = function intersection(object_1, object_2) {
-    first_keys = Object.keys(object_1); {
-    } for (attribute_name of first_keys) {
-        if (attribute_name !== '' && object_2[attribute_name]) {
-            object_2[attribute_name] = object_1[attribute_name]; {
-            }
-        } else {
-            delete object_1[attribute_name]; {
-            }
-        } { }
-    } { }
+    first_keys = Object.keys(object_1);
+    for (attribute_name of first_keys) {
+        if (attribute_name !== '' && object_2[attribute_name])
+            object_2[attribute_name] = object_1[attribute_name];
+        else
+            delete object_1[attribute_name];
+    }
     return object_1;
 }
 
