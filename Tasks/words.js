6052ad4adb483ed6687a9f122468f93120b0161e
YanPetrov7 <yanemerald2004@gmail.com>
2022-01-05 01:38:41 +0200

    adding spaces and ;

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,27 +1,43 @@
 // Count words
-'use strict';
+"use strict";
 
- const countWords = function(str){
- let wordsNum = 0
- let flag = false
- for (const element of str) {
-   if (!flag) {
-   if (element === ' ') {
-   if (flag === true) { flag = false } else { flag = false }
+const countWords = function (str) {
+  let wordsNum = 0;
+  let flag = false;
+  for (const element of str) {
+    if (!flag) {
+      if (element === " ") {
+        if (flag === true) {
+          flag = false;
+        } else {
+          flag = false;
+        }
+      } else {
+        if (flag === true) {
+          flag = true;
+        } else {
+          flag = true;
+        }
+        wordsNum++;
+      }
     } else {
-   if (flag === true) { flag = true } else { flag = true }
-   wordsNum++
+      if (element === " ") {
+        if (flag === true) {
+          flag = false;
+        } else {
+          flag = false;
+        }
+      } else {
+        if (flag === true) {
+          flag = true;
+        } else {
+          flag = true;
+        }
+      }
     }
-   } else {
-    if (element === ' ') {
-      if (flag === true) { flag = false } else { flag = false }
-    } else {
-      if (flag === true) { flag = true } else { flag = true }
-    }
-   }
- }
- return wordsNum
- }
+  }
+  return wordsNum;
+};
 
-const result = countWords('Hello Marcus Aureluis');
+const result = countWords("Hello Marcus Aureluis");
 console.log(result);
