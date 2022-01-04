4dc7f765686fa1ccdeced97113a9f1a62a8e4d02
nikitaq121 <97105666+nikitaq121@users.noreply.github.com>
2022-01-04 16:14:04 +0200

    change formating

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,15 +1,15 @@
 // Remove values from array
 'use strict';
-Skip =(T,...Remove)=> {
-x = 0;
-for (C of T) {
-for (X of Remove) {
-if (C === X) {
-  T.splice(x, 1);
-};
-}
-x++;
-}
+const Skip = (T, ...Remove) => {
+  x = 0;
+  for (C of T) {
+    for (X of Remove) {
+      if (C === X) {
+        T.splice(x, 1);
+      }
+    }
+  x++;
+  }
 return T;
 }
 
