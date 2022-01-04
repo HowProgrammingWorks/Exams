eb85e8f0862b723dd892bd7acecd2f0f34d6d6e1
NikitaP0203 <nvnworkf01@gmail.com>
2022-01-05 01:02:25 +0200

    Renaming vars and removing useless k

--- Solutions/between.js
+++ Solutions/between.js
@@ -1,19 +1,19 @@
 // Extract substring between prefix and suffix
 'use strict';
 
-const getvaluebetween = (str, p, s) => {
-  let i = str.indexOf(p)
-    if ( i === -1 ) return ''
+const getValueBetween = (str, start, end) => {
+  let startIndex = str.indexOf(start)
+    if ( startIndex === -1 ) return ''
     else {
-      let k = i + p.length
-      str =  str.substring(k)
-      if ( s) {
-        i = str.indexOf( s )
-        if (i === -1) {
-          return  '' } else { str =  str.substring(0 , i)
+      startIndex += start.length
+      str =  str.substring(startIndex)
+      if ( end) {
+        let endIndex = str.indexOf( end )
+        if (endIndex === -1) {
+          return  '' } else { str =  str.substring(0 , endIndex)
             }}}
   return str
 }
 
-const result = getvaluebetween('Hello <username> and bye!', '<', '>')
+const result = getValueBetween('Hello <username> and bye!', '<', '>')
 console.log(result)
