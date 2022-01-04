7ee693fb35a92d19cc7379109747e27aa9b583e4
Гайдей Мария, ИМ-12 <89260967+jinworldwildhandsome@users.noreply.github.com>
2022-01-04 14:58:12 +0200

    used eslint and prettier

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,15 +2,14 @@
 'use strict';
 
 const intersection = (object1, object2) => {
-    const keys = Object.keys(object1); 
-    const result = {}; 
-    for (const key of keys) {
-        if (object2[key]) {
-            result[key] = object1[key]; 
-        } 
-    } 
-    return result;
-}
-
+  const keys = Object.keys(object1);
+  const result = {};
+  for (const key of keys) {
+    if (object2[key]) {
+      result[key] = object1[key];
+    }
+  }
+  return result;
+};
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,17 +1,17 @@
 // Merge two dictionaries
 'use strict';
-const merge =(object1, object2) => {
-    const result = {}; 
-    const key1 = Object.keys(object1);
-    const key2 = Object.keys(object2);
-    for (const key  of key1) {
-        result[key] = object1[key]; 
-    }
-    for (const key of key2) {
-        result[key] = object2[key]; 
-    }
-    return result; 
-}
+const merge = (object1, object2) => {
+  const result = {};
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  for (const key  of keys1) {
+    result[key] = object1[key];
+  }
+  for (const key of keys2) {
+    result[key] = object2[key];
+  }
+  return result;
+};
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
