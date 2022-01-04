161f13eea8d1c5388ae259e4d03edcc8bdd16510
Daria Petruk <90788327+Mikkador@users.noreply.github.com>
2022-01-04 16:23:30 +0200

    simplify IF operator

--- Tasks/find.js
+++ Tasks/find.js
@@ -3,14 +3,8 @@
 find=(object,...rest) => {
    value = rest.pop(1);
    for (name in object) {
-      if ((typeof object[name]) == (typeof value)) {
-       if (typeof name)
-       return name
-       if (typeof object)
-       return object
-      }
+      if ((typeof object[name]) == (typeof value))  return name;
    }
-
 }
 
 const result = find({ a: 1, b: 2, c: 'hello', d: false }, 'hello');
