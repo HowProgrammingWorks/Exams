0c2be29800c2fd2bd1be0333884bf2ea496e9883
Dimdim28 <wwwdimanes1@gmail.com>
2022-01-04 15:45:32 +0200

    fixed problems  folowing  your comments

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,8 +1,10 @@
 // Finde an intersection of two dictionaries
 'use strict';
+
 const intersection = (obj1, obj2) => {
   const intersec = {};
-  for (const key in obj1) {
+  const firstKeys = Object.keys(obj1);
+  for (const key of firstKeys) {
     if (obj1[key] === obj2[key]) {
       intersec[key] = obj1[key];
     }
