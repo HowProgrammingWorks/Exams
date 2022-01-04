2cec9ab1bbf62e28009880ad5c87cafe50d56c7b
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-04 22:37:19 +0200

    making sure that the day we enter is in lower case

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,6 +4,7 @@ const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
 function parser(day) {
   for (let i = 0; i < days.length; i++) {
+    day = day.toLowerCase()
     if (day.startsWith(days[i].toLowerCase())) {
       return `match found: ${i+1}`
     };
