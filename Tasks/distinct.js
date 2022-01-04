93c0af416856f235401afe3c3f927a4e7064e54b
GAlexandrD <o.horbatko.im11@kpi.ua>
2022-01-04 15:34:53 +0200

    renaming

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,18 +1,18 @@
 // Deduplicate values in array
 
-const DISTINCT = data => {
-  const A = new Set();
-  let w = 0;
-  data.forEach((a) => {
-    if (A.has(a)) {
-      data.splice(w, 1)
+const distinct = array => {
+  const unicalElements = new Set();
+  let index = 0;
+  array.forEach((a) => {
+    if (unicalElements.has(a)) {
+      array.splice(index, 1)
     } else {
-      A.add(a)
+      unicalElements.add(a)
     }
-    w++;
+    index++;
   });
-  return data;
+  return array;
 }
 
-const result = DISTINCT([1, 2, 1, 3, 1, 4]);
+const result = distinct([1, 2, 1, 3, 1, 4]);
 console.log(result);
