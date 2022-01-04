826eb48df4f88e59d011c4a9ff0f6f4ca28609f6
Misha <mykhailoholoborodko@gmail.com>
2022-01-04 20:24:48 +0200

    use camelCase

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,11 +2,11 @@
 'use strict';
 
 const getValueBetween = (string, prefix, suffix) => {
-  const ERR = -1;
+  const wrongIndex = -1;
   let substring = '';
   const prefixIndex = string.indexOf(prefix);
   const suffixIndex = string.indexOf(suffix);
-  if (prefixIndex !== ERR && suffixIndex  !== ERR) {
+  if (prefixIndex !== wrongIndex && suffixIndex  !== wrongIndex) {
     const firstIndexSubstring = prefixIndex + 1;
     substring = string.substring(firstIndexSubstring, suffixIndex);
   }
