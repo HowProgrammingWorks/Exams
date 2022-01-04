ee5edcbcd0563727f2971febe645e5a684a7c511
Misha <mykhailoholoborodko@gmail.com>
2022-01-04 12:58:21 +0200

    simplified the code, add an err const and remove chaining writing it into 1 method

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,12 +2,13 @@
 'use strict';
 
 const getValueBetween = (string, prefix, suffix) => {
+  const ERR = -1;
   let substring = '';
   const prefixIndex = string.indexOf(prefix);
   const suffixIndex = string.indexOf(suffix);
-  if (prefixIndex !== -1 && suffixIndex  !== -1) {
-    const lengthSubstring = suffixIndex - prefixIndex - 1;
-    substring = string.substring(prefixIndex + 1).substring(0, lengthSubstring);
+  if (prefixIndex !== ERR && suffixIndex  !== ERR) {
+    const firstIndexSubstring = prefixIndex + 1;
+    substring = string.substring(firstIndexSubstring, suffixIndex);
   }
   return substring;
 };
