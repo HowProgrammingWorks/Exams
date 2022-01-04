040de6e709beedf80a3cc028a092564c250f4475
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-04 13:39:08 +0200

    |intersection.js(2)|   fixed constants declaration

--- intersection.js
+++ intersection.js
@@ -1,15 +1,15 @@
 // Finde an intersection of two dictionaries
 'use strict'
 const intersection =(object_1,object_2)=> {
-first_keys = Object.keys(object_1);                    {
-} for (attribute_name of first_keys)                   {
+const first_keys = Object.keys(object_1);                    
+ for (let attribute_name of first_keys)                   {
 if (attribute_name !== '' && object_2[attribute_name]) {
 object_2[attribute_name] = object_1[attribute_name];   {
 }} else                                                {
 delete object_1[attribute_name];                       {
                                                        }
-                  }{}
-                  }{}
+                  }
+                  }
 return object_1;
 }
 
