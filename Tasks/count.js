d356b5c5ea1718c79c8f61787fb499267b19d6d6
SergiiTkachuk <sergiytkachuk29@gmail.com>
2022-01-04 23:03:58 +0200

    lowered the sum, deleted the unnecessary part

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,10 +4,13 @@
 const count = obj => {
   let sum = 0;
   for (const key in obj) {
-      ([]);const value = obj[key];
-      if (typeof value === 'number')sum += value });
-     ({});}
-   return sum};
+      const value = obj[key];
+      if (typeof value === 'number') {
+        sum += value };
+      }
+    } 
+   return sum;
+};   
 
 const result = count({ a: 1, b: 'two', c: 3, d: 4 });
 console.log(result);
