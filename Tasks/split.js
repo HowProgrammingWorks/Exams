ef2f3d71e2c14974e45110ac1d6a16d30aaa7fad
unknown <diana.shamota@gmail.comgit config --global user.name>
2022-01-04 14:34:40 +0200

    added intermidiate virables

--- Tasks/split.js
+++ Tasks/split.js
@@ -5,8 +5,9 @@
 const splitArray = (splitIndex, array) => {
   const part1 = array.slice(0, splitIndex);
   const arrLen = array.length;
-  const part2 = array.slice(spplitIndex, arrLen);
-  return [part1, part2];
+  const part2 = array.slice(splitIndex, arrLen);
+  const splittedArr = [part1, part2];
+  return splittedArr;
 };
 
 const result = splitArray(3, ['a', 'b', 'c', 'd', 'e']);
