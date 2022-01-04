417e12735c83e171fcd398ba0d9ec46867e772e8
Misha <mykhailoholoborodko@gmail.com>
2022-01-04 10:33:07 +0200

    reworking into the correct style with all indents

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,15 +3,19 @@
 
 const getValueBetween = (string, prefix, suffix) => {
   const prefixIndex = string.indexOf(prefix)
-    if (prefixIndex === -1 ) return ''
-    else {
-     const nextStringIndex = prefixIndex + prefix.length
-      string =  string.substring(nextStringIndex)
-      if ( suffix) {
-       const suffixIndex = string.indexOf( suffix )
-        if (suffixIndex  === -1) {
-          return  '' } else { string =  string.substring(0 , suffixIndex )
-            }}}
+  if (prefixIndex === -1) return ''
+  else {
+    const nextStringIndex = prefixIndex + prefix.length
+    string =  string.substring(nextStringIndex)
+    if (suffix) {
+      const suffixIndex = string.indexOf(suffix)
+      if (suffixIndex  === -1) {
+        return ''
+      } else {
+        string =  string.substring(0, suffixIndex)
+      }
+    }
+  }
   return string
 }
 
