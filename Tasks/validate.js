696c2f842f11012f627a4073f27611c138b7b605
Masha Martyniuk <mmartyniuk004@gmail.com>
2022-01-04 22:38:03 +0200

    Update validate.js
    added 2 space indentation + improved style bugs

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,18 +1,18 @@
 // Validate person name
 'use strict'
 
-const Valid = (name) => {
-if (name === '') return false;
-if (typeof name !== 'string') return false;
-for (const words of name) {
-    const a = 'a'.charCodeAt(0);
-    const z = 'z'.charCodeAt(0);
-    if ( words.toLowerCase().charCodeAt(0) < a || words.toLowerCase().charCodeAt(0) > z) {
+const valid = (name) => {
+  if (name === '') return false;
+  if (typeof name !== 'string') return false;
+  for (const words of name) {
+     const a = 'a'.charCodeAt(0);
+     const z = 'z'.charCodeAt(0);
+     if ( words.toLowerCase().charCodeAt(0) < a || words.toLowerCase().charCodeAt(0) > z) {
       return false;
-    }
-  return true;
+     }
+    return true;
   }
 }
 
-const result = Valid('Marcus Aurelius');
+const result = valid ('Marcus Aurelius');
 console.log(result);
