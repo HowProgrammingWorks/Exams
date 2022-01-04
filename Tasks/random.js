a2dd7ffc450e51a613156486b2dc72588dbc1621
Mahaon17 <89358047+Mahaon17@users.noreply.github.com>
2022-01-04 20:39:34 +0200

    Removed reduntant return and added min assingment

--- Tasks/random.js
+++ Tasks/random.js
@@ -4,10 +4,10 @@
 const random = (min, max) => {
   if (!max) {
     max = min;
-    return Math.floor(Math.random() * (max + 1));
+    min = 0;
   }
   return min + Math.floor(Math.random() * (max - min + 1));
 };
 
-const result = random(5);
+const result = random(1, 9);
 console.log(result);
