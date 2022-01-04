35fffc103fb81d39e3a4c987e0e7f117fd8f6083
Den <denissytnikmacho@gmail.com>
2022-01-04 14:10:52 +0200

    переписал циклы в одну строчку

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -7,20 +7,12 @@ const findDifference = function( object1,object2 ) {
     let res;
 
     if ( Object.keys(obj1) > Object.keys(obj2) ) {
-        for (const key in obj2) {
-            if ( obj1[key] === obj2[key] ) {
-                delete obj1[key];
-            }
-        }
-        res = obj1;
-    } else {
-        for (const key in obj1) {
-            if ( obj2[key] === obj1[key] ) {
-                delete obj2[key];
-            }
-        }
-        res = obj2;
-    }
+        for (const key in obj2) if ( obj1[key] === obj2[key] ) delete obj1[key];
+        res = obj1; }
+    else { 
+        for (const key in obj1) if ( obj2[key] === obj1[key] ) delete obj2[key];
+        res = obj2; }
+        
     return res;
 };
 
