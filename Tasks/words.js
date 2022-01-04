0962aab10523a2626aeca46a8f356815003cca5e
YanPetrov7 <yanemerald2004@gmail.com>
2022-01-05 01:57:57 +0200

    delete useless code

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,38 +1,22 @@
 // Count words
 "use strict";
 
-const countWords = str => {
+const countWords = (str) => {
   let wordsNum = 0;
   let flag = false;
   for (const element of str) {
     if (!flag) {
       if (element === " ") {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
+        flag = false;
       } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-        wordsNum++;
+        flag = true;
       }
+      wordsNum++;
     } else {
       if (element === " ") {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
+        flag = false;
       } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
+        flag = true;
       }
     }
   }
