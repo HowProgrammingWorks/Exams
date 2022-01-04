a9f1fc30cdd543fea8a05c1a7b5208935894f360
Valentyn13 <valentyn13git@gmail.com>
2022-01-04 13:12:34 +0200

    use Prettier

--- ExamTasks/distinct.js
+++ ExamTasks/distinct.js
@@ -1,17 +1,17 @@
-'use strict'
+"use strict";
 // Deduplicate values in array
 
 const distinct = (arr) => {
-    const set = new Set(); 
-    let spliceIndex = 0;
-    for(const arrElement of arr){
-      if(set.has(arrElement)) {
-        arr.splice(spliceIndex, 1);
-      } else {
-        set.add(arrElement)
-      }
-      spliceIndex++;
+  const set = new Set();
+  let spliceIndex = 0;
+  for (const arrElement of arr) {
+    if (set.has(arrElement)) {
+      arr.splice(spliceIndex, 1);
+    } else {
+      set.add(arrElement);
     }
+    spliceIndex++;
+  }
 
   return arr;
 };
