c585c0b1910ecdef51bafd1b1b36879d8016d6a6
Illyakravchuk <90210111+Illyakravchuk@users.noreply.github.com>
2022-01-04 13:09:42 +0200

    Reworked the code according to the rules of syntax

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,10 +1,11 @@
 // Generate random integer value in given range
 
 const randomInteger = (min, max) => {
-    if (min === undefined || max === undefined) return 'Enter the data correctly!';
-   
-    const randomNumber = min + Math.random() * (max + 1 - min);
-    return Math.floor( randomNumber );
+  if (min === undefined || max === undefined)
+    return 'Enter the data correctly!';
+
+  const randomNumber = min + Math.random() * (max + 1 - min);
+  return Math.floor(randomNumber);
 };
 
 const result = randomInteger(1, 9);
