a68cc9a8ac4b24a8042a3fee956c8224d1dde45c
AlinaG <88611367+alinagts@users.noreply.github.com>
2022-01-04 23:24:37 +0200

    added spaces and ';'

--- Tasks/words.js
+++ Tasks/words.js
@@ -2,10 +2,10 @@
 // Count words
 
 const words = (string) => {
- const arrOfWords = string.split(" ");
- const numberOfWords = arrOfWords.length;
- return numberOfWords;
-}
+  const arrOfWords = string.split(" ");
+  const numberOfWords = arrOfWords.length;
+  return numberOfWords;
+};
 
 const result = words('Hello Marcus Aureluis');
 console.log(result);
