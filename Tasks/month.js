e6273eeab201c3f0ffa5de5497e412b3531e9018
111Timur111 <93223108+111Timur111@users.noreply.github.com>
2022-01-04 16:12:00 +0200

    Update month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,13 +1,16 @@
 // Get month number
-'use script';
+'use strict';
 
-const month = (s) => 
-    {   for (let i = 0; i <months.length; i++)
-        if (s.startswith(months[i]))
-         return i + 1
+const parseMonth = month => {
 
-};
+    for (let i = 0; i < months.length; i++) {
+  
+      const monthsPrefixes = months[i].toLowerCase();
+      if (month.startsWith(monthsPrefixes))
+        return i + 1;
+    }
+  };
 
-const month = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
-const result = month (s, 'february')
-console.log (result)
+  const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+const result = parseMonth('may');
+console.log(result);
