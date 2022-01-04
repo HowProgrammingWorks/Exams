94052584f0ee3e98e8edf12a9ebc881779ef3395
oneOdin <97091092+hryshniakovNazar@users.noreply.github.com>
2022-01-05 01:53:26 +0200

    FilterCommit 2

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,12 +2,13 @@
 
 'use strict';
 
-Filter =(T,t)=> {
-for (C of T) {
-x = T.indexOf(C);
-if (typeof T[x] !== t) { T.splice(x, 1) }; };
-return T;
-}
-
+Filter = (T, t) => {
+  for (C of T) {
+    x = T.indexOf(C);
+    if (typeof T[x] !== t) { T.splice(x, 1); }
+  }
+  return T;
+};
+  
 const result = Filter([1, 2, 'three', 4, 5, 'six'], 'number');
 console.log(result);
