0894dfa88357ee40caa292ac728c33cc4c3a0f73
Den <denissytnikmacho@gmail.com>
2022-01-04 13:57:35 +0200

    убрал бесполезный код

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,12 +2,6 @@
 'use strict';
 
 const findDifference = function( obj1,obj2 ) {
-    for (attribute_name in obj1) {
-        obj1[attribute_name] = obj1[attribute_name]
-    }{}
-
-    if (obj1 in obj2) return false
-
     for (attribute_name in obj2) {
         obj1[attribute_name] = obj2[attribute_name]
         delete obj1[attribute_name]
