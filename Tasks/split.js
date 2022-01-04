2c70807d9533bc7e854cc0b1e9d38cbac930a557
Illyakravchuk <90210111+Illyakravchuk@users.noreply.github.com>
2022-01-04 14:36:52 +0200

    Reworked the code according to the rules of syntax

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,9 +1,10 @@
 // Split array into two parts
 
-const splitArray=(index,array)=>{
-  const part1=array.slice(0,index);
- const  part2=array.slice(index,array.length);
-    return [part1 ,part2];
+
+const splitArray = (index, array) => {
+  const part1 = array.slice(0, index);
+  const  part2 = array.slice(index, array.length);
+  return [part1, part2];
 };
 
 const result = splitArray(3, ['a', 'b', 'c', 'd', 'e']);
