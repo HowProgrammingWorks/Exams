858c9ce08ab5c651f74d54f809aafc3bec15bbd1
artem <artemkusch2@gmail.com>
2022-01-04 20:25:54 +0200

    Added two space indentation for better readability

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -2,9 +2,9 @@
 'use strict';
 
 const invertArray = (array) => {
-    const invertedArray = Object.keys(array);
-    for (const key of invertedArray) invertedArray[key] = array.pop();
-    return invertedArray;
+  const invertedArray = Object.keys(array);
+  for (const key of invertedArray) invertedArray[key] = array.pop();
+  return invertedArray;
 };
 
 const result = invertArray([100, 200, 300, 400]);
