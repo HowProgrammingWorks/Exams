00c6894e84aabbe72ddafb06995f78c45b0bdbda
Den <denissytnikmacho@gmail.com>
2022-01-04 13:44:58 +0200

    переделал проверку свойств, убрав лишнее

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -3,6 +3,7 @@
 const compareDictionaries = function( obj1,obj2 ) {
   const obj1Keys = Object.keys(obj1).sort();
   const obj2Keys = Object.keys(obj2).sort();
+  let e = true;
 
   if ( obj1Keys.length !== obj2Keys.length ) return false;
 
@@ -10,13 +11,10 @@ const compareDictionaries = function( obj1,obj2 ) {
     if ( obj1Keys[i] !== obj2Keys[i] ) return false; 
   }
 
-  let e = true;
-
-  for ( const key of obj1Keys ) {
-    if ( obj1[key] === obj2[key] ) e = e && true;
-    else { e = e && false; }
+  for (const key of obj1Keys) {
+    if (obj1[key] !== obj2[key]) e = false;
   }
-
+  
   return e;
 };
 
