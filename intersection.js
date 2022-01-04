55aa3d3b34d1e83152ba1b3f91489474cef61ca8
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-04 16:37:23 +0200

    |intersection.js(6)|   verified with ESLint

--- intersection.js
+++ intersection.js
@@ -1,15 +1,15 @@
 // Finde an intersection of two dictionaries
-'use strict'
-const dictIntersection =(firstDict,secDict)=> {
+'use strict';
+const dictIntersection = (firstDict, secDict) => {
   const firstDictKeys = Object.keys(firstDict);
-  let result = {}
- for (let element of firstDictKeys){
-if (firstDict[element] === secDict[element]) {
-        result[element] = firstDict[element]
-} 
-}
-return result;
-}
+  const result = {};
+  for (const element of firstDictKeys) {
+    if (firstDict[element] === secDict[element]) {
+      result[element] = firstDict[element];
+    }
+  }
+  return result;
+};
 
 const result = dictIntersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
