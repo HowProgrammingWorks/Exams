09da67b63f6e0cd75658a7a72fee76fc40b54196
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-01-04 18:16:39 +0200

    using spread syntax and rewroting the function in the one line

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,10 +1,7 @@
 'use strict';
 // Merge two dictionaries
 
-const mergeTwoObjects = (object1, object2) => {
-  const mergedObject = {};
-  return Object.assign(mergedObject, object1, object2);
-};
+const mergeTwoObjects = (object1, object2) => ({ ...object1, ...object2 });
 
 const result = mergeTwoObjects({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
