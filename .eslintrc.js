b4a5a9af409d9c87cf39fb69ec1ca1b6678f1402
gonnagetbetter <denissss906@gmail.com>
2022-01-04 11:48:51 +0200

    Adding Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 13,
+  },
+  'rules': {   
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"]
+
+  },
+};
