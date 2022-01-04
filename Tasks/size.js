952ccbe809a01ab21d3d6b57b062d1fa822a717c
m1guelten <footballMiguelTen@gmail.com>
2022-01-04 20:59:14 +0200

    Антипаттерн (одинаковые имена функции и переменных (size))

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,13 +1,13 @@
 // Convert number to file size in bytes, kb, mb, and gb
 
-let size=size=>{
-  if (size === 0) return '0';
+let size=number=>{
+  if (number === 0) return '0';
   else {
-    const exp = Math.floor(Math.log(size)/Math.log(1000));
-    if (exp==0) return size + ' byte'
-    else if(exp==1) return Math.round(size / 1000) + ' kb'
-    else if(exp==2) return Math.round(size / 1000000) + ' mb'
-    else if(exp==3) return Math.round(size / 1000000000) + ' gb'
+    const exp = Math.floor(Math.log(number)/Math.log(1000));
+    if (exp==0) return number + ' byte'
+    else if(exp==1) return Math.round(number / 1000) + ' kb'
+    else if(exp==2) return Math.round(number / 1000000) + ' mb'
+    else if(exp==3) return Math.round(number / 1000000000) + ' gb'
   }
 }
 
