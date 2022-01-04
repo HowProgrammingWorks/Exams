900886f6cf402b3d9156798820522dc9b67b1c31
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-01-04 02:12:14 +0200

    you can also use the prototype of array

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -16,3 +16,6 @@ const plane = array => {
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
+
+/* Also this function can be replaced with
+const result = [1, [[2]], [3, 4], [5], [6, [7, 8]]].flat(Infinity); */
