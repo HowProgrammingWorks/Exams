ef0a2509040376be74c2cadb961a2b2f2c306af3
thesemik <semivolos.evgen1@gmail.com>
2022-01-04 23:19:15 +0200

    deleted unnecessary code

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,15 +1,11 @@
 // Make nested array plane
 
 const_plane = function(arr, res=[]) {
-  j = 0; 
-  for(let i = 0, length = arr.length; i < length; i++) {
+  for(let i = 0; i < arr.length; i++) {
     value = arr[i];
-    j = i;
-    if (Array.isArray(value, typeof value) && [i, length]) {
+    if (Array.isArray(value)) {
       res.push(...const_plane(value));
-      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1]
       res.push(value);
     }
   }
