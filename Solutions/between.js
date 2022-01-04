6399cd630e739df2816a47f9c07b192ad7ef8425
NikitaP0203 <nvnworkf01@gmail.com>
2022-01-05 01:13:37 +0200

    change code style and add ;

--- Solutions/between.js
+++ Solutions/between.js
@@ -1,19 +1,24 @@
-// Extract substring between prefix and suffix
 'use strict';
 
 const getValueBetween = (str, start, end) => {
-  let startIndex = str.indexOf(start)
-    if ( startIndex === -1 ) return ''
-    else {
-      startIndex += start.length
-      str =  str.substring(startIndex)
-      if ( end) {
-        let endIndex = str.indexOf( end )
-        if (endIndex === -1) {
-          return  '' } else { str =  str.substring(0 , endIndex)
-            }}}
-  return str
+    let startIndex = str.indexOf(start);
+    if (startIndex === -1) {
+        return '';  
+    } else {
+        startIndex += start.length;
+        str = str.substring(startIndex);
+
+        if (end) {
+            const endIndex = str.indexOf(end);
+            if (endIndex === -1) {
+                return '';
+            } else {
+                str =  str.substring(0 , endIndex);
+            }
+        }
+    }
+    return str;
 }
 
-const result = getValueBetween('Hello <username> and bye!', '<', '>')
-console.log(result)
+const result = getValueBetween('Hello <username> and bye!', '<', '>');
+console.log(result);
