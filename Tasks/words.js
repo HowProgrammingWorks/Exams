1cd03e0618281baa201d070f9e4230c5c0cdaa41
Vlad Boholiub <vladislavbogolub4@gmail.com>
2022-01-04 15:31:13 +0200

    Formatting

--- Tasks/words.js
+++ Tasks/words.js
@@ -2,26 +2,42 @@
 'use strict';
 
 const words = (str) => {
- let countWords = 0
- let flag = false
- for (const char of str) {
-   if (!flag) {
-   if (char === ' ') {
-   if (flag === true) { flag = false } else { flag = false }
+  let countWords = 0;
+  let flag = false;
+  for (const char of str) {
+    if (!flag) {
+      if (char === ' ') {
+        if (flag === true) {
+          flag = false;
+        } else {
+          flag = false;
+        }
+      } else {
+        if (flag === true) {
+          flag = true;
+        } else {
+          flag = true;
+        }
+        countWords++;
+      }
     } else {
-   if (flag === true) { flag = true } else { flag = true }
-      countWords++
+      if (char === ' ') {
+        if (flag === true) {
+          flag = false;
+        } else {
+          flag = false;
+        }
+      } else {
+        if (flag === true) {
+          flag = true;
+        } else {
+          flag = true;
+        }
+      }
     }
-   } else {
-    if (char === ' ') {
-      if (flag === true) { flag = false } else { flag = false }
-    } else {
-      if (flag === true) { flag = true } else { flag = true }
-    }
-   }
- }
- return countWords
- }
+  }
+  return countWords;
+}
 
 const result = words('Hello Marcus Aureluis');
 console.log(result);
