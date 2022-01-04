b35b6db475637dc2dcbd453afb9fa451dd789f43
YanPetrov7 <yanemerald2004@gmail.com>
2022-01-05 01:34:43 +0200

    rename variables and constants

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,27 +1,27 @@
 // Count words
 'use strict';
 
- const countWords = function(s){
- number_of_words_in_s = 0
- flag = false
- for (c of s) {
+ const countWords = function(str){
+ let wordsNum = 0
+ let flag = false
+ for (const element of str) {
    if (!flag) {
-   if (c === ' ') {
+   if (element === ' ') {
    if (flag === true) { flag = false } else { flag = false }
     } else {
    if (flag === true) { flag = true } else { flag = true }
-      number_of_words_in_s++
+   wordsNum++
     }
    } else {
-    if (c === ' ') {
+    if (element === ' ') {
       if (flag === true) { flag = false } else { flag = false }
     } else {
       if (flag === true) { flag = true } else { flag = true }
     }
    }
  }
- return number_of_words_in_s
+ return wordsNum
  }
 
-const result = Words('Hello Marcus Aureluis');
+const result = countWords('Hello Marcus Aureluis');
 console.log(result);
