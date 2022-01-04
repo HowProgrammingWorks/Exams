fbcd6f44d726d9793acb82b83e1f53f979e91ea4
Misha <mykhailoholoborodko@gmail.com>
2022-01-04 10:13:59 +0200

    add const

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,14 +1,14 @@
 // Extract substring between prefix and suffix
 'use strict';
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p)
+const getvaluebetween = (str, p, s) => {
+  const i = str.indexOf(p)
     if ( i === -1 ) return ''
     else {
-      k = i + p.length
+     const k = i + p.length
       str =  str.substring(k)
       if ( s) {
-        i = str.indexOf( s )
+       const i = str.indexOf( s )
         if (i === -1) {
           return  '' } else { str =  str.substring(0 , i)
             }}}
