69c739fab10271c36ce1c6ddf45f8a6fd8aed62b
knvlewel <90198240+knvlewel@users.noreply.github.com>
2022-01-04 16:18:11 +0200

    const instead of let

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -4,13 +4,13 @@
 const validName = (name) => {
   if (typeof name === 'string') {
     const words = name.split(' ');
-      for (let word of words) {
+      for (const word of words) {
       let charCode = word.charCodeAt(0);
       if (charCode < 65 || charCode > 90) return false;
-      for (let index = 1; index < word.length; index++) { 
-        charCode = word.charCodeAt(index); 
-        if (charCode < 97 || charCode > 122) return false; 
-      } 
+      for (let index = 1; index < word.length; index++) {
+        charCode = word.charCodeAt(index);
+        if (charCode < 97 || charCode > 122) return false;
+      }
     }
     return true;
   }
