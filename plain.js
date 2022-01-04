784ee3f4d9551d57e744573538ce623fc3a4d013
gonnagetbetter <denissss906@gmail.com>
2022-01-04 11:57:39 +0200

    Corrected with Eslint

--- plain.js
+++ plain.js
@@ -4,7 +4,7 @@
 
 const arrPlain = (arr) => {
   const res = [];
-  for(let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     const value = arr[i];
     if (Array.isArray(value)) {
       res.push(...arrPlain(value));
@@ -13,7 +13,7 @@ const arrPlain = (arr) => {
     }
   }
   return res
-}
+};
 
 const result = arrPlain([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
--- words.js
+++ words.js
@@ -5,14 +5,14 @@
 const words = function(str) {
   let wordsInString = 0;
   let checkForChar = true;
-    for (const char of str) {
-      if (checkForChar === true && char !== ' ') { 
-        checkForChar = false;
-        wordsInString++;
-      } else if (char === ' ') checkForChar = true;
-    }
+  for (const char of str) {
+    if (checkForChar === true && char !== " ") {
+      checkForChar = false;
+      wordsInString++;
+    } else if (char === " ") checkForChar = true;
+  }
   return wordsInString
-}
+};
 
-const result = words('Hello Marcus Aureluis');
+const result = words("Hello Marcus Aureluis");
 console.log(result);
