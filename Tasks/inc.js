0aebccf549cd5433ac2623c646c097e43cf027a1
danil2205 <04danil@gmail.com>
2022-01-04 22:09:20 +0200

    month.js, inc.js => used `eslint` and `prettier`, replace `for` on `for...of`, delete variable `len`, add empty line after `use strict`

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,5 +1,6 @@
 // Increment all numbers in dictionary
 'use strict';
+
 const incNumbers = (dict) => {
   const keys = Object.keys(dict);
   for (const key of keys) {
--- Tasks/month.js
+++ Tasks/month.js
@@ -1,11 +1,11 @@
 // Get month number
 'use strict';
+
 const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
 const monthNum = (yourMonth) => {
-  const len = months.length;
-  for (let i = 0; i < len; i++) {
-    if (yourMonth.toLowerCase().startsWith(months[i])) return i + 1;
+  for (const key of months.keys()) {
+    if (yourMonth.toLowerCase().startsWith(months[key])) return key + 1;
   }
   return -1;
 };
