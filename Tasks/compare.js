14e3d265e2a3ebd5ff2bdd81d53bb354c8eb3c8a
thesemik <semivolos.evgen1@gmail.com>
2022-01-04 22:30:21 +0200

    extra parameters in join()

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -5,7 +5,7 @@
 const compare = (list1, list2) => {
   const key1 = Object.keys(list1);
   const key2 = Object.keys(list2);
-  if (key1.join('-') !== key2.join('-')) return false;
+  if (key1.join() !== key2.join()) return false;
   let e = true;
   for (let key of key1) {
     if (list1[key] === list2[key]) e = e && true;
