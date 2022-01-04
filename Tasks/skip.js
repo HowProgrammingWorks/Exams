42807b0f81fb8a8a536a7e5814ba6b0b4bca8e7c
vania <i.p.yeremenko@gmail.com>
2022-01-04 16:58:41 +0200

    renaming 'chars' to 'values'

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -2,20 +2,20 @@
 
 // Remove values from array
 
-const skip = (array, ...charsToRemove) => {
-  let removedChar = 0;
-  for (const selectedChars of array) {
-    for (const selectedCharsToRemove of charsToRemove) {
-      if (selectedChars === selectedCharsToRemove) {
+const skip = (array, ...valuesToRemove) => {
+  let removedValue = 0;
+  for (const selectedValues of array) {
+    for (const selectedValuesToRemove of valuesToRemove) {
+      if (selectedValues === selectedValuesToRemove) {
         const numberOfRemoves = 1;
-        array.splice(removedChar, numberOfRemoves);
+        array.splice(removedValue, numberOfRemoves);
       }
     }
-    removedChar++;
+    removedValue++;
   }
   return array;
 };
 
-const arrayOfChars = [1, 2, 3, 4, 5];
-const result = skip(arrayOfChars, 2, 5);
+const arrayOfValues = [1, 2, 3, 4, 5];
+const result = skip(arrayOfValues, 2, 5);
 console.log(result);
