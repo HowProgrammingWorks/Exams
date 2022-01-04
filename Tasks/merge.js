b5aa9f230ee541e6275a0a81987c457fdd54e868
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-04 11:45:15 +0200

    fix(merge): fixed all semicolons

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,13 +4,13 @@
 const merge = (obj1, obj2) => {
     const result = {};
    for (const key in obj1) {
-        result[key] = obj1[key]
+        result[key] = obj1[key];
   }
    for (const key in obj2) {
-     result[key] = obj2[key]
+     result[key] = obj2[key];
     }
 return result;
-}
+};
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
