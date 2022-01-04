57ce0e367cc232e3a28c8ae6544aaa4b08d79d0b
TangiresH <dimon4ik1010@gmail.com>
2022-01-04 18:12:14 +0200

    fixed semicolon

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,15 +1,15 @@
 // Generate random password
 'use strict';
 
-const generatePassword = (symbolPassword , lengthPassword) => {
-  const max = symbolPassword.length
+const generatePassword = (symbolPassword, lengthPassword) => {
+  const max = symbolPassword.length;
   let password = '';
-  for (let i = 0; i < lengthPassword; i++){
+  for (let i = 0; i < lengthPassword; i++) {
     const index = Math.floor(Math.random() * max);
     password = password + symbolPassword[index];
-  };
-  return password
-}
+  }
+  return password;
+};
 
 const result = generatePassword('abc123', 7);
 console.log(result);
