4db3e57f767d9322ea7aeb3f08fb47c3f9ea8258
Misha <mykhailoholoborodko@gmail.com>
2022-01-04 11:19:43 +0200

    some fix for the previous commit and chaining. Final

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,15 +2,14 @@
 'use strict';
 
 const getValueBetween = (string, prefix, suffix) => {
-  const substring = '';
+  let substring = '';
   const prefixIndex = string.indexOf(prefix);
   const suffixIndex = string.indexOf(suffix);
   const lengthSubstring = suffixIndex - prefixIndex - 1;
-  if (prefixIndex !== -1 || suffixIndex  !== -1) {
-    string = string.substring(prefixIndex + 1);
-    string = string.substring(0, lengthSubstring);
+  if (prefixIndex !== -1 && suffixIndex  !== -1) {
+    substring = string.substring(prefixIndex + 1).substring(0, lengthSubstring);
   }
-  return string;
+  return substring;
 };
 
 const result = getValueBetween('Hello <username> and bye!', '<', '>');
