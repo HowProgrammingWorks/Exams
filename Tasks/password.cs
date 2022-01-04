86abfd2a098cef7eb63c0db7a34783507ec5e63d
SavageVictor <victor.v.oliinyk@gmail.com>
2022-01-05 01:07:15 +0200

    function changed to lambda expression
    Actually I don't understand what for is lambda function here. In my opinion it makes code look worse, and in this case you don't have any performance improvements, cause you need to go trough delegate. Would be very grateful if you can explain this to me.

--- Tasks/password.cs
+++ Tasks/password.cs
@@ -4,14 +4,32 @@ namespace ConsoleApplication2
 {
     internal class Password
     {
+        private delegate string GenPassword(string alphabet, int passwordLength);
+        
         private static Random _random = new Random();
         
         public static void Main()
         {
+            Console.WriteLine(new GenPassword((alphabet, passwordLength) =>
+            {
+                int maxAlphabetIndex = alphabet.Length;
+                string password = "";
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    int index = (int) Math.Floor(_random.NextDouble() * maxAlphabetIndex);
+                    password += alphabet[index];
+                }
+
+                return password;
+            })("abcd123", 7));
+            
+            /*
             string result = GeneratePassword("abc123", 7);
             Console.WriteLine(result);
+            */
         }
 
+        /*
         private static string GeneratePassword(string alphabet, int passwordLength)
         {
             int maxAlphabetIndex = alphabet.Length;
@@ -24,5 +42,6 @@ namespace ConsoleApplication2
 
             return password;
         }
+        */
     }
 }
