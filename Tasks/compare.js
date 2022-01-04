db030c0633a0a4d0a0bd84036dd8e74590233c19
Den <denissytnikmacho@gmail.com>
2022-01-04 13:49:08 +0200

    добавил strict и отступы

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,21 +1,22 @@
 // Compare two dictionaries
+'use strict';
 
-const compareDictionaries = function( obj1,obj2 ) {
-  const obj1Keys = Object.keys(obj1).sort();
-  const obj2Keys = Object.keys(obj2).sort();
-  let e = true;
-
-  if ( obj1Keys.length !== obj2Keys.length ) return false;
-
-  for ( let i = 0; i < obj1Keys.length; i++ ) {
-    if ( obj1Keys[i] !== obj2Keys[i] ) return false; 
-  }
-
-  for (const key of obj1Keys) {
-    if (obj1[key] !== obj2[key]) e = false;
-  }
-  
-  return e;
+const compareDictionaries = function ( obj1, obj2 ) {
+    const obj1Keys = Object.keys(obj1).sort();
+    const obj2Keys = Object.keys(obj2).sort();
+    let e = true;
+    
+    if ( obj1Keys.length !== obj2Keys.length ) return false;
+    
+    for ( let i = 0; i < obj1Keys.length; i++ ) {
+        if ( obj1Keys[i] !== obj2Keys[i] ) return false; 
+    }
+    
+    for (const key of obj1Keys) {
+        if (obj1[key] !== obj2[key]) e = false;
+    }
+    
+    return e;
 };
 
 const result = compare({ a: 1, c: 'hello' }, { a: 1, c: 'hello' });
