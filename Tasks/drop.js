605c6194d27c38bf60b3c4a16beebf68d36d71a3
vladyatsuk <90204297+vladyatsuk@users.noreply.github.com>
2022-01-04 23:09:42 +0200

    now there is a 2 space indentation, 'drop' function is without side effects

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,12 +1,20 @@
 // Delete listed keys from dictionary
 'use strict';
+
 const drop = (obj, ...keysToDrop) => {
-    for (const keyObj in obj) {
-        for (const key of keysToDrop) {
-            if (keyObj === key) delete obj[keyObj];
-        }
-    }               
-    return obj;
+  const pureObj = {};
+
+  for (const keyObj in obj) {
+    pureObj[keyObj] = obj[keyObj];
+  }
+
+  for (const keyPureObj in pureObj) {
+    for (const key of keysToDrop) {
+      if (keyPureObj === key) delete pureObj[keyPureObj];
+    }
+  }
+
+  return pureObj;
 };
 
 const result = drop({ a: 'uno', b: 'due', c: 'tre' }, 'b', 'f');
