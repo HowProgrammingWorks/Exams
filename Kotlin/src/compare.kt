5557fb974666346a1c91971fcf2957950ab4c266
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-04 15:01:09 +0200

    Used return early principle

--- Kotlin/src/compare.kt
+++ Kotlin/src/compare.kt
@@ -3,12 +3,12 @@ fun <K,V> compare(firstMap: Map<K,V>, secondMap: Map<K,V>): Boolean {
     val keysOfFirstMap = firstMap.keys
     val keysOfSecondMap = secondMap.keys
     var compareBool = true
-    return if (keysOfFirstMap.size == keysOfSecondMap.size) {
+    if (keysOfFirstMap.size == keysOfSecondMap.size) {
         for (key in keysOfFirstMap)
             if (firstMap[key] != secondMap[key]) compareBool = false
-         compareBool
+        return compareBool
     }
-    else compareBool
+    else return compareBool
 }
 fun main(){
     val result = compare(mapOf('a' to 1, 'c' to "hello"), mapOf('a' to 1, 'c' to "hello"))
