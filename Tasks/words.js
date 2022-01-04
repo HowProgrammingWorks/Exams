8e8349c2600c61525feca1c7b32293f1e0f81302
Dolaran <gyv220427@gmail.com>
2022-01-04 04:47:27 +0200

    Update words.js
    removed useless code

--- Tasks/words.js
+++ Tasks/words.js
@@ -8,32 +8,16 @@ const words = function(str) {
   for (let element of str) {
     if (!flag) {
       if (element === ' ') {
-        if (flag === true) {
-          flag = false; 
-        } else {
-          flag = false;
-        }
+        flag = false; 
       } else {
-        if (flag === true) { 
-          flag = true; 
-        } else { 
-          flag = true; 
-        }
-        countOfWords++;
+        flag = true; 
       }
+      countOfWords++;
     } else {
       if (element === ' ') {
-        if (flag === true) { 
-          flag = false; 
-        } else { 
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true; 
-        } else { 
+        flag = false;
+      } else { 
         flag = true;
-        }
       }
     }
   }
