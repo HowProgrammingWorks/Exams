2fac005be1f7fd1c3287ecb8b67bbb9f9c6dac59
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-04 12:04:47 +0200

    |sum.js(6)|   fixed the code, made it work

--- sum.js
+++ sum.js
@@ -1,13 +1,14 @@
 // Sum all numbers from array
 'use strict'
 const sum = (s) => {
-  for (i of s)
-  {
+  let summ = 0
+  for ( let i of s) {
   let t = typeof i;
   if (t === 'number'){
-    
+    summ += i
     }
   }
+  return summ
   };
 
 const result = sum([5, true, 'string', 7, 'hello']);
