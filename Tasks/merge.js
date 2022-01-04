6d1e9130274b1c02e2c29547b0d1d7628e3cf04e
Hrystyna <yaskulskahrystyna@gmail.com>
2022-01-04 21:50:37 +0200

    Fix last syntax errors

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,7 +1,7 @@
 // Merge two dictionaries
 'use strict'
 
-const merge = (obj1, obj2) => ({...obj1, ...obj2});
+const merge = (obj1, obj2) => ({ ...obj1, ...obj2 });
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
