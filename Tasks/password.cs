9a3ca676f100404450acb542fddd800eecb4db02
SavageVictor <victor.v.oliinyk@gmail.com>
2022-01-04 10:36:55 +0200

    +password +size (in C# translated/fixed scripts)

---
+++ Tasks/password.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Password
+    {
+        private static Random _random = new Random();
+        
+        public static void Main()
+        {
+            string result = GeneratePassword("abc123", 7);
+            Console.WriteLine(result);
+        }
+
+        private static string GeneratePassword(string alphabet, int passwordLength)
+        {
+            int maxAlphabetIndex = alphabet.Length;
+            string password = "";
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int index = (int) Math.Floor(_random.NextDouble() * maxAlphabetIndex);
+                password += alphabet[index];
+            }
+
+            return password;
+        }
+    }
+}
---
+++ Tasks/size.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Size
+    {
+        static void Main()
+        {
+            Console.Clear();
+            
+            long userFileSize = Convert.ToInt64(Console.ReadLine());
+            
+            string result = FileSizeConverter(userFileSize);
+            Console.WriteLine(result);
+        }
+
+        static string FileSizeConverter( long fileSize )
+        {
+            if(fileSize <= 0)
+            {
+                return "0";   
+            }
+            else
+            {
+                long exp = (long) Math.Floor(Math.Log(fileSize)/Math.Log(1000));
+        
+                if(exp == 0)
+                {
+                    return fileSize + " byte";
+                }
+                else if(exp == 1)
+                {
+                    return fileSize / 1000 + " kb";
+                }
+                else if(exp == 2)
+                {
+                    return fileSize / 1000000 + " mb";
+                }
+                else if(exp >= 3)
+                {
+                    return fileSize / 1000000000 + " gb";
+                }
+            }
+
+            return "Invalid number";
+        }
+    }
+}
