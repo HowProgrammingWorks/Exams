0089e6d6261328b6e1a1597dded21659f4123e37
Oleh <Luibichenko.Oleg@gmail.com>
2022-01-04 12:21:50 +0200

    Added output method for arrayString

--- Tasks/MySolution/shuffle.java
+++ Tasks/MySolution/shuffle.java
@@ -6,7 +6,8 @@ public class shuffle {
     public static void main(String[] args) {
         Object[] array = { "a", "b", "c", "d", "e" };
         shuffleProcess(array);
-        System.out.println(array);
+        String arrayString = Arrays.toString(array);
+        System.out.println(arrayString);
     }
 
     public static void shuffleProcess(Object[] array) {
