cefb6ba3eaaec2ff844a8ca4992b9e5440db076f
svitbar <svitbar1102@gmail.com>
2022-01-04 15:35:17 +0200

    Changed names, added brackets to argument

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -2,19 +2,19 @@
 
 // Deduplicate values in array
 
-const DISTINCT = data => {
-  const A = new Set();
+const distinctValues = (data) => {
+  const col = new Set();
   let w = 0;
   data.forEach((a) => {
-    if (A.has(a)) {
+    if (col.has(a)) {
       data.splice(w, 1);
     } else {
-      A.add(a);
+      col.add(a);
     }
     w++;
   });
   return data;
 };
 
-const result = DISTINCT([1, 2, 1, 3, 1, 4]);
+const result = distinctValues([1, 2, 1, 3, 1, 4]);
 console.log(result);
