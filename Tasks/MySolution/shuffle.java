3f51aadde3b275f4c9d3472a11226e0945b999f7
Oleh <Luibichenko.Oleg@gmail.com>
2022-01-04 12:19:34 +0200

    Added shufleProcess() in main method that works with array

--- Tasks/MySolution/shuffle.java
+++ Tasks/MySolution/shuffle.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class shuffle {
     public static void main(String[] args) {
         Object[] array = { "a", "b", "c", "d", "e" };
+        shuffleProcess(array);
     }
 
     public static void shuffleProcess(Object[] array) {
