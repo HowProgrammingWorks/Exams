0ad11a87a140513da30440556de8be8287abb245
GAlexandrD <o.horbatko.im11@kpi.ua>
2022-01-04 16:01:19 +0200

    eslint

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -6,8 +6,8 @@ const plane = (array) => {
     if (Array.isArray(item)) result.push(...plane(item));
     else result.push(item);
   }
-  return result
-}
+  return result;
+};
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
