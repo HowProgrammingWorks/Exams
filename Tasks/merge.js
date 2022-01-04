86393503cd0e64797d442507f7d60713fb297b8e
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-04 15:21:15 +0200

    fix(merge): rolled back to loops

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,7 +1,21 @@
 'use strict';
 // Merge two dictionaries
 
-const merge = (obj1, obj2) => ({ ...obj1, ...obj2 });
+const merge = (obj1, obj2) => {
+  const result = {};
+  for (const key in obj1) {
+    result[key] = obj1[key];
+  }
+
+  for (const key in obj2) {
+    result[key] = obj2[key];
+  }
+
+  return result;
+};
+
+// 1-line version of function
+// const merge = (obj1, obj2) => ({ ...obj1, ...obj2 });
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
