3bedd49c65d5d3a8c6d16def88d38a52010d85c9
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-01-04 21:35:50 +0200

    2 space indentation was added

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -3,13 +3,13 @@
 // Finde a difference of two dictionaries
 
 const diff = (firstDict, secondDict) => {
-    const diffDict = {};
-    const keys = Object.keys(firstDict);
-    for (const key of keys) {
-        const value = firstDict[key];
-        if (value !== secondDict[key]) diffDict[key] = firstDict[key];
-    }
-    return diffDict;
+  const diffDict = {};
+  const keys = Object.keys(firstDict);
+  for (const key of keys) {
+    const value = firstDict[key];
+    if (value !== secondDict[key]) diffDict[key] = firstDict[key];
+  }
+  return diffDict;
 }
 
 const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
