c69a84e0304b6fe057556132cc5c43d9122e21e2
m1guelten <footballMiguelTen@gmail.com>
2022-01-04 21:22:10 +0200

    виправлення синтаксису

--- Tasks/size.js
+++ Tasks/size.js
@@ -3,11 +3,16 @@
 let size=number=>{
   if (number === 0) return '0';
   const exp = Math.floor(Math.log(number)/Math.log(1000));
-  if (exp==0) return number + ' byte'
-  if(exp==1) return Math.round(number / 1000) + ' kb'
-  if(exp==2) return Math.round(number / 1000000) + ' mb'
-  if(exp==3) return Math.round(number / 1000000000) + ' gb'
+  if(exp===0) return number + ' byte';
+  if(exp===1) return Math.round(number / 1000) + ' kb';
+  if(exp===2) return Math.round(number / 1000000) + ' mb';
+  if(exp===3) return Math.round(number / 1000000000) + ' gb';
 }
 
-const result = size(123456)
-console.log(result)
+//const result = size(123456)
+console.log(size(0));
+console.log(size(123));
+console.log(size(1234));
+console.log(size(12345678));
+console.log(size(123456789123));
+console.log(size(12345678912345));
