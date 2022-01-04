1a1dbb84b9fa795d3a20abb356fbc96045f86f35
EvhenyiAlekseevych <96980103+EvhenyiAlekseevych@users.noreply.github.com>
2022-01-04 11:57:16 +0200

    |sum.js(5)|   delete useless code

--- sum.js
+++ sum.js
@@ -5,13 +5,10 @@ const sum = (s) => {
   {
   let t = typeof i;
   if (t === 'number'){
-    if (sum.length > 0){
-       const new_Sum = sum[sum.length - 1] + i
-        sum.push(new_Sum)
+    
     }
   }
   };
-}
 
 const result = sum([5, true, 'string', 7, 'hello']);
 console.log(result);
