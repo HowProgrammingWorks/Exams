7ab7997245714824ab2fef25aa58acb1fb32a7e1
vadumpast <vadumpast@gmail.com>
2022-01-04 23:55:40 +0200

    removed unnecessary else-operator

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -3,20 +3,19 @@
 Replace = (str, substr, newstr) => {
   if (substr === '') {
     return str;
-  } else {
-    src = str;
-    res = '';
-    do {
-      const _index = src.indexOf(substr);
-      if (_index === -1) {
-        return res + src;
-      } else {
-        const start = src.substring(0, _index);
-        src = src.substring(_index + substr.length, src.length);
-        res += start + newstr;
-      }
-    } while (true);
   }
+  src = str;
+  res = '';
+  do {
+    const _index = src.indexOf(substr);
+    if (_index === -1) {
+      return res + src;
+    } else {
+      const start = src.substring(0, _index);
+      src = src.substring(_index + substr.length, src.length);
+      res += start + newstr;
+    }
+  } while (true);
 }
 
 const result = Replace('Hello <username> and bye!', '<username>', 'Marcus');
