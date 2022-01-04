1be2bb380b68c5642e093465404dc95792655b21
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-04 11:46:01 +0200

    fix(merge): fixed all spaces and indents

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -2,14 +2,16 @@
 // Merge two dictionaries
 
 const merge = (obj1, obj2) => {
-    const result = {};
-   for (const key in obj1) {
-        result[key] = obj1[key];
+  const result = {};
+  for (const key in obj1) {
+    result[key] = obj1[key];
   }
-   for (const key in obj2) {
-     result[key] = obj2[key];
-    }
-return result;
+
+  for (const key in obj2) {
+    result[key] = obj2[key];
+  }
+
+  return result;
 };
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
