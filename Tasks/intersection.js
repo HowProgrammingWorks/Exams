e951f073a2c8375e11ceaa43649b77322066aff7
GddgdgMen <antonsvirskiy690@gmail.com>
2022-01-04 13:51:40 +0200

    Used eslint to update code

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict'
 // Find an intersection of two dictionaries
 
 const intersection = (object1, object2) => {
-  const key = Object.keys(object1);
+  const key = Object.keys(object1)
   for (const name of key) {
-    if (name !== "" && object2[name]) {
-        object2[name] = object1[name];
+    if (name !== '' && object2[name]) {
+      object2[name] = object1[name]
     } else {
-      delete object1[name];
+      delete object1[name]
     }
   }
-  return object1;
-};
-const result = intersection({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
-console.log(result);
+  return object1
+}
+const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' })
+console.log(result)
