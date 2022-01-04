66d7cb6093d37c637bafb441713aeb9ff7bc2568
Misha <mykhailoholoborodko@gmail.com>
2022-01-04 11:44:03 +0200

    use the correct indentation

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,16 +1,16 @@
 // Finde a difference of two dictionaries
-//'use strict';
+'use strict';
 
 const diff = (objectFirst,objectSecond) => {
-    for (const attributeName in objectFirst) {
+  for (const attributeName in objectFirst) {
     objectFirst[attributeName] = objectFirst[attributeName]
-    }{}
-    if (objectFirst in objectSecond) return false
-    for (const attributeName in objectSecond) {
+  }{}
+  if (objectFirst in objectSecond) return false
+  for (const attributeName in objectSecond) {
     objectFirst[attributeName] = objectSecond[attributeName]
     delete objectFirst[attributeName]
-    }{}
-    return objectFirst
+  }{}
+  return objectFirst
 };
 
 const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
