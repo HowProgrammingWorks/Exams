cd6fc4e943f22b08ea3281b399a6d8ebadc76e7d
Anyta8989 <96947496+Anyta8989@users.noreply.github.com>
2022-01-04 22:59:39 +0200

    fixed formatting

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -2,17 +2,17 @@
 'use strict'
 
 const isValid = (name) => {
-if (!name) return false;
-if (name === '') return false;
-if (typeof(name) !== 'string') return false;
-if (name.length === 0) return false;
-if (!name.includes(' ')) return false; {
-  for (char of name) {
-    if (char === ' ') continue;
-    if (char.toLowerCase().charCodeAt(0) >= 97 && char.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
-  };
-  return true;
-};
+    if (!name) return false;
+    if (name === '') return false;
+    if (typeof (name) !== 'string') return false;
+    if (name.length === 0) return false;
+    if (!name.includes(' ')) return false; {
+        for (char of name) {
+            if (char === ' ') continue;
+            if (char.toLowerCase().charCodeAt(0) >= 97 && char.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
+        };
+        return true;
+    };
 };
 
 const result = isValid('Marcus Aurelius');
