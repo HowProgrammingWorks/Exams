e9e728acd63f09dbbc11a96ed2cd41184dd2f48a
Den <denissytnikmacho@gmail.com>
2022-01-04 13:28:36 +0200

    function expression

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,24 +1,22 @@
 // Compare two dictionaries
 
-let compare = (
-  (first_values,...parameters_LIST) => {
+const compareDictionaries = function (first_values,...parameters_LIST) {
 
-    const second_values = parameters_LIST[0]
-    let a = Object.keys(first_values)
-    let b = Object.keys(second_values)
+  const second_values = parameters_LIST[0]
+  let a = Object.keys(first_values)
+  let b = Object.keys(second_values)
 
-    if (a.join('-') !== b.join('-')) return false;
+  if (a.join('-') !== b.join('-')) return false;
 
-    let e = true;
+  let e = true;
 
-    for (c of a) {
-      if (first_values[c] === second_values[c]) e = e && true
-      else { e = e && false }
-    }
-
-    return e;
+  for (c of a) {
+    if (first_values[c] === second_values[c]) e = e && true
+    else { e = e && false }
   }
-)
+
+  return e;
+};
 
 const result = compare({ a: 1, c: 'hello' }, { a: 1, c: 'hello' });
 console.log(result);
