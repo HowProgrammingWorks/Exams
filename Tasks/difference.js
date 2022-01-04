3c4e9557d8cb283385b3fab2952921ca7ebcbb61
unknown <diana.shamota@gmail.comgit config --global user.name>
2022-01-04 14:56:56 +0200

    renamed identifieres

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,12 +2,12 @@
 
 // Finde a difference of two dictionaries
 
-const diff = (object_1, object_2) => {
-  for (attribute_name in object_2) {
-    delete object_1[attribute_name];
+const difference = (object1, object2) => {
+  for (key in object2) {
+    delete object1[key];
   }
-  return object_1;
+  return object1;
 };
 
-const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
+const result = difference({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
