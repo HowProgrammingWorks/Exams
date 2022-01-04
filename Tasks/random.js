a7e8a88aacf2d30bdb50f23ef63379a20ba7a94e
TangiresH <dimon4ik1010@gmail.com>
2022-01-04 21:54:22 +0200

    reworked the test on 'undefined'

--- Tasks/random.js
+++ Tasks/random.js
@@ -5,9 +5,12 @@ const Random = (min, max) => {
     if (max === undefined){
         max = min;
         return Math.floor(Math.random() * (max + 1));
-    } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+    }
+    if (max === undefined && min === undefined) {
+        return 'Data entered incorrectly.';
+    }
+    const random = min + Math.floor(Math.random() * (max - min + 1));
+    return Math.floor(random);
 };
 
 const result = Random(1, 9);
