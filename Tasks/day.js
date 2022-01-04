4ae45281cd165dd3b2a05ed349255b7ec3327cd1
SergiiTkachuk <sergiytkachuk29@gmail.com>
2022-01-04 23:28:25 +0200

    changed let to const and moved the array to the end

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,11 @@
 // Get day number
 'use strict'
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
 
 _parse_day_=s=>{
   let i; for(i=0;i<D.length;i++){
   if(s.startsWith(D[i].toLowerCase())){return i +1};};return -1;
 };
-
+const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 const result = _parse_day_('friday')
 console.log(result)
