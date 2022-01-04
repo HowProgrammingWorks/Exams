e21c16d15f16826fc8c3c32419c4fcf5876c8301
Oleh <Luibichenko.Oleg@gmail.com>
2022-01-04 11:50:52 +0200

    Added in main method output in console. Also added random.class

--- Tasks/MySolution/random.java
+++ Tasks/MySolution/random.java
@@ -2,7 +2,8 @@ package Tasks.MySolution;
 
 public class random {
     public static void main(String[] args) {
-        int result = randomProcess(1,9);
+        int result = randomProcess(1, 9);
+        System.out.println(result);
     }
 
     public static int randomProcess(int min, int max) {
