819ec99e784842671b3e8464e4572676498c2e3c
jugular237 <oleksa032003@gmail.com>
2022-01-04 04:14:59 +0200

    Replaced object type of parameters with generics

--- tasks1(difference)/Program1.cs
+++ tasks1(difference)/Program1.cs
@@ -1,14 +1,14 @@
-﻿    Funct <Dictionary<object, object>, Dictionary<object, object>, Dictionary<object, object>> Difference =
-        (Dictionary<object, object> dict1, Dictionary<object, object> dict2) =>
+﻿    Dictionary<Tvalue1, Tvalue2> Difference<Tvalue1, Tvalue2>
+        (Dictionary<Tvalue1, Tvalue2> dict1, Dictionary<Tvalue1, Tvalue2> dict2)
     {
-        foreach (object dictKey in dict2.Keys)
+        foreach (Tvalue1 dictKey in dict2.Keys)
             dict1.Remove(dictKey);
         return dict1;
-    };
+    }
 
-    Dictionary<object, object> result = Difference
-        (new Dictionary<object, object> { { 'a', "uno" }, { 'b', "due"} }, 
-        new Dictionary<object,object>{ { 'a', "uno" }, { 'c', "tre" } });
-    foreach (object key in result.Keys)
+    Dictionary<char, string> result = Difference<char, string>
+        (new Dictionary<char, string> { { 'a', "uno" }, { 'b', "due" } }, 
+        new Dictionary<char, string>{ { 'a', "uno" }, { 'c', "tre" } });
+    foreach (char key in result.Keys)
         Console.Write($"{key}:{result[key]} ");
     Console.ReadKey();
