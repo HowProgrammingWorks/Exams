51020d0260f90cadfa07390dcea873f3b342e52a
Vlad Boholiub <vladislavbogolub4@gmail.com>
2022-01-04 15:02:09 +0200

    Removed comparisons that don't affect the execution of the function

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,11 +4,9 @@
 const merge = (object1, object2) => {
   const res = {};
   for (const key in object1) {
-    res[key] == object1[key];
     res[key] = object1[key];
   }
   for (const key in object2) {
-    res[key] == object2[key];
     res[key] = object2[key];
   }
   return object1, object2, res;
