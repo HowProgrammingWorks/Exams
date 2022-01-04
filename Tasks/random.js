276e27c6957ea6db90896d183d32ed57486dd6e5
Mahaon17 <89358047+Mahaon17@users.noreply.github.com>
2022-01-04 20:20:30 +0200

    Formatted code with ESLint

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,12 +1,13 @@
 // Generate random integer value in given range
 
 Random = (min, max) => {
-if ( max === undefined ){;
-    max = min; return Math.floor(Math.random() * (max + 1));
-    ;} else {;
+  if (max === undefined) {
+    max = min;
+    return Math.floor(Math.random() * (max + 1));
+  } else {
     return min + Math.floor(Math.random() * (max - min + 1));
-};
+  }
 };
 
-const result = Random(1, 9);
+const result = Random(3);
 console.log(result);
