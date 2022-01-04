63db4d184fed498652328fa23b95abe4ca238b51
yarikkot04 <yarikkotenkoim13@gmail.com>
2022-01-04 16:20:51 +0200

    put a literal array

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,7 +4,7 @@
 // eslint-disable-next-line arrow-parens
 const parseDay = (searchDay) => {
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const toLowerCaseDay = new Array();
+  const toLowerCaseDay = [];
   for (let i = 0; i < days.length; i++) {
     toLowerCaseDay[i] = days[i].toLowerCase();
     if (searchDay.startsWith(toLowerCaseDay[i])) return i + 1;
