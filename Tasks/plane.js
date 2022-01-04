9e6ed4e4eaaa06acaee7b0c5c94ce18a4f7d5fe5
MytsV <mytsvictoria@gmail.com>
2022-01-04 16:50:14 +0200

    Improved readability of plane.js, included brackets for single argument lambda functions

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,7 +1,12 @@
 'use strict';
 // Make nested array plane
 
-const plane = arr => arr.flatMap(value => (Array.isArray(value) ? plane(value) : value));
+const plane = (arr) => arr.flatMap((value) => {
+  if (Array.isArray(value)) {
+    return plane(value);
+  }
+  return value;
+});
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
