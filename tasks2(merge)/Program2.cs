36605e154112c42ab46e44b8de6927e629457433
jugular237 <oleksa032003@gmail.com>
2022-01-04 14:36:57 +0200

    Deleted extra spaces and rows.Splited long rows

--- tasks2(merge)/Program2.cs
+++ tasks2(merge)/Program2.cs
@@ -1,21 +1,21 @@
-﻿Dictionary<object, object> merge_two_objects(Dictionary<object, object> object_1, Dictionary<object, object> object_2)
-            {
-                Dictionary<object, object> object_3 = new Dictionary<object,object>();
+﻿Dictionary<object, object> merge_two_objects
+    (Dictionary<object, object> object_1, Dictionary<object, object> object_2)
+    {
+        Dictionary<object, object> object_3 = new Dictionary<object,object>();
+        foreach (object attribute_name in object_1.Keys)
+        {
+            object_3[attribute_name] = object_1[attribute_name];
+        }
+        foreach (object attribute_name in object_2.Keys)
+        {
+            object_3[attribute_name] = object_2[attribute_name];
+        }
+        return object_3;
+    }
 
-                foreach (object attribute_name in object_1.Keys)
-     {
-
-                    object_3[attribute_name] = object_1[attribute_name];
-                }
-                foreach (object attribute_name in object_2.Keys)
-                {
-
-                    object_3[attribute_name] = object_2[attribute_name];
-            }
-                return object_3;
-            }
-
-            var result = merge_two_objects(new Dictionary<object,object>() { { 'a', "uno" }, { 'b', "due" } }, new Dictionary<object, object> { { 'c', "tre" } });
-            foreach (char a in result.Keys)
-                Console.Write($"{a}:{result[a]} ");
-            Console.ReadKey();
+var result = merge_two_objects
+(new Dictionary<object,object>() { { 'a', "uno" }, { 'b', "due" } }, 
+new Dictionary<object, object> { { 'c', "tre" } });
+foreach (char a in result.Keys)
+    Console.Write($"{a}:{result[a]} ");
+Console.ReadKey();
