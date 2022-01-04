aa41bd5bf6ca5deb7c117cfd153e1823483cdfb4
Misha <mykhailoholoborodko@gmail.com>
2022-01-04 10:44:28 +0200

    delete unnecessary line and add substring

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,15 +2,15 @@
 'use strict';
 
 const getValueBetween = (string, prefix, suffix) => {
+  const substring = '';
   const prefixIndex = string.indexOf(prefix);
-  if (prefixIndex === -1) return '';
+  if (prefixIndex === -1) return substring;
   else {
-    const nextStringIndex = prefixIndex + prefix.length;
-    string =  string.substring(nextStringIndex);
+    string = string.substring(prefixIndex + 1);
     if (suffix) {
       const suffixIndex = string.indexOf(suffix);
       if (suffixIndex  === -1) {
-        return '';
+        return substring;
       } else {
         string =  string.substring(0, suffixIndex);
       }
