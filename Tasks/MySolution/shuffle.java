dcf56cfad5657993e945268b584bca574ef13c5c
Oleh <Luibichenko.Oleg@gmail.com>
2022-01-04 12:17:42 +0200

    Added Object[] array in main method

--- Tasks/MySolution/shuffle.java
+++ Tasks/MySolution/shuffle.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class shuffle {
     public static void main(String[] args) {
-
+        Object[] array = { "a", "b", "c", "d", "e" };
     }
 
     public static void shuffleProcess(Object[] array) {
