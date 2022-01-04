694218cfd531e518a6088cbcce4708dc39e6852f
Oleh <Luibichenko.Oleg@gmail.com>
2022-01-04 12:21:02 +0200

    Added String arrayString which contains our shuffle array in normal way for output

--- Tasks/MySolution/shuffle.java
+++ Tasks/MySolution/shuffle.java
@@ -6,6 +6,7 @@ public class shuffle {
     public static void main(String[] args) {
         Object[] array = { "a", "b", "c", "d", "e" };
         shuffleProcess(array);
+        System.out.println(array);
     }
 
     public static void shuffleProcess(Object[] array) {
