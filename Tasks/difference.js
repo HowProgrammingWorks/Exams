1bc113e87111ebd459164d9791398f64b09f5ea4
Den <denissytnikmacho@gmail.com>
2022-01-04 14:00:44 +0200

    сделал внутренние переменные функции

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,13 +1,16 @@
 // Finde a difference of two dictionaries
 'use strict';
 
-const findDifference = function( obj1,obj2 ) {
+const findDifference = function( object1,object2 ) {
+    let obj1 = object1;
+    let obj2 = object2;
+
     for (attribute_name in obj2) {
         obj1[attribute_name] = obj2[attribute_name]
         delete obj1[attribute_name]
     }{}
 
-    return obj1
+    return obj1;
 };
 
 const result = findDifference({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
