71f81cb344b15a0bf1627ba6292eb1b7c77eb570
Oleh <Luibichenko.Oleg@gmail.com>
2022-01-04 11:48:22 +0200

    Changed main method. Added int result which contains randomProcess result in diffferent ranges

--- Tasks/MySolution/random.java
+++ Tasks/MySolution/random.java
@@ -2,7 +2,7 @@ package Tasks.MySolution;
 
 public class random {
     public static void main(String[] args) {
-
+        int result = randomProcess(1,9);
     }
 
     public static int randomProcess(int min, int max) {
