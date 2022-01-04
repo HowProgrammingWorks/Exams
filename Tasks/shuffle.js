6a46f83b045f9916be53d23bc06c4d5bf0bfca7a
Daria Petruk <90788327+Mikkador@users.noreply.github.com>
2022-01-04 20:12:28 +0200

    addind semicolons and making a single argument

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,10 +1,9 @@
 // Shuffle an array
 'use strict';
-let Shuffle = ( ...List) => {
-  const [arr] = List;
+let Shuffle = (arr) => {
   arr.sort(() => Math.random() - 0.5); 
-  return arr 
-
+  return arr;
+};
 
 const result = Shuffle(['a', 'b', 'c', 'd', 'e']);
 console.log(result);
