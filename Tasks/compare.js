21c2865150895033a458d3c401b588d0e54a06b1
Den <denissytnikmacho@gmail.com>
2022-01-04 13:26:00 +0200

    сделал код более читабельным сделав отступы

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,17 +1,22 @@
 // Compare two dictionaries
 
-let compare=(
+let compare = (
   (first_values,...parameters_LIST) => {
-   const second_values = parameters_LIST[0]
-   let a = Object.keys(first_values)
-   let b = Object.keys(second_values)
-   if (a.join('-') !== b.join('-')) return false;
-   let e = true;
-   for (c of a) {
-    if (first_values[c] === second_values[c]) e = e && true
-   else { e = e && false }
-   }
-  return e;
+
+    const second_values = parameters_LIST[0]
+    let a = Object.keys(first_values)
+    let b = Object.keys(second_values)
+
+    if (a.join('-') !== b.join('-')) return false;
+
+    let e = true;
+
+    for (c of a) {
+      if (first_values[c] === second_values[c]) e = e && true
+      else { e = e && false }
+    }
+
+    return e;
   }
 )
 
