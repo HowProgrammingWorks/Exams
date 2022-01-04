8b1fb5e70b444c5c7dbc0547daf005716393b9fb
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-04 23:18:13 +0200

    organize the code for better look

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,15 +3,19 @@
 
 getvaluebetween = (str, p, s) => {
   i = str.indexOf(p)
-    if ( i === -1 ) return ''
-    else {
-      k = i + p.length
-      str =  str.substring(k)
-      if ( s) {
-        i = str.indexOf( s )
-        if (i === -1) {
-          return  '' } else { str =  str.substring(0 , i)
-            }}}
+  if (i === -1) return ''
+  else {
+    k = i + p.length
+    str = str.substring(k)
+    if (s) {
+      i = str.indexOf(s)
+      if (i === -1) {
+        return ''
+      } else {
+        str = str.substring(0, i)
+      }
+    }
+  }
   return str
 }
 
