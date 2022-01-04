2565dc747e8e108d56d79136fb143c96d0f920da
jugular237 <oleksa032003@gmail.com>
2022-01-04 03:47:32 +0200

    Removed extra spaces, rows.Splited long row

--- tasks1(difference)/Program1.cs
+++ tasks1(difference)/Program1.cs
@@ -1,20 +1,19 @@
-﻿Funct <Dictionary<object, object>, Dictionary<object, object>, Dictionary<object, object>> d =
-                (Dictionary<object, object> object_1, Dictionary<object, object> object_2) =>
-{
-               
-                { }
+﻿    Funct <Dictionary<object, object>, Dictionary<object, object>, Dictionary<object, object>> d =
+        (Dictionary<object, object> object_1, Dictionary<object, object> object_2) =>
+    {
+        { }
+        foreach (object attribute_name in object_2.Keys)
+        {
+            object_1[attribute_name] = object_2[attribute_name];
+            object_1.Remove(attribute_name);
+        }
+        { }
+        return object_1;
+    };
 
-            foreach (object attribute_name in object_2.Keys)
-                {
-                    object_1[attribute_name] = object_2[attribute_name];
-                    object_1.Remove(attribute_name);
-                }
-                { }
-                return object_1;
-        };
-
-            Dictionary<object, object> result = d
-                    (new Dictionary<object, object> { { 'a', "uno" }, { 'b', "due"} }, new Dictionary<object,object>{ { 'a', "uno" }, { 'c', "tre" } });
-            foreach (object k in result.Keys)
-                Console.Write($"{k}:{result[k]} ");
-            Console.ReadKey();
+    Dictionary<object, object> result = d
+        (new Dictionary<object, object> { { 'a', "uno" }, { 'b', "due"} }, 
+        new Dictionary<object,object>{ { 'a', "uno" }, { 'c', "tre" } });
+    foreach (object k in result.Keys)
+        Console.Write($"{k}:{result[k]} ");
+    Console.ReadKey();
