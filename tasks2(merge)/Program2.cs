9e2ea2e31c7f5b8586d9fd02808ee96c94640788
jugular237 <oleksa032003@gmail.com>
2022-01-04 14:56:30 +0200

    Deleted braces for one row loops, replaced var

--- tasks2(merge)/Program2.cs
+++ tasks2(merge)/Program2.cs
@@ -3,17 +3,13 @@
     {
         Dictionary<object, object> dictionary3 = new Dictionary<object,object>();
         foreach (object key in dictionary1.Keys)
-        {
             dictionary3[key] = dictionary1[key];
-        }
         foreach (object key in dictionary2.Keys)
-        {
             dictionary3[key] = dictionary2[key];
-        }
         return dictionary3;
     }
 
-var result = MergeTwoDictionaries
+Dictionary<object, object> result = MergeTwoDictionaries
 (new Dictionary<object,object>() { { 'a', "uno" }, { 'b', "due" } }, 
 new Dictionary<object, object> { { 'c', "tre" } });
 foreach (object resultKey in result.Keys)
