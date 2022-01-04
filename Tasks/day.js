8de6fd975084b6819629c17ade4f6c248cc33410
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-04 22:25:45 +0200

    renaming s into day for more understanding

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,9 +2,9 @@
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-const parser = s => {
+const parser = day => {
   for (let i = 0; i < days.length; i++) {
-    if (s.startsWith(days[i].toLowerCase())) {
+    if (day.startsWith(days[i].toLowerCase())) {
       return i + 1
     };
   };
