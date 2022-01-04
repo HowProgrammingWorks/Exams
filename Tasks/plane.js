279f78419f8e947b2a14eac428bc24a81803d21d
GAlexandrD <o.horbatko.im11@kpi.ua>
2022-01-04 15:52:28 +0200

    delete unnecessary variables and assignment operations

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,15 +1,11 @@
 // Make nested array plane
 
 const const_plane = function (arr, res = []) {
-  let j = 0
   for (let i = 0, length = arr.length; i < length; i++) {
     let value = arr[i];
-    let j = i
     if (Array.isArray(value, typeof value) && [i, length]) {
       res.push(...const_plane(value));
-      arr[i] = res[i - 1]
     } else {
-      arr[i] = res[j - 1]
       res.push(value);
     }
   }
