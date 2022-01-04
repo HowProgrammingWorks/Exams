50660f853397914867aa4ac90ed72247ac179c31
GAlexandrD <o.horbatko.im11@kpi.ua>
2022-01-04 10:34:12 +0200

    formating code using eslint

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const plane = (arr) => {
-    const result = [];
-    for (let item of arr) {
-        if (Array.isArray(item)) result.push(...plane(item));
-        else result.push(item);
-    }
-    return result
-}
+  const result = [];
+  for (const item of arr) {
+    if (Array.isArray(item)) result.push(...plane(item));
+    else result.push(item);
+  }
+  return result;
+};
 
 const result = plane([1, [[2]], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
