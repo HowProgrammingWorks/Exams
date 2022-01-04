01ba85b512b1f1dc5cf5ade75b09d59f54eff88a
NikitaP0203 <nvnworkf01@gmail.com>
2022-01-05 01:49:15 +0200

    Change code style

--- Solutions/intersection.js
+++ Solutions/intersection.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const intersection = (firstDictionary,secondDictionary) => {
-const firstKeys = Object.keys(firstDictionary);
-for (let key of firstKeys)
-if ( !(key in secondDictionary) || firstDictionary[key] !== secondDictionary[key]) {
-delete firstDictionary[key];                                                                         
-}               
-return firstDictionary;
+const intersection = (firstDictionary, secondDictionary) => {
+    const firstKeys = Object.keys(firstDictionary);
+    for (let key of firstKeys) {
+        if ( !(key in secondDictionary) || firstDictionary[key] !== secondDictionary[key]) {
+            delete firstDictionary[key];                                                                         
+        }  
+    }             
+    return firstDictionary;
 }
 
 const result = intersection({ a: 'uno', b: 'due', c: 'tre'}, { a: 'uno', c: 'tre' });
