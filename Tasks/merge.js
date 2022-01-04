8cad10ab9da9a962082fad652394c9f73ada3a9c
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-01-04 20:29:50 +0200

    replacing spread syntax with a for...of

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,7 +1,18 @@
 'use strict';
 // Merge two dictionaries
 
-const mergeTwoObjects = (object1, object2) => ({ ...object1, ...object2 });
+const mergeTwoObjects = (object1, object2) => {
+  const mergedObject = {};
+  const key1 = Object.keys(object1);
+  const key2 = Object.keys(object2);
+  for (const key of key1) {
+    mergedObject[key] = object1[key];
+  }
+  for (const key of key2) {
+    mergedObject[key] = object2[key];
+  }
+  return mergedObject;
+};
 
 const result = mergeTwoObjects({ a: 'uno', b: 'due' }, { c: 'tre' });
 console.log(result);
