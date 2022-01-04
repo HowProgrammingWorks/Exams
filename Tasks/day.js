e020fc6b82df27a17667ab4fca61507cdcc54798
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-04 22:29:39 +0200

    changing the returns for a better understanding

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,10 +5,10 @@ const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 function parser(day) {
   for (let i = 0; i < days.length; i++) {
     if (day.startsWith(days[i].toLowerCase())) {
-      return i + 1
+      return `match found: ${i+1}`
     };
   };
-  return -1;
+  return `no match: ${-1}`;
 };
 
 const result = parser('friday')
