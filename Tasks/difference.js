5ea98690699c21a638cb0fa8ef8e45fdd8f5052d
GddgdgMen <antonsvirskiy690@gmail.com>
2022-01-04 13:51:45 +0200

    Used eslint to update code

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict'
 // Find a difference of two dictionaries
 
 const diff = (object1, object2) => {
   const keys2 = Object.keys(object2)
   for (const key of keys2) {
-    delete object1[key];
+    delete object1[key]
   }
-  return object1;
+  return object1
 }
-const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
-console.log(result);
+const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' })
+console.log(result)
