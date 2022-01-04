f85dea3f705f13a7fe38122d0720bd7410941d78
nikitaq121 <97105666+nikitaq121@users.noreply.github.com>
2022-01-04 16:25:51 +0200

    formated

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,26 +1,26 @@
 // Count words
 'use strict';
- Words=function(s) {
- number_of_words_in_s = 0
- flag = false
- for (c of s) {
-   if (!flag) {
-   if (c === ' ') {
-   if (flag === true) { flag = false } else { flag = false }
+Words = function (s) {
+  number_of_words_in_s = 0
+  flag = false
+  for (c of s) {
+    if (!flag) {
+      if (c === ' ') {
+        if (flag === true) { flag = false } else { flag = false }
+      } else {
+        if (flag === true) { flag = true } else { flag = true }
+        number_of_words_in_s++
+      }
     } else {
-   if (flag === true) { flag = true } else { flag = true }
-      number_of_words_in_s++
+      if (c === ' ') {
+        if (flag === true) { flag = false } else { flag = false }
+      } else {
+        if (flag === true) { flag = true } else { flag = true }
+      }
     }
-   } else {
-    if (c === ' ') {
-      if (flag === true) { flag = false } else { flag = false }
-    } else {
-      if (flag === true) { flag = true } else { flag = true }
-    }
-   }
- }
- return number_of_words_in_s
- }
+  }
+  return number_of_words_in_s
+}
 
 const result = Words('Hello Marcus Aureluis');
 console.log(result);
