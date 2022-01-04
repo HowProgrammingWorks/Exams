5df501dc9eba4161ad48da3af625cb64f09ea403
Masha Martyniuk <mmartyniuk004@gmail.com>
2022-01-04 18:19:39 +0200

    Update validate.js
    delete the unnecessary if condition

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -5,7 +5,6 @@ const Valid = (name) => {
 if (name === '') return false;
 if (typeof name !== 'string') return false;
 for (const words of name) {
-    if (words === ' ') continue;
     const a = 'a'.charCodeAt(0);
     const z = 'z'.charCodeAt(0);
     if ( words.toLowerCase().charCodeAt(0) < a || words.toLowerCase().charCodeAt(0) > z) {
