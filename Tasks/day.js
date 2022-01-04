89473a6c63d559ae38ca1a7bf85536c916fa28e2
m1guelten <footballMiguelTen@gmail.com>
2022-01-04 22:07:31 +0200

    виправляю синтаксис

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,11 +2,15 @@
 
 // Get day number
 
-let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const dayNumber = day =>{
-  let i; for(i=0;i<week.length;i++){
-  if(day.startsWith(week[i].toLowerCase())){return i +1};};return -1;
+  for(let i=0; i<week.length; i++){
+    if(day.startsWith(week[i].toLowerCase())){
+      return ++i;
+    };
+  };
+  return -1;
 };
 
 const result = dayNumber('friday')
