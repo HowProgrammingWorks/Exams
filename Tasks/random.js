01fd5ce9a9e4224d9d87b8d018f3dba3ff7a720a
TangiresH <dimon4ik1010@gmail.com>
2022-01-04 21:38:07 +0200

    made the code more readable, fixed semicolons

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,11 +2,12 @@
 'use strict';
 
 const Random = (min, max) => {
-if ( max === undefined ){;
-    max = min; return Math.floor(Math.random() * (max + 1));
-    ;} else {;
+    if (max === undefined){
+        max = min;
+        return Math.floor(Math.random() * (max + 1));
+    } else {
     return min + Math.floor(Math.random() * (max - min + 1));
-};
+  }
 };
 
 const result = Random(1, 9);
