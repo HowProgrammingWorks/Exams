c80f725a49a4eed19d2ef5c63b9fd81757860445
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-01-04 14:57:44 +0200

    deleted unneeded else's

--- Kotlin/src/replace.kt
+++ Kotlin/src/replace.kt
@@ -1,23 +1,21 @@
 fun replace( string: String, subString: String, newString: String): String{
     if(subString == "") return string
-    else{
-        var stringElse = string
-        var resultElse = ""
-        do{
-            val index = stringElse.indexOf(subString)
-            if(index == -1){
-                resultElse += stringElse
-                return resultElse
-            }
-            else{
-                val subStringElse = stringElse.substring(0,index)
-                val indexSubString = index + subString.length
-                stringElse = stringElse.substring(indexSubString,stringElse.length)
-                resultElse += subStringElse + newString
-            }
-        }while (true)
-    }
+    var stringElse = string
+    var resultElse = ""
+    do{
+        val index = stringElse.indexOf(subString)
+        if(index == -1){
+            resultElse += stringElse
+            return resultElse
+        }
+        val subStringElse = stringElse.substring(0,index)
+        val indexSubString = index + subString.length
+        stringElse = stringElse.substring(indexSubString,stringElse.length)
+        resultElse += subStringElse + newString
+
+    }while (true)
 }
+
 fun main(){
     val result = replace("Hello <username> and bye!", "<username>", "Marcus")
     println(result)
