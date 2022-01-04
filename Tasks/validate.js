953eb1b385aeb7c41f781dd3098b8ff3dc4dcbe5
neroheroes6@gmail.com <neroheroes6@gmail.com>
2022-01-04 14:25:46 +0200

    Revert "Added 'for' which will check if other letters in a word are lowercase"
    This reverts commit 79dc221b9242972a6690e8811e03f1be2abd20e1.

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -7,10 +7,6 @@ const validName = (name) => {
       for (let word of words) {
       let charCode = word.charCodeAt(0);
       if (charCode < 65 || charCode > 90) return false;
-      for (let index = 1; index < word.length; index++) {
-        charCode = word.charCodeAt(index);
-        if (charCode < 97 || charCode > 122) return false;
-      }
     }
     return true;
   }
