91b69d8f035e60fffa82be5c4028a667256a846e
Daria Petruk <90788327+Mikkador@users.noreply.github.com>
2022-01-04 16:45:00 +0200

    renaming and adding ";" , " "

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,10 +1,10 @@
 // Find key by value
 'use strict';
-const find=(object, rest) => {
+const find = (object, value) => {
    for (let name in object) {
-      if (object[name]) == rest)  return name;
+      if (object[name]) == value)  return name;
    }
-}
+};
 
 const result = find({ a: 1, b: 2, c: 'hello', d: false }, 'hello');
 console.log(result);
