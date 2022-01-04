7149bbb5210f9c26ae595ebef37e1f0e38a55793
GddgdgMen <antonsvirskiy690@gmail.com>
2022-01-04 13:11:57 +0200

    Changed for... in to for... of

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,7 +2,8 @@
 // Find a difference of two dictionaries
 
 const diff = (object1, object2) => {
-  for (const key in object2) {
+  const keys2 = Object.keys(object2)
+  for (const key of keys2) {
     delete object1[key];
   }
   return object1;
