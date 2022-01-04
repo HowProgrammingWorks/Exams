939f296d1d2cd53b5ffabd17846b13cf3a996da5
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-04 22:17:14 +0200

    renaming the days array

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,11 @@
 // Get day number
 
-const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
 const parser = s => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < days.length; i++) {
+    if (s.startsWith(days[i].toLowerCase())) {
       return i + 1
     };
   };
