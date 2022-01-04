30d318c34df66e16a50f2aa53fd967fee7ede9fc
GAlexandrD <o.horbatko.im11@kpi.ua>
2022-01-04 15:37:39 +0200

    fix  eslint errors

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,18 +1,18 @@
-// Deduplicate values in array
+'use strict';
 
-const distinct = array => {
+const distinct = (array) => {
   const unicalElements = new Set();
   let index = 0;
   array.forEach((a) => {
     if (unicalElements.has(a)) {
-      array.splice(index, 1)
+      array.splice(index, 1);
     } else {
-      unicalElements.add(a)
+      unicalElements.add(a);
     }
     index++;
   });
   return array;
-}
+};
 
 const result = distinct([1, 2, 1, 3, 1, 4]);
 console.log(result);
