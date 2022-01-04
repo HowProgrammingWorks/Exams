77ca2ab8efadb20e2cd01d194c20cd7ffb597e27
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-04 10:46:11 +0200

    fix(merge): renamed 'object_3' to 'result'

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -2,18 +2,18 @@
 // Merge two dictionaries
 
 const merge = (obj1, obj2) => {
-    const object_3 = {};
+    const result = {};
    for (attribute_name in obj1) {
        (
-          object_3[attribute_name] == obj1[attribute_name]
+          result[attribute_name] == obj1[attribute_name]
        );
-        object_3[attribute_name] = obj1[attribute_name]
+        result[attribute_name] = obj1[attribute_name]
   }
    for (attribute_name in obj2) {
-      (object_3[attribute_name] == obj2[attribute_name])
-     object_3[attribute_name] = obj2[attribute_name]
+      (result[attribute_name] == obj2[attribute_name])
+     result[attribute_name] = obj2[attribute_name]
     }
-return obj1, obj2, object_3;
+return obj1, obj2, result;
 }
 
 const result = merge({ a: 'uno', b: 'due' }, { c: 'tre' });
