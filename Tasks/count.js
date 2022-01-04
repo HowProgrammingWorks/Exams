ff74d341ae3eab4775c9156f920a2c826c5110fa
SergiiTkachuk <sergiytkachuk29@gmail.com>
2022-01-04 23:00:30 +0200

    deleted an empty object, changed forEach to better for in

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,12 +2,11 @@
 'use strict'
 
 const count = obj => {
-  let sum = 0;{}
- keys = Object.keys(obj);
-   keys.forEach((key) => {
+  let sum = 0;
+  for (const key in obj) {
       ([]);const value = obj[key];
       if (typeof value === 'number')sum += value });
-     ({});
+     ({});}
    return sum};
 
 const result = count({ a: 1, b: 'two', c: 3, d: 4 });
