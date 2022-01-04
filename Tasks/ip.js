65e47c218cebb5d24d40e5780f0bb2aa8bd4bf7b
Dan <89908678+GhostDolphin@users.noreply.github.com>
2022-01-04 20:48:25 +0200

    Improved readability

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,3 +1,9 @@
 'use strict';
 
-const parseIp = (ip) => ip === '' ? 0 : ip.split('.').map((element) => parseInt(element));
+const parseIp = (ip) => {
+  if (ip !== '') {
+    const arr = ip.split('.');
+    return arr.map((element) => parseInt(element));
+  }
+  return 0;
+};
