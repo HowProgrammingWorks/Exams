b27314d7d8381eb6569ab86366c643eae94c74f3
Andriyaschenko <vovan4ik041@gmail.com>
2022-01-05 13:11:24 +0200

    Changed mechanism and added difference from second object in result

--- Tasks/difference.py
+++ Tasks/difference.py
@@ -1,9 +1,10 @@
-def difference(FirtsObject, SecondObject):
-    if FirtsObject == SecondObject:
+def difference(FirstObject, SecondObject):
+    if FirstObject == SecondObject:
         return False
-    for element in SecondObject:
-        FirtsObject[element] = SecondObject[element]
-        del FirtsObject[element]
-    return FirtsObject
+    FirstDiff = FirstObject.keys() - SecondObject.keys()
+    FirstDiffResult = {k: FirstObject[k] for k in FirstDiff}
+    SecondDiff = SecondObject.keys() - FirstObject.keys()
+    SecondDiffResult = {k: SecondObject[k] for k in SecondDiff}
+    return FirstDiffResult, SecondDiffResult
 result = difference({ 'a': 'uno', 'b': 'due' }, { 'a': 'uno', 'c': 'tre' })
 print(result)
