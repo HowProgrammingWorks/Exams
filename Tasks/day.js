2aea8dda0ab3f2a18a8d91f1fd1a9dc191fb4e6c
Mikhailo Moliakov <mikhailo.moliakov@gmail.com>
2022-01-05 14:04:41 +0200

    Moved days out of function

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,8 +1,8 @@
 // Get day number
 'use strict';
 
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const getDayNumber = function(text) {
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   for (let i = 0; i < days.length; i++) {
     if (text !== null && text.toLowerCase().startsWith(days[i].toLowerCase())) {
       return i + 1;
