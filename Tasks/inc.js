1edc31060b20f5745ecd8d8ac4400e221c114161
VladimirDan <50290969+VladimirDan@users.noreply.github.com>
2022-01-05 13:39:56 +0200

    Update inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -3,14 +3,14 @@
 "use strict"
 
 const inc_numbers = (format_complete) => {
-      const keys = Object.keys(format_complete);
-   	for (let key of keys) {
-      	let propType = typeof format_complete[key];
-      	if (propType === "number"){
-      		format_complete[key]++;
-      	}
-  	 }
-  	 return format_complete;
+  const keys = Object.keys(format_complete);
+    for (let key of keys) {
+      let propType = typeof format_complete[key];
+      if (propType === "number"){
+        format_complete[key]++;
+      }
+    }
+    return format_complete;
 };
 
 
