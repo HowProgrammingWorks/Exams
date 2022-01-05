9649cbc42baedb6186d81b80c20a4d95ac1c493d
Andriyaschenko <vovan4ik041@gmail.com>
2022-01-05 12:19:35 +0200

    added tranlated files from js to python

---
+++ Tasks/difference.py
@@ -0,0 +1,11 @@
+def diff(object_1,object_2):
+    for attribute_name in object_1:
+        object_1[attribute_name] = object_1[attribute_name]
+    if object_1 == object_2:
+        return False
+    for attribute_name in object_2:
+        object_1[attribute_name]=object_2[attribute_name]
+        del object_1[attribute_name]
+    return object_1
+result = diff({'a':'uno', 'b':'due'},{'a':'uno', 'c':'tre'})
+print(result)
---
+++ Tasks/merge.py
@@ -0,0 +1,9 @@
+def merge_two_objects(object_1,object_2):
+    object_3=[{}][0]
+    for attribute_name in object_1:
+        object_3[attribute_name]=object_1[attribute_name]
+    for attribute_name in object_2:
+        object_3[attribute_name]=object_2[attribute_name]
+    return object_1,object_2,object_3
+result=merge_two_objects({'a':'uno','b':'due'},{'c':'tre'})
+print(result)
