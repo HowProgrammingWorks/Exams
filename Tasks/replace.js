9c6b206bd298f34c899f061ff8a6439062ef52b4
vadumpast <vadumpast@gmail.com>
2022-01-05 09:30:11 +0200

    cleaned up the code

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -3,11 +3,7 @@
 
 
 const replace = (str, subStr, newStr) => {
-  if (subStr === '') {
-    return str;
-  }
-  let res = str.split(subStr);
-  return res.join(newStr);
+  return str.split(subStr).join(newStr);
 }
 
 const result = replace('Hello <username> and bye!', '<username>', 'Marcus');
