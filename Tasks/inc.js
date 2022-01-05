c5913567661fde16972911ec5ab96bddf4aef384
VladimirDan <50290969+VladimirDan@users.noreply.github.com>
2022-01-05 14:01:35 +0200

    Update inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -5,12 +5,12 @@
 const inc_numbers = (format_complete) => {
   const keys = Object.keys(format_complete);
     for (let key of keys) {
-      if (typeof format_complete[key] === "number"){
+      if (typeof format_complete[key] === "number") {
         format_complete[key]++;
       }
     }
     return format_complete;
-};
+}
 
 
 const result = inc_numbers({ a: 1, b: 2, c: 'hello', d: false });
