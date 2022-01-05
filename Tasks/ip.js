b10c6361c9f2907ad422cd67e7e7646a6ab18d4d
oneOdin <97091092+hryshniakovNazar@users.noreply.github.com>
2022-01-05 05:27:43 +0200

    IpCommit 2

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -3,17 +3,18 @@
 'use strict';
 
 Parseip = i => {
-  a = []
-    if (i === '' ) return 0  ;
+  a = [];
+  if (i === '') return 0;
   else {
-  B = i.split('.');
-  j = 0
-    for ( const b of B) {
-  a[j] = parseInt(b);
-  j++;
-  }};
+    B = i.split('.');
+    j = 0;
+    for (const b of B) {
+      a[j] = parseInt(b);
+      j++;
+    }
+  }
   return a;
 };
 
-const result = Parseip('127.0.0.1')
-console.log(result)
+const result = Parseip('127.0.0.1');
+console.log(result);
