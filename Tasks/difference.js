7fd4d159f79c1add414237480cf33dc19414459e
Mila <l.gorbunova.im12@kpi.ua>
2022-01-05 12:02:56 +0200

    changed the cloning of the object, replaced for...in loop with for...of loop

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,11 +1,18 @@
 // Find a difference of two dictionaries
 'use strict';
 
-const dictDifference = ({ ...obj1 }, obj2) => {
-  for (const key in obj2) {
-    delete obj1[key];
+const dictDifference = (obj1, obj2) => {
+  const clone1 = {};
+  const keys1 = Object.keys(obj1);
+  for (const key of keys1) {
+    clone1[key] = obj1[key];
   }
-  return obj1;
+
+  const keys2 = Object.keys(obj2);
+  for (const key of keys2) {
+    delete clone1[key];
+  }
+  return clone1;
 };
 
 const dict1 = { a: 'uno', b: 'due' };
