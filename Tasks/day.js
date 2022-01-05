c0e3f3297ae4b4d4f7671812b856f1f295837de5
Mikhailo Moliakov <mikhailo.moliakov@gmail.com>
2022-01-05 14:23:19 +0200

    Rewrite expression to make it more readable

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,9 +2,12 @@
 'use strict';
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 const getDayNumber = function(text) {
+  const dayName = text !== null ? text.toLowerCase() : '';
   for (let i = 0; i < days.length; i++) {
-    if (text !== null && text.toLowerCase().startsWith(days[i].toLowerCase())) {
+    const dayPrefix = days[i].toLowerCase();
+    if (dayName.startsWith(dayPrefix)) {
       return i + 1;
     }
   }
