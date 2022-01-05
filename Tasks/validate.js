8b29ce9f8c56e5847c451bd191d7529acee4c3c1
Anyta8989 <96947496+Anyta8989@users.noreply.github.com>
2022-01-05 13:27:35 +0200

    fixed indents and brought const values from loop

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -2,17 +2,17 @@
 'use strict'
 
 const isValid = (name) => {
-    if (name === '') return false;
-    if (typeof (name) !== 'string') return false;
-    if (!name.includes(' ')) return false;
-    for (const char of name) {
-        if (char === ' ') continue;
-        const charCode = char.toLowerCase().charCodeAt(0);
-        const aCode = 'a'.charCodeAt(0);
-        const zCode = 'z'.charCodeAt(0);
-        if (charCode < aCode || charCode > zCode) return false;
-    }
-    return true;
+  if (name === '') return false;
+  if (typeof(name) !== 'string') return false;
+  if (!name.includes(' ')) return false;
+  const aCode = 'a'.charCodeAt(0);
+  const zCode = 'z'.charCodeAt(0);
+  for (const char of name) {
+    if (char === ' ') continue;
+    const charCode = char.toLowerCase().charCodeAt(0);
+    if (charCode < aCode || charCode > zCode) return false;
+  }
+  return true;
 }
 
 const result = isValid('Marcus Aurelius');
