2752017a3344fa1e955561a5ed901b4b2f63d0a3
Mila <l.gorbunova.im12@kpi.ua>
2022-01-05 12:27:28 +0200

    code simplification

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,17 +2,14 @@
 'use strict';
 
 const dictDifference = (obj1, obj2) => {
-  const clone1 = {};
+  const result = {};
   const keys1 = Object.keys(obj1);
   for (const key of keys1) {
-    clone1[key] = obj1[key];
+    if (!obj2[key]) {
+      result[key] = obj1[key];
+    }
   }
-
-  const keys2 = Object.keys(obj2);
-  for (const key of keys2) {
-    delete clone1[key];
-  }
-  return clone1;
+  return result;
 };
 
 const dict1 = { a: 'uno', b: 'due' };
