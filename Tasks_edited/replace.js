af71dcb29957341db10af644f19d5d183683727f
Святослав Олексин <sviat1001@gmail.com>
2022-01-05 04:54:28 +0200

    Deleted brackets

--- Tasks_edited/replace.js
+++ Tasks_edited/replace.js
@@ -9,8 +9,8 @@ function replace(str, substr, newstr){
     const start = str.substring(0, index);
     const end = str.substring(index + substr.length, str.length)
     return start + newstr + end
-  };
-};
+  }
+}
 
 const result = replace('Hello <username> and bye!', '<username>', 'Marcus')
 console.log(result)
