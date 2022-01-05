9f883cf312ca06d2866c07ddcad1a9fe2efae5de
vita133 <vitandrukhiv@gmail.com>
2022-01-05 13:25:59 +0200

    added two space indentation, used eslint and prettier

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,13 +1,13 @@
 // Finde a difference of two dictionaries
 
-"use strict";
+'use strict';
 const difference = (obj1, obj2) => {
-    let diffObj = { ...obj1 };
-    for (const key in obj2) {
-      diffObj[key] = obj2[key];
-      delete diffObj[key];
-    }
-    return diffObj;
-  };
-const result = difference({ a: "uno", b: "due" }, { a: "uno", c: "tre" });
+  let diffObj = {...obj1};
+  for (const key in obj2) {
+    diffObj[key] = obj2[key];
+    delete diffObj[key];
+  }
+  return diffObj;
+};
+const result = difference({a: 'uno', b: 'due'}, {a: 'uno', c: 'tre'});
 console.log(result);
