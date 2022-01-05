9f712b82f076774fe60f949a97ba1fbb0e51820e
GddgdgMen <antonsvirskiy690@gmail.com>
2022-01-05 10:13:55 +0200

    Used "prettier" to edit code

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,11 +2,11 @@
 // Find a difference of two dictionaries
 
 const diff = (object1, object2) => {
-  const keys2 = Object.keys(object2)
+  const keys2 = Object.keys(object2);
   for (const attributeName of keys2) {
-    delete object1[attributeName]
+    delete object1[attributeName];
   }
-  return object1
+  return object1;
 }
-const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' })
-console.log(result)
+const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
+console.log(result);
--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,15 +2,15 @@
 // Find an intersection of two dictionaries
 
 const intersection = (object1, object2) => {
-  const firstKeys = Object.keys(object1)
+  const firstKeys = Object.keys(object1);
   for (const attributeName of firstKeys) {
     if (attributeName !== '' && object2[attributeName]) {
-      object2[attributeName] = object1[attributeName]
+      object2[attributeName] = object1[attributeName];
     } else {
-      delete object1[attributeName]
+      delete object1[attributeName];
     }
   }
-  return object1
+  return object1;
 }
-const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' })
-console.log(result)
+const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
+console.log(result);
