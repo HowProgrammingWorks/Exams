225146ee125e5593259ffd974b0f0baad1b2dd98
Святослав Олексин <sviat1001@gmail.com>
2022-01-05 13:57:26 +0200

    Reviewed difference.js (2- space identations, arrow function, semicolons, ===, kept incoming parameters intact, run eslint, used for in)

--- Tasks_edited/difference.js
+++ Tasks_edited/difference.js
@@ -1,12 +1,15 @@
-'use strict'
-
-function diff(dictionary1, dictionary2){
-    for(const index in dictionary2){
-        dictionary1[index] = dictionary2[index]
-        delete dictionary1[index]
+'use strict';
+const diff = (dictionary1, dictionary2) => {
+  const differ = {};
+  for (const index of Object.keys(dictionary1)) {
+    differ[index] = dictionary1[index];
+  }
+  for (const index of Object.keys(dictionary2)) {
+    if  (dictionary1[index] === dictionary2[index]) {
+      delete differ[index];
     }
-    return dictionary1
-}
-
-const result = diff({a: 'uno', b: 'due'}, {a: 'uno', c: 'tre'});
+  }
+  return dictionary2;
+};
+const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
 console.log(result);
