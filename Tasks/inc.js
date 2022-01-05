c963be59d9b59a8af96050cbfbcd8e6788c728c3
VladimirDan <50290969+VladimirDan@users.noreply.github.com>
2022-01-05 14:14:03 +0200

    Update inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,16 +1,16 @@
 // Increment all numbers in dictionary
 
-"use strict"
+'use strict';
 
-const inc_numbers = (format_complete) => {
+const inc_numbers = format_complete => {
   const keys = Object.keys(format_complete);
-    for (let key of keys) {
-      if (typeof format_complete[key] === "number") {
-        format_complete[key]++;
-      }
+  for (const key of keys) {
+    if (typeof format_complete[key] === 'number') {
+      format_complete[key]++;
     }
-    return format_complete;
-}
+  }
+  return format_complete;
+};
 
 
 const result = inc_numbers({ a: 1, b: 2, c: 'hello', d: false });
