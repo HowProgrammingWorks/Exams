c08eacc0c37830499a835705ba0e85512e7b7a79
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-05 12:44:56 +0200

    making visual changes and not changing inc params

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,15 +2,16 @@
 'use strict';
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const lowerCaseDays = [];
 
 const parser = (day) => {
   for (let i = 0; i < days.length; i++) {
-    if (day.toLowerCase().startsWith(days[i].toLowerCase())) {
-      return `match found: ${i + 1}`;
-    }
+    lowerCaseDays[i] = days[i].toLowerCase();
+    if (day.toLowerCase().startsWith(lowerCaseDays[i])) return i + 1;
   }
-  return `no match: ${-1}`;
-}
+  return -1;
+};
 
 const result = parser('friday');
 console.log(result);
+
