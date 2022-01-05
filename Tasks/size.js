1b232f90ea99bba69ffa622f99f1ae6f074e76f4
Snare295 <72049715+Snare295@users.noreply.github.com>
2022-01-05 12:25:13 +0200

    Changed switch to expression, argument in rounds, skip through eslint

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,18 +1,14 @@
 // Convert number to file size in bytes, kb, mb, and gb
 
-const getRoundSize=size=>{
-    if (size < 0 || typeof size != 'number') return null;
+const getRoundSize = (size) => {
+    if ((size < 0) || (typeof size != 'number'))
+        return null;
     else {
-      const exp = Math.floor(Math.log(size)/Math.log(1000));
-      switch(exp){
-        case 0: return (size+' b');
-        case 1: return (Math.round(size / 1000) + ' kb');
-        case 2: return (Math.round(size / 1000000) + ' mb');
-        case 3: return (Math.round(size / 1000000000) + ' gb');
-        default: return (Math.round(size / 1000000000000) + ' tb');
+        const exp = Math.floor(Math.log(size) / Math.log(1000));
+        const PREFIXES = ['b', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+        return (Math.round(size / (1000 ^ exp)) + ' ' + PREFIXES[exp]);
     }
-  }
-}
-  
-const result = getRoundSize(123456)
-console.log(result)
+};
+
+const result = getRoundSize(123456);
+console.log(result);
