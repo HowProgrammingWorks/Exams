6facc0146a181c3cee0a285435a982e3eff28bee
MrSampy <mynameisserzheo@gmail.com>
2022-01-05 11:46:21 +0200

    fixing formatting & adding '++'  to variable 'numberofwordsinstr' in return

--- Tasks/words.cpp
+++ Tasks/words.cpp
@@ -6,16 +6,16 @@ using namespace std;
 const int words(const string str)
 {
   int numberofwordsinstr = 0;
-  for (unsigned int index=0; index < str.size(); ++index)
+  for (unsigned int index = 0; index < str.size(); ++index)
   {
-      if ((str[index + 1] != ' ' && str[index + 1] != '\0') && str[index] == ' ')
-      numberofwordsinstr++;
+    if ((str[index + 1] != ' ' && str[index + 1] != '\0') && str[index] == ' ') numberofwordsinstr++;
   }
-  return numberofwordsinstr;
+  return ++numberofwordsinstr;
 }
 int main()
 {
   const int result = words("Hello Marcus Aureluis");
   cout << result;
   return 0;
+  
 }
