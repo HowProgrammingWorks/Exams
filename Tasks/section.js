ea2ca65a4b47122abdddf7b45f281ac8b9496014
romanlitv <youlikeyoupass@gmail.com>
2022-01-05 13:19:00 +0200

    (section.js)Changed variable 'i' for constant , also was used eslint and prettier

--- Tasks/section.js
+++ Tasks/section.js
@@ -2,19 +2,19 @@
 
 'use strict';
 
-const sectionString   = (string, separator ) => {
-    let finalArr = [];
-    let i = string.indexOf(separator); 
-    if(i < 0) {
-        finalArr = [string, ''];
-        return finalArr;
-    } else {
-        const beforeSeparator = string.slice(0, i);
-        const afterSeparator = string.slice(i + separator.length); 
-        finalArr = [beforeSeparator, afterSeparator];
-        return finalArr;
-    } 
-}; 
+const sectionString = (string, separator) => {
+  let finalArr = [];
+  const i = string.indexOf(separator);
+  if (i < 0) {
+    finalArr = [string, ''];
+    return finalArr;
+  } else {
+    const beforeSeparator = string.slice(0, i);
+    const afterSeparator = string.slice(i + separator.length);
+    finalArr = [beforeSeparator, afterSeparator];
+    return finalArr;
+  }
+};
 
 const result = sectionString('All you need is JavaScript', 'is');
 console.log(result);
