6fb38e8045cdd08a0beefec707f38c314bf934d0
Святослав Олексин <sviat1001@gmail.com>
2022-01-05 04:39:12 +0200

    Deleted an endless cycle + 1 more space

--- Tasks_edited/replace.js
+++ Tasks_edited/replace.js
@@ -1,23 +1,21 @@
 // Replace substring with newstr
 
-function replace(str, substr,newstr){
+function replace(str, substr, newstr){
   if(substr.length == 0){
     return str
   }
   else{
     replaced = ''
-    do{
-      const index = str.indexOf(substr); 
-        if(index === -1){
-          return replaced + str
+    const index = str.indexOf(substr); 
+    if(index === -1){
+      return replaced + str
+    }
+    else{
+      const start = str.substring(0, index);
+      const end = str.substring(index + substr.length, str.length)
+      replaced += start + newstr + end
+    return replaced
         }
-        else{
-          const start = str.substring(0, index);
-          const end = str.substring(index + substr.length, str.length)
-          replaced += start + newstr + end
-          return replaced
-        }
-    }while (true);
   };
 };
 
