3806c580be450b188a81f062176a64428ed12113
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-05 08:00:28 +0200

    replacing the args like dot with /dot for regexp

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,9 +3,12 @@
 
 const extract = (str, prefix, suffix) => {
 
+  prefix = prefix.replace(/(\W+)/i, '\\$1')
+	suffix = suffix.replace(/(\W+)/i, '\\$1')
+
   const regexp = new RegExp(`^.*?${prefix}(.*?)${suffix}.*?$`);
   return str.match(regexp)[1];
 };
 
-const result = extract('Hello <username> and bye!', '<', '>');
+const result = extract('Hello .username. and bye!', '.', '.');
 console.log(result);
