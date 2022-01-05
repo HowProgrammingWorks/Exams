b6af6b7fd8852a98ca1db67519e85c9d63bd9d8e
Alina Dubchak <alina.dubchak.ev@gmail.com>
2022-01-05 15:19:55 +0200

    eslint

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,14 +1,13 @@
-'use strict'
+'use strict';
 
-const inc = (format) => {
-  let res = [];
-     for ( let element of format){
-      if (typeof element === "number") element ++;
-      res.push(element);
-   }
+const inc = format => {
+  const res = [];
+  for (let element of format) {
+    if (typeof element === 'number') element++;
+    res.push(element);
+  }
   return res;
-  };
+};
 
-
-const obj = [1,2,'hello', false];
+const obj = [1, 2, 'hello', false];
 console.log(inc(obj));
