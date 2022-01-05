77ceb2fa0c445e7a2f1ad50313a76bf11fdbfed0
Snare295 <72049715+Snare295@users.noreply.github.com>
2022-01-05 12:27:37 +0200

    Argument in round brackets, skip through eslint

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,15 +1,18 @@
 // Split string by the first occurrence of separator
 
-const splitIP = entrStrg => {
-  extArr = [];
-  if (entrStrg === '' ) return null;
+const splitIP = (entrStrg) => {
+    extArr = [];
+    if (entrStrg === '')
+        return null;
     else {
-      spltNums = entrStrg.split('.');
-      for (let j=0; j<spltNums.length; j++) {
-        extArr[j] = parseInt(spltNums[j]);
-  }};
-  return extArr;
+        spltNums = entrStrg.split('.');
+        for (let j = 0; j < spltNums.length; j++) {
+            extArr[j] = parseInt(spltNums[j]);
+        }
+    };
+    return extArr;
 };
 
-const result = splitIP('127.0.0.1')
-console.log(result)
+const result = splitIP('127.0.0.1');
+console.log(result);
+
