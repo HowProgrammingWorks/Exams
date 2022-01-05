d0fe5041901c91d4bb9552fc59438ce3042de032
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-05 08:13:39 +0200

    making sure code works with multiple characters

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,12 +3,13 @@
 
 const extract = (str, prefix, suffix) => {
 
-  prefix = prefix.replace(/(\W+)/i, '\\$1')
-	suffix = suffix.replace(/(\W+)/i, '\\$1')
+  prefix = prefix.replace(/(\W)/gi, '\\$1')
+  suffix = suffix.replace(/(\W)/gi, '\\$1')
 
   const regexp = new RegExp(`^.*?${prefix}(.*?)${suffix}.*?$`);
   return str.match(regexp)[1];
 };
 
-const result = extract('Hello .username. and bye!', '.', '.');
+const result = extract('Hello ??username?? and bye!', '??', '??');
 console.log(result);
+
