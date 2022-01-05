b4b97800008926ee065278e4d665876abbf7b441
Святослав Олексин <sviat1001@gmail.com>
2022-01-05 04:10:49 +0200

    Deleted useless code

--- Tasks_edited/difference.js
+++ Tasks_edited/difference.js
@@ -1,12 +1,6 @@
 // Finde a difference of two dictionaries
 
 function diff(object_1,object_2){
-    for(attribute_name in object_1){
-        object_1[attribute_name] = object_1[attribute_name]
-    }
-    if(object_1 in object_2){
-        return false
-    }
     for(attribute_name in object_2){
         object_1[attribute_name] = object_2[attribute_name]
         delete object_1[attribute_name]
