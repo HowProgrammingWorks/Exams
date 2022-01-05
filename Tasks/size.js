e8d79c2115dcbee0486fd0a70426abba41babf4b
m1guelten <footballMiguelTen@gmail.com>
2022-01-05 14:55:53 +0200

    made with using collections

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,14 +1,17 @@
 // Convert number to file size in bytes, kb, mb, and gb
 
+const EXP = [
+  /* 0 */ ' byte',
+  /* 1 */ ' kb',
+  /* 2 */ ' mb',
+  /* 3 */ ' gb',
+];
 const size = (number) => {
   if (number === undefined) return;
   if (number === 0) return '0';
-  const exp = Math.floor(Math.log(number)/Math.log(1000));
-  if(exp===0) return number + ' byte';
-  if(exp===1) return Math.round(number / 1000) + ' kb';
-  if(exp===2) return Math.round(number / 1000000) + ' mb';
-  return Math.round(number / 1000000000) + ' gb';
-}
+  const indexExp = Math.floor(Math.log(number)/Math.log(1000));
+  return Math.round(number/(Math.pow(1000,indexExp)))+EXP[indexExp];
+};
 
 //const result = size(123456)
 console.log(size());
@@ -17,4 +20,3 @@ console.log(size(123));
 console.log(size(1234));
 console.log(size(12345678));
 console.log(size(123456789123));
-console.log(size(12345678912345));
