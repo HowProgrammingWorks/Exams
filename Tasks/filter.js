a21f413871b3a71a7fd3370066015bcc6131ed40
oneOdin <97091092+hryshniakovNazar@users.noreply.github.com>
2022-01-05 02:27:41 +0200

    FilterCommit 5

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -3,12 +3,11 @@
 'use strict';
 
 const Filter = (typesForFilter, typeName) => {
-  for (let counter of typesForFilter) {
-    let index = typesForFilter.indexOf(counter);
+  for (const counter of typesForFilter) {
+    const index = typesForFilter.indexOf(counter);
     if (typeof typesForFilter[index] !== typeName) { typesForFilter.splice(index, 1); }
   }
   return typesForFilter;
 };
-  
-const result = Filter([1, 2, 'three', 4, 5, 'six'], 'number');
-console.log(result);
+
+console.log(Filter([1, 2, 'three', 4, 5, 'six'], 'number'));
