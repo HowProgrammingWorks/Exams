cf947a32e4a67997ee7fefd05565d9c3872730ee
Snare295 <72049715+Snare295@users.noreply.github.com>
2022-01-05 09:14:43 +0200

    Changed heights of rows for readability

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,12 +1,12 @@
 // Split string by the first occurrence of separator
 
 const splitIP = entrStrg => {
-  extArr = []
-    if (entrStrg === '' ) return null;
-  else {
-   spltNums = entrStrg.split('.');
-    for (let j=0; j<spltNums.length; j++) {
-  extArr[j] = parseInt(spltNums[j]);
+  extArr = [];
+  if (entrStrg === '' ) return null;
+    else {
+      spltNums = entrStrg.split('.');
+      for (let j=0; j<spltNums.length; j++) {
+        extArr[j] = parseInt(spltNums[j]);
   }};
   return extArr;
 };
