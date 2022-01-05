233a3002177d8c5ef5d9cb05df9ff6c20a7670d5
Snare295 <72049715+Snare295@users.noreply.github.com>
2022-01-05 10:29:29 +0200

    Added default to switch for high values

--- Tasks/size.js
+++ Tasks/size.js
@@ -9,6 +9,7 @@ let size=size=>{
         case 1: return (Math.round(size / 1000) + ' kb');
         case 2: return (Math.round(size / 1000000) + ' mb');
         case 3: return (Math.round(size / 1000000000) + ' gb');
+        default: return (Math.round(size / 1000000000000) + ' tb');
     }
   }
 }
