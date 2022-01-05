0265178e59dab629ec307fa002f8e18e6603196e
MrSampy <mynameisserzheo@gmail.com>
2022-01-05 11:35:47 +0200

    checking substr == ""  before str.find(substr)

--- Tasks/Replace.cpp
+++ Tasks/Replace.cpp
@@ -3,15 +3,13 @@
 using namespace std;
 const string replace(string str = "", const string substr = "", const string newstr = "")
 {
+  if (substr == "") return str;
   const int positionofsubstr = str.find(substr);
-  if (substr == "" || positionofsubstr == -1) return str;
 
   return str.erase(positionofsubstr, substr.size()).insert(positionofsubstr, newstr);
-  
 }
 int main()
 {
   cout << replace("Hello <username> and bye!", "<username>", "Marcus");
   return 0;
-
 }
