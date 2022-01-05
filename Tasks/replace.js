97da659ef2ecff8b2773848057e7565f51ccc5fb
vadumpast <vadumpast@gmail.com>
2022-01-05 09:28:32 +0200

    changed logic to saving intervals between required variable

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -6,15 +6,8 @@ const replace = (str, subStr, newStr) => {
   if (subStr === '') {
     return str;
   }
-  let res = '';
-  let index = str.indexOf(subStr);
-  while (index !== -1) {
-    const start = str.substring(0, index);
-    const end = str.substring(index + subStr.length, str.length);
-    str = start + newStr + end;
-    index = str.indexOf(subStr);
-  }
-  return str;
+  let res = str.split(subStr);
+  return res.join(newStr);
 }
 
 const result = replace('Hello <username> and bye!', '<username>', 'Marcus');
