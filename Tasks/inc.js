ea235e7b1eab10aea720083c38a689d898728d08
VladimirDan <50290969+VladimirDan@users.noreply.github.com>
2022-01-05 13:33:12 +0200

    Update inc.js
    замінив for in на for of

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -3,13 +3,14 @@
 "use strict"
 
 const inc_numbers = (format_complete) => {
-   	for (let key in format_complete) {
+      const keys = Object.keys(format_complete);
+   	for (let key of keys) {
       		if (typeof format_complete[key] === "number"){
       			format_complete[key]++;
       		}
-  	}
-  	return format_complete;
- };
+  	   }
+  	   return format_complete;
+};
 
 
 const result = inc_numbers({ a: 1, b: 2, c: 'hello', d: false });
