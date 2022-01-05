e02f2240330d56f65d5b03aa3e44fe66db16aa56
Святослав Олексин <sviat1001@gmail.com>
2022-01-05 04:22:30 +0200

    Changed spaces and more tabulation

--- Tasks_edited/replace.js
+++ Tasks_edited/replace.js
@@ -1,22 +1,23 @@
 // Replace substring with newstr
 
 Replace = (str, substr,newstr)=>{
-  if (substr    === ''){
+  if(substr === ''){
     return str
   }
   else{
     src = str
     res = ''
-    do {
-      const _index = src.indexOf( substr) ; 
-        if (_index === -1){
-          return res + src }
+    do{
+      const _index = src.indexOf( substr); 
+        if(_index === -1){
+          return res + src
+        }
         else{
           const start = src.substring(0,_index);
-            src = src.substring(_index+substr.length,src.length)
-            res += start + newstr
+          src = src.substring(_index+substr.length,src.length)
+          res += start + newstr
         }
-    } while (true);
+    }while (true);
   };
 };
 
