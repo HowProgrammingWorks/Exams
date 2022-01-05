774a3a0fbd20669efdc2bdd3022e27e588089cfa
Andriyaschenko <vovan4ik041@gmail.com>
2022-01-05 12:37:30 +0200

    Added spaces for easier reading

--- Tasks/merge.py
+++ Tasks/merge.py
@@ -1,5 +1,5 @@
-def merge_two_objects(FirstObject,SecondObject):
-    Merge=FirstObject|SecondObject
-    return FirstObject,SecondObject,Merge
-result=merge_two_objects({'a':'uno','b':'due'},{'c':'tre'})
+def merge_two_objects(FirstObject, SecondObject):
+    Merge = FirstObject | SecondObject
+    return FirstObject, SecondObject, Merge
+result = merge_two_objects({ 'a': 'uno', 'b': 'due' },{ 'c':' tre' })
 print(result)
