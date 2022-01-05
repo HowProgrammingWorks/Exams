49b8b1ed568c925fb9858bc3c744909a72a1061a
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-05 08:25:01 +0200

    fixing the look of the code

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,15 +3,17 @@
 
 const extract = (str, prefix, suffix) => {
 
+  prefix = prefix.replace(/(\W)/gi, '\\$1');
+  suffix = suffix.replace(/(\W)/gi, '\\$1');
+
   try {
-    prefix = prefix.replace(/(\W)/gi, '\\$1');
-    suffix = suffix.replace(/(\W)/gi, '\\$1');
     const regexp = new RegExp(`^.*?${prefix}(.*?)${suffix}.*?$`);
     return str.match(regexp)[1];
   } catch (error) {
-  	console.log("wrong");
-	}
+    console.log("wrong");
+  }
 };
 
 const result = extract('Hello ??username?? and bye!', '??', '??');
 console.log(result);
+
