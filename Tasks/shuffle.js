7d82efb297f1ae33d2d320d034abcd59cadbef64
Daria Petruk <90788327+Mikkador@users.noreply.github.com>
2022-01-05 12:48:35 +0200

    fixing changing incoming parameters

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,9 +1,11 @@
 // Shuffle an array
 'use strict';
+
+const CHANCE_EQUALISER = 0.5;
 const shuffle = (array) => {
-  const CHANCE_EQUALISER = 0.5;
-  array.sort(() => Math.random() - CHANCE_EQUALISER); 
-  return array;
+  const newArray = Array.from(array);
+  newArray.sort(() => Math.random() - CHANCE_EQUALISER); 
+  return newArray;
 };
 
 const result = shuffle(['a', 'b', 'c', 'd', 'e']);
