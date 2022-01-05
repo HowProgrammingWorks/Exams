304981090f30c0e15494e6d6a81f2359f1518ade
Andriyaschenko <vovan4ik041@gmail.com>
2022-01-05 12:34:16 +0200

    changed for more optimized and shorter code

--- Tasks/merge.py
+++ Tasks/merge.py
@@ -1,9 +1,5 @@
 def merge_two_objects(object_1,object_2):
-    object_3=[{}][0]
-    for attribute_name in object_1:
-        object_3[attribute_name]=object_1[attribute_name]
-    for attribute_name in object_2:
-        object_3[attribute_name]=object_2[attribute_name]
+    object_3=object_1|object_2
     return object_1,object_2,object_3
 result=merge_two_objects({'a':'uno','b':'due'},{'c':'tre'})
 print(result)
