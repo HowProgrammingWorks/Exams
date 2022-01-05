e58305f0961eb7b6703ac8566bb3fc42441b6096
olegoofy <martynenkooleg77@gmail.com>
2022-01-05 11:22:21 +0200

    Corrections to intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,13 +1,16 @@
 'use strict'
+
 // Finde an intersection of two dictionaries
-const intersection = (object1,object2) => {
-    const resObject = {};
-    for (const key in object1) {
-        if (object1[key] === object2[key]) {
-            resObject[key] = object1[key];
-        }
+
+const intersection = (object1, object2) => {
+  const res = {};
+  const keys = Object.keys(object1);
+  for (const key of keys) {
+    if (object2[key]) {
+      res[key] = object1[key];
     }
-return resObject;
+  }
+return res;
 }
 
 const result = intersection({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
