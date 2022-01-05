078697468d0db856153ce5d5079dd902bc498e6c
cchebelok <lotokhinivan@gmail.com>
2022-01-05 13:31:26 +0200

    used return-early strategy(if i understood it correctly), change test length because of eslint

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -8,16 +8,16 @@ const Replace = (str, substr, newstr) => {
     let res = '';
     while (true) {
       const index = str.indexOf(substr);
-      if (index === -1) {
-        return res + str;
-      } else {
+      if (index !== -1) {
         const start = str.substring(0, index);
         str = str.substring(index + substr.length, str.length);
         res += start + newstr;
+      } else {
+        return res + str;
       }
     }
   }
 };
 const result = Replace('Hello <username> and bye!', '<username>', 'Marcus');
-const result2 = Replace('Hi <username>, How are you? Great job <username>!',  '<username>',  'Marcus');
+const result2 = Replace('AVE <user>, <user>, <user> !!',  '<user>',  'Marcus');
 console.log(result, result2);
