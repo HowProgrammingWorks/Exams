a381a3231be425a296665f6d571067134ecb0acd
vadumpast <vadumpast@gmail.com>
2022-01-05 12:24:01 +0200

    changed split/map to for. Cleaned up the code with ESLint

--- Tasks/quotes.js
+++ Tasks/quotes.js
@@ -1,18 +1,19 @@
 // Change douple quotation to open or close quotation
 'use strict';
 
-const EMPTY = '';
 const BRACKETS = ['«', '»'];
 
 const quotes = (str) => {
+  let res = '';
   let indexBracket = 0;
-  return str.split(EMPTY).map( element => {
+  for (let element of str) {
     if (element === '"') {
       element = BRACKETS[(indexBracket++) % 2];
     }
-    return element;
-  }).join(EMPTY);
-}
+    res += element;
+  }
+  return res;
+};
 
 const result = quotes('Hello "Marcus"! Ave "Marcus"!');
 console.log(result);
