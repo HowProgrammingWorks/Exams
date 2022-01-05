e604b1ae7d3f3d9e0512b22a4d06be0b93bcc38c
vadumpast <vadumpast@gmail.com>
2022-01-05 13:01:55 +0200

    rewrote to loop. Changed variable names

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -2,9 +2,16 @@
 'use strict';
 
 
-const replace = (str, subStr, newStr) => {
-  return str.split(subStr).join(newStr);
-}
+const replace = (string, subString, newString) => {
+  let index = string.indexOf(subString);
+  while (index !== -1) {
+    const prefix = string.substring(0, index);
+    const suffix = string.substring(index + subString.length);
+    string = prefix + newString + suffix;
+    index = string.indexOf(subString);
+  }
+  return string;
+};
 
 const result = replace('Hello <username> and bye!', '<username>', 'Marcus');
 console.log(result);
