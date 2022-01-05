c63cd6074a3f023234851a8bdf267f86cd6faf50
Mahaon17 <89358047+Mahaon17@users.noreply.github.com>
2022-01-05 12:44:30 +0200

    Created new variables instead of changing max,min

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,13 +2,7 @@
 // Generate random integer value in given range
 
 const random = (min, max) => {
-  if (!max) {
-    max = min;
-    min = 0;
-  }
-  const multiplier = max - min + 1;
-  return min + Math.floor(Math.random() * multiplier);
+  const multiplier = max ? (max - min + 1) : (min + 1);
+  const summand = max ? min : 0;
+  return summand + Math.floor(Math.random() * multiplier);
 };
-
-const result = random(1, 9);
-console.log(result);
