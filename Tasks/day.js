7ab76ac1fdbcb265f8af3e064b75108015c57b84
VladimirDan <50290969+VladimirDan@users.noreply.github.com>
2022-01-05 14:12:44 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,15 @@
 // Get day number
-"use strict"
+'use strict';
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const parseDay = (searchedDay) => {
-  const dayIndex = days.findIndex((val) => 
+const parseDay = searchedDay => {
+  const dayIndex = days.findIndex(val =>
     searchedDay.startsWith(val.toLowerCase()));
   const dayNumber = dayIndex + 1;
   return dayNumber === 0 ? -1 : dayNumber;
-}
-  
+};
+
 
 
 const result = parseDay('friday');
