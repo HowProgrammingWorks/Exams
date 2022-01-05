781fb632ee607de08e165f4615bb28d5aa39e5cb
Михайло Креславський <kreslavskiy.m@icloud.com>
2022-01-05 12:59:52 +0200

    except fixed

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,12 +1,21 @@
 // Copy all values from dict except listed
 
+// Fixes:
+// - not changing incoming parameters
+// - improved code style
+// - used for...of instead of .map
+
 'use strict';
 
 const except = (incomingValues, ...toDelete) => {
-  toDelete.map((element) => {
-      delete incomingValues[element];
-  });
-  return incomingValues;
+  const keys = Object.keys(incomingValues);
+  const result = {};
+  for (const key of keys) {
+    if (!toDelete.includes(key)) {
+      result[key] = incomingValues[key];
+    }
+  }
+  return result;
 };
 
 const result = except({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
