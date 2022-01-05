6c964f2e3f6cbdd7c3d06ad41d702996b10fbb46
Святослав Олексин <sviat1001@gmail.com>
2022-01-05 04:05:18 +0200

    Changed tabulation

--- Tasks_edited/difference.js
+++ Tasks_edited/difference.js
@@ -2,12 +2,14 @@
 
 function diff(object_1,object_2){
     for (attribute_name in object_1) {
-    object_1[attribute_name] = object_1[attribute_name]
+        object_1[attribute_name] = object_1[attribute_name]
     }
-    if (object_1 in object_2) return false
+    if (object_1 in object_2){
+        return false
+    }
     for (attribute_name in object_2) {
-    object_1[attribute_name] = object_2[attribute_name]
-    delete object_1[attribute_name]
+        object_1[attribute_name] = object_2[attribute_name]
+        delete object_1[attribute_name]
     }
     return object_1
 }
