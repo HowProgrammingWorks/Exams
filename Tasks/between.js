3cf882181e71ab6a20b6032b06643c6e490e908c
kizzzzzzzz <96995375+tomatorepublic@users.noreply.github.com>
2022-01-05 11:59:49 +0200

    using eslint and prettier

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,28 +1,20 @@
 // Extract substring between prefix and suffix
-'use strict';
+"use strict";
 
-const extract = (str, prefix, suffix) =>
-{
+const extract = (str, prefix, suffix) => {
+  let index = str.indexOf(prefix);
+  if (index !== -1) {
+    let ilsum = index + prefix.length;
+    str = str.substring(ilsum);
+    index = str.indexOf(suffix);
 
-	let index = str.indexOf(prefix);
-	if (index !== -1)
-	{
+    if (index !== -1) {
+      str = str.substring(0, index);
+    } else str = "wrong";
+  } else str = "wrong";
 
-		let ilsum = index + prefix.length;
-		str = str.substring(ilsum);
-		index = str.indexOf(suffix);
-
-		if (index !== -1)
-		{
-			str = str.substring(0, index);
-		}
-		else str = "wrong";
-
-	}
-	else str = "wrong";
-
-	return str;
+  return str;
 };
 
-const result = extract('Hello <username> and bye!', '<', '>');
+const result = extract("Hello <username> and bye!", "<", ">");
 console.log(result);
