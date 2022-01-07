81d2d1637d42dbb290e58546227282642c98b8d5
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-07 23:10:04 +0200

    Breaks in long lines

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -8,7 +8,8 @@ if (T.length === 0) return false;
 if (!T.includes(' ')) return false; {
   for (C of T) {
     if (C === ' ') continue;
-    if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
+    if (C.toLowerCase().charCodeAt(0) >= 97 &&
+    C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
   };
   return true;
 };
