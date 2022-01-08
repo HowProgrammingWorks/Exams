e66e038114724947eaf1aee05d5a59c2b187c734
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-08 20:51:05 +0200

    Improve naming

--- Example/4-size.js
+++ Example/4-size.js
@@ -9,10 +9,10 @@
 
 const SIZE_UNITS = ['byte', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
 
-const size = (size) => {
-  if (size === 0) return '0';
-  const exp = Math.floor(Math.log(size) / Math.log(1000));
-  const value = Math.round(size / 1000 ** exp);
+const size = (bytes) => {
+  if (bytes === 0) return '0';
+  const exp = Math.floor(Math.log(bytes) / Math.log(1000));
+  const value = Math.round(bytes / 1000 ** exp);
   const unit = SIZE_UNITS[exp];
   return value.toString() + ' ' + unit;
 };
