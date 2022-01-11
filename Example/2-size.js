903cc9a6a9fd647ab511f2e53375f4628406148a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-11 09:33:10 +0200

    Fix initial task and optimization sequence

--- Example/2-size.js
+++ Example/2-size.js
@@ -9,7 +9,7 @@
 'use strict';
 
 const size = (bytes) => {
-  if (bytes === 0) return '0';
+  if (bytes === 0) return '0 byte';
   const exp = Math.floor(Math.log(bytes) / Math.log(1000));
   const value = Math.round(bytes / 1000 ** exp);
   let unit = ' byte';
--- Example/4-size.js
+++ Example/4-size.js
@@ -10,7 +10,7 @@
 const SIZE_UNITS = ['byte', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
 
 const size = (bytes) => {
-  if (bytes === 0) return '0';
+  if (bytes === 0) return '0 ' + SIZE_UNITS[0];
   const exp = Math.floor(Math.log(bytes) / Math.log(1000));
   const value = Math.round(bytes / 1000 ** exp);
   const unit = SIZE_UNITS[exp];
