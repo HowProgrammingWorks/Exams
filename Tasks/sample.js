8e50f960b23ea5afda710f5d0c95553c2c00fff8
daiyunjie <daiyunjie508@gmail.com>
2022-01-13 02:45:27 +0800

    improve sample

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,10 +1,8 @@
 // Get one random element from array
 
-const SAMPLE = arr => {
-  arr = arr[
-    Math.floor(Math.random()* arr.length)
-  ]; return arr
-};
+'use strict';
 
+const SAMPLE = arr => arr[Math.floor(Math.random() * arr.length)];
 const result = SAMPLE([123, 456, 789, 222, 333, 444]);
 console.log(result);
+  
