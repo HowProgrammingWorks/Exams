3430066339ce916a3cf8a32927b9f305529c4a02
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-15 23:46:47 +0200

    for is not needed if we use Set

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,12 +3,8 @@
 // Return an array without duplicates
 
 const distinct = (array) => {
-  const set = new Set();
-  array.forEach((element, index) => {
-    if (set.has(element)) array.splice(index, 1);
-    else set.add(element);
-  });
-  return array;
+  const set = new Set(array);
+  return [...set];
 };
 
 const array = [1, 2, 1, 3, 1, 4];
