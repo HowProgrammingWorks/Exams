50ac632d64e038c61fa4142eae711e2883bd8f66
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-16 00:30:12 +0200

    Duplicate: It is not needed to verify that n === 0

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -3,14 +3,11 @@
 // Return an array without duplicates
 
 const duplicate = (value, n) => {
-  if (n <= 0) return [];
-  else {
-    const res = [];
-    for (let i = 0; i < n; i++) {
-      res[i] = value;
-    }
-    return res;
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    res[i] = value;
   }
+  return res;
 };
 
 const result = duplicate("abc", 5);
