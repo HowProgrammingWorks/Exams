a33c7fdebbeb3ea5855a9948149625cd74f73fab
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-16 00:31:04 +0200

    Duplicate: fix function description

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,8 +1,7 @@
 "use strict";
 
-// Return an array without duplicates
-
-const duplicate = (value, n) => {
+// Fill array with given value N times
+const fillArray = (value, n) => {
   const res = [];
   for (let i = 0; i < n; i++) {
     res[i] = value;
