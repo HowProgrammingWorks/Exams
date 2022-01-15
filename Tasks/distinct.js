e11c6ce59f7dcd43ca79a0d205ccfe722033df74
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-15 22:52:54 +0200

    Use more understandable variable names

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -2,19 +2,19 @@
 
 // Return an array without duplicates
 
-const DISTINCT = (data) => {
-  const A = new Set();
-  let w = 0;
-  data.forEach((a) => {
-    if (A.has(a)) {
-      data.splice(w, 1);
+const distinct = (data) => {
+  const set = new Set();
+  let iteration = 0;
+  data.forEach((element) => {
+    if (set.has(element)) {
+      data.splice(iteration, 1);
     } else {
-      A.add(a);
+      set.add(element);
     }
-    w++;
+    iteration++;
   });
   return data;
 };
 
-const result = DISTINCT([1, 2, 1, 3, 1, 4]);
+const result = distinct([1, 2, 1, 3, 1, 4]);
 console.log(result);
