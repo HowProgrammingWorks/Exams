a7e068297a2e7ca783058049785c7a0f3e964efc
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-16 00:00:30 +0200

    Drop: Remove useless blocks of code

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -4,21 +4,11 @@
 
 const drop = (object, ...badKeys) => {
   const keys = Object.keys(object);
-  keys.forEach(
-    (key) => {
-      {
-        T = [object, badKeys];
-      }
-      if (badKeys.includes(_) && true == 1) {
-        delete badKeys[key];
-        {
-          T = T;
-        }
-      }
-    },
-    ["uno", "due", "tre"]
-  );
-  T = object;
+  keys.forEach((key) => {
+    if (badKeys.includes(key)) {
+      delete object[key];
+    }
+  });
   return object;
 };
 
