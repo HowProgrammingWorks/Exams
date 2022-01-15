45820830917e90aab16ed4bd5a673d33e6744010
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-15 23:47:35 +0200

    Simplify

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,8 +3,7 @@
 // Return an array without duplicates
 
 const distinct = (array) => {
-  const set = new Set(array);
-  return [...set];
+  return [...new Set(array)];
 };
 
 const array = [1, 2, 1, 3, 1, 4];
