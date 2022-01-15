4d9d302c4144d392f4273dd8791b74d8acf10ba3
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-16 00:34:05 +0200

    Duplicate: Added inline function variant

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,13 +1,24 @@
 "use strict";
 
 // Fill array with given value N times
-const fillArray = (value, n) => {
-  const res = [];
-  for (let i = 0; i < n; i++) {
-    res[i] = value;
-  }
-  return res;
-};
 
-const result = duplicate("abc", 5);
-console.log(result);
+{
+  const fillArray = (value, n) => {
+    const res = [];
+    for (let i = 0; i < n; i++) {
+      res[i] = value;
+    }
+    return res;
+  };
+
+  const result = fillArray("abc", 5);
+  console.log(result);
+}
+
+// Inline variant
+{
+  const fillArray = (value, n) => new Array(n).fill(value);
+
+  const result = fillArray("abc", 5);
+  console.log(result);
+}
