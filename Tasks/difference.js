6b3bc451214c0c4a99139086aca1eb2153137c68
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-16 17:01:31 +0200

    Diff: Do not update the input data

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,16 +2,17 @@
 
 // Find a difference between two dictionaries
 const diff = (objectA, objectB) => {
+  const result = {};
+
   for (const key in objectA) {
-    objectA[key] = objectA[key];
+    if (objectA[key] === objectB[key]) continue;
+    result[key] = objectA[key];
   }
 
   for (const key in objectB) {
-    objectA[key] = objectB[key];
-    delete objectA[key];
   }
 
-  return objectA;
+  return result;
 };
 
 const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
