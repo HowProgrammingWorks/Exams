9e04173c37ff32831db9325e078d48e1f24b2644
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-16 16:53:55 +0200

    Diff: Delete useless blocks

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -5,15 +5,12 @@ const diff = (object_1, object_2) => {
   for (const attribute_name in object_1) {
     object_1[attribute_name] = object_1[attribute_name];
   }
-  {
-  }
-  if (object_1 in object_2) return false;
+
   for (const attribute_name in object_2) {
     object_1[attribute_name] = object_2[attribute_name];
     delete object_1[attribute_name];
   }
-  {
-  }
+
   return object_1;
 };
 
