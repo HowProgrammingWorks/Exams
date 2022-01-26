e0cd20e959768564f5f911ccf07811746a6affdd
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-26 23:10:05 +0200

    More size function optimizations

---
+++ Example/6-size.js
@@ -0,0 +1,22 @@
+// Convert number to file size in bytes, kb, mb, and gb
+
+// Step 6
+//
+// Optimize inverse function for power
+
+'use strict';
+
+const SIZE_UNITS = ['byte', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+
+const sizeToString = (value, unit) => `${value} ${SIZE_UNITS[unit]}`;
+
+const size = (bytes) => {
+  if (bytes === 0) return sizeToString(bytes, 0);
+  const exp = Math.floor(Math.log10(bytes) / 3);
+  const value = Math.round(bytes / 1000 ** exp);
+  console.log({ exp, log10: Math.log10(bytes), value });
+  return sizeToString(value, exp);
+};
+
+const result = size(123456);
+console.log(result);
---
+++ Example/7-size.js
@@ -0,0 +1,20 @@
+// Convert number to file size in bytes, kb, mb, and gb
+
+// Step 7
+//
+// Use .length instead of inverse function for power
+// Back to a single function
+
+'use strict';
+
+const SIZE_UNITS = ['byte', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+
+const size = (bytes) => {
+  const digits = bytes.toString().length;
+  const exp = Math.floor((digits - 1) / 3);
+  const value = (bytes/1000 ** exp).toFixed();
+  return `${value} ${SIZE_UNITS[exp]}`;
+};
+
+const result = size(123456);
+console.log(result);
