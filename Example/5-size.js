cb1ab6c968df1faed3e286167e1d0db776383789
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-26 23:10:05 +0200

    More size function optimizations

---
+++ Example/5-size.js
@@ -0,0 +1,22 @@
+// Convert number to file size in bytes, kb, mb, and gb
+
+// Step 5
+//
+// Remove duplicates
+// Extract serialization
+
+'use strict';
+
+const SIZE_UNITS = ['byte', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+
+const sizeToString = (value, unit) => `${value} ${SIZE_UNITS[unit]}`;
+
+const size = (bytes) => {
+  if (bytes === 0) return sizeToString(bytes, 0);
+  const exp = Math.floor(Math.log(bytes) / Math.log(1000));
+  const value = Math.round(bytes / 1000 ** exp);
+  return sizeToString(value, exp);
+};
+
+const result = size(123456);
+console.log(result);
