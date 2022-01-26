b240cd6b9722a7a65993d04e7993e0c8db0a04bb
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-26 23:13:04 +0200

    Add difference implementations

---
+++ Example/2-difference.js
@@ -0,0 +1,23 @@
+// Find a difference between two dictionaries
+
+// Step 2
+// Remove unused identifiers
+// Change identifiers case
+// Add `const` and `let`
+
+'use strict';
+
+const diff = (object1, object2) => {
+  for (const attributeName in object1) {
+    object1[attributeName] = object1[attributeName];
+  }
+  if (object1 in object2) return false;
+  for (const attributeName in object2) {
+    object1[attributeName] = object2[attributeName];
+    delete object1[attributeName];
+  }
+  return object1;
+};
+
+const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
+console.log(result);
