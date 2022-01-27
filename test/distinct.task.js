7e39f833c67ef759152f33529bfb901675ada515
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-27 19:38:48 +0300

    Tests[Distinct]: Error messages, test cases in array

--- test/distinct.task.js
+++ test/distinct.task.js
@@ -3,8 +3,17 @@
 const assert = require('node:assert/strict');
 const distinct = require('../Tasks/distinct');
 
-assert.equal(typeof distinct, 'function');
-assert.deepStrictEqual(distinct([1, 2, 1, 3, 1, 4]), [1, 2, 3, 4]);
-assert.deepStrictEqual(distinct([4, 6, 2, 6, 2]), [4, 6, 2]);
+const ERR_FUNCTION_NOT_IMPORTED = 'The function is expected';
+
+const testCases = [
+  { input: [[1, 2, 1, 3, 1, 4]], expected: [1, 2, 3, 4] },
+  { input: [[4, 6, 2, 6, 2]], expected: [4, 6, 2] },
+];
+
+assert.equal(typeof distinct, 'function', ERR_FUNCTION_NOT_IMPORTED);
+
+for (const testCase of testCases) {
+  assert.deepStrictEqual(distinct(...testCase.input), testCase.expected);
+}
 
 console.log('[Distinct] Tests passed successfully');
