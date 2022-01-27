513473bbc4255d24a40c3649d7d3eca6781ad285
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-27 20:18:12 +0300

    Tests[Runner]: Invoke eslint before testing

--- test/tasks.js
+++ test/tasks.js
@@ -2,6 +2,26 @@
 
 const fs = require('node:fs');
 const path = require('node:path');
+const { spawn } = require('node:child_process');
+
+const lintCode = (taskName) => {
+  const taskFilePath = path.join(__dirname, '../Tasks', `${taskName}.js`);
+
+  const childOptions = { shell: true, stdio: 'inherit' };
+  const eslintProcess = spawn('npx', ['eslint', taskFilePath], childOptions);
+
+  console.log(`[Lint] Running eslint on ${taskName}`);
+
+  eslintProcess.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  eslintProcess.on('close', () => {
+    console.log(`[Lint] Finished eslint on ${taskName}\n`);
+  });
+
+  return eslintProcess;
+};
 
 const cliArguments = process.argv.slice(2);
 if (cliArguments.length === 0) {
@@ -14,5 +34,6 @@ for (const taskName of cliArguments) {
     throw new Error(`Failed to find test file for "${taskName}"`);
   }
 
-  require(filePath);
+  const eslintProcess = lintCode(taskName);
+  eslintProcess.on('close', () => require(filePath));
 }
