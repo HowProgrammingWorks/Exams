fa499af5a1e883daeb27423405cdbdffa65bbc42
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-30 23:42:06 +0200

    Fix indentation

--- Example/7-size.js
+++ Example/7-size.js
@@ -12,7 +12,7 @@ const SIZE_UNITS = ['byte', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
 const size = (bytes) => {
   const digits = bytes.toString().length;
   const exp = Math.floor((digits - 1) / 3);
-  const value = (bytes/1000 ** exp).toFixed();
+  const value = (bytes / 1000 ** exp).toFixed();
   return `${value} ${SIZE_UNITS[exp]}`;
 };
 
