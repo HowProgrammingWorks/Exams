f7404e86d993fdc6d13c38012c747bd321057e95
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-30 23:55:02 +0200

    Implement size with while loop

---
+++ Example/8-size.js
@@ -0,0 +1,22 @@
+// Convert number to file size in bytes, kb, mb, and gb
+
+// Step 8
+//
+// Use while instead of log
+
+'use strict';
+
+const SIZE_UNITS = ['byte', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+
+const size = (bytes) => {
+  let e = 0;
+  let n = bytes;
+  while (n >= 1000) {
+    n /= 1000;
+    e++;
+  }
+  return `${n.toFixed()} ${SIZE_UNITS[e]}`;
+};
+
+const result = size(123456);
+console.log(result);
