b8e95e939ffe41fcfc8a66b389acbe2af97a713e
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-30 23:55:22 +0200

    Implement size with recursion

---
+++ Example/9-size.js
@@ -0,0 +1,15 @@
+// Convert number to file size in bytes, kb, mb, and gb
+
+// Step 9
+//
+// Implement with recursion
+
+'use strict';
+
+const SIZE_UNITS = ['byte', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+
+const size = (n, e = 0) =>
+  (n < 1000 ? `${n.toFixed()} ${SIZE_UNITS[e]}` : size(n / 1000, ++e));
+
+const result = size(123456);
+console.log(result);
