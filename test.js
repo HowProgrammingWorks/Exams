f9fe9ecf48ee45c2ba3d7b385354d603d4e4d831
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-02-01 01:46:16 +0200

    Improve test output

--- test.js
+++ test.js
@@ -2,7 +2,6 @@
 
 const assert = require('assert').strict;
 
-const COLOR_WARN = '\x1b[1;33m';
 const COLOR_ERROR = '\x1b[0;31m';
 const COLOR_INFO = '\x1b[1;37m';
 const COLOR_NORM = '\x1b[0m';
@@ -11,26 +10,36 @@ const logger = (color) => (s) => {
   console.log(color + s + COLOR_NORM);
 };
 
-logger.warn = logger(COLOR_WARN);
 logger.error = logger(COLOR_ERROR);
 logger.info = logger(COLOR_INFO);
+
+const serialize = (args) => [...args].map((x) => {
+  const type = typeof x;
+  if (type === 'object') return JSON.stringify(x);
+  if (type === 'string') return `'${x}'`;
+  if (type === 'number') return x.toString();
+  return x.toString();
+}).join(', ');
 
 module.exports = (cases) => (fn) => {
   let passed = 0;
   for (const [args, expected] of cases) {
-    const msg = `Case: ${fn.name}(${[...args].join(', ')}) ->`;
+    const msg = `Case: ${fn.name}(${serialize(args)}) ->`;
     let res;
     try {
       res = fn(...args);
     } catch (err) {
-      logger.error(`${msg} ${res}, exception: ${err.stack}`);
+      const result = JSON.stringify(res);
+      logger.error(`${msg} ${result}, exception: ${err.stack}`);
       continue;
     }
     try {
       assert.deepEqual(res, expected);
       passed++;
     } catch {
-      logger.warn(`${msg} ${res}, expected: ${expected}`);
+      const result = JSON.stringify(res);
+      const ex = JSON.stringify(expected);
+      logger.error(`${msg} ${result}, expected: ${ex}`);
     }
   }
   logger.info(`Passed: ${passed} of ${cases.length}`);
