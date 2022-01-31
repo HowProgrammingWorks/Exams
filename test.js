d044ea409ce939fe167f13c0d0d302ec5ebfd536
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-01-31 23:59:28 +0200

    Add colors

--- test.js
+++ test.js
@@ -2,6 +2,19 @@
 
 const assert = require('assert').strict;
 
+const COLOR_WARN = '\x1b[1;33m';
+const COLOR_ERROR = '\x1b[0;31m';
+const COLOR_INFO = '\x1b[1;37m';
+const COLOR_NORM = '\x1b[0m';
+
+const logger = (color) => (s) => {
+  console.log(color + s + COLOR_NORM);
+};
+
+logger.warn = logger(COLOR_WARN);
+logger.error = logger(COLOR_ERROR);
+logger.info = logger(COLOR_INFO);
+
 module.exports = (cases) => (fn) => {
   let passed = 0;
   for (const [x, expected] of cases) {
@@ -10,15 +23,15 @@ module.exports = (cases) => (fn) => {
     try {
       res = fn(x);
     } catch (err) {
-      console.log(`${msg} ${res}, exception: ${err.stack}`);
+      logger.error(`${msg} ${res}, exception: ${err.stack}`);
       continue;
     }
     try {
       assert.deepEqual(res, expected);
       passed++;
     } catch {
-      console.log(`${msg} ${res}, expected: ${expected}`);
+      logger.warn(`${msg} ${res}, expected: ${expected}`);
     }
   }
-  console.log(`Passed: ${passed} of ${cases.length}`);
+  logger.info(`Passed: ${passed} of ${cases.length}`);
 };
