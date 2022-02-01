34c257ca84bc2dd32abcaa0b7f8a458264cc4e97
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-02-01 16:28:26 +0200

    Fox plane (not working example)
    Closes: https://github.com/HowProgrammingWorks/Exams/issues/129

--- NotFormatted/plane.js
+++ NotFormatted/plane.js
@@ -1,7 +1,7 @@
 // Make nested array plane
 
 const_plane = function(arr, res=[]) {
-  j = 0 for(let i = 0, length = arr.length; i < length; i++){
+  j = 0; for(let i = 0, length = arr.length; i < length; i++){
     value = arr[i];
     j = i
     if (Array.isArray(value, typeof value) && [i, length]) {
