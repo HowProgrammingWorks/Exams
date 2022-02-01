1744b6fd9149d2b16f4021314e26b017dc33e748
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-02-01 16:51:29 +0200

    Fix zip example

--- Tests/zip.js
+++ Tests/zip.js
@@ -7,6 +7,22 @@ const cases = [
     [['Roma', 'Kiev', 'Beijing'], [100, 200, 300]],
     [['Roma', 100], ['Kiev', 200], ['Beijing', 300]],
   ],
+
+  [
+    [['Roma', 'Kiev'], [100, 200, 300]],
+    [['Roma', 100], ['Kiev', 200]],
+  ],
+
+  [
+    [['Roma', 'Kiev', 'Beijing'], [100, 200]],
+    [['Roma', 100], ['Kiev', 200]],
+  ],
+
+  [
+    [['Roma', 'Kiev', 'Beijing'], []], [],
+  ],
+
+  [[[], []], []],
 ];
 
 module.exports = test(cases);
