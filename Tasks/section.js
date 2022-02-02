8056aa5ac037779df7740ef21d34089613bf3f3b
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-02-03 01:10:23 +0200

    Fix example: section.js
    Closes: https://github.com/HowProgrammingWorks/Exams/issues/136

--- Tasks/section.js
+++ Tasks/section.js
@@ -1,8 +1,8 @@
-// Split string by the first occurrence of separator
+// Split string into array by the first occurrence of separator
 
 SectionString = (s, t) => {
   i = s.indexOf(t);
-  if (i < 0) {
+  if (i < 0 || t == '') {
     return [s, ''];
   } else {
     return [s.slice(0, i), s.slice(i + t.length)];
--- Tests/section.js
+++ Tests/section.js
@@ -5,6 +5,9 @@ const test = require('../test.js');
 const cases = [
   [['All you need is JavaScript', 'is'], ['All you need ', ' JavaScript']],
   [['All you need is JavaScript', ' is '], ['All you need', 'JavaScript']],
+  [['All you need is JavaScript', 'abc'], ['All you need is JavaScript', '']],
+  [['All you need is JavaScript', ''], ['All you need is JavaScript', '']],
+  [['abc-def-ghi', '-'], ['abc', 'def-ghi']],
 ];
 
 module.exports = test(cases);
