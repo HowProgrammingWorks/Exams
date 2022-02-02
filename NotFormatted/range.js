12406d6c4d3b4b2835a83b70e01459b8640875b8
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-02-02 15:34:57 +0200

    Fix example: range
    Refs: https://github.com/HowProgrammingWorks/Exams/issues/136

--- NotFormatted/range.js
+++ NotFormatted/range.js
@@ -2,7 +2,7 @@
 
 ;_range = ( ...Range ) => {;
   let [from,to] = Range
-  if ( to   > from) {;
+  if ( to   >= from) {;
     Range = new Array(to - from + 1);
     for ( let i=from;i<=to;i++ ){
       Range[i - from] = i;
