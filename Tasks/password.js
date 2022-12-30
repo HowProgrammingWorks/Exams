dfc26ee2f13d5fc2a2cae18112faa16598709871
Victoria <80917462+witchkaa@users.noreply.github.com>
2022-12-31 01:20:32 +0200

    Change naming style and variable declarations

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,13 +1,13 @@
 // Generate random password
 
-let GeneratePassword = (alphabet, length) => {
+const generatePassword = (alphabet, length) => {
   const MAX = alphabet.length;
-  key = '';
+  let key = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
+    const index = Math.floor(Math.random() * MAX);
+    key = key + alphabet[index];
   }
   return key;
 };
 
-require('../Tests/password.js')(GeneratePassword);
+require('../Tests/password.js')(generatePassword);
