55572c5696ba805b10c4fc515a396244ed5d8612
Victoria <80917462+witchkaa@users.noreply.github.com>
2022-12-31 01:36:25 +0200

    Style fix

--- Tasks/password.js
+++ Tasks/password.js
@@ -7,8 +7,8 @@ const generatePassword = (symbols, length) => {
   let key = '';
   for (let i = 0; i < length; i++) {
     const index = Math.floor(Math.random() * MAX);
-    key = key + symbols[index];
-  }
+    key += symbols[index];
+  };
   return key;
 };
 
