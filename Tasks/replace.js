b3fcd99d8ff3171c8a803dc2d3e19cf79f957b2e
BbIXOD <108470719+BbIXOD@users.noreply.github.com>
2023-01-01 12:43:19 +0200

    Remove unnececary breckets
    And add semicolon in first line

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,18 +1,15 @@
-'use strict'
+'use strict';
 // Replace substring with newStr
 
 replace = (str, substr, newStr) => {
-  if (substr === '') {
-    return str;
-  }
+  if (substr === '') return str;
   else {
     let src = str;
     let res = '';
     while (true) {
       const index = src.indexOf(substr);
-      if (index === -1) {
-        return res + src;
-      } else {
+      if (index === -1) return res + src;
+      else {
         const start = src.substring(0, index);
         src = src.substring(index + substr.length, src.length);
         res += start + newStr;
