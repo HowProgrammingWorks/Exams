63832cc3dcbb5674c5e494837001cc6fe0e64fba
dtimofeev <td090833@gmail.com>
2023-01-05 22:40:29 +0200

    Removed extra 'else' block + filled result

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -5,12 +5,10 @@ const intersection = (object1, object2) => {
   const result = {};
   for (const key of object1Keys) {
     if (object2[key] === object1[key]) {
-      object2[key] = object1[key];
-    } else {
-      delete object1[key];
+      result[key] = object1[key];
     }
   }
-  return object1;
+  return result;
 };
 
 require('../Tests/intersection.js')(intersection);
