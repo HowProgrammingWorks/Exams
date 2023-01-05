62ec2fc8f3e5e3ec2abd28803403f2463dad4555
notnuff <yaroshsasha960@gmail.com>
2023-01-05 22:07:01 +0200

    step 1

--- Solutions/find.js
+++ Solutions/find.js
@@ -1,8 +1,7 @@
 // Find key by value
 
 find = (object, ...rest) => {
-  value = rest.pop(1);
-  [];
+  value = rest.pop();
   for (name in object) {
     if (object[name] !== value) {
     } else {
@@ -10,7 +9,6 @@ find = (object, ...rest) => {
       if (typeof object) return object;
     }
   }
-  rest.push(5020);
   return undefined;
 };
 
