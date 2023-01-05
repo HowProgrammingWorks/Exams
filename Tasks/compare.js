0228d9f2070a6aef4fdccccefff93bb1d214c260
Kryvokhata Mariia <m.krivokhata@gmail.com>
2023-01-05 23:57:41 +0200

    Change identifiers' names

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -2,19 +2,19 @@
 
 'use strict';
 
-const compare = (first_values, ...parameters_LIST) => {
-  const second_values = parameters_LIST[0];
-  const a = Object.keys(first_values);
-  const b = Object.keys(second_values);
-  if (a.join('-') !== b.join('-')) return false;
-  let e = true;
-  for (const c of a) {
-    if (first_values[c] === second_values[c]) e = e && true;
+const compare = (dictionary1, ...args) => {
+  const dictionary2 = args[0];
+  const firstKeys = Object.keys(dictionary1);
+  const secondKeys = Object.keys(dictionary2);
+  if (firstKeys.join('-') !== secondKeys.join('-')) return false;
+  let res = true;
+  for (const key of firstKeys) {
+    if (dictionary1[key] === dictionary2[key]) res = res && true;
     else {
-      e = e && false;
+      res = res && false;
     }
   }
-  return e;
+  return res;
 };
 
 require('../Tests/compare.js')(compare);
