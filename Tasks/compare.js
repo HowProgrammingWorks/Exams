915df5f5b013766c75906c0ea15e31dd925b2014
Kryvokhata Mariia <m.krivokhata@gmail.com>
2023-01-06 00:24:15 +0200

    Remove variable 'res'

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -7,14 +7,10 @@ const compare = (dictionary1, ...args) => {
   const firstKeys = Object.keys(dictionary1);
   const secondKeys = Object.keys(dictionary2);
   if (firstKeys.join('-') !== secondKeys.join('-')) return false;
-  let res = true;
   for (const key of firstKeys) {
-    if (dictionary1[key] === dictionary2[key]) res = res && true;
-    else {
-      res = res && false;
-    }
+    if (dictionary1[key] !== dictionary2[key]) return false;
   }
-  return res;
+  return true;
 };
 
 require('../Tests/compare.js')(compare);
