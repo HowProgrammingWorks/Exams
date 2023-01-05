fd6f543cc4af3389f736ba1b7d5a27aba91a58ab
dtimofeev <td090833@gmail.com>
2023-01-05 22:01:49 +0200

    adding tabulations

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,15 +1,16 @@
 // Make nested array plane
 'use strict';
 const planedArray = (arr) => {
-    const res = [];
-    for(const value of arr) {
+  const res = [];
+  for (const value of arr) {
     if (Array.isArray(value)) {
-    res.push(...planedArray(value));
-    }else{
-    res.push(value);
+      res.push(...planedArray(value));
+    } else {
+      res.push(value);
     }
   }
   return res;
 };
-
+const result = planedArray([[1, [[2]], [3, 4], [5], [6, [7, 8]]]]);
+console.log(result);
 require('../Tests/plane.js')(planedArray);
