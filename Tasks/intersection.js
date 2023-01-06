89bf4b5043af18a43bd26156bc2fed5d61731076
ipassternak <retriumpf@gmail.com>
2023-01-06 16:02:43 +0200

    1-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,23 +1,19 @@
 // Find an intersection of two dictionaries
 
+// Step 1
+// Add 'use strict';
+// Remove empty blocks
+
+'use strict';
+
 intersection = function intersection(object_1, object_2) {
   first_keys = Object.keys(object_1);
-  {
-  }
   for (attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
     } else {
       delete object_1[attribute_name];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object_1;
 };
