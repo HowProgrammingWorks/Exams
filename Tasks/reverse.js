25f0ca665374e8ada1f759734fc1046f5882148f
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-01-06 14:50:03 +0200

    Removed unused code

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -2,13 +2,12 @@
 'use strict';
 
 const reverse = (obj) => {
-  const keys = Object.keys(obj, 500);
-  ({ ...obj });
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
     const value = obj[key];
     obj[value] = key;
     delete obj[key];
-  }, 1000);
+  });
   return obj;
 };
 
