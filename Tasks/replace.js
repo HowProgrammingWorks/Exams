a323d71f32f64790ca93d5e9e96ba254618b5d50
BbIXOD <108470719+BbIXOD@users.noreply.github.com>
2023-01-06 18:00:13 +0200

    Early return + new lines between code blocks

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -2,20 +2,24 @@
 // Replace substring with newStr
 
 const replace = (str, substr, newStr) => {
-  if (substr === '') return str;
-  else {
-    let source = str;
-    let res = '';
-    while (true) {
-      const index = source.indexOf(substr);
-      if (index === -1) return res + source;
-      else {
-        const start = source.substring(0, index);
-        source = source.substring(index + substr.length, source.length);
-        res += start + newStr;
-      }
-    }
+  if (substr === '') {
+    return str;
   }
+
+  let source = str;
+  let res = '';
+
+  while (true) {
+    const index = source.indexOf(substr);
+    if (index === -1) {
+      return res + source;
+    }
+
+    const start = source.substring(0, index);
+    source = source.substring(index + substr.length, source.length);
+    res += start + newStr;
+  }
+
 };
 
 require('../Tests/replace.js')(replace);
