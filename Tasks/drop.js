e2862512db047f41db444ce22c0c803237256b2d
ipassternak <retriumpf@gmail.com>
2023-01-06 15:35:40 +0200

    3-drop.js

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,20 +1,17 @@
 // Delete listed keys from dictionary
 
-// Step 2
-// Add `const`
-// Change identifiers names and case
+// Step 3
+// Use `for (...of...)` instead of `forEach`
 
 'use strict';
 
 const drop = (dict, ...listedKeys) => {
   const keys = Object.keys(dict);
-  keys.forEach(
-    (x) => {
-      if (listedKeys.includes(x)) {
-        delete dict[x];
-      }
+  for (const key of keys) {
+    if (listedKeys.includes(key)) {
+      delete dict[key];
     }
-  );
+  }
   return dict;
 };
 
