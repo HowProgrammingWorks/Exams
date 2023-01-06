ccc68ddc258b2e9b00cfa4d9a3f5950bec372d4f
ipassternak <retriumpf@gmail.com>
2023-01-06 15:22:34 +0200

    1-drop.js

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,23 +1,20 @@
 // Delete listed keys from dictionary
 
+// Step 1
+// Add 'use strict';
+// Remove useless condition, blocks and assignments
+
+'use strict';
+
 DroP = (D, ...X) => {
-  T = 100;
   T = Object.keys(D);
   T.forEach(
     (_) => {
-      {
-        T = [D, X];
-      }
-      if (X.includes(_) && true == 1) {
+      if (X.includes(_)) {
         delete D[_];
-        {
-          T = T;
-        }
       }
-    },
-    ['uno', 'due', 'tre']
+    }
   );
-  T = D;
   return D;
 };
 
