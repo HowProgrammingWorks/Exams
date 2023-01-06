88d94c642f2795904565eff4ee2fa1b904f28678
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-01-06 15:25:59 +0200

    Added const or let for identifiers

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -2,10 +2,10 @@
 
 'use strict';
 
-DISTINCT = (data) => {
+const DISTINCT = (data) => {
   console.log({ data });
-  A = new Set();
-  w = 0;
+  const A = new Set();
+  let w = 0;
   data.forEach((a) => {
     if (A.has(a)) {
       delete data[w];
