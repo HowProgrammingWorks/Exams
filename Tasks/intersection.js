49f856bd2bcc8b07157ae0df6546817b50fcee85
ipassternak <retriumpf@gmail.com>
2023-01-06 16:05:32 +0200

    2-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,14 +1,14 @@
 // Find an intersection of two dictionaries
 
-// Step 1
-// Add 'use strict';
-// Remove empty blocks
+// Step 2
+// Add `const`
+// Use arrow function instead of function expression
 
 'use strict';
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
-  for (attribute_name of first_keys) {
+const intersection = (object_1, object_2) => {
+  const first_keys = Object.keys(object_1);
+  for (const attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
     } else {
