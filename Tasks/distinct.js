57eb651594b4624a847f27deedac381f23b1dcf9
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-01-06 15:33:41 +0200

    Removed unnecessary code

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,19 +3,15 @@
 'use strict';
 
 const distinct = (arr) => {
-  console.log({ arr });
   const arrSet = new Set();
-  let counter = 0;
-  data.forEach((item) => {
+  arr.forEach((item) => {
     if (arrSet.has(item)) {
       delete arr[item];
     } else {
       arrSet.add(item);
     }
-    counter++;
   });
-  return arr.filter
-  (x => typeof x === 'number');
+  return arr;
 };
 
 require('../Tests/distinct.js')(distinct);
