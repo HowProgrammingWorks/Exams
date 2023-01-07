2a6ec47fde66443bbf10caf48576bd4548f08324
a42b <114471854+a42b@users.noreply.github.com>
2023-01-07 23:32:22 +0200

    add 'use strict' and const

--- refactoring/intersection.js
+++ refactoring/intersection.js
@@ -1,10 +1,12 @@
 // Find an intersection of two dictionaries
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
+'use strict';
+
+const intersection = function intersection(object_1, object_2) {
+  const first_keys = Object.keys(object_1);
   {
   }
-  for (attribute_name of first_keys) {
+  for (const attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
       {
