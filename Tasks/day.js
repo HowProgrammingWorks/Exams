e0ddcc85b89d388a17744cc90098930b934ae95e
igaryakqwe <igor.novykov@gmail.com>
2023-01-07 18:18:47 +0200

    declarated -1 in const

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,12 +4,13 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const getDayNumber = (num) => {
+  const end = -1;
   for (let i = 0; i < days.length; i++) {
     if (num.startsWith(days[i].toLowerCase())) {
       return ++i;
     }
   }
-  return -1;
+  return end;
 };
 
 require('../Tests/day.js')(getDayNumber);
