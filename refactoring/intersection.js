d9331758b84bd4eee77771ff85398ee3e1487471
a42b <114471854+a42b@users.noreply.github.com>
2023-01-07 23:29:32 +0200

    choose task - intersection

---
+++ refactoring/intersection.js
@@ -0,0 +1,25 @@
+// Find an intersection of two dictionaries
+
+intersection = function intersection(object_1, object_2) {
+  first_keys = Object.keys(object_1);
+  {
+  }
+  for (attribute_name of first_keys) {
+    if (object_1[attribute_name] === object_2[attribute_name]) {
+      object_2[attribute_name] = object_1[attribute_name];
+      {
+      }
+    } else {
+      delete object_1[attribute_name];
+      {
+      }
+    }
+    {
+    }
+  }
+  {
+  }
+  return object_1;
+};
+
+require('../Tests/intersection.js')(intersection);
