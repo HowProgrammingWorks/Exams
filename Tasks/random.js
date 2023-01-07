f9a6660d68d0817277cba41e4b76b8612626e74e
igaryakqwe <igor.novykov@gmail.com>
2023-01-07 16:59:19 +0200

    deleted extra elements

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,12 +2,7 @@
 'use strict';
 
 const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+  return min + Math.floor(Math.random() * (max - min + 1));
 };
 
 require('../Tests/random.js')(random);
