ed61c0a70c10a4a17a40c8a85e7e5c9fe392e199
igaryakqwe <igor.novykov@gmail.com>
2023-01-07 17:00:53 +0200

    improved code readability

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const random = (min, max) => {
-  return min + Math.floor(Math.random() * (max - min + 1));
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 require('../Tests/random.js')(random);
