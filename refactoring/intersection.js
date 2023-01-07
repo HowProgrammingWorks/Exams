056de8d8ff96ff4d0c123a8ba4d4fa61f3a521b9
a42b <114471854+a42b@users.noreply.github.com>
2023-01-07 23:39:04 +0200

    create new object, remove else
    create new object in order to not modify incoming data

--- refactoring/intersection.js
+++ refactoring/intersection.js
@@ -3,15 +3,14 @@
 'use strict';
 
 const intersection = (object_1, object_2) => {
+  const intersecObj = {};
   const first_keys = Object.keys(object_1);
   for (const attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
-    } else {
-      delete object_1[attribute_name];
-    }
+      intersecObj[attribute_name] = object_1[attribute_name];
+    } 
   }
-  return object_1;
+  return intersecObj;
 };
 
 require('../Tests/intersection.js')(intersection);
