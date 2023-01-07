cb167d33e4c33b6cad8924092e93680f0fb26ae1
mgcvegetable <maaagicvegetable@gmail.com>
2023-01-07 02:57:45 +0200

    removed another useless variable

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -2,17 +2,16 @@
 // Replace substring with newstr
 
 const replace = (str, substr, newstr) => {
-  let searchStr = str;
   let res = '';
   if (substr !== '') {
-    let index = searchStr.indexOf(substr);
+    let index = str.indexOf(substr);
     while (index !== -1){
-      res += searchStr.substring(0, index) + newstr;
-      searchStr = searchStr.substring(index + substr.length);
-      index = searchStr.indexOf(substr);
+      res += str.substring(0, index) + newstr;
+      str = str.substring(index + substr.length);
+      index = str.indexOf(substr);
     };
   }
-  return res + searchStr;
+  return res + str;
 };
 
 require('../Tests/replace.js')(replace);
