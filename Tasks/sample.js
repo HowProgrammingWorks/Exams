aca03b071a7aada5fc75b0aeb7a48243767f12e1
igaryakqwe <igor.novykov@gmail.com>
2023-01-07 16:19:35 +0200

    change name of function

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,9 +1,9 @@
 // Get one random element from an array
 'use strict';
 
-const SAMPLE = (arr) => {
+const randomElement = (arr) => {
   arr = arr[Math.floor(Math.random() * arr.length)];
   return arr;
 };
 
-require('../Tests/sample.js')(SAMPLE);
+require('../Tests/sample.js')(randomElement);
