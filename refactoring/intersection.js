48071bf5635ad654b1b9ef93bdced7eab734043a
a42b <114471854+a42b@users.noreply.github.com>
2023-01-07 23:33:38 +0200

    delete empty block statements

--- refactoring/intersection.js
+++ refactoring/intersection.js
@@ -4,22 +4,12 @@
 
 const intersection = function intersection(object_1, object_2) {
   const first_keys = Object.keys(object_1);
-  {
-  }
   for (const attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
     } else {
       delete object_1[attribute_name];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object_1;
 };
