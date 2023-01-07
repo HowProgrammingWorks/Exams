855a9a41e3c0147a9cfcf9bb5de56e334d8c765e
mgcvegetable <maaagicvegetable@gmail.com>
2023-01-07 19:59:56 +0200

    removed useless assignment operation

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,11 +3,14 @@
 
 const getValueBetween = (str, pref, suf) => {
   const indexPref = str.indexOf(pref);
-  if (indexPref === -1) return '';
+  const indexSuf = str.indexOf(suf);
+  if (indexPref === -1) return "";
   else {
-    str = str.substring(indexPref + pref.length);
-    const indexSuf = str.indexOf(suf);
-    return suf ? (indexSuf !== -1 ? str.substring(0, indexSuf) : "") : str;
+    return suf
+      ? indexSuf !== -1
+        ? str.substring(indexPref + pref.length, indexSuf)
+        : ""
+      : str.substring(indexPref + pref.length);
   }
 };
 
