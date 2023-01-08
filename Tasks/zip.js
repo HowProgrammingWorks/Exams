2dbf364199ff01f16b60c939346680d5c742def7
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2023-01-08 17:25:16 +0200

    added semicolon

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,5 +1,5 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
-'use strict'
+'use strict';
 
 const zip = (arr1, arr2) => {
   let i = 0, 
@@ -7,7 +7,7 @@ const zip = (arr1, arr2) => {
   for (const element of arr2) {
     const tempCell = [arr1[i++], element];
     i < j ? delete arr1[i++] : arr2[j++] = tempCell;
-    if (tempCell[0] == null) arr2.length -= 1
+    if (tempCell[0] == null) arr2.length -= 1;
   }
   return arr2;
 };
