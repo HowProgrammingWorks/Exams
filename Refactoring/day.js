0b497472096651d8ad788fb7756f7649c24ab6b1
Kabadzh0b <chubbudda@gmail.com>
2023-01-08 19:27:02 +0200

    Tasks choosed

---
+++ Refactoring/day.js
@@ -0,0 +1,11 @@
+// Get day number
+
+let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+_parse_day_=s=>{
+  let i; for(i=0;i<D.length;i++){
+  if(s.startsWith(D[i].toLowerCase()))
+  {return i +1};};return -1;
+};
+
+require('../Tests/day.js')(_parse_day_);
---
+++ Refactoring/month.js
@@ -0,0 +1,15 @@
+// Get month number
+
+Months = [
+'jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'
+]
+
+Month=s=>{
+  l=Months.length
+    for(let i=0;i<l;i++){;
+      if(s.toLowerCase().startsWith(Months[i]))return(i+1)
+        };
+          return -1
+            }
+
+require('../Tests/month.js')(Month);
