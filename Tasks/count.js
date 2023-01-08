e30af229ce15b3e61cfe87b1e177ef35b23ac92e
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2023-01-08 12:12:04 +0200

    simplified the code

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,10 +2,9 @@
 'use strict'
 
 const count = obj => {
- let sum = 0;
-  const keys = Object.keys(obj);
-  for (const key of keys) {
-    const value = obj[key];
+  let sum = 0;
+  const values = Object.values(obj);
+  for(const value of values) {
     if (typeof value === 'number') sum += value;
   }
   return sum;
