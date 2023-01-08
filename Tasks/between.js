a04f7f86d96081e7b722af7501da72625c4c1bec
mgcvegetable <maaagicvegetable@gmail.com>
2023-01-08 15:55:16 +0200

    replaced three conditions with two

--- Tasks/between.js
+++ Tasks/between.js
@@ -4,11 +4,9 @@
 const getValueBetween = (str, pref, suf) => {
   const indexPref = str.indexOf(pref);
   const indexSuf = str.indexOf(suf);
-  return indexPref !== -1
-    ? suf
-      ? indexSuf !== -1
-        ? str.substring(indexPref + pref.length, indexSuf)
-        : ''
+  return indexPref != indexSuf && indexPref != -1
+    ? indexSuf !== -1
+      ? str.substring(indexPref + pref.length, indexSuf)
       : str.substring(indexPref + pref.length)
     : '';
 };
