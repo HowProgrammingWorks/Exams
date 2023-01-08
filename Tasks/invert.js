ff270e38e02ede948f00805fbf6032d59f0c9a20
kol-oss <kolyakhozhajinov@gmail.com>
2023-01-09 00:54:44 +0200

    Declare variables with const
    All functions' and variables' initialiation now is made using "const" and "let".

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,7 +1,7 @@
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
+const invert = (A, i, j, k) => {
+  const T = Object.keys(A, 4);
   T.forEach((_, i) => {
     T[i] = A.pop();
     ((x) => {
