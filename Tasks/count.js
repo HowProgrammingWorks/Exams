95de36e4de0ecc5ef0b4152a2fc050b9e36fdff9
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2023-01-08 12:08:58 +0200

    forEach change to for...of

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,10 +4,10 @@
 const count = obj => {
  let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
-  });
+  }
   return sum;
 };
 
