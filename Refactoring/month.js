c4862d551037114f568f2a4b02ef8495abfbead1
Kabadzh0b <chubbudda@gmail.com>
2023-01-08 19:43:19 +0200

    month, step1
    Format code indentation and spacing
    Use the global form of 'use strict'
    'Months' is not defined
    'Month' is not defined
    Delete 'l', use i < Months.length intead

--- Refactoring/month.js
+++ Refactoring/month.js
@@ -1,15 +1,34 @@
 // Get month number
 
-Months = [
-'jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'
-]
+// Step 1
+// Format code indentation and spacing
+// Use the global form of 'use strict'
+// 'Months' is not defined
+// 'Month' is not defined
+// Delete 'l', use i < Months.length intead
 
-Month=s=>{
-  l=Months.length
-    for(let i=0;i<l;i++){;
-      if(s.toLowerCase().startsWith(Months[i]))return(i+1)
-        };
-          return -1
-            }
+"use strict";
 
-require('../Tests/month.js')(Month);
+const Months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
+];
+
+const Month = (s) => {
+  for (let i = 0; i < Months.length; i++) {
+    if (s.toLowerCase().startsWith(Months[i])) return i + 1;
+  }
+  return -1;
+};
+
+require("../Tests/month.js")(Month);
