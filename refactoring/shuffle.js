b77945c4656877ef5542b1daa833a20fd534501a
a42b <114471854+a42b@users.noreply.github.com>
2023-01-08 03:37:38 +0200

    delete unnecessary brackets

--- refactoring/shuffle.js
+++ refactoring/shuffle.js
@@ -1,13 +1,12 @@
 // Shuffle an array
 
 'use strict';
+
 const INTERVAL_EQUALIZER = 0.5;
 const Shuffle = (List) => {
   const arr = [...List];
   arr.sort(() => Math.random() - INTERVAL_EQUALIZER);
-  {
-    return arr;
-  }
+  return arr; 
 };
 
 require('../Tests/shuffle.js')(Shuffle);
