14fdbaaaf07df8ae56181abf168d60e5e4e424e6
mgcvegetable <maaagicvegetable@gmail.com>
2023-01-08 16:52:41 +0200

    fixed first condition

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,11 +5,11 @@ const getValueBetween = (str, pref, suf) => {
   const indexPref = str.indexOf(pref);
   const cutStartIndex = indexPref + pref.length;
   const cutEndIndex = str.indexOf(suf);
-  return cutStartIndex !== cutEndIndex && indexPref !== -1
+  return indexPref !== -1 && cutStartIndex < cutEndIndex
     ? cutEndIndex !== -1
       ? str.substring(cutStartIndex, cutEndIndex)
       : str.substring(cutStartIndex)
-    : '';
+    : "";
 };
 
 require('../Tests/between.js')(getValueBetween);
