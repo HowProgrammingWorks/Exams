94b4048fe5e809cd6008e912e034c97f827fcfd0
Kabadzh0b <chubbudda@gmail.com>
2023-01-08 19:50:04 +0200

    month, step2

--- Refactoring/month.js
+++ Refactoring/month.js
@@ -1,34 +1,31 @@
 // Get month number
 
-// Step 1
-// Format code indentation and spacing
-// Use the global form of 'use strict'
-// 'Months' is not defined
-// 'Month' is not defined
-// Delete 'l', use i < Months.length intead
+// Step 2
+// Rename 's' on 'month'
+// Strings must use singlequote
 
-"use strict";
+'use strict';
 
 const Months = [
-  "jan",
-  "feb",
-  "mar",
-  "apr",
-  "may",
-  "jun",
-  "jul",
-  "aug",
-  "sep",
-  "oct",
-  "nov",
-  "dec",
+  'jan',
+  'feb',
+  'mar',
+  'apr',
+  'may',
+  'jun',
+  'jul',
+  'aug',
+  'sep',
+  'oct',
+  'nov',
+  'dec',
 ];
 
-const Month = (s) => {
+const Month = (month) => {
   for (let i = 0; i < Months.length; i++) {
-    if (s.toLowerCase().startsWith(Months[i])) return i + 1;
+    if (month.toLowerCase().startsWith(Months[i])) return i + 1;
   }
   return -1;
 };
 
-require("../Tests/month.js")(Month);
+require('../Tests/month.js')(Month);
