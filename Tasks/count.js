bea06f8f9fd304cf10b08880e13c45b17f2fa415
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2023-01-08 12:06:13 +0200

    added let for 'sum' and const for 'keys'

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,8 +2,8 @@
 'use strict'
 
 const count = obj => {
-  sum = 0;
-  keys = Object.keys(obj);
+ let sum = 0;
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
