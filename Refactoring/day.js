f1afa2156047ef2036a3dd7c0a69293fb6fbe3d7
Kabadzh0b <chubbudda@gmail.com>
2023-01-08 19:29:38 +0200

    Add 'use strict' '_parse_day_' is not defined Format code indentation and spacing

--- Refactoring/day.js
+++ Refactoring/day.js
@@ -1,11 +1,22 @@
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+// Step 1
+// Add 'use strict';
+// fix '_parse_day_' is not defined
+// Format code indentation and spacing
 
-_parse_day_=s=>{
-  let i; for(i=0;i<D.length;i++){
-  if(s.startsWith(D[i].toLowerCase()))
-  {return i +1};};return -1;
+"use strict";
+
+let D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const _parse_day_ = (s) => {
+  let i;
+  for (i = 0; i < D.length; i++) {
+    if (s.startsWith(D[i].toLowerCase())) {
+      return i + 1;
+    }
+  }
+  return -1;
 };
 
-require('../Tests/day.js')(_parse_day_);
+require("../Tests/day.js")(_parse_day_);
