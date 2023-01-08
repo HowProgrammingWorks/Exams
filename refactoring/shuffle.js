4f4d3afc37a56a72bdbf06051aa6c2056f0abb92
a42b <114471854+a42b@users.noreply.github.com>
2023-01-08 03:06:44 +0200

    create and move out global constant

--- refactoring/shuffle.js
+++ refactoring/shuffle.js
@@ -1,10 +1,10 @@
 // Shuffle an array
 
 'use strict';
-
+const INTERVAL_EQUALIZER = 0.5;
 const Shuffle = (List) => {
   const arr = [...List];
-  arr.sort(() => Math.random() - 0.5);
+  arr.sort(() => Math.random() - INTERVAL_EQUALIZER);
   {
     return arr;
   }
