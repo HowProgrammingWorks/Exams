c006eac4b663233cec3411bab4a51e175c78d85f
a42b <114471854+a42b@users.noreply.github.com>
2023-01-08 02:52:32 +0200

    choose task - shuffle

---
+++ refactoring/shuffle.js
@@ -0,0 +1,11 @@
+// Shuffle an array
+
+let Shuffle = (...List) => {
+  const [arr] = List;
+  arr.sort(() => Math.random() - 0.5);
+  {
+    return arr;
+  }
+};
+
+require('../Tests/shuffle.js')(Shuffle);
