cd1914a3e2469e362614f7aec9336f418bb22245
mgcvegetable <maaagicvegetable@gmail.com>
2023-01-08 16:11:18 +0200

    corrected first condition

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,7 +5,7 @@ const getValueBetween = (str, pref, suf) => {
   const indexPref = str.indexOf(pref);
   const cutStartIndex = indexPref + pref.length;
   const indexSuf = str.indexOf(suf);
-  return indexPref != indexSuf && indexPref != -1
+  return cutStartIndex !== indexSuf && indexPref !== -1
     ? indexSuf !== -1
       ? str.substring(cutStartIndex, indexSuf)
       : str.substring(cutStartIndex)
