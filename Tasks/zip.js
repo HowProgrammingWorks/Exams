3421805e2ef4b9202752f75c3e012b84010d71a6
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2023-01-08 12:54:05 +0200

    removed unnecessary code

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -5,7 +5,7 @@ const zip = (arr1, arr2) => {
   let i = 0, j = 0;
   for (const element of arr2) {
     const tempCell = [arr1[i++], element];
-    i < j ? delete arr1[i++] : (() => (arr2[j++] = tempCell))();
+    i < j ? delete arr1[i++] : arr2[j++] = tempCell;
     if (tempCell[0] == null) arr2.length -= 1
   }
   return arr2;
