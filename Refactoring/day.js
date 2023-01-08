0dd22ec3944d204eb419c6096517dcb947164de7
Kabadzh0b <chubbudda@gmail.com>
2023-01-08 19:33:18 +0200

    Rename 'D' into 'Days' Identifier '_parse_day_' is not in camel case

--- Refactoring/day.js
+++ Refactoring/day.js
@@ -1,20 +1,21 @@
 // Get day number
 
-// Step 2
-// 'D' is never reassigned. Use 'const' instead
+// Step 3
+// Rename 'D' into 'Days'
+// Identifier '_parse_day_' is not in camel case
 
 "use strict";
 
-const D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-const _parse_day_ = (s) => {
+const parseDay = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < Days.length; i++) {
+    if (s.startsWith(Days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-require("../Tests/day.js")(_parse_day_);
+require("../Tests/day.js")(parseDay);
