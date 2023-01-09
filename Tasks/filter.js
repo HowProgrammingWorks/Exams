b5154f3dd324199828360e820e12dc5114651489
Tytianiuk Artem <tytyanyuk04@gmail.com>
2023-01-09 14:18:29 +0200

    used const to initialize function and constant

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,15 +2,15 @@
 
 'use strict'
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
+const Filter = (T, t) => {
+  const remove = [];
+  for (const C of T) {
+    const x = T.indexOf(C);
     if (typeof T[x] !== t) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
+  for (const x of remove) T.splice(x, 1);
   return T;
 };
 
