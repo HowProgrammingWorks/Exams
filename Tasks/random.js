b54c8884ebe189e51d1820b2454d48be199780e6
DanyloM73 <danyamalanin7@gmail.com>
2023-01-09 21:42:55 +0200

    Simplify return expression

--- Tasks/random.js
+++ Tasks/random.js
@@ -3,7 +3,7 @@
 
 const random = (min, max) => {
   if (max === undefined) max = min;
-  return min + Math.floor(Math.random() * (max - min + 1));
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
 require('../Tests/random.js')(random);
