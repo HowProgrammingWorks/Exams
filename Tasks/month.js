0a3c8f0c5d6c737f5b8ee730c3b9e99150eb7132
Tytianiuk Artem <tytyanyuk04@gmail.com>
2023-01-09 14:30:51 +0200

    used const to initialize function and constants

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-Months = [
+const Months = [
   'jan',
   'feb',
   'mar',
@@ -17,8 +17,8 @@ Months = [
   'dec',
 ];
 
-Month = (s) => {
-  l = Months.length;
+const Month = (s) => {
+  const l = Months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
