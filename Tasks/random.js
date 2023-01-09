13202ee7db41fe9d034a84c77f8ba49a540cc991
DanyloM73 <danyamalanin7@gmail.com>
2023-01-09 21:42:09 +0200

    Remove block statement in if statement

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,9 +2,7 @@
 'use strict';
 
 const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-  }
+  if (max === undefined) max = min;
   return min + Math.floor(Math.random() * (max - min + 1));
 };
 
