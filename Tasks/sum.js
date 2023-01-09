117a43fd504fdaa7d89c5e3e352df0138f5ed375
DanyloM73 <danyamalanin7@gmail.com>
2023-01-09 21:26:55 +0200

    Remove unneeded blocks

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -3,19 +3,15 @@
 
 const sum = function (s = () => {}) {
   const sum = [0];
-  let k = 5;
   for (const i of s) {
-    let t = typeof i;
-    if (t === 'number') {
+    if (typeof i === 'number') {
       if (sum.length > 0) {
         const new_Sum = sum[sum.length - 1] + i;
         sum.push(new_Sum);
       }
     }
   }
-  sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
 };
 
 require('../Tests/sum.js')(sum);
