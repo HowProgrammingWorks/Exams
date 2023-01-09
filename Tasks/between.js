9cf1aa4f5fc1fa9549359e0ed55f00ecf6707e56
magicvegetable <105551545+magicvegetable@users.noreply.github.com>
2023-01-09 22:57:24 +0200

    Update between.js
    fixed between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,11 +5,17 @@ const getValueBetween = (str, pref, suf) => {
   const indexPref = str.indexOf(pref);
   const cutStartIndex = indexPref + pref.length;
   const cutEndIndex = str.indexOf(suf);
-  return indexPref !== -1 && cutStartIndex < cutEndIndex
-    ? cutEndIndex !== -1
-      ? str.substring(cutStartIndex, cutEndIndex)
-      : str.substring(cutStartIndex)
-    : "";
+  let res = '';
+  if(indexPref !== -1 && cutStartIndex < cutEndIndex) {
+    if(cutEndIndex !== -1) {
+      res = str.substring(cutStartIndex, cutEndIndex)
+    } else {
+      res = str.substring(cutStartIndex)
+    }
+  } else {
+    res = ''
+  }
+  return res;
 };
 
 require('../Tests/between.js')(getValueBetween);
