4055e33b74c2012173087c4603441305c932aab3
DanyloM73 <danyamalanin7@gmail.com>
2023-01-09 21:30:38 +0200

    Change function parameter

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,7 +1,7 @@
 // Sum all numbers from an array
 'use strict';
 
-const sum = function (array = () => {}) {
+const sum = function (array) {
   const sum = [0];
   for (const element of array) {
     if (typeof element === 'number') {
