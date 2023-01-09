ff01c70e14a70512c414fa36e6892179b9401c4b
magicvegetable <105551545+magicvegetable@users.noreply.github.com>
2023-01-09 22:58:38 +0200

    Update replace.js
    fixed replace.js

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,18 +1,7 @@
 'use strict';
 // Replace substring with newstr
 
-const replace = (str, substr, newstr) => {
-  let res = '';
-  if (substr) {
-    const substrLen = substr.length;
-    let substrIndex = str.indexOf(substr);
-    while (substrIndex !== -1){
-      res += str.substring(0, substrIndex) + newstr;
-      str = str.substring(substrIndex + substrLen);
-      substrIndex = str.indexOf(substr);
-    };
-  }
-  return res + str;
-};
+const replace = (str, substr, newstr) =>
+  substr ? str.replaceAll(substr, newstr) : str;
 
 require('../Tests/replace.js')(replace);
