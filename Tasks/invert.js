084b75ee283b466c7ed1e22982be3e110a408209
kol-oss <kolyakhozhajinov@gmail.com>
2023-01-10 19:51:39 +0200

    Remove unnecessary brackets

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,13 +1,13 @@
 // Reverse an array, you can't use .reverse()
 'use strict';
 
-const invert = (arr) => {
+const invert = arr => {
   const arrLength = arr.length;
   const invertedArr = new Array(arrLength);
   
-  for (let index = 0; index < arrLength; index++){
+  for (let index = 0; index < arrLength; index++)
     invertedArr[index] = arr[arrLength - 1 - index];
-  };
+  
   return invertedArr;
 };
 
