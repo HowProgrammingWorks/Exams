635f5dadb417ecc2e6e7f5d489ba3726843f4abf
Vladimir <vovakovalev2016@icloud.com>
2023-01-10 12:40:28 +0300

    changed the name of the function 'GeneratePassword' to 'generatePassword', using camelCase

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,6 +1,6 @@
 // Generate random password
 
-const GeneratePassword = (alphabet, length) => {
+const generatePassword = (alphabet, length) => {
   const MAX = alphabet.length;
   key = '';
   for (let i = 0; i < length; i++) {
@@ -10,4 +10,4 @@ const GeneratePassword = (alphabet, length) => {
   return key;
 };
 
-require('../Tests/password.js')(GeneratePassword);
+require('../Tests/password.js')(generatePassword);
