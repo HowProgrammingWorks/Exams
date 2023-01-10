fdc093e4f031cdaa3a6272f1985479b581319dfc
Dmutre <drago.modern@gmail.com>
2023-01-10 12:24:57 +0200

    Used linter

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -3,7 +3,7 @@
 
 const parseIp = (ip) => {
   const arr = ip.split('.');
-  if (arr.length != 4) return;
+  if (arr.length !== 4) return;
   for (let i = 0; i < 4; i++) {
     arr[i] = parseInt(arr[i]);
     if (isNaN(arr[i])) return;
