e1b6ae4e3a58aea140f56f55618722a029901746
notnuff <yaroshsasha960@gmail.com>
2023-01-10 18:33:57 +0200

    added 'use strict', 'const' in find.js

--- Solutions/find.js
+++ Solutions/find.js
@@ -1,8 +1,10 @@
 // Find key by value
 
-find = (object, ...rest) => {
-  value = rest.pop(1);
-  for (name in object) {
+'use strict';
+
+const find = (object, ...rest) => {
+  const value = rest.pop(1);
+  for (const name in object) {
     if (object[name] !== value) {
     } else {
       if (typeof name) return name;
