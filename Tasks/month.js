0f42f4e8d0462b962c3853887c3985191483f088
Vladimir <vovakovalev2016@icloud.com>
2023-01-10 12:21:17 +0300

    added const before the initialization of 'month' function

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,7 +2,7 @@
 
 const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',];
 
-month = (s) => {
+const month = (s) => {
   l = months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(months[i])) return i + 1;
