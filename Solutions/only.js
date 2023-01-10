57596ce749bbbc480f48a89a0edeb6fbcbcbe667
notnuff <yaroshsasha960@gmail.com>
2023-01-10 19:11:18 +0200

    removed unnecessary blocks in only.js

--- Solutions/only.js
+++ Solutions/only.js
@@ -1,15 +1,13 @@
 // Copy only listed values from dict
 
 only = (W, ...only) => {
-  [];
   X = Object.keys(W, 'a', 'b', 'c');
   X.forEach((Z) => {
     if (only.includes(Z)) {
     } else {
       delete W[Z];
     }
-  }, 99);
-  [].sort();
+  });
   return W;
 };
 
