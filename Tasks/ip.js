48b3b48a14080d76ad6c8f1244fab6c7bb2c1e98
Dmutre <drago.modern@gmail.com>
2023-01-10 12:22:55 +0200

    Deleted needless variable
    changed parsedIp on using arr and manipulating with it

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,12 +1,11 @@
-"use strict";
+'use strict';
 // Split string by the first occurrence of separator
 
 const parseIp = (ip) => {
-  const arr = [];
-  const parsedIp = ip.split('.');
-  if (parsedIp.length != 4) return;
-  for(let i = 0; i < 4; i++){
-    arr[i] = parseInt(parsedIp[i]);
+  const arr = ip.split('.');
+  if (arr.length != 4) return;
+  for (let i = 0; i < 4; i++) {
+    arr[i] = parseInt(arr[i]);
     if (isNaN(arr[i])) return;
   }
   return arr;
