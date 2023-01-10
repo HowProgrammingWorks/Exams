0367890d36401097260c62a980bd4054b7581bde
notnuff <yaroshsasha960@gmail.com>
2023-01-10 18:30:06 +0200

    removed unused blocks in find.js

--- Solutions/find.js
+++ Solutions/find.js
@@ -2,7 +2,6 @@
 
 find = (object, ...rest) => {
   value = rest.pop(1);
-  [];
   for (name in object) {
     if (object[name] !== value) {
     } else {
@@ -10,7 +9,6 @@ find = (object, ...rest) => {
       if (typeof object) return object;
     }
   }
-  rest.push(5020);
   return undefined;
 };
 
