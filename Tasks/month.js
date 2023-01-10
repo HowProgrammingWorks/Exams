69805eeb99c2c02c758bc86988b0100fe83fe833
Vladimir <vovakovalev2016@icloud.com>
2023-01-10 12:24:20 +0300

    changed the name of the 'l' identifier to 'length'

--- Tasks/month.js
+++ Tasks/month.js
@@ -3,8 +3,8 @@
 const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',];
 
 const month = (s) => {
-  l = months.length;
-  for (let i = 0; i < l; i++) {
+  length = months.length;
+  for (let i = 0; i < length; i++) {
     if (s.toLowerCase().startsWith(months[i])) return i + 1;
   }
   return -1;
