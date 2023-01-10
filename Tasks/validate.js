7a86d4515a845f686226cf44f665c31686e81cbe
Dmutre <drago.modern@gmail.com>
2023-01-10 19:45:29 +0200

    deleted useless black of code

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -5,13 +5,11 @@ const isValidate = (personName) => {
   if (!personName) return false;
   if (personName.length === 0) return false;
   if (!personName.includes(' ')) return false;
-  {
-    for (const char of personName) {
-      if (char === ' ') continue;
-      if (!char.toLowerCase().match(/[a-z]/)) return false;
-    }
-    return true;
+  for (const char of personName) {
+    if (char === ' ') continue;
+    if (!char.toLowerCase().match(/[a-z]/)) return false;
   }
+  return true;
 };
 
 require('../Tests/validate.js')(isValidate);
