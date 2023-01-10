ffef2202ce77b5fa3bba98f1eb900aa3af45224d
Dmutre <drago.modern@gmail.com>
2023-01-10 12:35:52 +0200

    changed naming

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,18 +1,18 @@
 "use strict";
 // Validate person name
 
-const isValidate = (T) => {
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
+const isValidate = (personName) => {
+  if (!personName) return false;
+  if (personName === '') return false;
+  if (typeof personName !== 'string') return false;
+  if (personName.length === 0) return false;
+  if (!personName.includes(' ')) return false;
   {
-    for (const C of T) {
-      if (C === ' ') continue;
+    for (const char of personName) {
+      if (char === ' ') continue;
       if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
+        char.toLowerCase().charCodeAt(0) >= 97 &&
+        char.toLowerCase().charCodeAt(0) <= 122
       ) {
       } else {
         return false;
