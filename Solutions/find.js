2cd014c1e5c54d0406abc4c52d99e8c09d3372ee
notnuff <yaroshsasha960@gmail.com>
2023-01-10 18:36:10 +0200

    removed unnecessary 'else' statement in find.js

--- Solutions/find.js
+++ Solutions/find.js
@@ -5,8 +5,7 @@
 const find = (object, ...rest) => {
   const value = rest.pop(1);
   for (const name in object) {
-    if (object[name] !== value) {
-    } else {
+    if (object[name] === value) {
       if (typeof name) return name;
       if (typeof object) return object;
     }
