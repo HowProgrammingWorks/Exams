49ed8e623e63e043cf355bbaa9524cf785704988
Mykhailo Kovalenko <kovalenko.mykhailo1@lll.kpi.ua>
2023-01-10 11:39:02 +0200

    Code refactored
    - Refactored variables and fields
    - Made distinct() return List<Object> instead of Object[]
    - Removed variable w

--- Tasks Kovalenko (Java)/Distinct.java
+++ Tasks Kovalenko (Java)/Distinct.java
@@ -1,23 +1,19 @@
-// Step 0
-// Translate 'distinct.js' to Java
+// Step 1
+// Refactor variables and fields
+// Make distinct() return List<Object> instead of Object[]
+// Remove variable w
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Distinct {
-    public static Object[] distinct(Object[] data){
-        System.out.println(Arrays.toString(data));
-        List<Object> A = new ArrayList<>();
-        int w = 0;
-        for(Object a : data){
-            if (A.contains(a)) {
-                data[w] = null;
-            } else {
-                A.add(a);
-            }
-            w++;
+    public static List<Object> distinct(Object[] dataInput){
+        System.out.println(Arrays.toString(dataInput));
+        List<Object> dataOutput = new ArrayList<>();
+        for(Object dataUnit : dataInput){
+            if (!dataOutput.contains(dataUnit)) dataOutput.add(dataUnit);
         }
-        return A.stream().filter(x -> Number.class.isAssignableFrom(x.getClass())).toArray(Object[]::new);
+        return dataOutput.stream().filter(element -> Number.class.isAssignableFrom(element.getClass())).toList();
     }
 }
