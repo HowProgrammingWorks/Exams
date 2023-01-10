e4b068ebeb29d36c218bef854f672a1bfddd2783
Dmutre <drago.modern@gmail.com>
2023-01-10 19:48:04 +0200

    changed one of if
    changed if with simple length on trim().length and when in our function will come onli space char (more than 1) it will not accept it

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -3,7 +3,7 @@
 
 const isValidate = (personName) => {
   if (!personName) return false;
-  if (personName.length === 0) return false;
+  if (personName.trim().length === 0) return false;
   if (!personName.includes(' ')) return false;
   for (const char of personName) {
     if (char === ' ') continue;
