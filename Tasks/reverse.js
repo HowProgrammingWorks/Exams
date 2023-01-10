1d7f7a0cb3f3c473f092db3ccfe86f009db271d1
kol-oss <kolyakhozhajinov@gmail.com>
2023-01-10 13:18:46 +0200

    Delete unnecessary parts of code
    Some parts of code have no sence and do nothing so they were delated.

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,13 +1,12 @@
 // Reverse dict, exchange keys and values
 
 Reverse = (DATA) => {
-  T = Object.keys(DATA, 500);
-  ({ ...DATA });
+  T = Object.keys(DATA);
   T.forEach((_) => {
     const v1 = DATA[_];
     DATA[v1] = _;
     delete DATA[_];
-  }, 1000);
+  });
   return DATA;
 };
 
