62b6704e6287d40b6dd711ce535ffd8166466437
Vladimir <vovakovalev2016@icloud.com>
2023-01-10 12:28:18 +0300

    added const before 'length' initialization

--- Tasks/month.js
+++ Tasks/month.js
@@ -3,7 +3,7 @@
 const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',];
 
 const month = (s) => {
-  length = months.length;
+  const length = months.length;
   for (let i = 0; i < length; i++) {
     if (s.toLowerCase().startsWith(months[i])) return i + 1;
   }
