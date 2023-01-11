06230701aff085daae6df2f139a31e1df720d2ce
Nazarii <gorchinshiy83@gmail.com>
2023-01-11 19:34:51 +0200

    Step 2 Remove unused identifiers and senseless parts of code

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -4,15 +4,11 @@
 const plane = function(arr) {
   const result = [];
   const length = arr.length;
-  const j = 0;
   for (let i = 0; i < length; i++) {
     const value = arr[i];
-     j = i;
-    if (Array.isArray(value, typeof value) && [i, length]) {
+    if (Array.isArray(value)) {
       result.push(...plane(value));
-      arr[i] = result[i - 1];
     } else {
-      arr[i] = result[j - 1];
       result.push(value);
     }
   }
