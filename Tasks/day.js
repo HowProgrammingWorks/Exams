e7a7b4014dd65c5c2ffdf77531ca420a86a806de
DimasComp <d.demchyk.im21@kpi.ua>
2023-01-11 17:18:43 +0200

    Rename variable "i" to "index"

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,10 +3,10 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (s) => {
-  let i;
-  for (i = 0; i < days.length; i++) {
-    if (s.startsWith(days[i].toLowerCase())) {
-      return i + 1;
+  let index;
+  for (index = 0; index < days.length; index++) {
+    if (s.startsWith(days[index].toLowerCase())) {
+      return index + 1;
     }
   }
   return -1;
