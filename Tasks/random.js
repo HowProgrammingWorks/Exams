86d2fd382a91dd0ddafa2f6d5164a09acbefbe60
makshv21 <khvits21@gmail.com>
2023-01-11 21:56:49 +0200

    Added +1 so as not to lose the value of the expression

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,7 +2,7 @@
 
 const random = (min, max) => {
   if (max === undefined)  max = min;
-  return Math.floor(Math.random() * (max - min) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 };
 
 require('../Tests/random.js')(Random);
