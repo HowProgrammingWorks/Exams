3adb36c2b9bb3c27ab4215999202a3ef3ae93dda
makshv21 <khvits21@gmail.com>
2023-01-11 22:02:17 +0200

    Added 'use strict' at the beginning of the program

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,5 +1,5 @@
 // Generate random integer value in given range
-
+'use strict'
 const random = (min, max) => {
   if (max === undefined)  max = min;
   return Math.floor(Math.random() * (max - min + 1) + min)
