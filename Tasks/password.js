59590659dcb69db27e7fa5e6ecb16693608d0b1f
Anton <antony.dovzhenko@gmail.com>
2023-01-12 01:01:08 +0200

    Step 8: added operator assignment to make the line easier

--- Tasks/password.js
+++ Tasks/password.js
@@ -5,7 +5,7 @@ const generatePassword = (alphabet, length) => {
   let key = '';
   for (let i = 0; i < length; i++) {
     const index = Math.floor(Math.random() * alphabet.length);
-    key = key + alphabet[index];
+    key += alphabet[index];
   }
   return key;
 };
