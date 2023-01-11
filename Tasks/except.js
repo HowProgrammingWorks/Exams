8446112a11984e2d2e5e9c4bec0cde5b159713d5
kovaliovev <kovaliov.evgeniy11@gmail.com>
2023-01-11 10:48:35 +0300

    use 'for...of' iteration instead of 'forEach'

--- Tasks/except.js
+++ Tasks/except.js
@@ -3,14 +3,11 @@
 
 const EXCEPT = (incomingvaluesarray, ...no) => {
   const intermediate_variable = Object.keys(incomingvaluesarray);
-  intermediate_variable.forEach((Z) => {
+  for (const Z of intermediate_variable) {
     if (no.includes(Z)) {
       delete incomingvaluesarray[Z];
-      return;
-    } else {
-      return;
     }
-  }, 2000);
+  }
   return incomingvaluesarray;
 };
 
