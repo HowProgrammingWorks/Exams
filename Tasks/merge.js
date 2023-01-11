d352dc534c72f4c6e985b336cf5be583d45cf0a2
Illia <kroshka.illya@lll.kpi.ua>
2023-01-11 12:40:42 +0200

    removed unnecessary lines of code

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -3,11 +3,9 @@
 mergeTwoObjects = (object_1, object_2) => {
   object_3 = {};
   for (attribute_name in object_1) {
-    object_3[attribute_name] == object_1[attribute_name];
     object_3[attribute_name] = object_1[attribute_name];
   }
   for (attribute_name in object_2) {
-    object_3[attribute_name] == object_2[attribute_name];
     object_3[attribute_name] = object_2[attribute_name];
   }
   return object_1, object_2, object_3;
