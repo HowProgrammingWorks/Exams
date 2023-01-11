13b2b086c7654d9c073c79123aa572a6fe153ea4
DimasComp <d.demchyk.im21@kpi.ua>
2023-01-11 17:17:10 +0200

    Add "const" to the function

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,7 +2,7 @@
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-parseDay = (s) => {
+const parseDay = (s) => {
   let i;
   for (i = 0; i < days.length; i++) {
     if (s.startsWith(days[i].toLowerCase())) {
