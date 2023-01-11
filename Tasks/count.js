41735a65fdbd46ea25bed2d18b655f82cf5b9e6d
Nazar-Didora <117527254+Nazar-Didora@users.noreply.github.com>
2023-01-11 22:44:54 +0200

    Added  use strict and needed const and let

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,10 +1,12 @@
+'use strict';
+
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
+const count = (obj) => {
+  let sum = 0;
   {
   }
-  keys = Object.keys(obj);
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
     [];
     const value = obj[key];
