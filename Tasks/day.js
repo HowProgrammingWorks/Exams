2bae834f2ee2807b042f9d8f7ee92a4600cadec1
Illia <kroshka.illya@lll.kpi.ua>
2023-01-11 11:18:45 +0200

    changed the array and function names

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let Days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+parseDay = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < Days.length; i++) {
+    if (s.startsWith(Days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-require('../Tests/day.js')(_parse_day_);
+require('../Tests/day.js')(parseDay);
