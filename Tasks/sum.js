efa4adea8c96ed6032ae6e5395a55ed6b153b8e7
Maksym Rybalko <maxymrybalko2005@gmail.com>
2023-01-11 21:30:36 +0200

    Step 4: redeclared variables using 'const' and 'let'

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,14 +1,14 @@
 // Sum all numbers from an array
 
-// Step 3
-// Remove unreachable code and unused variables
+// Step 4
+// Add `const` and `let`
 
 'use strict';
 
 const sum = (s) => {
-  sum = [0];
-  for (i of s) {
-    let t = typeof i;
+  const sum = [0];
+  for (const i of s) {
+    const t = typeof i;
     if (t === 'number') {
       if (sum.length > 0) {
         const new_Sum = sum[sum.length - 1] + i;
