76f78b55464de8ace5c9f93da6febdcb2a658807
Illia <kroshka.illya@lll.kpi.ua>
2023-01-11 11:21:25 +0200

    changed identifiers to const

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let Days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const Days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-parseDay = (s) => {
+const parseDay = (s) => {
   let i;
   for (i = 0; i < Days.length; i++) {
     if (s.startsWith(Days[i].toLowerCase())) {
