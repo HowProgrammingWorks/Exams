7c35c66865c9f2295c02bef6d7722c5f9959d7dd
makshv21 <khvits21@gmail.com>
2023-01-11 22:57:16 +0200

    Replaced Months[i] to months[i] to go through the elements of the array months

--- Tasks/month.js
+++ Tasks/month.js
@@ -4,7 +4,7 @@ const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov
 
 const findMonth = month => {
   for (let i = 0; i < months.length; i++) {
-    if (month.toLowerCase().startsWith(Months[i])) return i + 1;
+    if (month.toLowerCase().startsWith(months[i])) return i + 1;
   }
   return -1;
 };
