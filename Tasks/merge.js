6045dd9d9b9249e6cd8f69f1d37bc56eb3d1e964
Illia <kroshka.illya@lll.kpi.ua>
2023-01-11 12:43:31 +0200

    renamed the parameter and object names

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,14 +1,14 @@
 'use strick';
 
-mergeTwoObjects = (object_1, object_2) => {
-  object_3 = {};
-  for (attribute_name in object_1) {
-    object_3[attribute_name] = object_1[attribute_name];
+mergeTwoObjects = (object1, object2) => {
+  object3 = {};
+  for (attribute_name in object1) {
+    object3[attribute_name] = object1[attribute_name];
   }
-  for (attribute_name in object_2) {
-    object_3[attribute_name] = object_2[attribute_name];
+  for (attribute_name in object2) {
+    object3[attribute_name] = object2[attribute_name];
   }
-  return object_3;
+  return object3;
 };
 
 require('../Tests/merge.js')(mergeTwoObjects);
