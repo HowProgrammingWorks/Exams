180291ed6794bcda358c11e8d6fd00a04fa373fa
DimasComp <d.demchyk.im21@kpi.ua>
2023-01-11 19:58:08 +0200

    Add "const" before the variable "index"

--- Tasks/password.js
+++ Tasks/password.js
@@ -4,7 +4,7 @@ const GeneratePassword = (alphabet, length) => {
   const max = alphabet.length;
   let key = '';
   for (let i = 0; i < length; i++) {
-    index = Math.floor(Math.random() * max);
+    const index = Math.floor(Math.random() * max);
     key = key + alphabet[index];
   }
   return key;
