f5a875b0004207476b7e567dfcbcaac8fc4700de
makshv21 <khvits21@gmail.com>
2023-01-11 22:28:27 +0200

    Deleted l = Months.length;

--- Tasks/month.js
+++ Tasks/month.js
@@ -3,7 +3,6 @@
 const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
 
 const findMonth = month => {
-  l = Months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
