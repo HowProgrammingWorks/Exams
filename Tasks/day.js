fb48a9d96e321dce3fbb12f18a8ff7a704b735b2
Illia <kroshka.illya@lll.kpi.ua>
2023-01-11 11:47:49 +0200

    removed unnecessary brackets

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,11 +3,8 @@
 const Days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = day => {
-  for (let i = 0; i < Days.length; i++) {
-    if (day.startsWith(Days[i])) {
-      return i + 1;
-    }
-  }
+  for (let i = 0; i < Days.length; i++) 
+    if (day.startsWith(Days[i])) return i + 1;
   return -1;
 };
 
