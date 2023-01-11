5d2da79ed7d5b5d12d8451bfde6224da130f24b3
Kushnir Mykola <kmm.w.825@gmail.com>
2023-01-11 23:33:51 +0200

    except: Add use strict directive

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,7 +1,9 @@
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+'use strict';
+
+const EXCEPT = (incomingvaluesarray, ...no) => {
+  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
   intermediate_variable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
