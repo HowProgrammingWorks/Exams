e4d9ca14016501bded0c2a332c259cecb2c0f760
Anton <antony.dovzhenko@gmail.com>
2023-01-12 01:14:40 +0200

    Step 7: moved declaration of i inside loop

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,8 +4,7 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (day) => {
-  let i;
-  for (i = 0; i < days.length; i++) {
+  for (let i = 0; i < days.length; i++) {
     if (day.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
