5fab5b19395c61abdf96a630ffc06b095234c33d
Nazar-Didora <117527254+Nazar-Didora@users.noreply.github.com>
2023-01-11 23:27:05 +0200

    Changed naming, added const

--- Tasks/split.js
+++ Tasks/split.js
@@ -3,9 +3,9 @@
 // Split an array into two parts
 
 const splitArray = (splitPoint, array) => {
-  const begin = array.splice(0, splitPoint);
-  array = array.splice(-splitPoint);
-  return [begin, array];
+  const firstPart = array.splice(0, splitPoint);
+  const secondPart = array.splice(-splitPoint);
+  return [firstPart, secondPart];
 };
 
 require('../Tests/split.js')(splitArray);
