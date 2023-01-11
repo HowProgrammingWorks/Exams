3ca6d4d651038fad804df5a6e4c625633fc4ed16
Nazar-Didora <117527254+Nazar-Didora@users.noreply.github.com>
2023-01-11 23:20:59 +0200

    Changed naming  function, changed naming first argument

--- Tasks/split.js
+++ Tasks/split.js
@@ -2,11 +2,11 @@
 
 // Split an array into two parts
 
-const split_Array = (index, array) => {
-  const begin = array.slice(0, index);
+const splitArray = (splitPoint, array) => {
+  const begin = array.slice(0, splitPoint);
   const len = array.length;
-  array = array.slice(index, len);
+  array = array.slice(splitPoint, len);
   return [begin, array];
 };
 
-require('../Tests/split.js')(split_Array);
+require('../Tests/split.js')(splitArray);
