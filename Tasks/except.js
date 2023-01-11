2c7243e42f30e242686695c56787b00e3facbb3a
kovaliovev <kovaliov.evgeniy11@gmail.com>
2023-01-11 10:52:16 +0300

    create new dict instead of modifying entered one

--- Tasks/except.js
+++ Tasks/except.js
@@ -2,13 +2,14 @@
 // Copy all values from dict except listed
 
 const copyWithoutExceptions = (dict, ...exceptions) => {
+  const result = {};
   const keys = Object.keys(dict);
   for (const key of keys) {
-    if (exceptions.includes(key)) {
-      delete dict[key];
+    if (!exceptions.includes(key)) {
+      result[key] = dict[key];
     }
   }
-  return dict;
+  return result;
 };
 
 require('../Tests/except.js')(copyWithoutExceptions);
