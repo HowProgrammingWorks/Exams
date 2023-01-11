53be23b1c6e6bd4dc0ea7e460dfa7ead9da8bca5
Illia <kroshka.illya@lll.kpi.ua>
2023-01-11 12:41:36 +0200

    removed unnecessary arguments of return

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -8,7 +8,7 @@ mergeTwoObjects = (object_1, object_2) => {
   for (attribute_name in object_2) {
     object_3[attribute_name] = object_2[attribute_name];
   }
-  return object_1, object_2, object_3;
+  return object_3;
 };
 
 require('../Tests/merge.js')(mergeTwoObjects);
