df797fbbe98354f08bfc1b70c40393cea323aabd
makshv21 <khvits21@gmail.com>
2023-01-11 22:35:26 +0200

    In the condition of the (for) loop, changed l to months.length

--- Tasks/month.js
+++ Tasks/month.js
@@ -3,7 +3,7 @@
 const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
 
 const findMonth = month => {
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < months.length; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
   return -1;
