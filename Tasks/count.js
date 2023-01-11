b79d013a428982bb3c423dce2435b7ca8578f38d
Nazar-Didora <117527254+Nazar-Didora@users.noreply.github.com>
2023-01-11 22:48:07 +0200

    Deleted unneeded elements

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,15 +4,11 @@
 
 const count = (obj) => {
   let sum = 0;
-  {
-  }
   const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
