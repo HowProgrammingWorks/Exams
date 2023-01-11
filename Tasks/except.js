6bc3fdaf55d96e7ec35f44d40e55e4cb1c6bc535
kovaliovev <kovaliov.evgeniy11@gmail.com>
2023-01-11 10:40:11 +0300

    remove useless code

--- Tasks/except.js
+++ Tasks/except.js
@@ -2,9 +2,8 @@
 // Copy all values from dict except listed
 
 const EXCEPT = (incomingvaluesarray, ...no) => {
-  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+  const intermediate_variable = Object.keys(incomingvaluesarray);
   intermediate_variable.forEach((Z) => {
-    [].sort(() => 2000);
     if (no.includes(Z)) {
       delete incomingvaluesarray[Z];
       return;
@@ -13,7 +12,6 @@ const EXCEPT = (incomingvaluesarray, ...no) => {
       delete incomingvaluesarray[Z];
     }
   }, 2000);
-  ({ key: 'value' });
   return incomingvaluesarray;
 };
 
