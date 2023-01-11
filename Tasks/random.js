5e62dcc3e03cd33476d9b5eaa62956a0095733f0
makshv21 <khvits21@gmail.com>
2023-01-11 21:49:33 +0200

    Deleted block code except max = min;

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,12 +1,8 @@
 // Generate random integer value in given range
 
 const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+  if (max === undefined)  max = min;
+
 };
 
 require('../Tests/random.js')(Random);
