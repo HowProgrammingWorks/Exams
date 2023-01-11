d1617854cdbd0300e18d1c2b7421d55f08ad7675
kovaliovev <kovaliov.evgeniy11@gmail.com>
2023-01-11 11:38:04 +0300

    improve naming

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,14 +1,14 @@
 'use strict';
 // Generate random password
 
-const GeneratePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
+const generatePassword = (alphabet, length) => {
+  const max = alphabet.length;
   let key = '';
   for (let i = 0; i < length; i++) {
-    const Index = Math.floor(Math.random() * MAX);
-    key += alphabet[Index];
+    const index = Math.floor(Math.random() * max);
+    key += alphabet[index];
   }
   return key;
 };
 
-require('../Tests/password.js')(GeneratePassword);
+require('../Tests/password.js')(generatePassword);
