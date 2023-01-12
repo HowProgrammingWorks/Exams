4dd91bc5ec65be30e790980c9eda7501ed225324
Bilyi Bohdan <114570417+TarnishedGhost@users.noreply.github.com>
2023-01-13 01:36:42 +0200

    Update except.js

--- Tasks/except.js
+++ Tasks/except.js
@@ -5,10 +5,6 @@ const intermediate_variable = Object.keys(incomingvaluesarray);
   for (const Z of intermediate_variable) {
     if (no.includes(Z)) {
       delete incomingvaluesarray[Z];
-      return;
-    } 
-    else {
-      return;
     }
   }
   return incomingvaluesarray;
