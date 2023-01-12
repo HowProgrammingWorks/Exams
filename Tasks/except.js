102a68ae204e3b838566380793aa1bba4bf46ea9
Bilyi Bohdan <114570417+TarnishedGhost@users.noreply.github.com>
2023-01-13 00:41:23 +0200

    add const

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,7 +1,7 @@
 'use strict';
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+const EXCEPT = (incomingvaluesarray, ...no) => {
+const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
   intermediate_variable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
