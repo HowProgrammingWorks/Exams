f19c055e0e801335952c5b435eea65ca666756fb
Bilyi Bohdan <114570417+TarnishedGhost@users.noreply.github.com>
2023-01-12 20:18:17 +0200

    Added 'use strict' and removed empty code

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,12 @@
-// Sum all number values in dict
+'use strict';
 
 count = (obj) => {
   sum = 0;
-  {
-  }
   keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
