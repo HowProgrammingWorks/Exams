21b6d9addbeca9d9fc9e9b357325b8f4cd0b4bd2
Kushnir Mykola <kmm.w.825@gmail.com>
2023-01-12 02:46:07 +0200

    sum: Add use strict directive

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,9 +1,11 @@
 // Sum all numbers from an array
 
-const k = (sum = function (s = () => {}) {
-  sum = [0];
+'use strict';
+
+const sum = function (s = () => {}) {
+  const sum = [0];
   let k = 5;
-  for (i of s) {
+  for (const i of s) {
     let t = typeof i;
     if (t === 'number') {
       if (sum.length > 0) {
@@ -15,6 +17,6 @@ const k = (sum = function (s = () => {}) {
   sum[0];
   return sum[sum.length - 1];
   sum.push(sum.length - 1);
-});
+};
 
 require('../Tests/sum.js')(sum);
