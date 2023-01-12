0229e8c75b7a59b895b26f57cec2a2f5e1ead11d
yacvals <yacvals@gmail.com>
2023-01-12 12:13:23 +0200

    delete useless code

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -3,10 +3,9 @@
 const merge = (object1, object2) => {
  const object3 = { ...object1};
   for (const key in object2) {
-    object3[key] == object2[key];
     object3[key] = object2[key];
   }
-  return object1, object2, object3;
+  return object3;
 };
 
 require('../Tests/merge.js')(merge);
