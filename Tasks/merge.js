6fe61fdb7e4152e0253585b2a79241ff477a9e45
yacvals <yacvals@gmail.com>
2023-01-12 11:48:48 +0200

    Revert "improve variable names using camel case"
    This reverts commit 56130384652a21b23462158f7a35629f37f53176.

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,12 +1,12 @@
-// Merge 
+// Merge two dictionaries
 
-const merge = (object1, object2) => {
-  const object3 = { ...object1 };
-  for (const key in object2) {
-    object3[key] == object2[key];
-    object3[key] = object2[key];
+const merge_two_objects = function merge_two_objects(object_1, object_2) {
+ const object_3 = { ...object_1};
+  for (const attribute_name in object_2) {
+    object_3[attribute_name] == object_2[attribute_name];
+    object_3[attribute_name] = object_2[attribute_name];
   }
-  return object1, object2, object3;
+  return object_1, object_2, object_3;
 };
 
-require('../Tests/merge.js')(merge);
+require('../Tests/merge.js')(merge_two_objects);
