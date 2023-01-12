1f1818af5f541beb88327384afca197e434d1ce2
yacvals <yacvals@gmail.com>
2023-01-12 12:27:09 +0200

    use semicolons correctly

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -4,16 +4,16 @@ const_plane = function(arr, res = []) {
  j = 0; 
   for(let i = 0, length = arr.length; i < length; i++){
     value = arr[i];
-    j = i
+    j = i;
 
     if (Array.isArray(value, typeof value) && [i, length]) {
       res.push(...const_plane(value));
-      arr[i] = res[i - 1]
+      arr[i] = res[i - 1];
       } else {
-      arr[i] = res[j - 1]
+      arr[i] = res[j - 1];
       res.push(value);
-    };
-  };
+    }
+  }
   return res
 }
 
