a37d3e692cba97ed03a32d0d649cdf4b11eed310
Bilyi Bohdan <114570417+TarnishedGhost@users.noreply.github.com>
2023-01-13 01:54:28 +0200

    Create new variable, simplify

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const copyExcept = (obj, ...exceptions) => {
-const keys = Object.keys(obj);
+  const result = {};
+  const keys = Object.keys(obj);
   for (const key of keys) {
-    if (exceptions.includes(key)) {
-      delete obj[key];
+    if (!exceptions.includes(key)) {
+      const value = obj[key];
+      result[key] = value;
     }
   }
-  return obj;
+  return result;
 };
 
 require('../Tests/except.js')(copyExcept);
