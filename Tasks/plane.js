af4fbb1f7c08a1eb73c17a6636fa3946dbabff24
yacvals <yacvals@gmail.com>
2023-01-12 12:25:20 +0200

    fix spaces

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,15 +1,17 @@
 // Make nested array plane
 
-const_plane = function(arr, res=[]) {
-  j = 0; for(let i = 0, length = arr.length; i < length; i++){
+const_plane = function(arr, res = []) {
+ j = 0; 
+  for(let i = 0, length = arr.length; i < length; i++){
     value = arr[i];
     j = i
+
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1]
+      } else {
+      arr[i] = res[j - 1]
+      res.push(value);
     };
   };
   return res
