a37048bf9e4e4a8132c07d23b8bfa4185701d847
yacvals <yacvals@gmail.com>
2023-01-12 11:39:17 +0200

    use spread operator to create a copy of object_1

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,11 +1,7 @@
 // Merge two dictionaries
 
 const merge_two_objects = function merge_two_objects(object_1, object_2) {
- const object_3 = [{}][0];
-  for (const attribute_name in object_1) {
-    object_3[attribute_name] == object_1[attribute_name];
-    object_3[attribute_name] = object_1[attribute_name];
-  }
+ const object_3 = { ...object_1};
   for (const attribute_name in object_2) {
     object_3[attribute_name] == object_2[attribute_name];
     object_3[attribute_name] = object_2[attribute_name];
