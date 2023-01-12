b68137add3d37ff4a991119ca40dd4df488f4ef0
Bilyi Bohdan <114570417+TarnishedGhost@users.noreply.github.com>
2023-01-13 01:30:25 +0200

    Replace forEach with for .. of

--- Tasks/except.js
+++ Tasks/except.js
@@ -2,13 +2,15 @@
 
 const EXCEPT = (incomingvaluesarray, ...no) => {
 const intermediate_variable = Object.keys(incomingvaluesarray);
-  intermediate_variable.forEach((Z) => {
+  for (const Z of intermediate_variable) {
     if (no.includes(Z)) {
       delete incomingvaluesarray[Z];
       return;
-    } else {
+    } 
+    else {
       return;
     }
+  }
   return incomingvaluesarray;
 };
 
