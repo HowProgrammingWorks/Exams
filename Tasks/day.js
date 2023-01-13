571c2b58aeaeefb23b4d74eccb5b6fa88f837e31
Illya <katinskyi.illia@lll.kpi.ua>
2023-01-13 10:16:44 +0200

    changed function name

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,7 +2,7 @@
 
 let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+parseDay = (s) => {
   let i;
   for (i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
@@ -12,4 +12,4 @@ _parse_day_ = (s) => {
   return -1;
 };
 
-require('../Tests/day.js')(_parse_day_);
+require('../Tests/day.js')(parseDay);
