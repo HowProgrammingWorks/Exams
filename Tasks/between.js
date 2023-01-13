2d9626e0716bab85f85eeeefc323924b40428cbb
nevrodegenerate <122379140+nevrodegenerate@users.noreply.github.com>
2023-01-13 15:48:47 +0200

    Removed unnecessary error checks

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,9 +1,7 @@
 // Extract substring between prefix and suffix
 
 getvaluebetween = (fullString, startString, endString) => {
-  if (fullString.indexOf(startString) === -1) return '';
   fullString = fullString.substring(fullString.indexOf(startString) + startString.length);
-  if (fullString.indexOf(endString) === -1) return '';
   fullString = fullString.substring(0, fullString.indexOf(endString));
   return fullString;
 };
