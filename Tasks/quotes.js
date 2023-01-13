a54e5b9cf4f389981ed101d60aaf0f7f8e752ab5
Bogdan Lepkov <122379140+nevrodegenerate@users.noreply.github.com>
2023-01-13 22:04:04 +0200

    Changed outputString from array to string and changed operators accordingly
    Also renamed variables i forgot in the previous commit

--- Tasks/quotes.js
+++ Tasks/quotes.js
@@ -1,31 +1,29 @@
 // Change double quotation to open or close quotation
 'use strict'
 
-const EMPTY = '';
-
 const quotes = function (inputString) {
-  let outputString = [];
+  let outputString = '';
   let open = false;
   for (const SYMBOL of inputString) {
     if (SYMBOL === '"') {
       for (let i of SYMBOL) {
         if (!open) {
-          outputString.push('«');
+          outputString += '«';
           open = true;
         } else {
-          outputString.push('»');
+          outputString += '»';
           open = false;
         }
       }
     } else {
       if (SYMBOL !== '"') {
         for (let i of SYMBOL) {
-          res.push(i);
+          outputString += i;
         }
       }
     }
   }
-  return outputString.join(EMPTY);
+  return outputString;
 };
 
 require('../Tests/quotes.js')(quotes);
