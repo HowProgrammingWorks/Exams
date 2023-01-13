5a10e43cbe3b94ad1ae15b1bae5e06f0f33aad95
Danil Kobzev <denthenttg@gmail.com>
2023-01-13 06:31:46 +0200

    If statements shortened Needless statements removed

--- Solutions/words.js
+++ Solutions/words.js
@@ -4,16 +4,12 @@ const Words = function (s) {
   let number_of_words_in_s = 0;
   let flag = false;
   for (const c of s) {
-    if (!flag) {
-      if (c !== ' ') {
-          flag = true;
+    if (!flag && c !== ' ') {
+        flag = true;
         number_of_words_in_s++;
-      }
-    } else {
-      if (c === ' ') {
+    } else if (c === ' ') {
           flag = false;
-      } else flag = true;
-    }
+      }
   }
   return number_of_words_in_s;
 };
