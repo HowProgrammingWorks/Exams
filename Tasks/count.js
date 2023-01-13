66ed3a559aac07c48d2b5368f43227f261b688a3
Maksym Yatsenko <m.yatsenko.im22@kpi.ua>
2023-01-13 23:38:03 +0200

    count: Use for...of instead of forEach()

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,9 +5,9 @@
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     if (typeof obj[key] === 'number') sum += obj[key];
-  });
+  }
   return sum;
 };
 
