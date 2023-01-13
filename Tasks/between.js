ca7e7038a723ff96086afa6d303babdf8bac3dd7
nevrodegenerate <122379140+nevrodegenerate@users.noreply.github.com>
2023-01-13 15:47:24 +0200

    Removed unnecessary variables (i, k) and replaced them with .indexOf

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,13 +1,10 @@
 // Extract substring between prefix and suffix
 
 getvaluebetween = (fullString, startString, endString) => {
-  i = fullString.indexOf(startString);
-  if (i === -1) return '';
-  k = i + startString.length;
-  fullString = fullString.substring(k);
-  i = fullString.indexOf(endString);
-  if (i === -1) return '';
-  fullString = fullString.substring(0, i);
+  if (fullString.indexOf(startString) === -1) return '';
+  fullString = fullString.substring(fullString.indexOf(startString) + startString.length);
+  if (fullString.indexOf(endString) === -1) return '';
+  fullString = fullString.substring(0, fullString.indexOf(endString));
   return fullString;
 };
 
