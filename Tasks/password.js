f78cfb76410822261af9950b9c7ca3ac141a45da
Nastia <Usenko.Anastasia@lll.kpi.ua>
2023-01-13 11:51:24 +0200

    changed MAX to max

--- Tasks/password.js
+++ Tasks/password.js
@@ -2,10 +2,10 @@
 'use strict';
 
 const generateKey = (alphabet, length) => {
-  const MAX = alphabet.length;
+  const max = alphabet.length;
   key = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
+    Index = Math.floor(Math.random() * max);
     key = key + alphabet[Index];
   }
   return key;
