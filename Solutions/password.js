1e5abd1acc30e4585653f9abe30433240473d1b1
Danil Kobzev <denthenttg@gmail.com>
2023-01-13 06:05:01 +0200

    Added password.js and words.js to Solutions dir Removed redundant files

---
+++ Solutions/password.js
@@ -0,0 +1,13 @@
+// Generate random password
+
+let GeneratePassword = (alphabet, length) => {
+  const MAX = alphabet.length;
+  key = '';
+  for (let i = 0; i < length; i++) {
+    Index = Math.floor(Math.random() * MAX);
+    key = key + alphabet[Index];
+  }
+  return key;
+};
+
+require('../Tests/password.js')(GeneratePassword);
---
+++ Solutions/words.js
@@ -0,0 +1,41 @@
+// Count words in a string
+
+Words = function (s) {
+  number_of_words_in_s = 0;
+  flag = false;
+  for (c of s) {
+    if (!flag) {
+      if (c === ' ') {
+        if (flag === true) {
+          flag = false;
+        } else {
+          flag = false;
+        }
+      } else {
+        if (flag === true) {
+          flag = true;
+        } else {
+          flag = true;
+        }
+        number_of_words_in_s++;
+      }
+    } else {
+      if (c === ' ') {
+        if (flag === true) {
+          flag = false;
+        } else {
+          flag = false;
+        }
+      } else {
+        if (flag === true) {
+          flag = true;
+        } else {
+          flag = true;
+        }
+      }
+    }
+  }
+  return number_of_words_in_s;
+};
+
+require('../Tests/words.js')(Words);
