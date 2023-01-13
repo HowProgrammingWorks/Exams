7df73b7a5b475a4dc588e7250c9538e9d64cc82d
Strelia Illia <seylvin2005@gmail.com>
2023-01-13 11:31:22 +0200

    added: change identifier 'keys' to 'arr_of_values' and 'key' to 'element'

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,9 +2,9 @@
 
  const adder = (obj) => {
   let sum = 0;
-  const keys = Object.values(obj);
-  for (const key of keys) {
-    if (typeof key === 'number') sum += key;
+  const arr_of_values = Object.values(obj);
+  for (const element of arr_of_values) {
+    if (typeof element === 'number') sum += element;
   }
   return sum;
 };
