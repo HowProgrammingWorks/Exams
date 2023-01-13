6cdfaaf80bca0c4a70d916a003701ffe0ca693c1
Jester <thettboy11@gmail.com>
2023-01-13 17:22:01 +0200

    change the (for) loop to (for of) and add the day constant

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,10 @@
 'use strict';
 const WEEK_DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 const parseDay = (string) => {
-  for (let i = 0; i < WEEK_DAYS.length; i++) {
-    if (string.startsWith(WEEK_DAYS[i].toLowerCase())) {
-      return i + 1;
-    }
+  let i = 0;
+  for (const day of WEEK_DAYS) {
+    if (string.startsWith(day.toLowerCase())) return i + 1;
+    i++;
   }
   return -1;
 };
