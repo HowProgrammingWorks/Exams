af67d5f89d22d360a1db2dad95bf67628b904d4d
Bogdan Lepkov <122379140+nevrodegenerate@users.noreply.github.com>
2023-01-13 16:24:28 +0200

    Added 'use strict' and defined function type

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,6 +1,7 @@
 // Extract substring between prefix and suffix
+'use strict'
 
-getvaluebetween = (fullString, startString, endString) => {
+const getvaluebetween = (fullString, startString, endString) => {
   fullString = fullString.substring(fullString.indexOf(startString) + startString.length);
   fullString = fullString.substring(0, fullString.indexOf(endString));
   return fullString;
