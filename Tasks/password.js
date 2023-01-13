4311f2e478f6534abc73d085c029e6484d08c349
Bogdan Lepkov <122379140+nevrodegenerate@users.noreply.github.com>
2023-01-13 22:57:59 +0200

    Removed unnecessary variables
    Also fixed mistakes made in previous commits

--- Tasks/password.js
+++ Tasks/password.js
@@ -2,13 +2,9 @@
 'use strict'
 
 const GeneratePassword = (alphabet, passLength) => {
-  const MAX = alphabet.length;
   let password = '';
-  for (let i = 0; i < passLength; i++) {
-    let Index = Math.floor(Math.random() * MAX);
-    password = password + alphabet[Index];
-  }
-  return key;
+  for (let i = 0; i < passLength; i++) password += alphabet[Math.floor(Math.random() * alphabet.length)];
+  return password;
 };
 
 require('../Tests/password.js')(GeneratePassword);
