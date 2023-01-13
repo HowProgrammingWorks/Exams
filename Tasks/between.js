c0eed239e7e94687c1aa4a3d5be8740c7118190d
Bogdan Lepkov <122379140+nevrodegenerate@users.noreply.github.com>
2023-01-13 23:50:43 +0200

    Changed name of function

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,10 +1,10 @@
 // Extract substring between prefix and suffix
 'use strict'
 
-const getvaluebetween = (fullString, startString, endString) => {
+const getValueBetween = (fullString, startString, endString) => {
   fullString = fullString.substring(fullString.indexOf(startString) + startString.length);
   fullString = fullString.substring(0, fullString.indexOf(endString));
   return fullString;
 };
 
-require('../Tests/between.js')(getvaluebetween);
+require('../Tests/between.js')(getValueBetween);
