fc060498d431df58289c19d82e9dc209cbc4e40d
Strelia Illia <seylvin2005@gmail.com>
2023-01-13 19:59:32 +0200

    deleted:comment added:'use strict'

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,5 +1,4 @@
-// Find an intersection of two dictionaries
-
+'use strict'
 intersection = function intersection(object_1, object_2) {
   first_keys = Object.keys(object_1);
   for (attribute_name of first_keys) {
