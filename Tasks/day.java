1d93c74ce76cc6a9c590c42d215160f361a0b6ed
Anton338 <70806607+Anton338@users.noreply.github.com>
2023-01-13 21:22:31 +0200

    password.java
    Translated and optimized the code to Java

---
+++ Tasks/day.java
@@ -0,0 +1,25 @@
+// Get day number
+
+package parseDay;
+
+import java.util.Scanner;
+
+public class parseDay {
+    public static void main(String[] args) {
+        System.out.println("day?");
+        Scanner scanner = new Scanner(System.in);
+        String Days = scanner.next();
+        System.out.println(parseDay(Days));
+    }
+
+    public static int parseDay(String s) {
+        String[] D = {"sunday", "monday", "tuesday", "wednesday", " thursday", "friday", "saturday"};
+        int i;
+        for (i = 0; i < D.length; i++) {
+            if (s.equals(D[i])) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
