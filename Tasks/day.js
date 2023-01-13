018ec136bd81086d162d7e2f1894016f7d4d4247
Illya <katinskyi.illia@lll.kpi.ua>
2023-01-13 12:27:42 +0200

    renamed array

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,11 @@
 // Get day number
 
-const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 parseDay = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < DAYS.length; i++) {
+    if (s.startsWith(DAYS[i].toLowerCase())) {
       return i + 1;
     }
   }
