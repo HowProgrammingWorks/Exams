3604eef55dc62c1051b7a83ca880d44f61dcbd0e
Strelia Illia <seylvin2005@gmail.com>
2023-01-13 11:02:30 +0200

    added:change 'value' to 'key'

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,9 +4,8 @@
   let sum = 0;
   const keys = Object.values(obj);
   for (const key of keys) {
-
-    if (typeof value === 'number') sum += value;
-  };
+    if (typeof key === 'number') sum += key;
+  }
   return sum;
 };
 
