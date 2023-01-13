7ce9e938e0b684a79d6d66893730d81b5bf4159f
Nastia <Usenko.Anastasia@lll.kpi.ua>
2023-01-13 11:54:36 +0200

    added const

--- Tasks/password.js
+++ Tasks/password.js
@@ -5,7 +5,7 @@ const generateKey = (alphabet, length) => {
   const max = alphabet.length;
   let key = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * max);
+    const Index = Math.floor(Math.random() * max);
     key = key + alphabet[Index];
   }
   return key;
