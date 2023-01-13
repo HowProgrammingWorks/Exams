7f070908e07ed08f97a11552fa68fd3bfb3be098
Strelia Illia <seylvin2005@gmail.com>
2023-01-13 10:57:47 +0200

    deleted:redundant string

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,9 +4,9 @@
   let sum = 0;
   const keys = Object.values(obj);
   for (const key of keys) {
-    const value = obj[key];
+
     if (typeof value === 'number') sum += value;
-  });
+  };
   return sum;
 };
 
