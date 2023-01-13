025538ce10ce320dc6ecb44451d5c4e751f97652
Illya <katinskyi.illia@lll.kpi.ua>
2023-01-13 12:32:04 +0200

    added const

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,7 +2,7 @@
 'use strict'
 const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-parseDay = (s) => {
+const parseDay = (s) => {
   let i;
   for (i = 0; i < DAYS.length; i++) {
     if (s.startsWith(DAYS[i].toLowerCase())) {
