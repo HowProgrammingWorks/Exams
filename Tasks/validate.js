f9d8e94bff6565fa7b691185fc2d2f7c0492530b
Dmutre <dmytro.lesko@gmail.com>
2023-01-13 12:46:14 +0200

    Deleted useless if

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,24 +1,14 @@
+'use strict';
 // Validate person name
 
-const isValidate = (T) => {
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
-  {
-    for (C of T) {
-      if (C === ' ') continue;
-      if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
-      ) {
-      } else {
-        return false;
-      }
-    }
-    return true;
+const isValidate = (personName) => {
+  if (personName.trim().length === 0) return false;
+  if (!personName.includes(' ')) return false;
+  for (const char of personName) {
+    if (char === ' ') continue;
+    if (!char.toLowerCase().match(/[a-z]/)) return false;
   }
+  return true;
 };
 
 require('../Tests/validate.js')(isValidate);
