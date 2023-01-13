28aad969ef635020de9e4be280cd2dd73045afa8
Danil Kobzev <denthenttg@gmail.com>
2023-01-13 07:33:30 +0200

    For loop and if statements changed on ternary operator

--- Solutions/words.js
+++ Solutions/words.js
@@ -1,12 +1,7 @@
 'use strict'
 
 const Words = (s) => {
-  let wordCounter = 0;
-  if(s !== '') wordCounter++;
-  for (const c of s) {
-    if (c === ' ') wordCounter++;
-  }
-  return wordCounter;
+  return ((s === '') ? 0 : s.split(" ").length);
 };
 
 require('../Tests/words.js')(Words);
