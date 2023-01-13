130a69cb3a3a72994a237361bde6e7e8e0a1317c
Victoria <80917462+witchkaa@users.noreply.github.com>
2023-01-13 18:57:24 +0200

    Fix indentation to 2 spaces

--- Tasks/words.js
+++ Tasks/words.js
@@ -3,18 +3,17 @@
 // Count words in a string
 
 const words = str => {
-    let amountOfWords = 0;
-    let charDetected = false;
-    for (let char of str) {
-        if (!charDetected && char !== ' ') {
-            charDetected = true;
-            amountOfWords++;
-        } else if (char === ' ') {
-            charDetected = false;
-        }
+  let amountOfWords = 0;
+  let charDetected = false;
+  for (let char of str) {
+    if (!charDetected && char !== ' ') {
+      charDetected = true;
+      amountOfWords++;
+    } else if (char === ' ') {
+      charDetected = false;
     }
-    return amountOfWords;
+  }
+  return amountOfWords;
 };
 
 require('../Tests/words.js')(words);
-  
