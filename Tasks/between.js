a81f0673da08659eeec2066d7f8832a031aae31e
Viktoriia Kapys <kapysva@gmail.com>
2023-01-14 22:12:09 +0200

    change 'if else' to the ternary operator '?'

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,19 +3,10 @@
 
 const getValueBetween = (string, firstSymbol, secondSymbol) => {
   let index = string.indexOf(firstSymbol);
-  if (index === -1) return '';
-  else {
-    const leftPosition = index + firstSymbol.length;
-    string = string.substring(leftPosition);
-    if (secondSymbol) {
-      index = string.indexOf(secondSymbol);
-      if (index === -1) {
-        return '';
-      } else {
-        string = string.substring(0, index);
-      }
-    }
-  }
+  const leftPosition = index + firstSymbol.length;
+  index !== -1 ? string = string.substring(leftPosition) : '';
+  index !== -1 ? index = string.indexOf(secondSymbol) : '';
+  string = string.substring(0, index);
   return string;
 };
 
