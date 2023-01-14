769c4b8a57a6836de8c46c0df2bf255069076db1
unknown <sachko.maxim@lll.kpi.ua>
2023-01-14 14:35:19 +0200

    Formatted naming

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,15 @@
 'use strict';
 // Get day number
 
-const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const _parse_day_ = (s) => {
-  for (const i of D) {
-    if (s.startsWith(i.toLowerCase())) {
-      return D.indexOf(i) + 1;
+const getDayNum = (dayName) => {
+  for (const day of daysOfWeek) {
+    if (dayName.startsWith(day.toLowerCase())) {
+      return daysOfWeek.indexOf(day) + 1;
     }
   }
   return -1;
 };
 
-require('../Tests/day.js')(_parse_day_);
+require('../Tests/day.js')(getDayNum);
