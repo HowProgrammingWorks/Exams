94e5d8e7644c2427c43e3f0d8d12b1f70594d621
RostislavHundarev <122154736+xastia@users.noreply.github.com>
2023-01-15 01:16:16 +0200

    rename elements of massive, delete useless method

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,11 @@
 // Get day number
 'use strict';
 
-const Week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const Week = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseWeek = (s) => {
   for (let i = 0; i < Week.length; i++) {
-    const weekParsing = s.startsWith(Week[i].toLowerCase());
+    const weekParsing = s.startsWith(Week[i]);
     if (weekParsing) return i + 1
   }
   return -1;
