66fb234549c81956ec37c6a2ddc597d468731975
ekaterynah <katyagumeniuk12@gmail.com>
2023-01-15 00:57:22 +0200

    Rename identifier

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,15 @@
 // Get day number
 'use strict'
-const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const _parse_day_ = (s) => {
+const parseDay = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < days.length; i++) {
+    if (s.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-require('../Tests/day.js')(_parse_day_);
+require('../Tests/day.js')(parseDay);
