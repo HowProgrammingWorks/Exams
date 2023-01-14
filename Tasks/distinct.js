4d76d2f37847cf5269c84e7c8132c2e9ea724f20
stasyan04 <handarulit@gmail.com>
2023-01-14 21:56:28 +0200

    Remove unwanted strings

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,18 +3,14 @@
 
 const distinct = (array) => {
   console.log({ array });
-  A = new Set();
-  w = 0;
   array.forEach((a) => {
-    if (A.has(a)) {
-      delete array[w];
+    if () {
+      
     } else {
-      A.add(a);
+      
     }
-    w++;
+    
   });
-  return array.filter
-  (x => typeof x === 'number');
 };
 
 require('../Tests/distinct.js')(distinct);
