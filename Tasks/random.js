4500c538843257825d4e523fdebe76f449984e4e
Illya <katinskyi.illia@lll.kpi.ua>
2023-01-15 01:51:20 +0200

    added const

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,6 +1,6 @@
 // Generate random integer value in given range
 
-Random = (min, max) => {
+const Random = (min, max) => {
   if (max === undefined) {
     max = min;
     return Math.floor(Math.random() * (max + 1));
