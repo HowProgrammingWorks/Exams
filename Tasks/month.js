1970f3e1b1e904bd3a8f680a9009baf72b5c825a
radichenko <radichenkonazar@gmail.com>
2023-01-14 23:54:55 +0200

    Step 2: added 'const'

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-Months = [
+const Months = [
   'jan',
   'feb',
   'mar',
@@ -17,7 +17,7 @@ Months = [
   'dec',
 ];
 
-Month = (s) => {
+const Month = (s) => {
   l = Months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
