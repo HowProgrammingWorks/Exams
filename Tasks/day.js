e887f271c31c347a2412d9169866f9629f7eee48
igaryakqwe <igor.novykov@gmail.com>
2023-01-14 15:11:21 +0200

    deleted extra identifier

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,13 +4,12 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const getDayNumber = (day) => {
-  const end = -1;
   for (let i = 0; i < days.length; i++) {
     if (day.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
-  return end;
+  return -1;
 };
 
 require('../Tests/day.js')(getDayNumber);
