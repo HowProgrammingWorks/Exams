12a1cf94c1d16a6e4504e44232a98588757ee5bd
farid <aslanovfarid04@gmail.com>
2023-01-14 18:59:05 +0200

    Remove all unnecessary code

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,11 +5,10 @@ const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
+    if (typeof value === 'number')
+      sum += value;
   });
-  ({});
   return sum;
 };
 
