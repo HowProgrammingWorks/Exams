09338937fe82a9560f8cfe7398f51ef5f67fcc7d
ekaterynah <katyagumeniuk12@gmail.com>
2023-01-15 00:34:46 +0200

    Remove the upper case of the identifier MAX

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,10 +1,10 @@
 // Generate random password
 'use strict'
 const GeneratePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
+  const max = alphabet.length;
   let key = '';
   for (let i = 0; i < length; i++) {
-    const Index = Math.floor(Math.random() * MAX);
+    const Index = Math.floor(Math.random() * max);
     key = key + alphabet[Index];
   }
   return key;
