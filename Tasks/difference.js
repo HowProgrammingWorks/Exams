1d9779d721c9672eb2973acd93b89e0e7b07a050
anastasiadyvnich <anastasiaduvnich@gmail.com>
2023-01-14 22:03:31 +0200

    deleted dead code and improved the algorithm

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,14 +2,9 @@
 'use strict';
 
 const dictDifference = (obj1, obj2) => {
-  for (const key in obj1) {
-    obj1[key] = obj1[key];
-  }{}
-  if (obj1 in obj2) return false;
   for (const key in obj2) {
-    obj1[key] = obj2[key];
     delete obj1[key];
-  }{}
+  }
   return obj1;
 };
 
