958c1a51a3b2cbc998b3604797a8205bdb83e8f4
stasyan04 <handarulit@gmail.com>
2023-01-14 22:19:38 +0200

    Remove unwanted braces

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,22 +3,12 @@
 
 intersection = function intersection(object_1, object_2) {
   first_keys = Object.keys(object_1);
-  {
-  }
   for (attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
     } else {
       delete object_1[attribute_name];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object_1;
 };
