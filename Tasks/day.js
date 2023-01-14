480a51ce995839ae61ee1918f7e763d41590a1ce
unknown <sachko.maxim@lll.kpi.ua>
2023-01-14 14:31:50 +0200

    Added for...of instead of and replace and used .indexOf

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,10 +4,9 @@
 const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const _parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
+  for (const i of D) {
+    if (s.startsWith(i.toLowerCase())) {
+      return D.indexOf(i) + 1;
     }
   }
   return -1;
