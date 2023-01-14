2da140e765a530c5bb040b34a3f8503b8c56d978
RostislavHundarev <122154736+xastia@users.noreply.github.com>
2023-01-15 00:09:45 +0200

    add assignments to "for"

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,8 +3,7 @@
 let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 _parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
+  for (let i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
