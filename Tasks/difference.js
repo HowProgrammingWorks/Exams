c2c2b8276be076d532674e8e219531e2d949b78d
anastasiadyvnich <anastasiaduvnich@gmail.com>
2023-01-14 22:07:10 +0200

    fixed problems

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,7 +1,7 @@
 // Find a difference between two dictionaries
 'use strict';
 
-const dictDifference = (obj1, obj2) => {
+const dictDifference = ({...obj1}, obj2) => {
   for (const key in obj2) {
     delete obj1[key];
   }
