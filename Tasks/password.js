a2df46c0143b0062b9c746aa5116cb8d99d51307
Nastia <Usenko.Anastasia@lll.kpi.ua>
2023-01-14 13:23:20 +0200

    Asignment '=' replaced by'+='

--- Tasks/password.js
+++ Tasks/password.js
@@ -3,12 +3,12 @@
 
 const generateKey = (alphabet, length) => {
   const max = alphabet.length;
-    let key = '';
-    for (let i = 0; i < length; i++) {
-      const index = Math.floor(Math.random() * max);
-      key = key + alphabet[index];
-    }
-    return key;
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * max);
+    key += alphabet[index];
+  }
+  return key;
 };
 
 require('../Tests/password.js')(generateKey);
