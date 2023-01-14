e7f3957b40d62dbcde73ff203c9f8e1b039fdf84
ekaterynah <katyagumeniuk12@gmail.com>
2023-01-15 01:00:14 +0200

    Add variable declaration

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,7 +4,7 @@ const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (s) => {
   let i;
-  for (i = 0; i < days.length; i++) {
+  for (let i = 0; i < days.length; i++) {
     if (s.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
