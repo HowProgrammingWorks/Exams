0b8ed8845446a952190da794e1df7c3cce6418c1
anastasiadyvnich <anastasiaduvnich@gmail.com>
2023-01-14 22:42:59 +0200

    fixed implicit declarations and tabs

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -2,19 +2,18 @@
 'use strict';
 
 const deduplicate = data => {
-  console.log({ data });
-  A = new Set();
-  w = 0;
+  const A = new Set();
+  let w = 0;
   data.forEach((a) => {
     if (A.has(a)) {
-      delete data[w];
-    } else {
-      A.add(a);
+      data.splice(w, 1);
     }
+    else {
+      A.add(a);
+    };
     w++;
   });
-  return data.filter
-  (x => typeof x === 'number');
-};
+  return data;
+}
 
 require('../Tests/distinct.js')(deduplicate);
