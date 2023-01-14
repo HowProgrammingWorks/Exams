bfb6d4a8173b1fe7c7cea2fe0eb8c4eccd47e3ed
ekaterynah <katyagumeniuk12@gmail.com>
2023-01-15 01:09:45 +0200

    Remove unnecessary variable declaration

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,7 +3,7 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (s) => {
-  let i;
+
   for (let i = 0; i < days.length; i++) {
     if (s.startsWith(days[i].toLowerCase())) {
       return i + 1;
