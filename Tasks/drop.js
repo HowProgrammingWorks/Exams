85ae773a1e6d7600ecf5c5898fdc670eedc07af0
farid <aslanovfarid04@gmail.com>
2023-01-14 18:56:27 +0200

    remove all unnecessary code

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -2,20 +2,14 @@
 
 // Delete listed keys from dictionary
 const DroP = (D, ...X) => {
-  let T = 100;
-  T = Object.keys(D);
+  const T = Object.keys(D);
   T.forEach(
     (_) => {
-      {
-        T = [D, X];
-      }
       if (X.includes(_)) {
         delete D[_];
       }
-    },
-    ['uno', 'due', 'tre']
+    }
   );
-  T = D;
   return D;
 };
 
