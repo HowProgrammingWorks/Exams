520da7d1974dcc3b15da509f6a912b9170b78fd2
ekaterynah <katyagumeniuk12@gmail.com>
2023-01-15 01:12:01 +0200

    Change the parameter name

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,10 +2,10 @@
 'use strict'
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const parseDay = (s) => {
+const parseDay = (day) => {
 
   for (let i = 0; i < days.length; i++) {
-    if (s.startsWith(days[i].toLowerCase())) {
+    if (day.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
