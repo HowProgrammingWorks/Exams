8ebe68d857fb2b023f12897834cca48712c78060
Kvezal_study <114149563+Yarule-study@users.noreply.github.com>
2023-01-15 21:30:19 +0200

    Update zip.js
    renaming variable

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -3,8 +3,8 @@
 const zip = (arrFirst, arrLast) => {
   let i = 0;
   let j = 0;
-  for (const x of arrLast) {
-    const CELL = [arrFirst[i++], x];
+  for (const element of arrLast) {
+    const CELL = [arrFirst[i++], element];
     (i < j) ? (delete arrFirst[i++]) : (arrLast[j++] = CELL);   
     CELL[0] == null ? arrLast.length -= 1 : arrLast.length;
     }
