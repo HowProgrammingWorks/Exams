4285c94822a0b658831606f0b8f5e843dea86f1f
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2023-01-15 14:05:31 +0200

    delete needless else

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,23 +1,22 @@
 // Replace substring with newstr
-'use strict';
+"use strict";
 
 const replaceSubstring = (str, substr, newstr) => {
-  if (substr === '') {
+  if (substr === "") {
     return str;
-  } else {
-    src = str;
-    res = '';
-    do {
-      const _index = src.indexOf(substr);
-      if (_index === -1) {
-        return res + src;
-      } else {
-        const start = src.substring(0, _index);
-        src = src.substring(_index + substr.length, src.length);
-        res += start + newstr;
-      }
-    } while (true);
   }
+  src = str;
+  res = "";
+  do {
+    const _index = src.indexOf(substr);
+    if (_index === -1) {
+      return res + src;
+    } else {
+      const start = src.substring(0, _index);
+      src = src.substring(_index + substr.length, src.length);
+      res += start + newstr;
+    }
+  } while (true);
 };
 
-require('../Tests/replace.js')(replaceSubstring);
+require("../Tests/replace.js")(replaceSubstring);
