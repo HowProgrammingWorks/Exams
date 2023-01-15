d64a19cd9511c8f2ba6b170b7df37ded2fd8952b
unknown <sachko.maxim@lll.kpi.ua>
2023-01-15 13:07:42 +0200

    Removed useless else

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -4,19 +4,17 @@
 const replaceSubString = (string, subString, newString) => {
   if (subString === '') {
     return string;
-  } else {
+  }
     let changeString = string;
     let res = '';
     while (true) {
       const stringIndex = changeString.indexOf(subString);
       if (stringIndex === -1) {
         return res + changeString;
-      } else {
+      }
         const start = changeString.substring(0, stringIndex);
         changeString = changeString.substring(stringIndex + subString.length, changeString.length);
         res += start + newString;
-      }
-    }
   }
 };
 
