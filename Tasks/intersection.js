ecb0a19896e3df6772ff099bf23c4c7cb924070f
Mihail-Ivannikov <116758780+Mihail-Ivannikov@users.noreply.github.com>
2023-01-15 03:16:17 +0200

    Update intersection.js
    Delete empty blocks
    Add use strict

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,23 +1,17 @@
 // Find an intersection of two dictionaries
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
-  {
-  }
-  for (attribute_name of first_keys) {
+//Delete empty blocks
+//Add use strict
+
+'use strict';
+const intersection = (object_1, object_2) => {
+  const first_keys = Object.keys(object_1);
+  for (const attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
     } else {
       delete object_1[attribute_name];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object_1;
 };
