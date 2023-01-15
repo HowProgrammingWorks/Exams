696695d71092444bb57b17b39e08dc62313c209f
stasyukhymenko <yukhymenko.stas@gmail.com>
2023-01-15 17:40:19 +0200

    fixed variables and deleted dead code

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,10 +1,10 @@
 // Sum all numbers from an array
 'use strict';
 
+let sum;
 const k = (sum = function (s = () => {}) {
   sum = [0];
-  let k = 5;
-  for (i of s) {
+  for (let i of s) {
     let t = typeof i;
     if (t === 'number') {
       if (sum.length > 0) {
@@ -13,9 +13,7 @@ const k = (sum = function (s = () => {}) {
       }
     }
   }
-  sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
 });
 
 require('../Tests/sum.js')(sum);
