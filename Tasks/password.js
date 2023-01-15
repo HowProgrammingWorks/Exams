19d62f9f84134af3d08b0a8ef711b0d3f896fc8a
matviikiliar <122607905+matviikiliar@users.noreply.github.com>
2023-01-16 01:54:47 +0200

    add const

--- Tasks/password.js
+++ Tasks/password.js
@@ -4,7 +4,7 @@ const GeneratePassword = (alphabet, length) => {
   const max = alphabet.length;
   let key = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * max);
+    const Index = Math.floor(Math.random() * max);
     key = key + alphabet[Index];
   }
   return key;
