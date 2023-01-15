d099176f98a32bd48748ff2fa8d4489d83bc8074
matviikiliar <122607905+matviikiliar@users.noreply.github.com>
2023-01-16 00:30:41 +0200

    delete braces

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,15 +2,11 @@ use 'strict'
 
 const count = (obj) => {
   let sum = 0;
-  {
-  }
   const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
