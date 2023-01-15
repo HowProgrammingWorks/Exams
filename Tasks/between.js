93d9644f3676711658389ce75c5de28a1b2a2b36
Viktoriia Kapys <kapysva@gmail.com>
2023-01-15 20:40:00 +0200

    delete the extra line

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,7 +5,6 @@ const getValueBetween = (string, firstSymbol, secondSymbol) => {
   let index = string.indexOf(firstSymbol);
   const leftPosition = index + firstSymbol.length;
   const newString = string.substring(leftPosition);
-  index !== -1 ? newString : '';
   index !== -1 ? index = newString.indexOf(secondSymbol) : '';
   const lastString = newString.substring(0, index);
   return lastString;
