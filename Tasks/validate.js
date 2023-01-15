b8f0f2bb5ee59ee25f78449a06e7e65f10bdddee
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2023-01-15 15:26:32 +0200

    add 'use strict' mode

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,14 +1,15 @@
 // Validate person name
+"use strict";
 
 const isValidate = (T) => {
   if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
+  if (T === "") return false;
+  if (typeof T !== "string") return false;
   if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
+  if (!T.includes(" ")) return false;
   {
     for (C of T) {
-      if (C === ' ') continue;
+      if (C === " ") continue;
       if (
         C.toLowerCase().charCodeAt(0) >= 97 &&
         C.toLowerCase().charCodeAt(0) <= 122
@@ -21,4 +22,4 @@ const isValidate = (T) => {
   }
 };
 
-require('../Tests/validate.js')(isValidate);
+require("../Tests/validate.js")(isValidate);
