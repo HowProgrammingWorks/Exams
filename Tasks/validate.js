e52b3760b015bd1f4412daa2ec3b9f7179403b65
Mihail-Ivannikov <116758780+Mihail-Ivannikov@users.noreply.github.com>
2023-01-15 03:39:33 +0200

    Update validate.js
    devide big function into two smaller

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,30 +1,24 @@
 // Validate person name
-
-//Add use strict
-//Change variables
-//Remove "Magic values"
-//delete needless IF
-
+//devide big function into two smaller
 'use strict';
 const isValidate = (name) => {
-	if (!name) return false;
-	if (typeof name !== 'string') return false;
-	if (name.length === 0) return false;
-	if (!name.includes(' ')) return false;
-	{
-		const LOWER_BORDER = 97;
-		const UPPER_BORDER = 122;
-		for (const letterOfName of name) {
-			if (letterOfName === ' ') continue;
-
-			const checkingSymbol = letterOfName.toLowerCase().charCodeAt(0);
-
-			if (checkingSymbol < LOWER_BORDER || checkingSymbol > UPPER_BORDER) {
-				return false;
-			}
-		}
-		return true;
-	}
+  if (!name) return false;
+  if (typeof name !== 'string') return false;
+  if (name.length === 0) return false;
+  if (!name.includes(' ')) return false;
+  if (symbolChecker(name) === false) return false;
+  return true;
+};
+const symbolChecker = (name) => {
+  const LOWER_BORDER = 97;
+  const UPPER_BORDER = 122;
+  for (const letterOfName of name) {
+    if (letterOfName === ' ') continue;
+    const checkingSymbol = letterOfName.toLowerCase().charCodeAt(0);
+    if (checkingSymbol < LOWER_BORDER || checkingSymbol > UPPER_BORDER) {
+      return false;
+    }
+  }
 };
 
 require('../Tests/validate.js')(isValidate);
