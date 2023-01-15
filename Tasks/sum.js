56d6cd1c7d1d9007aeb40a1051e56a43aab7b83d
stasyukhymenko <yukhymenko.stas@gmail.com>
2023-01-15 18:09:37 +0200

    added const and changed function definition

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,11 +1,10 @@
 // Sum all numbers from an array
 'use strict';
 
-function sum(...args) {
+const sum = (args) => {
   let res = 0;
-  let array;
-  array = args.flat(Infinity);
-  for(let i = 0; i<array.length; i++){
+  const array = args.flat(Infinity);
+  for(let i = 0; i < array.length; i++){
     if (typeof array[i] === 'number'){
       res += array[i];
     }
