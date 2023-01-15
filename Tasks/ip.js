63f0b1be2fabfd02ed62d92de13084347e2e0c68
Mihail-Ivannikov <116758780+Mihail-Ivannikov@users.noreply.github.com>
2023-01-15 03:47:25 +0200

    Update ip.js
    Add use strict
    Change variables
    remove "Magic values"

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,19 +1,29 @@
 // Split string by the first occurrence of separator
 
-Parseip = (i) => {
-  a = [];
-  if (i === '') return;
+//Add use strict
+//Change variables
+//remove "Magic values"
+
+'use strict';
+const Parseip = (IP) => {
+  const REQUIRED_LENGTH = 4;
+  const SPLIT_SYMBOL = '.';
+
+  const ipArray = [];
+
+  if (IP === '') return;
   else {
-    B = i.split('.');
-    if (B.length != 4) return;
-    j = 0;
-    for (const b of B) {
-      a[j] = parseInt(b);
-      if (isNaN(a[j])) return;
-      j++;
+    const splittedIp = IP.split(SPLIT_SYMBOL);
+    if (splittedIp.length !== REQUIRED_LENGTH) return;
+
+    let key = 0;
+    for (const element of splittedIp) {
+      ipArray[key] = parseInt(element);
+      if (isNaN(ipArray[key])) return;
+      key++;
     }
   }
-  return a;
+  return ipArray;
 };
 
 require('../Tests/ip.js')(Parseip);
