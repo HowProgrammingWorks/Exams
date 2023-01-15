1c3b7477617bf4ed4220bba8bbb09777be44c9ba
RostislavHundarev <122154736+xastia@users.noreply.github.com>
2023-01-15 02:36:04 +0200

    use ESlint

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,7 +5,7 @@ const Week = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseWeek = (string) => {
   for (let i = 0; i < Week.length; i++) {
-    if (string.startsWith(Week[i])) return i + 1
+    if (string.startsWith(Week[i])) return i + 1;
   }
   return -1;
 };
