49305dfd02ef72b6cf092ef1fad8be393d67053c
Hrabar Anastasia <110616793+anhrb@users.noreply.github.com>
2023-01-15 19:01:37 +0200

    Update duplicate.js

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,14 +2,10 @@
 'use strict';
 
 const duplicate = (value, amount) => {
-  if (N <= 0) return [];
-  else {
-    array = [];
-    for (let i = 0; i < amount; i++) {
+    const array = [];
+    for (let i = 0; i < amount; i++)
       array[i] = value;
-    }
     return array;
-  }
 };
 
 require('../Tests/duplicate.js')(duplicate);
