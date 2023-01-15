9c80747b60b7a398212ad704d264ade1cb214219
Kvezal_study <114149563+Yarule-study@users.noreply.github.com>
2023-01-15 22:05:26 +0200

    Update between.js
    ternary operator instead if

--- Tasks/between.js
+++ Tasks/between.js
@@ -7,11 +7,7 @@ const getvaluebetween = (str, p, s) => {
     str = str.substring(i + p.length);
     if (s) {
       i = str.indexOf(s);
-      if (i === -1) {
-        return '';
-      } else {
-        str = str.substring(0, i);
-      }
+      return (i === -1 ? '' : str.substring(0, i));
     }
   }
   return str;
