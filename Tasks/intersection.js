7a11f650ba8d87c82441262eafba264aeab17684
Mihail-Ivannikov <116758780+Mihail-Ivannikov@users.noreply.github.com>
2023-01-15 03:29:02 +0200

    Update intersection.js
    delete needless blocks
    don't mutate incoming parameters

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,18 +1,18 @@
 // Find an intersection of two dictionaries
 
-//change some variables
+//delete needless blocks
+//don't mutate incoming parameters
 
 'use strict';
 const intersection = (object1, object2) => {
-  const objectKeys = Object.keys(object1);
-  for (const key of objectKeys) {
-    if (object1[key] === object2[key]) {
-      object2[key] = object1[key];
-    } else {
-      delete object1[key];
-    }
-  }
-  return object1;
+	const objectKeys = Object.keys(object1);
+	const result = {};
+	for (const key of objectKeys) {
+		if (object1[key] === object2[key]) {
+			result[key] = object1[key];
+		}
+	}
+	return result;
 };
 
 require('../Tests/intersection.js')(intersection);
