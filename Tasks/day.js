a2bfe6b8b14722f992e12786ded7efbf2775ca89
Illya112 <122596697+Illya112@users.noreply.github.com>
2023-01-15 14:30:10 +0200

    Update Tasks/day.js
    Co-authored-by: Anastasia Usenko <114734522+Nastia0703@users.noreply.github.com>

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,6 +1,6 @@
 // Get day number
 'use strict';
-const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat',];
 
 const parseDay = (day) => {
   for (let i = 0; i < DAYS.length; i++) {
