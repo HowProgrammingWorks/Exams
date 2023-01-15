5f7c170c0013b4620709e42bb0e740d2a32fa27c
matviikiliar <122607905+matviikiliar@users.noreply.github.com>
2023-01-16 00:44:50 +0200

    unnecessary string

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,9 +4,8 @@ const count = (obj) => {
   let sum = 0;
   const values = Object.values(obj);
   for (const key of keys) {
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
+   if (typeof value === 'number') sum += value;
+  };
   return sum;
 };
 
