1923a9ff07283998ea2ebbdac12bdff2795d8fd7
aleksandrgarachun <115419603+aleksandrgarachun@users.noreply.github.com>
2023-01-15 22:32:01 +0300

    Update calculator.js

--- Project calculator/calculator.js
+++ Project calculator/calculator.js
@@ -12,3 +12,37 @@ function calculate({a, b, operation}){
 
 }
 
+
+/*    Старый код
+const OPERATIONS = {
+  sum: "+",
+  substruction: "-",     
+  multiplication: "*",
+  division: "/"
+};
+function calculate({a, b, operation}){
+  let result = null;
+  switch (operation) {
+      case OPERATIONS.sum:
+          result = sum(a, b);
+          break;
+      
+      case OPERATIONS.substruction:
+          result = substraction(a, b);
+          break;
+      
+      case OPERATIONS.multiplication:
+          result = multiplication(a, b);
+          break;
+      case OPERATIONS.division:
+          result = divison(a, b);
+          break;
+      
+      default:
+          break;
+  }
+  
+ 
+  return result;
+}
+*/
