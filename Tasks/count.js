8fe4b675e049f04f4a70f6d652f12ce846786de9
chernikova__d <dashkachernykova1995@gmail.com>
2023-01-15 16:26:06 +0200

    count.js > change forEach() to map()

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,7 +4,7 @@
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  keys.map((key) => {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
