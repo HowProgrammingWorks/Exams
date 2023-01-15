813872c1415a9ad9144b43067879a47b0e239e9f
RostislavHundarev <122154736+xastia@users.noreply.github.com>
2023-01-15 02:20:08 +0200

    add 'use strict'

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,9 +1,10 @@
 // Find longest string
+'use strict';
 
 const longest = function (line = []) {
-  x = -1;
- 
- for (i of line) {
+  let x = -1;
+  let s = undefined;
+  for (const i of line) {
     if (i.length > x) {
       x = i.length;
       s = i;
