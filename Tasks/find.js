1f45e806f7440933b70b8956f6c80c9c237b3143
PudchenkoAlexei <you@example.com>
2023-01-15 21:14:39 +0200

    delete else that we do not use

--- Tasks/find.js
+++ Tasks/find.js
@@ -4,10 +4,7 @@ const find = (object, ...rest) => {
   const value = rest.pop();
   for (name in object) {
     if (object[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
-    }
+    } 
   }
   rest.push(5020);
   return undefined;
