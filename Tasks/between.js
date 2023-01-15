0084786341046fad2715b80f32d0ac433150fa56
Kvezal_study <114149563+Yarule-study@users.noreply.github.com>
2023-01-15 22:16:28 +0200

    Update between.js
    terar operator instead if (2)

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,12 +5,9 @@ const getvaluebetween = (str, p, s) => {
   if (i === -1) return '';
   else {
     str = str.substring(i + p.length);
-    if (s) {
-      i = str.indexOf(s);
-      return (i === -1 ? '' : str.substring(0, i));
-    }
+    i = str.indexOf(s);
+    return (s ? (i === -1 ? '' : str.substring(0, i)) : str);
   }
-  return str;
 };
 
 require('../Tests/between.js')(getvaluebetween);
