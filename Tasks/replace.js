140a6a3ff07a3d8fad94ff4e8083362081ca8970
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2023-01-15 15:01:52 +0200

    delete needless variables

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -5,15 +5,18 @@ const replaceSubstring = (str, substr, newstr) => {
   if (substr === "") {
     return str;
   }
+
   const index = str.indexOf(substr);
+
   if (index === -1) {
     return str;
   } else {
     const startOfString = str.substring(0, index);
-    const oldString = str.substring(index + substr.length, str.length);
-    const newString = startOfString + newstr;
-    const fullNewSring = newString + oldString;
-    return replaceSubstring(fullNewSring, substr, newstr);
+
+    const newString =
+      startOfString + newstr + str.substring(index + substr.length, str.length);
+
+    return replaceSubstring(newString, substr, newstr);
   }
 };
 
