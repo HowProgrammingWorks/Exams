ceda77772ddaac8067e161cc3b215e6554e8c890
stasyukhymenko <yukhymenko.stas@gmail.com>
2023-01-15 19:47:16 +0200

    changed function definition and added const

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,10 +1,10 @@
 // Find an intersection of two dictionaries
 'use strict';
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
+const intersection = (object_1, object_2) => {
+  const first_keys = Object.keys(object_1);
 
-  for (attribute_name of first_keys) {
+  for (const attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
 
