da368aeeac073ecc53815416426db3a549a4d95a
stasyukhymenko <yukhymenko.stas@gmail.com>
2023-01-15 18:42:13 +0200

    changed if with ternary operator

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -5,9 +5,7 @@ const sum = (args) => {
   let res = 0;
   const array = args.flat(Infinity);
   for(const value of array){
-    if (typeof value === 'number'){
-      res += value;
-    }
+    res = (typeof value === 'number') ? res += value : res;
   }
   return res;
 }
