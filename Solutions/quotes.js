c8da2f97bb663d9f44813e5056c6c6d98b7399dc
Danil Kobzev <denthenttg@gmail.com>
2023-01-15 07:38:58 +0200

    Add 'use strict', "let", "const"

--- Solutions/quotes.js
+++ Solutions/quotes.js
@@ -1,13 +1,13 @@
-// Change double quotation to open or close quotation
+'use strict'
 
 const EMPTY = '';
 
-quotes = function (s) {
-  res = [];
-  open = false;
-  for (c of s) {
+const quotes = function (s) {
+  let res = [];
+  let open = false;
+  for (let c of s) {
     if (c === '"') {
-      for (i of c) {
+      for (let i of c) {
         if (!open) {
           res.push('«');
           open = true;
@@ -18,7 +18,7 @@ quotes = function (s) {
       }
     } else {
       if (c !== '"') {
-        for (i of c) {
+        for (let i of c) {
           res.push(i);
         }
       }
