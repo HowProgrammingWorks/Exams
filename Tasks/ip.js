e5173545657750769ae074d8b91b260a25a2ea04
Mihail-Ivannikov <116758780+Mihail-Ivannikov@users.noreply.github.com>
2023-01-15 03:51:55 +0200

    Update ip.js
    devide into two smaller functions

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,27 +1,25 @@
 // Split string by the first occurrence of separator
-
-//Add use strict
-//Change variables
-//remove "Magic values"
+//devided into two smaller functions
 
 'use strict';
 const Parseip = (IP) => {
   const REQUIRED_LENGTH = 4;
   const SPLIT_SYMBOL = '.';
-
   const ipArray = [];
-
   if (IP === '') return;
-  else {
-    const splittedIp = IP.split(SPLIT_SYMBOL);
-    if (splittedIp.length !== REQUIRED_LENGTH) return;
+  const splittedIP = IP.split(SPLIT_SYMBOL);
+  if (splittedIP.length !== REQUIRED_LENGTH) return;
+  const result = fillIpArray(ipArray, splittedIP);
 
-    let key = 0;
-    for (const element of splittedIp) {
-      ipArray[key] = parseInt(element);
-      if (isNaN(ipArray[key])) return;
-      key++;
-    }
+  return result;
+};
+
+const fillIpArray = (ipArray, splittedIP) => {
+  let key = 0;
+  for (const element of splittedIP) {
+    ipArray[key] = parseInt(element);
+    if (isNaN(ipArray[key])) return;
+    key++;
   }
   return ipArray;
 };
