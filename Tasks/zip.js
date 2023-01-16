6181829f9c4b753857ab7b60a3ce8d6931caf2fd
unknown <sachko.maxim@lll.kpi.ua>
2023-01-16 14:40:10 +0200

    Removed useless code

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -2,7 +2,6 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
 const zipArray = (arr1, arr2) => {
-  let j = 0;
   const length = arr1.length > arr2.length ? arr2.length : arr1.length;
   const res = [];
   for (let i = 0; i < length; i++) {
