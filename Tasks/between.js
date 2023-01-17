a84d8ca09d11051b771da9fa7fc5f0de022dd6eb
Illia Kornyk <forest.bukson@gmail.com>
2023-01-17 17:48:17 +0200

    fix: name of function, decleare variables for indexes

--- Tasks/between.js
+++ Tasks/between.js
@@ -4,17 +4,24 @@ const getValueBetween = (str, p, s) => {
   let value;
   const prefixStartIndex = str.indexOf(p);
 
-  if (prefixStartIndex === -1) return "";
+  if (prefixStartIndex === -1) {
+    return "";
+  }
 
   const prefixEndIndex = prefixStartIndex + p.length;
   value = str.substring(prefixEndIndex);
-  
-  const suffixStartIndex = value.indexOf(s);
 
-  if (suffixStartIndex === -1) return "";
-  value = value.substring(0, suffixStartIndex);
+  if (value) {
+    const suffixIndex = value.indexOf(s);
 
+    if (suffixIndex === -1) {
+      return "";
+    } else {
+      value = value.substring(0, suffixIndex);
+    }
+  }
   return value;
 };
+
 
 require("../Tests/between.js")(getValueBetween);
