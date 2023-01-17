20ebb9725fa389075cbe58182715dbbedf22319d
Illia Kornyk <forest.bukson@gmail.com>
2023-01-17 16:35:59 +0200

    fix: change let to const and change function name

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,10 +1,10 @@
-// Split an array into two parts
+// // Split an array into two parts
 
-let split_Array = (index, array) => {
+const splitArray = (index, array) => {
   begin = array.slice(0, index);
   const len = array.length;
   array = array.slice(index, len);
   return [begin, array];
 };
 
-require('../Tests/split.js')(split_Array);
+require('../Tests/split.js')(splitArray);
