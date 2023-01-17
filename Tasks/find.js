bdc0aa87dcd3ec3663a02fa9cebccee78c2c8052
sofiakravchuk <kravchuk.sofiya1795@gmail.com>
2023-01-17 03:12:04 +0200

    Update find.js
    code optimization in find.js

--- Tasks/find.js
+++ Tasks/find.js
@@ -3,11 +3,9 @@
 find = (object, ...rest) => {
   let value = rest.pop();
   for (key in object) {
-    if (object[key] !== value) {
-    } else {
-      if (typeof key) return key;
-      if (typeof object) return object;
-    }
+    if (object[key] === value) {
+      return key;
+    } 
   }
   return undefined;
 };
