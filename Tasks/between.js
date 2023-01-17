b20693fd518f9e203bdf6edbfc523e17bb50e740
Illia Kornyk <forest.bukson@gmail.com>
2023-01-17 22:25:45 +0200

    fix: add conditional, use slice method

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,22 +1,13 @@
 // Extract substring between prefix and suffix
 
 const getValueBetween = (str, p, s) => {
-  let value;
   const prefixStartIndex = str.indexOf(p);
-
-  if (prefixStartIndex === -1) return "";
-
   const prefixEndIndex = prefixStartIndex + p.length;
-  value = str.substring(prefixEndIndex);
-  
-  const suffixStartIndex = value.indexOf(s);
+  const suffixStartIndex = str.indexOf(s);
 
-  if (suffixStartIndex === -1) return "";
-  value = value.substring(0, suffixStartIndex);
-
-  return value;
+  if ([prefixStartIndex, suffixStartIndex].includes(-1)) {
+    return "";
+  }
+  return str.slice(prefixEndIndex, suffixStartIndex);
 };
-
-
-
 require("../Tests/between.js")(getValueBetween);
