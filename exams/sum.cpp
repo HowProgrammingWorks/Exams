eedf742243d30e8ae2b56fbbfd50b2d32b9b0521
karoll1naa <karollinnajan@gmail.com>
2023-01-18 18:32:20 +0200

    Added tasks

---
+++ exams/sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    float Sum = 0, A[i];
+    cout<<"Enter size Array = "<<endl;
+    cin>>i;
+    cout<<"Array: "<<endl;
+    for (int j = 0; j < i; j++){
+        cin>>A[i];
+        Sum+= A[i];
+    }
+    cout<<"Sum: "<<endl;
+    cout << Sum <<endl;
+
+}
