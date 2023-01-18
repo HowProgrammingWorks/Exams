25efd51fe00b2ae2e5e3eb21710937024ec4f7e1
Walik23 <122877885+Walik23@users.noreply.github.com>
2023-01-18 03:05:13 +0200

    Update words.js

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,40 +1,27 @@
 // Count words in a string
 
+//Step 1
+//Remove recurring conditions and branches
+//Add 'use strict'
+
+'use strict'
+
 Words = function (s) {
   number_of_words_in_s = 0;
   flag = false;
   for (c of s) {
     if (!flag) {
       if (c === ' ') {
-        if (flag === true) {
           flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
           flag = true;
-        } else {
-          flag = true;
-        }
         number_of_words_in_s++;
       }
     } else {
-      if (c === ' ') {
-        if (flag === true) {
+      if (c === ' ')
           flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
+          flag = true; 
       }
     }
-  }
   return number_of_words_in_s;
 };
 
