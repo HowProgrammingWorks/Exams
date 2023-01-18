8b2e5eeef20594f7fdd819d69562e28197ac5d6c
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 11:29:28 +0200

    Update size.js
    adding constant

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,6 +1,6 @@
 // Convert number to file size in bytes, kb, mb, and gb
 
-let size = (size) => {
+const size = (size) => {
   if (size === 0) return '0 byte';
   else {
     const exp = Math.floor(Math.log(size) / Math.log(1000));
