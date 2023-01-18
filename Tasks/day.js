fb3d7ae051aa929ad469335f2102666f1bd370df
DipodidaeA <114568927+DipodidaeA@users.noreply.github.com>
2023-01-18 21:18:38 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,8 @@
-// Get day number
-
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
-};
-
-require('../Tests/day.js')(_parse_day_);
+let Day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let i;
+const AmountDay = Day.length;
+for (i = 0; i < AmountDay; i++) {
+    let IndexDay = Day.indexOf(Day[i]);
+    let NumberDay = IndexDay + 1;
+    console.log (`Day: ${Day[i]}, his naumber ${NumberDay}`);
+}
