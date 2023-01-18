d0b34955688252da6fe1746f3f415d7b129c7dcc
Walik23 <122877885+Walik23@users.noreply.github.com>
2023-01-18 15:33:18 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,18 +1,16 @@
 // Get day number
 
-//Step 2
-//Add 'use strict';
-//Add const
-//Turn `i + 1` into `++i`
-
-'use strict';
+//Step 1
+//Change `let` into `const`
+//Move `let i` into `for`
+//Remove useless brackets
 
 const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const _parse_day_ = (s) => {
+_parse_day_ = (s) => {
   for (let i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) 
-      return ++i;
+      return i + 1;
   }
   return -1;
 };
