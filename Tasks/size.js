9e343c7540142faf327d7ec74a2326224072eea5
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 11:59:36 +0200

    Update size.js
    declared an array, so  => not "DRY"

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,13 +1,12 @@
 // Convert number to file size in bytes, kb, mb, and gb
+const size_units = ["unit", "kb", "mb", "gb"];
 
 const size = (size) => {
   if (size === 0) return "0 byte";
 
   const exp = Math.floor(Math.log(size) / Math.log(1000));
   if (exp === 0) return size + " byte";
-  if (exp === 1) return Math.round(size / 10e2) + " kb";
-  if (exp === 2) return Math.round(size / 10e5) + " mb";
-  if (exp === 3) return Math.round(size / 10e8) + " gb";
+  return Math.round(size / 1000 ** exp) + " " + size_units[exp];
 };
 
 require("../Tests/size.js")(size);
