aa3292fc809c15f2f4b77be1def30cf22507f1f5
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 12:24:38 +0200

    Update day.js
    easier code understanding

--- Tasks/day.js
+++ Tasks/day.js
@@ -6,7 +6,8 @@ const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const _parse_day_ = (s) => {
   for (let index = 0; index < days.length; index++) {
-    if (s.startsWith(days[index].toLowerCase())) {
+    const checkCase = days[index].toLowerCase();
+    if (s.startsWith(checkCase)) {
       return ++index;
     }
   }
