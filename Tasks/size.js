5f9b41fae18817eada303206a31ab79cd99e5dc5
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 12:01:53 +0200

    Update size.js

--- Tasks/size.js
+++ Tasks/size.js
@@ -4,7 +4,7 @@ const size_units = ["unit", "kb", "mb", "gb"];
 const size = (size) => {
   if (size === 0) return "0 byte";
 
-  const exp = Math.floor(Math.log(size) / Math.log(1000));
+  const exp = Math.floor(Math.log(size) / Math.log(10e2));
   if (exp === 0) return size + " byte";
   return Math.round(size / 1000 ** exp) + " " + size_units[exp];
 };
