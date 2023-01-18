c2d0092ba597ba57fb5300693a83489e8b6c6805
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 12:15:34 +0200

    Update day.js
    adding constants

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,8 +1,8 @@
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+const _parse_day_ = (s) => {
   let i;
   for (i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
