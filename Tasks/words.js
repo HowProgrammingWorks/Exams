fb923fb0eae8b83adfa61cc0ce37346a81a8a7a3
Walik23 <122877885+Walik23@users.noreply.github.com>
2023-01-18 09:18:55 +0200

    Update words.js

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,15 +1,14 @@
 // Count words in a string
 
-//Step 1
-//Remove recurring conditions and branches
-//Add 'use strict'
+//Step 2
+//Add `const` and `let`
 
 'use strict'
 
-Words = function (s) {
-  number_of_words_in_s = 0;
-  flag = false;
-  for (c of s) {
+const Words = function (s) {
+  let number_of_words_in_s = 0;
+  let flag = false;
+  for (const c of s) {
     if (!flag) {
       if (c === ' ') {
           flag = false;
