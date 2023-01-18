5bbfe0098cf73210e5101348ce46b9d477e4b4f8
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 11:32:34 +0200

    Update size.js
    Else are not necessary

--- Tasks/size.js
+++ Tasks/size.js
@@ -2,13 +2,11 @@
 
 const size = (size) => {
   if (size === 0) return "0 byte";
-  else {
-    const exp = Math.floor(Math.log(size) / Math.log(1000));
-    if (exp == 0) return size + " byte";
-    else if (exp == 1) return Math.round(size / 1000) + " kb";
-    else if (exp == 2) return Math.round(size / 1000000) + " mb";
-    else if (exp == 3) return Math.round(size / 1000000000) + " gb";
-  }
+  const exp = Math.floor(Math.log(size) / Math.log(1000));
+  if (exp == 0) return size + " byte";
+  if (exp == 1) return Math.round(size / 1000) + " kb";
+  if (exp == 2) return Math.round(size / 1000000) + " mb";
+  if (exp == 3) return Math.round(size / 1000000000) + " gb";
 };
 
 require("../Tests/size.js")(size);
