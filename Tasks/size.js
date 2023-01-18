39a5814d5f4ff2555bd481fb6af4d9c4e506f3b5
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 11:33:56 +0200

    Update size.js
    better readability

--- Tasks/size.js
+++ Tasks/size.js
@@ -4,9 +4,9 @@ const size = (size) => {
   if (size === 0) return "0 byte";
   const exp = Math.floor(Math.log(size) / Math.log(1000));
   if (exp == 0) return size + " byte";
-  if (exp == 1) return Math.round(size / 1000) + " kb";
-  if (exp == 2) return Math.round(size / 1000000) + " mb";
-  if (exp == 3) return Math.round(size / 1000000000) + " gb";
+  if (exp == 1) return Math.round(size / 10e2) + " kb";
+  if (exp == 2) return Math.round(size / 10e5) + " mb";
+  if (exp == 3) return Math.round(size / 10e8) + " gb";
 };
 
 require("../Tests/size.js")(size);
