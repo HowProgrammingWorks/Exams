cadbf766df6a295afafb39413c290c2558d66c71
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 12:19:10 +0200

    Update day.js
    index + 1 to ++index

--- Tasks/day.js
+++ Tasks/day.js
@@ -7,7 +7,7 @@ const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const _parse_day_ = (s) => {
   for (let index = 0; index < days.length; index++) {
     if (s.startsWith(days[index].toLowerCase())) {
-      return index + 1;
+      return ++index;
     }
   }
   return -1;
