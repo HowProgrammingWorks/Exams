80e13f28cb81c8d3426214205314687a283ccde9
Kirill Yaymchuk <stbestich@gmail.com>
2023-01-18 11:30:34 +0200

    Update size.js
    Little bit style change

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,14 +1,14 @@
 // Convert number to file size in bytes, kb, mb, and gb
 
 const size = (size) => {
-  if (size === 0) return '0 byte';
+  if (size === 0) return "0 byte";
   else {
     const exp = Math.floor(Math.log(size) / Math.log(1000));
-    if (exp == 0) return size + ' byte';
-    else if (exp == 1) return Math.round(size / 1000) + ' kb';
-    else if (exp == 2) return Math.round(size / 1000000) + ' mb';
-    else if (exp == 3) return Math.round(size / 1000000000) + ' gb';
+    if (exp == 0) return size + " byte";
+    else if (exp == 1) return Math.round(size / 1000) + " kb";
+    else if (exp == 2) return Math.round(size / 1000000) + " mb";
+    else if (exp == 3) return Math.round(size / 1000000000) + " gb";
   }
 };
 
-require('../Tests/size.js')(size);
+require("../Tests/size.js")(size);
