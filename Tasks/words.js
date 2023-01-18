1987a82f2b72dbd945c10d94d3c479d2bbaf2610
Nikita <115003350+kikoser@users.noreply.github.com>
2023-01-18 07:49:42 +0200

    Update words.js
    We replace part of the branches with the Ternary operator
    We change the function to an arrow

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,21 +1,13 @@
 // Count words in a string
 
-Words = function (s) {
-  number_of_words_in_s = 0;
-  flag = false;
-  for (c of s) {
-    if (!flag) {
-      if (c === ' ') flag = false; 
-       else {
-            flag = true;
-            number_of_words_in_s++;
-      }
-    }else {
-      if (c === ' ') flag = false;
-       else  flag = true; 
-    }
+const Words = (s) => {
+  let number_of_words_in_s = 0;
+  let flag ;
+  for ( c of s) {
+    if (!flag)   number_of_words_in_s++; 
+      c === ' ' ? flag = false : flag = true;
   }
   return number_of_words_in_s;
-};
-
+}
 require('../Tests/words.js')(Words);
+
