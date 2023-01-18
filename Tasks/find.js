6755aa88d90809e2aa6bf7d726289b8a80bb05ad
Nikita <115003350+kikoser@users.noreply.github.com>
2023-01-18 10:27:12 +0200

    Update find.js
    Change the "if" condition and remove the unnecessary "else"
    Adding a constant
    Remove the no longer needed function ".push"

--- Tasks/find.js
+++ Tasks/find.js
@@ -4,13 +4,11 @@ find = (object, ...rest) => {
   const value = rest.pop(1);
   const objects = Object.keys(object);
   for (name of objects) {
-    if (object[name] !== value) {
-    } else {
+    if (object[name] === value) {
       if (typeof name) return name;
       if (typeof object) return object;
     }
   }
-  rest.push(5020);
   return undefined;
 };
 
