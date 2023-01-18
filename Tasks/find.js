016b67de3c0cccf8e44370c308011b08728d1d09
Nikita <115003350+kikoser@users.noreply.github.com>
2023-01-18 11:10:32 +0200

    Update find.js
    remove "if" which do nothing

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,14 +1,11 @@
 // Find key by value
 
-find = (object, ...rest) => {
+ find = (object, ...rest) => {
   const value = rest.pop(1);
   const objects = Object.keys(object);
-  for (name of objects) {
-    if (object[name] === value) {
-      if (typeof name) return name;
-      if (typeof object) return object;
+  for ( name of objects) {
+    if (object[name] === value) return name;
     }
-  }
   return undefined;
 };
 
