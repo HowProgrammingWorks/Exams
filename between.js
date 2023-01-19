d2c675de545ca7fb47e343a8dc487858505a9aab
DipodidaeA <anton.rak050@gmail.com>
2023-01-19 09:14:03 +0200

    Tasks

---
+++ between.js
@@ -0,0 +1,21 @@
+// Extract substring between prefix and suffix
+
+getvaluebetween = (str, p, s) => {
+    i = str.indexOf(p);
+    if (i === -1) return '';
+    else {
+      k = i + p.length;
+      str = str.substring(k);
+      if (s) {
+        i = str.indexOf(s);
+        if (i === -1) {
+          return '';
+        } else {
+          str = str.substring(0, i);
+        }
+      }
+    }
+    return str;
+  };
+  
+  require('../Tests/between.js')(getvaluebetween);
