e906ae8f42e0ca1f9b6e4d21d1384eb5b7a81f2a
bifynok <121981921+bifynok@users.noreply.github.com>
2023-01-19 20:37:04 +0200

    Change to arrow function

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,6 +1,6 @@
 // Find an intersection of two dictionaries
 
-intersection = function intersection(object_1, object_2) {
+const intersection = (object_1, object_2) => {
   first_keys = Object.keys(object_1);
   for (attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
