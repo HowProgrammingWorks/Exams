1df4fbbfee5d6d813981d382abe6633f3ef815ae
VadimOpt <vadimdidur20@gmail.com>
2023-01-19 23:23:37 +0200

    3-count

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,15 @@
-//Step 2
-//Reduced code in function
-//Removed other useless things
-
+//Step 3
+//Used array methods to sort and sum elements
+//Cleared useless things
 
 const count = (obj) => {
- let sum = 0;
-  
-  Object.keys(obj).forEach((key) => {
-    if (typeof obj[key] === 'number') sum += obj[key];
-  });
-  
+  let sum = 0;
+  Object.values(obj)
+    .filter((value) => typeof value === "number")
+    .map((key) => {
+      sum += Number(key);
+    });
+
   return sum;
 };
-
 require('../Tests/count.js')(count);
