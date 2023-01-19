0a606b4158394427a3f9fc7b5c804604780454cc
bifynok <121981921+bifynok@users.noreply.github.com>
2023-01-19 20:35:44 +0200

    Remove useless brackets

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,22 +2,13 @@
 
 intersection = function intersection(object_1, object_2) {
   first_keys = Object.keys(object_1);
-  {
-  }
   for (attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
-    } else {
+    } 
+    else {
       delete object_1[attribute_name];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object_1;
 };
