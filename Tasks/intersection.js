d071830fc1cc38c59127aaf3c1ef3ca373190754
bifynok <121981921+bifynok@users.noreply.github.com>
2023-01-19 20:39:09 +0200

    Remove unnecessary variable

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,8 +1,7 @@
 // Find an intersection of two dictionaries
 
 const intersection = (object_1, object_2) => {
-  first_keys = Object.keys(object_1);
-  for (attribute_name of first_keys) {
+  for (attribute_name of Object.keys(object_1)) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
     } 
