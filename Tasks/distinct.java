417e7823841d83cc1da3626a2f4f79e911f70107
Rembqq <nikitavolkovskyi@gmail.com>
2023-01-20 00:02:01 +0200

    Add distinct class and func

---
+++ Tasks/distinct.java
@@ -0,0 +1,19 @@
+// Return an array without duplicates
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class distinct
+{
+    public static Set<Integer> dist(int[] data)
+    {
+        Set<Integer> s = new HashSet<Integer>();
+        return s;
+    }
+    public static void main(String[] args)
+    {
+        int[] l = {1, 2, 1, 3, 1, 4};
+        System.out.println(dist(l));  
+    }
+}
+
