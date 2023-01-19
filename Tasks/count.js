21a34408d5612cceacaa18e53917757a471520b2
VadimOpt <vadimdidur20@gmail.com>
2023-01-19 23:40:55 +0200

    5-count

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,8 +1,16 @@
-//Step 4
-//Used reduce method to sum all array elements
-//Reduced code space to minimal:)(this is joke)
+//Step 5
+//include 'use strict'
 
 
+/*Normal version 
+'use strict';
+const count = (obj) => {
+  return Object.values(obj)
+  .filter((value) => typeof value === "number")
+  .reduce((acc, key) => acc + Number(key), 0);
+}; 
+*/
+'use strict';
 const count = (obj) => Object.values(obj).filter((value) => typeof value === "number").reduce((acc, key) => acc + Number(key), 0);
 
 require('../Tests/count.js')(count);
