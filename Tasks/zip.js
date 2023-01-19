08fdc266abf07ac5d737517afc8e9873dcc1d665
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-01-19 10:51:04 +0000

    Added retreats, let and const to identifiers.

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,17 +1,25 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
 const zip = function (a = [], b = []) {
+
   let i = 0;
-  j = 0;
+  let j = 0;
+
   for (x of b) {
-    CELL = [a[i++], x];
+
+    const CELL = [a[i++], x];
+
     if (i < j) {
       delete a[i++];
-    } else {
+    } 
+
+    else {
       (() => (b[j++] = CELL))();
     }
+
     if (CELL[0] == undefined) b.length -= 1
   }
+  
   return b;
 };
 
