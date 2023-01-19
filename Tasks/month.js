066dc5f2c65f18d32f113e063b754ee5c8225cc0
snow <44925043+oppressedsnow@users.noreply.github.com>
2023-01-19 19:38:30 +0200

    Changed arrow function
    Remove "l=months.length" and moved in for loop

--- Tasks/month.js
+++ Tasks/month.js
@@ -15,9 +15,8 @@ Months = [
   'dec',
 ];
 
-Month = (s) => {
-  l = Months.length;
-  for (let i = 0; i < l; i++) {
+const Month = (s) => {
+  for (let i = 0; i < Months.length; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
   return -1;
