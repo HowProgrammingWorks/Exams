10da7fe0f35540f74802f49c1f9dca65a2fd97c5
bifynok <121981921+bifynok@users.noreply.github.com>
2023-01-19 20:47:33 +0200

    Format code

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,10 +3,10 @@
 const intersection = (object1, object2) => {
   for (const key of Object.keys(object1)) {
     object1[key] === object2[key]
-    ?  object2[key] = object1[key]
-    :  delete object1[key];
+      ? (object2[key] = object1[key])
+      : delete object1[key];
   }
   return object1;
 };
 
-require('../Tests/intersection.js')(intersection);
+require("../Tests/intersection.js")(intersection);
