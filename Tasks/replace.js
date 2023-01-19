58a2005739964c870442f56af600faeb6ef350d5
anastasia-sl <anastasiaslynko2206@gmail.com>
2023-01-19 20:30:02 +0200

    logic change

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -6,19 +6,20 @@ const replace = (str, substr, newstr) => {
     return str;
   }
 
-  let src = str;
-  let result = '';
+  let result = str;
 
   do {
-    const startIndex = src.indexOf(substr);
+    const startIndex = result.indexOf(substr);
 
     if (startIndex === -1) {
-      return result + src;
+      return result;
     }
 
-    const start = src.substring(0, startIndex);
-    src = src.substring(startIndex + substr.length, src.length);
-    result += start + newstr;
+    const endIndex = startIndex + substr.length;
+    const startString = result.substring(0, startIndex);
+    const endString = endIndex > result.length ?
+      '' : result.substring(endIndex);
+    result = startString + newstr + endString;
   } while (true);
 };
 
