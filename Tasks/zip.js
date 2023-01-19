2b6f068a049149a41463bb18e98b9cc527f1a20f
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-01-19 19:22:34 +0000

    indx fix and retreats

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,17 +1,26 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
 const zip = function (array1 = [], array2 = []) {
-  let i = 0;
-  j = 0;
+  let indx1 = 0;
+  let indx2 = 0;
+  
   for (x of b) {
-    CELL = [array1[i++], x];
+
+    CELL = [array1[indx1++], x];
+
     if (i < j) {
-      delete array1[i++];
+
+      delete array1[indx2++];
+
     } else {
-      (() => (array2[j++] = CELL))();
+
+      (() => (array2[indx2++] = CELL))();
+
     }
+
     if (CELL[0] == undefined) b.length -= 1
   }
+
   return b;
 };
 
