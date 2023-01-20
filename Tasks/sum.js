aa0b1e44977eec5a992662a07779003f87d16010
Lopatinskiy Nazar <114024919+hikivkomori@users.noreply.github.com>
2023-01-20 15:02:12 +0000

    2-sum

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,20 +1,16 @@
-// Step 1
-// Removed useless lexical environment setting
-// Remowed eseless variables such as k and redused code in function
-// Added const and let where needed for variables
+// Step 2
+// Reduced useless code in function
 
 const sum = (s) =>  {
-  res = [0];
+  const res = [0];
   for (i of s) {
-    let t = typeof i;
-    if (t === 'number') {
+    if (typeof i === 'number') {
       if (res.length > 0) {
         const new_Sum = res[res.length - 1] + i;
         res.push(new_Sum);
       }
     }
   }
-  res[0];
   return res[res.length - 1];
 };
 
