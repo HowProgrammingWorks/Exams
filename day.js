d2d68b749e4f7792e0df6cb1fba47127c4ba47ea
DipodidaeA <anton.rak050@gmail.com>
2023-01-20 08:12:08 +0200

    day.js 2

--- day.js
+++ day.js
@@ -1,11 +1,7 @@
 let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
+let i;
+for (i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
-  }
-  return -1;
-};
+}
