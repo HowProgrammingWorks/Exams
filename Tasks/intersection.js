6083dc8239b159fb94a3902d44f8422cb71c3c6a
VadimOpt <105482228+VadimOpt@users.noreply.github.com>
2023-01-20 16:19:40 +0200

    1-intersection

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,25 +1,19 @@
-// Find an intersection of two dictionaries
+// Step 1
+//Added 'use strict'
+//Removed unnecessary blocks
+//Added "const" and "let" to variables 
+'use strict'
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
-  {
-  }
-  for (attribute_name of first_keys) {
+const intersection = function intersection(object_1, object_2) {
+  const first_keys = Object.keys(object_1);
+  for (let attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
-    } else {
+      } else {
       delete object_1[attribute_name];
-      {
-      }
+       }
     }
-    {
-    }
-  }
-  {
-  }
-  return object_1;
+ return object_1;
 };
 
 require('../Tests/intersection.js')(intersection);
