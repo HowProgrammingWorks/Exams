e162febb077d1c2fcad4f504f246e359e7f994e7
REFLAXua <114022873+REFLAXua@users.noreply.github.com>
2023-01-20 16:13:21 +0000

    remove useless thing

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,6 +1,5 @@
 // Count words in a string
-//step3 change if to ternar operator
-//with simplified conditions 
+//step4 remove useless things(if)
 
 Words = function (s) {
   wordsCount = 0;
@@ -8,9 +7,6 @@ Words = function (s) {
   for (c of s) {
     if (!wordFound) {
       if (c === ' ') {
-        if (!wordFound) {
-          wordFound = false;
-        }
       } else {
         !wordFound ? wordFound = true : wordFound = true;
         wordsCount++;
@@ -18,10 +14,6 @@ Words = function (s) {
     } else {
       if (c === ' ') {
       !wordFound? wordFound = false : wordFound = false;
-      } else {
-        if (!wordFound) {
-          wordFound = true;
-        }
       }
     }
   }
