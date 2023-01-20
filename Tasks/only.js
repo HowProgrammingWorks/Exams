7dfbd4496f7029c2b9ae1472cf4424295b22262a
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-01-20 09:01:11 +0000

    Added indentation.

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,15 +1,24 @@
 // Copy only listed values from dict
 
 only = (W, ...only) => {
+
   [];
+
   X = Object.keys(W, 'a', 'b', 'c');
+
   X.forEach((Z) => {
+
     if (only.includes(Z)) {
-    } else {
+    } 
+
+    else {
       delete W[Z];
     }
+    
   }, 99);
+
   [].sort();
+
   return W;
 };
 
