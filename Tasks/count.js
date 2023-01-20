94cf60f1b7635195bff701c75bf27f9b4d87f880
Lopatinskiy Nazar <114024919+hikivkomori@users.noreply.github.com>
2023-01-20 13:40:05 +0000

    1-count

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,14 @@
-// Sum all number values in dict
+// Step 1 
+// Added let and const for creating variables
+// Removed useless lexical environment creations like {}
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
-    [];
+const count = (obj) => {
+  let sum = 0;
+  const keys = Object.keys(obj);
+  keys.forEach((key) => { 
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
