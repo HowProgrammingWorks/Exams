66bcdafdd7186a4cd14d7c433bd1792d930714ce
VitaliyP12 <122998670+VitaliyP12@users.noreply.github.com>
2023-01-20 19:11:51 +0200

    2-sum

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,21 +1,17 @@
-//Step 1
-//removed useless lexical environment setting 
-//added const and let where needed for variables
-//removed useless variables and reduced code in function
+//Step 2
+//reduced code in function 
 
 const sum = (s) => {
   res = [0];
   let k = 5;
   for (i of s) {
-    let t = typeof i;
-    if (t === 'number') {
+    if (typeof i === 'number') {
       if (res.length > 0) {
         const new_Sum = res[res.length - 1] + i;
         res.push(new_Sum);
       }
     }
   }
-  res[0];
   return sum[sum.length - 1];
 };
 
