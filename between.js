b23c74453bb933a8ebac14aa94acb0a39034f25f
DipodidaeA <anton.rak050@gmail.com>
2023-01-20 08:25:23 +0200

    between.js 1

---
+++ between.js
@@ -0,0 +1,17 @@
+getvaluebetween = (str, p, s) => {
+  i = str.indexOf(p);
+  if (i === -1) return '';
+  else {
+    k = i + p.length;
+    str = str.substring(k);
+    if (s) {
+      i = str.indexOf(s);
+      if (i === -1) {
+        return '';
+      } else {
+        str = str.substring(0, i);
+      }
+    }
+  }
+  return str;
+};
