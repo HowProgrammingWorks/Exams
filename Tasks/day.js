dc996da011a77a1ce3c2fce768edd982a6798920
bifynok <121981921+bifynok@users.noreply.github.com>
2023-01-20 14:47:41 +0200

    Put 'let' inside cycle

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,8 +3,7 @@
 const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (days) => {
-  let i;
-  for (i = 0; i < week.length; i++) {
+  for (let i = 0; i < week.length; i++) {
     if (days.startsWith(week[i].toLowerCase())) {
       return i + 1;
     }
