b9a739eacaae4ae765f17cac779ca081d1b7f999
VladiusVostokus <115622905+VladiusVostokus@users.noreply.github.com>
2023-01-20 10:54:34 +0000

    One if instead if/else.

--- Tasks/only.js
+++ Tasks/only.js
@@ -3,14 +3,12 @@
 
 const only = (W, ...only) => {
 
-
   const X = Object.keys(W, 'a', 'b', 'c');
 
   for(const Z of X){
 
-    if (only.includes(Z));
-    
-    else delete W[Z];
+    if (!only.includes(Z)) delete W[Z];
+
   }
 
   return W;
