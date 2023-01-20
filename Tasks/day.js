cca2b80a663bda6ef3fd71a1931f7173782ec590
bifynok <121981921+bifynok@users.noreply.github.com>
2023-01-20 14:37:45 +0200

    Change naming

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,15 @@
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+parseDay = (days) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < week.length; i++) {
+    if (days.startsWith(week[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-require('../Tests/day.js')(_parse_day_);
+require('../Tests/day.js')(parseDay);
