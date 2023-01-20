efdd2b8f5b91af8b2551484fa717a5397b8cf1cb
VitaliyP12 <122998670+VitaliyP12@users.noreply.github.com>
2023-01-20 18:49:54 +0200

    3-sections

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,10 +1,9 @@
-//Step 2
-//added const let for variables
-//changed function declaration for arrow expression
+//Step 3 
+//"use strict"
 
+'use strict'
 const intersection = (firstDictionaries, secondDictionaries) => {
-  const first_keys = Object.keys(firstDictionaries);
-  for (attribute_name of first_keys) {
+  for (const attribute_name of Object.keys(firstDictionaries)) {
     if (firstDictionaries[attribute_name] === secondDictionaries[attribute_name]) {
       secondDictionaries[attribute_name] = firstDictionaries[attribute_name];
     }else {
