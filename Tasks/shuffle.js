c68903430cae191dbfa4ea33595731501333f73f
vladriabyi <121768334+vladriabyi@users.noreply.github.com>
2023-01-20 23:18:03 +0200

    Update shuffle.js

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,11 +1,18 @@
 // Shuffle an array
-
-let Shuffle = (...List) => {
-  const [arr] = List;
-  arr.sort(() => Math.random() - 0.5);
-  {
+const array = [1,2,3,4,5]
+  const shuffle = function (arr){
+    for (let i = arr.length - 1; i > 0; i--){
+      const current = arr [i];
+      const random = Math.floor(Math.random() * (i + 1));
+      
+      arr[i] = arr [random]
+      arr[random] = current;
+    }
     return arr;
   }
-};
+  
+  for (let i = 0; 1 < 5; i++) {
+    console.log(shuffle(array).join(*,*))
+  }
 
 require('../Tests/shuffle.js')(Shuffle);
