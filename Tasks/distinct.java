c6ffdd35e3f47add827ec8cc14b0bab023a9ce0d
Rembqq <nikitavolkovskyi@gmail.com>
2023-01-20 17:32:30 +0200

    distinct add tests&comments. Finished

--- Tasks/distinct.java
+++ Tasks/distinct.java
@@ -5,6 +5,7 @@ public class distinct
 {
     public static Set<Integer> dist(int[] data)
     {
+        //create collection which deletes duplicates
         Set<Integer> s = new HashSet<Integer>();
         for(int i = 0; i < data.length; ++i)
         {
@@ -14,7 +15,26 @@ public class distinct
     }
     public static void main(String[] args)
     {
-        int[] l = {1, 2, 1, 3, 1, 4};
-        System.out.println(dist(l));
+        //tests
+        System.out.println();
+        int[] test1 = {1, 2, 1, 3, 1, 4};
+        int[] test2 = {1, 2, -1, 3, 0, 4};
+        int[] test3 = {1};
+        int[] test4 = {1, 1, 1};
+        int[] test5 = {0};
+        int[] test6 = {0, 0};
+        int[] test7 = {0, 0, 0};
+        int[] test8 = {0, 0, 0, 0};
+        int[] test9 = {};
+        
+        System.out.println(dist(test1));
+        System.out.println(dist(test2));
+        System.out.println(dist(test3));
+        System.out.println(dist(test4));
+        System.out.println(dist(test5));
+        System.out.println(dist(test6));
+        System.out.println(dist(test7));
+        System.out.println(dist(test8));
+        System.out.println(dist(test9));
     }
 }
