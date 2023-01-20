c2a6df8f5e074290bb8c6d93e94859302d66612c
DipodidaeA <anton.rak050@gmail.com>
2023-01-20 08:31:51 +0200

    between.js 4

--- between.js
+++ between.js
@@ -1,11 +1,9 @@
 const Word = 'different';
 const Prefix = 'di';
 const Suffix = 'en';
-i = str.indexOf(p);
-if (i == -1) return;
-k = i + p.length;
-str = str.substring(k);
+const IndexPrefixInWord = Word.indexOf(Prefix);
+if (IndexPrefixInWord == -1) return;
 i = str.indexOf(s);
 if (i == -1) return;
+k = i + p.length;
 str = str.substring(0, i);
-
