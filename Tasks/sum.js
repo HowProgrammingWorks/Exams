991751158e2d0a185e24a93620924d4ee6937de0
vadikd21 <123008596+vadikd21@users.noreply.github.com>
2023-01-20 16:04:37 +0000

    2-sum

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,20 +1,15 @@
-// step1
-//removed useless variables and reduced code in function 
-//const added 
-// useless lexical removed 
+// step2
+// useless code reduced in function 
 const sum = (s) => {
-  res = [0];
-  let k = 5;
-  for (i of s) {
-    let t = typeof i;
-    if (t === 'number') {
+  const res = [0];
+  for (i of s) { 
+    if (typeof i === 'number') {
       if (res.length > 0) {
         const new_Sum = res[res.length - 1] + i;
         res.push(new_Sum);
       }
     }
   }
-  res[0];
   return res[res.length - 1];
 };
 
