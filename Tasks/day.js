297c036e5a050bdc00872f9dd0e1819f2c7a2d2c
Andry Savchuk IM-24 <deadpoulll@gmail.com>
2023-01-20 23:14:35 +0200

    1-day

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,11 @@
-// Get day number
+//Step 1
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+'use strict';
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
-};
-
-require('../Tests/day.js')(_parse_day_);
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', ];
+const day = (s) => {
+  const hit = days.indexOf(s);
+  return hit +1 || -1;
+  };
+  
+require('../Tests/day.js')(day);
