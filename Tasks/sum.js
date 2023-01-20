069bf9a7d5fda16b857a60f9b27e9ad84c359b65
VitaliyP12 <122998670+VitaliyP12@users.noreply.github.com>
2023-01-20 19:07:35 +0200

    1-sum

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,20 +1,22 @@
-// Sum all numbers from an array
+//Step 1
+//removed useless lexical environment setting 
+//added const and let where needed for variables
+//removed useless variables and reduced code in function
 
-const k = (sum = function (s = () => {}) {
-  sum = [0];
+const sum = (s) => {
+  res = [0];
   let k = 5;
   for (i of s) {
     let t = typeof i;
     if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
+      if (res.length > 0) {
+        const new_Sum = res[res.length - 1] + i;
+        res.push(new_Sum);
       }
     }
   }
-  sum[0];
+  res[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
-});
+};
 
 require('../Tests/sum.js')(sum);
