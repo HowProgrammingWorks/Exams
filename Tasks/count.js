b6e8a4e85ff99475e0d3d36669499aac7d49e2bd
vladriabyi <121768334+vladriabyi@users.noreply.github.com>
2023-01-21 13:54:50 +0200

    aded 'use strict' and fix errors

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,11 +1,10 @@
+'use strict';
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
+const count = (obj) => {
+  	let sum = 0;
+  	const keys = Object.keys(obj);
+    keys.forEach((key) => {
     [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
