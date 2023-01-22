0442528a775236c7ff0d4c93d91d8bfb42f9aaed
Dmutre <dmytro.lesko@gmail.com>
2023-01-22 21:22:21 +0200

    Fixed magic Number
    Додаю цей коміт вже опісля, бо перечитував код і звернув увагу на невідоме число, яке можна винести в конст

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,10 +1,12 @@
 'use strict';
 // Split string by the first occurrence of separator
 
+const AMOUNT_NUMBERS_IN_IP = 4;
+
 const parseIp = (ip) => {
   const arr = ip.split('.');
-  if (arr.length !== 4) return;
-  for (let i = 0; i < 4; i++) {
+  if (arr.length !== AMOUNT_NUMBERS_IN_IP) return;
+  for (let i = 0; i < AMOUNT_NUMBERS_IN_IP; i++) {
     arr[i] = parseInt(arr[i]);
     if (isNaN(arr[i])) return;
   }
