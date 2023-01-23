179e3af277e238d0850a1726a1517ad45296441c
DipodidaeA <anton.rak050@gmail.com>
2023-01-23 23:19:10 +0200

    day.js 4.

--- day.js
+++ day.js
@@ -1,8 +1,12 @@
-let Day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const Parse_Day = (S) => {
+const Day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 let i;
 const AmountDay = Day.length;
 for (i = 0; i < AmountDay; i++) {
     let IndexDay = Day.indexOf(Day[i]);
     let NumberDay = IndexDay + 1;
-    console.log (`Day: ${Day[i]}, his naumber ${NumberDay}`);
-}
+    console.log (`Day: ${Day[i]}, his naumber ${NumberDay}`)
+  }
+  return;
+};
+const day_js = Parse_Day(1);
